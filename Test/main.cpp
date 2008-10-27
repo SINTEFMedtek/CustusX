68ba64cbb323663c26f0cf762cb50549e862db2e
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2008-10-27 15:13:22 +0000

    Modified the test to show how to use the view, rep and image classes together, this example will now render the volume.
    git-svn-id: https://wush.net/svn/ssc@6 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 07121ae5a2d834f89a37bc5ce911dc6d897d25c3

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,19 +1,35 @@
 
 #include <QApplication>
+#include <QMainWindow>
 #include <iostream>
+#include <boost/shared_ptr.hpp>
 #include "sscDataManager.h"
+#include "sscImage.h"
+#include "sscVolumeRep.h"
+#include "sscView.h"
 
 /** Test app for SSC
  */
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
-	
-	std::string testFile = "/home/christiana/snw2_trunk/C_code/SonowandSinfefCooperation/test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd";
-	                       //"/home/christiana/snw2_trunk/C_code/SonowandSintefCooperation/test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd" 
-	ssc::DataManager::instance()->loadImage(testFile, ssc::rtMETAIMAGE);
-	
-	//int val = app.exec();
-	//return val;
-	return 0;
+	QMainWindow mainwindow;
+
+	std::string testFile = "/Users/jbake/jbake/dev/ssc/Test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd";
+	                       //"/home/christiana/snw2_trunk/C_code/SonowandSintefCooperation/test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd"
+	ssc::ImagePtr imagePtr = ssc::DataManager::instance()->loadImage(testFile, ssc::rtMETAIMAGE);
+
+	typedef boost::shared_ptr<class ssc::VolumeRep> VolumeRepPtr;
+	VolumeRepPtr volumeRepPtr;
+	volumeRepPtr.reset(new ssc::VolumeRep(imagePtr->getUid(), imagePtr->getName(),imagePtr));
+
+	ssc::View view;
+	view.setRep(volumeRepPtr);
+
+	mainwindow.setCentralWidget(&view);
+	mainwindow.show();
+
+	int val = app.exec();
+	return val;
+	//return 0;
 }
