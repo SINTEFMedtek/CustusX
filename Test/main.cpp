52b4a03b5de04dd0279d5abb47e22f716bd15dd7
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2008-10-31 13:18:23 +0000

    Main now tests the dummytool and dummytoolmanager
    git-svn-id: https://wush.net/svn/ssc@11 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 04d8ffb944ffc0bf556959c069699edec451c32e

--- Test/main.cpp
+++ Test/main.cpp
@@ -10,13 +10,58 @@
 #include "sscVolumetricRep.h"
 #include "sscView.h"
 
+//TOOL&TOOLMANAGER TEST
+#include <vtkRenderWindow.h>
+#include <vtkCallbackCommand.h>
+#include <vtkRenderWindowInteractor.h>
+#include "sscDummyToolManager.h"
+#include "sscToolRep3D.h"
+
 /** Test app for SSC
  */
+void timerUpdate(vtkObject* obj, unsigned long,
+                          void*, void*)
+{
+  vtkRenderWindowInteractor* iren =
+    vtkRenderWindowInteractor::SafeDownCast(obj);
+    iren->GetRenderWindow()->Render();
+    iren->CreateTimer(VTKI_TIMER_UPDATE);
+}
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
 	QMainWindow mainwindow;
 
+	//TEST FOR DUMMYTOOL AND DUMMYTOOLMANAGER START
+	ssc::View* view = new ssc::View();
+
+	mainwindow.setCentralWidget(view);
+	mainwindow.resize(QSize(1000,500));
+	mainwindow.show();
+
+	ssc::ToolManager* toolmanager = ssc::DummyToolManager::getInstance();
+	toolmanager->configure();
+	toolmanager->initialize();
+	toolmanager->startTracking();
+
+	ssc::ToolPtr tool = toolmanager->getDominantTool();
+
+	ssc::ToolRepPtr toolrep = ssc::ToolRepPtr(new ssc::ToolRep3D(tool->getUid(),tool->getName(),tool));
+
+	view->setRep(toolrep);
+
+    vtkRenderWindowInteractor * iren = view->GetInteractor();
+
+    vtkCallbackCommand* callback = vtkCallbackCommand::New();
+    callback->SetCallback(timerUpdate);
+
+    iren->AddObserver(vtkCommand::TimerEvent, callback);
+    iren->CreateTimer(VTKI_TIMER_FIRST);
+    std::cout << "GET THE WINDOWS FOCUS AND PRESS \"R\" TO SEE THE ACTOR!" << std::endl;
+    //TEST FOR DUMMYTOOL AND DUMMYTOOLMANAGER END
+
+    //TEST FOR VIEWS, REPS AND IMAGES START
+	/*
 	std::string testFile1 = //"/Users/frankl/fl/dev/Repository/data/nevro/MetaImage/3DT1.mhd";
 	                       //"/Users/frankl/fl/dev2/cvsStable/ssc/Test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd";
 						   "/Users/jbake/jbake/dev/ssc/Test/TestData/Fantomer/Kaisa/MetaImage/Kaisa.mhd";
@@ -65,6 +110,8 @@ int main(int argc, char **argv)
 	volumeRepPtr1->setImage(imagePtr2);
 	view2->getRenderer()->ResetCamera();
 	view2->getRenderer()->Render();
+	*/
+    //TEST FOR VIEWS, REPS AND IMAGES START END
 
 	int val = app.exec();
 	return val;
