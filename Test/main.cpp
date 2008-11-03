bdc762c266d13f809908c8c5b25a739cbff43f4c
Frank Lindseth <Frank.Lindseth@sintef.no>
2008-11-03 23:18:57 +0000

    Added machine independent path to test data
    git-svn-id: https://wush.net/svn/ssc@17 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 7cdbf3acca2093f14345c6b86ce5ece61a5118a5

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,9 @@
+#include <string>
 
 #include <QApplication>
+
+//#include "vtkTestUtilities.h"
+#include "sscConfig.h"
 
 #include "sscTestInterface.h"
 
@@ -7,6 +11,13 @@
  */
 int main(int argc, char **argv)
 {
+
+	//char* fname = vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/combxyz.bin");
+    //std::cout << "fname: " << fname << std::endl;
+	std::string dataRoot = SSC_DATA_ROOT;
+	std::string fileName = dataRoot + "Fantomer/Kaisa/MetaImage/Kaisa.mhd";
+	cout << fileName << endl;
+
 	QApplication app(argc, argv);
 
 	ssc::TestInterface testInterface;
