7c1e5e3c5fc234cdcb67f6dd12b5801226b11bd1
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2008-11-04 11:57:37 +0000

    Bug fix
    git-svn-id: https://wush.net/svn/ssc@23 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: b0761e433ce9e418d848db0bd938f6209d7f47f2

--- Test/sscTestDataManager.cpp
+++ Test/sscTestDataManager.cpp
@@ -7,16 +7,16 @@
  */
 int main(int argc, char **argv)
 {
-	
+
 	std::cout << ssc::TestUtilities::GetDataRoot() << std::endl;
 
-	std::string testFile1 = TestUtilities::ExpandDataFileName("/Fantomer/Kaisa/MetaImage/Kaisa.mhd");
-	std::string testFile2 = TestUtilities::ExpandDataFileName("/MetaImage/20070309T105136_MRT1.mhd");
-	std::string testFile3 = TestUtilities::ExpandDataFileName("/MetaImage/20070309T102309_MRA.mhd");
+	std::string testFile1 = ssc::TestUtilities::ExpandDataFileName("/Fantomer/Kaisa/MetaImage/Kaisa.mhd");
+	std::string testFile2 = ssc::TestUtilities::ExpandDataFileName("/MetaImage/20070309T105136_MRT1.mhd");
+	std::string testFile3 = ssc::TestUtilities::ExpandDataFileName("/MetaImage/20070309T102309_MRA.mhd");
 	std::cout << testFile1 << std::endl;
 	std::cout << testFile2 << std::endl;
 	std::cout << testFile3 << std::endl;
-	
+
 
 	return 0;
 }
