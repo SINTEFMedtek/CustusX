fa86d63f8d599af04a9fd02417b4bfee288e78ab
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2008-11-07 10:34:13 +0000

    Fixed linking bug.
    git-svn-id: https://wush.net/svn/ssc@41 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: caf16dd9f51c70638551bbb0203e16ebdb76abd2

--- CMake/SSCUse.cmake
+++ CMake/SSCUse.cmake
@@ -1,11 +1,13 @@
 include (CMakeImportBuildSettings)
 cmake_import_build_settings (${SSC_BUILD_SETTINGS_FILE})
-include_directories (${SSC_INCLUDE_DIRS})
-link_directories (${SSC_LIBRARY_DIRS})
 
+find_package(Boost REQUIRED)
 
 find_package(VTK REQUIRED)
 include(${VTK_USE_FILE})
 
 find_package( Qt4 REQUIRED )
-include( ${QT_USE_FILE} ) 
+include( ${QT_USE_FILE})
+
+include_directories( ${SSC_INCLUDE_DIRS}  ${SSC_BINARY_DIR} ${QT_INCLUDES} ${Boost_INCLUDE_DIRS} )
+link_directories (${SSC_LIBRARY_DIRS})
