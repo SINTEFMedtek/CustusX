dc75c7b59669256e58de9037e89d249c0bf1017b
Christian Askeland <Christian.Askeland@sintef.no>
2009-02-06 22:31:37 +0000

    Fine-tuned the axis rep.
    git-svn-id: https://wush.net/svn/ssc@104 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 5f6ee05f58207f941a4bc0579d2f00eafa6c1964

--- Code/Rep/sscAxesRep.cpp
+++ Code/Rep/sscAxesRep.cpp
@@ -2,6 +2,10 @@
 
 #include <vtkAxesActor.h>
 #include <vtkRenderer.h>
+#include <vtkMatrix4x4.h>
+#include <vtkCaptionActor2D.h>
+#include <vtkTextProperty.h>
+#include <vtkAssembly.h>
 
 #include "sscView.h"
 
@@ -11,10 +15,66 @@ namespace ssc
 AxesRep::AxesRep(const std::string& uid) :
 	RepImpl(uid)
 {
-
+	mAssembly = vtkAssemblyPtr::New();
 	mActor = vtkAxesActorPtr::New();
-	mActor->SetTotalLength( 150, 150, 150 );
+	mAssembly->AddPart(mActor);
+	setAxisLength(50);
 
+	addCaption("x", Vector3D(1,0,0));
+	addCaption("y", Vector3D(0,1,0));
+	addCaption("z", Vector3D(0,0,1));
+	setTransform(Transform3D());
+	setFontSize(0.04);
+}
+
+/**set font size to a fraction of the normalized viewport.
+ *
+ */
+void AxesRep::setFontSize(double size)
+{
+	mFontSize = size;
+
+	for (unsigned i=0; i<mCaption.size(); ++i)
+	{
+		mCaption[i]->SetWidth(mFontSize);
+		mCaption[i]->SetHeight(mFontSize);
+	}
+}
+
+/**set axis length to a world length
+ *
+ */
+void AxesRep::setAxisLength(double length)
+{
+	mSize = length;
+	mActor->SetTotalLength( mSize, mSize, mSize );
+	setTransform(Transform3D(mAssembly->GetUserMatrix()));
+}
+
+/**Set the position of the axis.
+ *
+ */
+void AxesRep::setTransform(Transform3D rMt)
+{
+	mAssembly->SetUserMatrix(rMt.matrix());
+
+	for (unsigned i=0; i<mCaption.size(); ++i)
+	{
+		Vector3D pos = rMt.coord(mSize*mCaptionPos[i]);
+		mCaption[i]->SetAttachmentPoint(pos.begin());
+	}
+}
+
+void AxesRep::addCaption(const std::string& label, Vector3D pos)
+{
+	vtkCaptionActor2DPtr cap = vtkCaptionActor2DPtr::New();
+	cap->SetCaption(label.c_str());
+	cap->GetCaptionTextProperty()->SetColor(pos.begin());
+	cap->LeaderOff();
+	cap->BorderOff();
+	cap->GetCaptionTextProperty()->ShadowOff();
+	mCaption.push_back(cap);
+	mCaptionPos.push_back(pos);
 }
 
 AxesRep::~AxesRep()
@@ -31,12 +91,16 @@ AxesRepPtr AxesRep::New(const std::string& uid)
 
 void AxesRep::addRepActorsToViewRenderer(View* view)
 {
-	view->getRenderer()->AddActor(mActor);
+	view->getRenderer()->AddActor(mAssembly);
+	for (unsigned i=0; i<mCaption.size(); ++i)
+		view->getRenderer()->AddActor(mCaption[i]);
 }
 
 void AxesRep::removeRepActorsFromViewRenderer(View* view)
 {
-	view->getRenderer()->RemoveActor(mActor);
+	view->getRenderer()->RemoveActor(mAssembly);
+	for (unsigned i=0; i<mCaption.size(); ++i)
+		view->getRenderer()->RemoveActor(mCaption[i]);
 }
 
 } // namespace ssc
--- Code/Rep/sscAxesRep.h
+++ Code/Rep/sscAxesRep.h
@@ -1,17 +1,20 @@
 #ifndef SSCAXESREP_H_
 #define SSCAXESREP_H_
 
-#include "vtkSmartPointer.h"
-typedef vtkSmartPointer<class vtkAxesActor> vtkAxesActorPtr;
-
+#include <vector>
 #include "sscRepImpl.h"
+#include <vtkSmartPointer.h>
+typedef vtkSmartPointer<class vtkAxesActor> vtkAxesActorPtr;
+typedef vtkSmartPointer<class vtkAssembly> vtkAssemblyPtr;
+typedef vtkSmartPointer<class vtkCaptionActor2D> vtkCaptionActor2DPtr;
+#include "sscTransform3D.h"
 
 namespace ssc
 {
 typedef boost::shared_ptr<class AxesRep> AxesRepPtr;
 
-/**Represenation for one Mesh.
- * Use this to render geometric poly data in a 3D scene.
+/**Representation for one 3D coordinate axis.
+ * Set the position using setTransform().
  */
 class AxesRep : public RepImpl
 {
@@ -21,13 +24,21 @@ public:
 	static AxesRepPtr New(const std::string& uid);
 
 	virtual std::string getType() const { return "ssc::AxesRep"; }
+	void setTransform(Transform3D rMt);
+	void setFontSize(double size);
+	void setAxisLength(double length);
 
 protected:
 	AxesRep(const std::string& uid);
 	virtual void addRepActorsToViewRenderer(View* view);
 	virtual void removeRepActorsFromViewRenderer(View* view);
-
+	void addCaption(const std::string& label, Vector3D pos);
 	vtkAxesActorPtr mActor;
+	vtkAssemblyPtr mAssembly;
+	std::vector<vtkCaptionActor2DPtr> mCaption;
+	std::vector<Vector3D> mCaptionPos;
+	double mSize;
+	double mFontSize;
 
 };
 
