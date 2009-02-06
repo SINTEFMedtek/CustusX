029a72cdc34bbac936fa130811c4ab6d0ae1b6cd
Christian Askeland <Christian.Askeland@sintef.no>
2009-02-06 09:18:39 +0000

    added a line
    git-svn-id: https://wush.net/svn/ssc@103 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: e8f7c43527135084ea31357354f3ddef6bbbf602

--- Code/sscForwardDeclarations.h
+++ Code/sscForwardDeclarations.h
@@ -18,6 +18,7 @@ typedef boost::shared_ptr<class Mesh> MeshPtr;
 
 // reps
 typedef boost::shared_ptr<class VolumetricRep> VolumetricRepPtr;
+typedef boost::shared_ptr<class ProgressiveLODVolumetricRep> ProgressiveLODVolumetricRepPtr;
 typedef boost::shared_ptr<class ToolRep3D> ToolRep3DPtr;
 typedef boost::shared_ptr<class SliceRepSW> SliceRepSWPtr;
 typedef boost::shared_ptr<class SliceProxy> SliceProxyPtr;
