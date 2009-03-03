55282aeae3ff5e982ca7b075a35f5caa5a865e5a
Christian Askeland <Christian.Askeland@sintef.no>
2009-03-03 12:19:37 +0000

    structured settool
    git-svn-id: https://wush.net/svn/ssc@111 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: fda5a87de3667965227eb64d50734d0593edfb6d

--- Code/Rep/sscToolRep3D.cpp
+++ Code/Rep/sscToolRep3D.cpp
@@ -48,42 +48,57 @@ ToolPtr ToolRep3D::getTool()
 
 void ToolRep3D::setTool(ToolPtr tool)
 {
-	if(!tool)
+	//std::cout<<"ToolRep3D::setTool() begin"<<std::endl;
+//	if(!tool)
+//	{
+//		std::cout<<"this tool is no longer vaild!"<<std::endl;
+//	}
+	
+	if (mTool)
 	{
-		std::cout<<"this tool is no longer vaild!"<<std::endl;
-	}
-	mTool = tool;
-	std::string filename = mTool->getGraphicsFileName();
-	if (!filename.empty() && filename.compare(filename.size()-3,3,"STL") == 0 )
-	{
-		//std::cout<<"reading filename :" << filename <<	std::endl;
-		mSTLReader->SetFileName( filename.c_str() ); 
-		mPolyDataMapper->SetInputConnection( mSTLReader->GetOutputPort() );	 //read a 3D model file of the tool
+		disconnect(mTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)),
+				this, SLOT(receiveTransforms(Transform3D, double)));
+
+		disconnect(mTool.get(), SIGNAL(toolVisible(bool)),
+				this, SLOT(receiveVisible(bool)));
 		
+		mToolActor->SetMapper(NULL);
 	}
-	else 
+	
+	mTool = tool;
+	
+	if (mTool)
 	{
-		mPolyDataMapper->SetInput( mTool->getGraphicsPolyData() ); // creates a cone, default
-		std::cout<<"filename empty, making a cone :" <<	std::endl;	
+		std::string filename = mTool->getGraphicsFileName();
+		//std::cout<<"reading filename :" << filename <<	std::endl;
+		if (!filename.empty() && filename.compare(filename.size()-3,3,"STL") == 0 )
+		{
+			mSTLReader->SetFileName( filename.c_str() ); 
+			mPolyDataMapper->SetInputConnection( mSTLReader->GetOutputPort() );	 //read a 3D model file of the tool
+			
+		}
+		else 
+		{
+			mPolyDataMapper->SetInput( mTool->getGraphicsPolyData() ); // creates a cone, default
+			//std::cout<<"filename empty, making a cone :" <<	std::endl;	
+		}
+		
+		if (mPolyDataMapper->GetInput())
+		{
+			mToolActor->SetMapper(mPolyDataMapper);
+		}
+	
+		//std::cout<<"visibility: " << tool->getVisible() << "uid: " << tool->getUid() <<	std::endl;	
+		mToolActor->SetVisibility( tool->getVisible() );
+		//mToolActor->SetVisibility(true);
+		receiveTransforms(mTool->get_prMt(), 0);
+		
+		connect(mTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)),
+				this, SLOT(receiveTransforms(Transform3D, double)));
+		connect(mTool.get(), SIGNAL(toolVisible(bool)),
+				this, SLOT(receiveVisible(bool)));
 	}
-	
-	if (mPolyDataMapper->GetInput())
-	{
-		mToolActor->SetMapper(mPolyDataMapper);
-	}
-
-	mToolActor->SetVisibility(tool->getVisible());
-	//mToolActor->SetVisibility(true);
-	receiveTransforms(mTool->get_prMt(), 0);
-	
-	connect(mTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)),
-			this, SLOT(receiveTransforms(Transform3D, double)));
-
-	connect(mTool.get(), SIGNAL(toolVisible(bool)),
-			this, SLOT(receiveVisible(bool)));
-
-	std::cout << "created toolrep3d " << std::endl;
-	
+	//std::cout << "ToolRep3D::setTool() end" << std::endl;	
 }
 bool ToolRep3D::hasTool(ToolPtr tool) const
 {
@@ -112,7 +127,7 @@ void ToolRep3D::receiveTransforms(Transform3D prMt, double timestamp)
 }
 void ToolRep3D::receiveVisible(bool visible)
 {
-//	std::cout << "ToolRep3D::receiveVisible " << visible << std::endl;
+	//std::cout << "ToolRep3D::receiveVisible " << visible << std::endl;
 	mToolActor->SetVisibility(visible);
 }
 
