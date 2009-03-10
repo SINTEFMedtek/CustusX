05c56829e8a4fbae3e0a15f5571e06f0032b12ae
Sonowand <sonowand@sonowand.no>
2009-03-10 09:21:34 +0000

    two files missing
    git-svn-id: https://wush.net/svn/ssc@120 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 5bc41ddb86b3ceccb7de2196c4b2027076c49469

---
+++ Code/Rep/sscSlicePlaneRep.cpp
@@ -0,0 +1,88 @@
+#include "sscSlicePlaneRep.h"
+#include <vtkImageActor.h>
+#include <vtkImagePlaneWidget.h>
+#include <vtkImageData.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include "sscView.h"
+#include "sscDataManager.h"
+
+namespace ssc
+{
+
+SlicePlaneRep::SlicePlaneRep(const std::string& uid) :
+	RepImpl(uid)
+{
+	mVtkImagePlaneWidgetA = vtkImagePlaneWidgetPtr::New();
+	mVtkImagePlaneWidgetA->SetPlaneOrientationToZAxes();//Convenience method sets the plane orientation normal to the x, y, or z axes
+}
+
+SlicePlaneRep::~SlicePlaneRep()
+{
+}
+
+SlicePlaneRepPtr SlicePlaneRep::New(const std::string& uid)
+{
+	SlicePlaneRepPtr retval(new SlicePlaneRep(uid));
+	retval->mSelf = retval;
+	return retval;
+}
+
+void SlicePlaneRep::addRepActorsToViewRenderer(View* view)
+{
+	mVtkImagePlaneWidgetA->SetInteractor( view->getRenderWindow()->GetInteractor() );
+	mVtkImagePlaneWidgetA->InteractionOn();
+	mVtkImagePlaneWidgetA->On();
+//	
+//	for(unsigned i=0; i<mSlicesPlanes.size(); ++i)
+//	{
+//		view->getRenderer()->AddActor( mSlicesPlanes.at(i)->getActor() );
+//	}
+}
+
+void SlicePlaneRep::removeRepActorsFromViewRenderer(View* view)
+{
+	for(unsigned i=0; i<mSlicesPlanes.size(); ++i)
+	{
+		view->getRenderer()->RemoveActor( mSlicesPlanes.at(i)->getActor() );
+	}
+}
+void SlicePlaneRep::setImage(ImagePtr image)
+{
+	mImage = image;
+	mImage->connectRep(mSelf);
+	mVtkImagePlaneWidgetA->SetInput( image->getBaseVtkImageData() );	
+}
+bool SlicePlaneRep::hasImage(ImagePtr image) const
+{
+	return (mImage != NULL);
+}
+void SlicePlaneRep::setSliceReps( std::vector<ssc::SliceRepSWPtr> slicerReps)
+{
+	mSlicesPlanes = slicerReps;
+	setImage(mSlicesPlanes.at(0)->getImage() );
+//	for(unsigned i=0; i<mSlicesPlanes.size(); ++i)
+//	{
+//		mImageActors.push_back( slicerReps.at(i)->getActor() );
+//	}
+	
+}
+void SlicePlaneRep::setSliceProxy(SliceProxyPtr proxy) //connect tool transform update to sliceRep
+{
+	if (mSlicer)
+	{
+		disconnect(mSlicer.get(), SIGNAL(transformChanged(Transform3D)), this, SLOT(sliceTransformChangedSlot(Transform3D)));	
+	}
+	mSlicer = proxy;
+	connect(mSlicer.get(), SIGNAL(transformChanged(Transform3D)), this, SLOT(sliceTransformChangedSlot(Transform3D)));
+	
+	Vector3D c = ssc::DataManager::getInstance()->getCenter();
+
+	
+}
+void SlicePlaneRep::sliceTransformChangedSlot(Transform3D sMr)
+{
+	
+}
+
+} // namespace ssc
---
+++ Code/Rep/sscSlicePlaneRep.h
@@ -0,0 +1,54 @@
+#ifndef SSCSLICEPLANEREP_H_
+#define SSCSLICEPLANEREP_H_
+
+#include "vtkSmartPointer.h"
+typedef	vtkSmartPointer<class vtkImagePlaneWidget> vtkImagePlaneWidgetPtr;
+
+#include "sscSlicerRepSW.h"
+#include "sscRepImpl.h"
+#include "sscImage.h"
+#include "sscSliceProxy.h"
+namespace ssc
+{
+typedef boost::shared_ptr<class SlicePlaneRep> SlicePlaneRepPtr;
+
+/**Represenation for one Image.
+ * Use this to slice an image.
+ */
+class SlicePlaneRep : public RepImpl
+{
+	Q_OBJECT
+public:
+	virtual ~SlicePlaneRep();
+
+	static SlicePlaneRepPtr New(const std::string& uid);
+
+	virtual std::string getType() const { return "ssc::SliceRep"; }
+	void setImage(ImagePtr image);
+	bool hasImage(ImagePtr image) const;
+	void setSliceProxy(SliceProxyPtr proxy);
+	void setSliceReps( std::vector<ssc::SliceRepSWPtr> slicerReps);
+	
+protected:
+	SlicePlaneRep(const std::string& uid);
+	virtual void addRepActorsToViewRenderer(View* view);
+	virtual void removeRepActorsFromViewRenderer(View* view);
+	
+private slots:
+	void sliceTransformChangedSlot(Transform3D sMr);
+	
+private:
+	SliceProxyPtr mSlicer;
+	std::vector<ssc::SliceRepSWPtr> mSlicesPlanes;
+	vtkImagePlaneWidgetPtr mVtkImagePlaneWidgetA;
+	vtkImagePlaneWidgetPtr mVtkImagePlaneWidgetC;
+	vtkImagePlaneWidgetPtr mVtkImagePlaneWidgetS;
+	
+	ImagePtr mImage;
+ 
+};
+
+} // namespace ssc
+
+
+#endif /*SSCSLICEPLANEREP_H_*/
