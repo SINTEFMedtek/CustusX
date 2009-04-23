52837687b96b43822b74ae6efda982c8be4ff6ec
Christian Askeland <Christian.Askeland@sintef.no>
2009-04-23 05:39:11 +0000

    reverted last commit
    git-svn-id: https://wush.net/svn/ssc@155 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 416c4b206d19312eb1589d737d0c687ba1f07a5c

--- Testing/sscTestSliceAndToolRep.cpp
+++ Testing/sscTestSliceAndToolRep.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 //#include <vtkPolyData.h>
 #include <vtkImageData.h>
@@ -46,15 +45,9 @@ int main(int argc, char **argv)
 
 TestSliceAndToolRep::TestSliceAndToolRep()
 {
-	mRenderCount = 0;
-	mTotalRender = 0;
-	mTotalOther = 0;
-	mLastRenderEnd = QTime::currentTime();
-
-	//this->resize(QSize(1000, 800));
+//	this->resize(QSize(1000, 800));
 	this->resize(QSize(800, 600));
 	this->show();
-	//this->showFullScreen();
 	mWidget = new QWidget;
 	this->setCentralWidget( mWidget );
 
@@ -71,21 +64,10 @@ void TestSliceAndToolRep::generateView(const std::string& uid)
 
 void TestSliceAndToolRep::generateSlice(const std::string& uid, ssc::ToolPtr tool, ssc::ImagePtr image, ssc::PLANE_TYPE plane)
 {
-	generateView(uid);
-
 	ssc::SliceProxyPtr proxy(new ssc::SliceProxy());
 	proxy->setTool(tool);
-	if (1)
-	{
-		proxy->setFollowType(ssc::ftFOLLOW_TOOL);
-		//proxy->setFollowType(ssc::ftFIXED_CENTER);
-		proxy->setOrientation(ssc::otOBLIQUE);
-	}
-	else
-	{
-		proxy->setFollowType(ssc::ftFIXED_CENTER);
-		proxy->setOrientation(ssc::otORTHOGONAL);
-	}
+	proxy->setFollowType(ssc::ftFIXED_CENTER);
+	proxy->setOrientation(ssc::otORTHOGONAL);
 	proxy->setPlane(plane);
 
 	ssc::SliceRepSWPtr rep = ssc::SliceRepSW::New(uid);
@@ -102,28 +84,21 @@ ssc::View* TestSliceAndToolRep::view(const std::string& uid)
 	return mLayouts[uid].mView;
 }
 
-void TestSliceAndToolRep::defineSlice(const std::string& uid, QGridLayout* sliceLayout, ssc::ToolPtr tool, ssc::ImagePtr image, ssc::PLANE_TYPE plane, int r, int c)
-{
-	//generateView(uid);
-	generateSlice(uid, tool, image, plane);
-	sliceLayout->addWidget(view(uid), r,c);
-}
-
-
 void TestSliceAndToolRep::start()
 {
-
+	
 	// generate imageFileName
-	//imageFileName1 = ssc::TestUtilities::ExpandDataFileName("Fantomer/Kaisa/MetaImage/Kaisa.mhd");
+	//std::string imageFileName1 = ssc::TestUtilities::ExpandDataFileName("Fantomer/Kaisa/MetaImage/Kaisa.mhd");
 	imageFileName1 = ssc::TestUtilities::ExpandDataFileName("MetaImage/20070309T105136_MRT1.mhd");
-	//imageFileName1 = ssc::TestUtilities::ExpandDataFileName("MetaImage/20070309T102309_MRA.mhd");
+	//std::string imageFileName1 = ssc::TestUtilities::ExpandDataFileName("MetaImage/20070309T102309_MRA.mhd");
 	std::cout << imageFileName1 << std::endl;
 
 	// read image
 	ssc::ImagePtr image1 = ssc::DataManager::getInstance()->loadImage(imageFileName1, ssc::rtMETAIMAGE);
-	ssc::ImagePtr image2 = ssc::DataManager::getInstance()->loadImage(imageFileName1, ssc::rtMETAIMAGE);
-	ssc::ImagePtr image3 = ssc::DataManager::getInstance()->loadImage(imageFileName1, ssc::rtMETAIMAGE);
 
+	generateView("A");
+	generateView("C");
+	generateView("S");
 	generateView("3D");
 
 	// volume rep
@@ -131,6 +106,7 @@ void TestSliceAndToolRep::start()
 	mRepPtr->setImage(image1);
 	mRepPtr->setName(image1->getName());
 	view("3D")->addRep(mRepPtr);
+
 
 	//ssc::AxesRepPtr axesRep = ssc::AxesRep::New("AxesRepUID");
 	//view->addRep(axesRep);
@@ -144,43 +120,32 @@ void TestSliceAndToolRep::start()
 	ssc::ToolPtr tool = mToolmanager->getDominantTool();
 	connect( tool.get(), SIGNAL( toolTransformAndTimestamp(Transform3D ,double) ), this, SLOT( updateRender()));
 
+
 	// Tool 3D rep
 	ssc::ToolRep3DPtr toolRep = ssc::ToolRep3D::New( tool->getUid(), tool->getName() );
 	toolRep->setTool(tool);
 	view("3D")->addRep(toolRep);
+
+	// Slices
+	generateSlice("A", tool, image1, ssc::ptAXIAL);
+	generateSlice("C", tool, image1, ssc::ptCORONAL);
+	generateSlice("S", tool, image1, ssc::ptSAGITTAL);
 
 	//gui controll
 	QVBoxLayout *mainLayout = new QVBoxLayout;
 	mWidget->setLayout(mainLayout);
 
 	QGridLayout* sliceLayout = new QGridLayout;
+	
 
 	mainLayout->addLayout(sliceLayout);//Slice layout
-	if (1)
-	{
-		defineSlice("Any1", sliceLayout, tool, image1, ssc::ptANYPLANE, 0, 0);
-		defineSlice("Dua1", sliceLayout, tool, image1, ssc::ptSIDEPLANE, 1, 0);
-		defineSlice("Any2", sliceLayout, tool, image2, ssc::ptANYPLANE, 0, 1);
-		defineSlice("Dua2", sliceLayout, tool, image2, ssc::ptSIDEPLANE, 1, 1);
-		defineSlice("Any3", sliceLayout, tool, image3, ssc::ptANYPLANE, 0, 2);
-		defineSlice("Dua3", sliceLayout, tool, image3, ssc::ptSIDEPLANE, 1, 2);
-	}
-	else
-	{
-		defineSlice("A1", sliceLayout, tool, image1, ssc::ptAXIAL, 0, 0);
-		defineSlice("C1", sliceLayout, tool, image1, ssc::ptCORONAL, 1, 0);
-		defineSlice("S1", sliceLayout, tool, image1, ssc::ptSAGITTAL, 2, 0);
-		defineSlice("A2", sliceLayout, tool, image2, ssc::ptAXIAL, 0, 1);
-		defineSlice("C2", sliceLayout, tool, image2, ssc::ptCORONAL, 1, 1);
-		defineSlice("S2", sliceLayout, tool, image2, ssc::ptSAGITTAL, 2, 1);
-		defineSlice("A3", sliceLayout, tool, image3, ssc::ptAXIAL, 0, 2);
-		defineSlice("C3", sliceLayout, tool, image3, ssc::ptCORONAL, 1, 2);
-		defineSlice("S3", sliceLayout, tool, image3, ssc::ptSAGITTAL, 2, 2);
-	}
-	//sliceLayout->addWidget(view("3D"), 3, 0);
+	sliceLayout->addWidget(view("C"), 0, 0);
+	sliceLayout->addWidget(view("S"), 0, 1);
+	sliceLayout->addWidget(view("A"), 1, 0);
+	sliceLayout->addWidget(view("3D"), 1, 1);
 
 	QHBoxLayout *controllLayout = new QHBoxLayout;
-
+	
 	QVBoxLayout *slidersLayout = new QVBoxLayout;
 	mBrightnessSlider = new QSlider(Qt::Horizontal);
 	mBrightnessSlider->setTickInterval(10);
@@ -189,35 +154,35 @@ void TestSliceAndToolRep::start()
 	slidersLayout->addWidget(new QLabel(tr("Brightness")) );
 	slidersLayout->addWidget(mContrastSlider);
 	slidersLayout->addWidget(new QLabel(tr("Contrasst")) );
-	slidersLayout->addWidget(mBrightnessSlider);
+	slidersLayout->addWidget(mBrightnessSlider);	
 	controllLayout->addLayout(slidersLayout);
-
+	
 	mainLayout->addLayout(controllLayout); //Buttons
-
-	connect(mContrastSlider, SIGNAL(sliderMoved(int)), this, SLOT(contrast(int)) );
+	
+	connect(mContrastSlider, SIGNAL(sliderMoved(int)), this, SLOT(contrast(int)) );	
 	connect(mBrightnessSlider, SIGNAL(sliderMoved(int)), this, SLOT(brightness(int)) );
-
+	
 	//update slidebar...
 	//int window = (int)10 * image1->lookupTable2D().getWindow();
 	//int level = (int)10 * image1->lookupTable2D().getLevel();
-
+	
 	mContrastSlider->setMaximum(256);
 	mContrastSlider->setValue(256);
-
+			
 	mBrightnessSlider->setMaximum(256);
 	mBrightnessSlider->setValue(128);
-
+	
 	for (LayoutMap::iterator iter=mLayouts.begin(); iter!=mLayouts.end(); ++iter)
 		iter->second.mView->getRenderer()->ResetCamera();
 	updateRender();
 }
 void TestSliceAndToolRep::contrast(int val)
 {
-	ssc::ImagePtr image1 = ssc::DataManager::getInstance()->getImage(imageFileName1);
-
-	image1->getLookupTable2D().setWindow(val/1.0);
+	ssc::ImagePtr image1 = ssc::DataManager::getInstance()->getImage(imageFileName1);	
+	
+	image1->getLookupTable2D().setWindow(val/1.0);	
 }
-
+	
 void TestSliceAndToolRep::brightness(int val)
 {
 	ssc::ImagePtr image1 = ssc::DataManager::getInstance()->getImage(imageFileName1);
@@ -226,38 +191,9 @@ void TestSliceAndToolRep::brightness(int val)
 
 void TestSliceAndToolRep::updateRender()
 {
-	if (mRenderCount==1)
-	{
-		mTotalRender = 0;
-		mTotalOther = 0;
-		++mRenderCount;
-		return;
-	}
-
-	++mRenderCount;
-	QTime pre = QTime::currentTime();
-	int other = mLastRenderEnd.msecsTo(pre);
-
+//	std::cout<<"render begin"<<std::endl;
 	for (LayoutMap::iterator iter=mLayouts.begin(); iter!=mLayouts.end(); ++iter)
 		iter->second.mView->getRenderWindow()->Render();
-
-	mLastRenderEnd = QTime::currentTime();
-
-	int render = pre.msecsTo(mLastRenderEnd);
-	int limit = 30;
-	mTotalRender += render;
-	mTotalOther += other;
-
-	if (render+other>limit) // warn if too much time spent rendering.
-	{
-		std::cout << "render: " << render << "/" << other << std::endl;
-	}
-	if (mRenderCount%50==0)
-	{
-		std::cout << "averagerender: " << mTotalRender/mRenderCount << "/" << mTotalOther/mRenderCount << std::endl;
-	}
-
-//	std::cout<<"render begin"<<std::endl;
 //	std::cout<<"render end"<<std::endl;
 }
 
