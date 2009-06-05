028b5959271fd2f92687351188cf04ecc1fa74a6
Christian Askeland <Christian.Askeland@sintef.no>
2009-06-05 07:34:41 +0000

    forgot to add files
    git-svn-id: https://wush.net/svn/ssc@180 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 5ca82e9918ac074482cee19231358cf3513bc9de

---
+++ Code/Utilities/sscAcceptanceBoxWidget.cpp
@@ -0,0 +1,80 @@
+#include "sscAcceptanceBoxWidget.h"
+
+#include <iostream>
+
+namespace ssc
+{
+
+AcceptanceBoxWidget::AcceptanceBoxWidget(QString text, QWidget* parent) : QFrame(parent)
+{
+	mAccepted = false;
+	QVBoxLayout* top = new QVBoxLayout(this);
+	QHBoxLayout* buttons = new QHBoxLayout;
+	mText = new QLabel;
+	setText(text);
+	mText->setFont(QFont("Arial", 14, 75));
+	top->addWidget(mText);
+	top->addLayout(buttons);
+	mAcceptButton = new QPushButton("Accept");
+	mRejectButton = new QPushButton("Reject");
+	buttons->addWidget(mAcceptButton);
+	//buttons->addStretch();
+	buttons->addWidget(mRejectButton);
+	connect(mAcceptButton, SIGNAL( clicked()), this, SLOT(accept()) );	
+	connect(mRejectButton, SIGNAL( clicked()), this, SLOT(reject()) );
+	mAcceptButton->setFocus();
+	//mAcceptButton->setShortcut(Qt::Key_Enter);
+}
+
+void AcceptanceBoxWidget::setText(QString text)
+{
+	text = "Accept the following statement:\n" + text;
+	
+#ifdef SSC_AUTOMATIC_TEST_ACCEPT
+	text += "\n[Auto mode: Accepted after " + QString::number(SSC_DEFAULT_TEST_TIMEOUT_SECS) + "s]";
+#endif	
+
+	mText->setText(text); 
+}
+
+bool AcceptanceBoxWidget::accepted() const
+{
+	return mAccepted;
+}
+
+/**trigger an event when shown. This gives visibility of the
+ * widget in a specified time (putting it in the constructor will
+ * include first render time in the shown time) 
+ */
+void AcceptanceBoxWidget::showEvent ( QShowEvent * event )
+{
+	QWidget::showEvent(event);
+
+	// autofinish if auto (this define lies in ssc/Code/Utilities/sscConfig.h.in)
+	#ifdef SSC_AUTOMATIC_TEST_ACCEPT
+	std::cout << "autofinishing..." << std::endl;	
+	QTimer::singleShot(SSC_DEFAULT_TEST_TIMEOUT_SECS*1000, this, SLOT(accept())); // terminate app after some seconds - this is an automated test!!	
+	#endif	
+}
+
+void AcceptanceBoxWidget::accept()
+{
+	finish(true);
+}
+
+void AcceptanceBoxWidget::reject()
+{
+	finish(false);	
+}
+
+void AcceptanceBoxWidget::finish(bool accepted)
+{
+	mAccepted = accepted;
+	std::cout << "success: " << accepted << std::endl;	
+	emit finished(mAccepted);
+	qApp->quit();
+}
+
+
+
+} // namespace ssc
