3f485d97823a59ac1b013012548e09de9f0a2c4d
Sonowand <sonowand@sonowand.no>
2010-04-13 10:51:50 +0000

    Add the missing DoubleRange class.
    git-svn-id: https://wush.net/svn/ssc@358 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: 07fc0d04c4e39b23b6ab67bbeeaa2bf7ce55df96

--- Code/Widgets/sscAbstractInterface.h
+++ Code/Widgets/sscAbstractInterface.h
@@ -15,6 +15,32 @@ enum appStates
 	APP_EXIT =		5
 };
 
+/** Utility class for describing a bounded numeric range. */
+struct DoubleRange
+{	
+	DoubleRange() : mMin(0), mMax(1), mStep(0.1) {}
+	DoubleRange(double min, double max, double step) : mMin(min), mMax(max), mStep(step) {}
+	double mMin;
+	double mMax;
+	double mStep;
+	double min() const { return mMin; } ///< minimum value
+	double max() const { return mMax; } ///< maximum value
+	double step() const { return mStep; } ///< smallest reasonable increment
+	double resolution() const { return (mMax-mMin)/mStep; } ///< number of steps in range (1 step means 1 resolution).
+	double range() const { return mMax-mMin; } ///< max - min
+	
+	/**Constrain the input to the range |min,max|
+	 */	
+	double constrainValue(double val)
+	{
+		if (val<=mMin)
+			return mMin;
+		if (val>=mMax)
+			return mMax;
+		return val;			
+	}
+};
+
 /** Abstract interface for interaction with internal double-valued data */
 class DoubleDataInterface : public QObject
 {
