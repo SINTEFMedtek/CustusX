7684ec2c84d8890d3d9abd00ed7db20cd24e64aa
ssc <ssc@ssc.no>
2010-04-21 11:44:12 +0000

    refactored cx registration, added landmark stuff
    git-svn-id: https://wush.net/svn/ssc@368 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: ec64c24b6b3abdba5f4f84f4a2b5b5e37c8c5857

---
+++ Code/Data/sscLandmark.cpp
@@ -0,0 +1,114 @@
+#include "sscLandmark.h"
+
+#include "sscTypeConversions.h"
+#include "sscTime.h"
+
+namespace ssc
+{
+
+
+Landmark::Landmark(std::string uid, Vector3D coord) :
+  mUid(uid), mCoord(coord)
+{
+  mTimestamp = QDateTime::currentDateTime();
+}
+
+Landmark::~Landmark()
+{}
+
+std::string Landmark::getUid() const
+{
+  return mUid;
+}
+
+ssc::Vector3D Landmark::getCoord() const
+{
+  return mCoord;
+}
+QDateTime Landmark::getTimestamp() const
+{
+  return mTimestamp;
+}
+
+void Landmark::addXml(QDomNode& dataNode)
+{
+  QDomDocument doc = dataNode.ownerDocument();
+
+  dataNode.toElement().setAttribute("uid", qstring_cast(mUid));
+
+  QDomElement coordNode = doc.createElement("coord");
+  coordNode.appendChild(doc.createTextNode(qstring_cast(mCoord)));
+  dataNode.appendChild(coordNode);
+
+  QDomElement timestampNode = doc.createElement("timestamp");
+  timestampNode.appendChild(doc.createTextNode(mTimestamp.toString(timestampSecondsFormat())));
+  dataNode.appendChild(timestampNode);
+}
+void Landmark::parseXml(QDomNode& dataNode)
+{
+  if (dataNode.isNull())
+    return;
+
+  QDomElement base = dataNode.toElement();
+
+  mUid = base.attribute("uid").toStdString();
+  mCoord = Vector3D::fromString(dataNode.namedItem("coord").toElement().text());
+  mTimestamp = QDateTime::fromString(dataNode.namedItem("timestamp").toElement().text(), timestampSecondsFormat());
+}
+
+
+///--------------------------------------------------------
+///--------------------------------------------------------
+///--------------------------------------------------------
+
+
+LandmarkProperty::LandmarkProperty(const std::string& uid, const std::string& name, bool active) : mUid(uid), mName(name), mActive(active)
+{
+  if (mName.empty())
+    mName = mUid;
+}
+
+void LandmarkProperty::setName(const std::string& name)
+{
+  mName = name;
+}
+
+void LandmarkProperty::setActive(bool active)
+{
+  mActive = active;
+}
+
+std::string LandmarkProperty::getUid() const
+{
+  return mUid;
+}
+
+bool LandmarkProperty::getActive() const
+{
+  return mActive;
+}
+
+std::string LandmarkProperty::getName() const
+{
+  return mName;
+}
+
+void LandmarkProperty::addXml(QDomNode& dataNode)
+{
+  dataNode.toElement().setAttribute("uid", qstring_cast(mUid));
+  dataNode.toElement().setAttribute("active", qstring_cast(mActive));
+  dataNode.toElement().setAttribute("name", qstring_cast(mName));
+}
+void LandmarkProperty::parseXml(QDomNode& dataNode)
+{
+  if (dataNode.isNull())
+    return;
+
+  QDomElement base = dataNode.toElement();
+  mUid = base.attribute("uid").toStdString();
+  mActive = base.attribute("uid").toInt();
+  mName = base.attribute("uid").toStdString();
+}
+
+
+} // namespace ssc
---
+++ Code/Data/sscLandmark.h
@@ -0,0 +1,66 @@
+#ifndef SSCLANDMARK_H_
+#define SSCLANDMARK_H_
+
+#include <QObject>
+#include <QDomNode>
+#include <QDateTime>
+#include <string>
+#include <map>
+#include "sscVector3D.h"
+/*
+ * sscLandmark.h
+ *
+ *  Created on: Apr 19, 2010
+ *      Author: jbake
+ */
+namespace ssc
+{
+class Landmark //: public QObject
+{
+	//Q_OBJECT
+public:
+	explicit Landmark(std::string uid="", Vector3D coord=Vector3D(0,0,0));
+	~Landmark();
+
+	std::string getUid() const;
+	ssc::Vector3D getCoord() const;
+	QDateTime getTimestamp() const;
+	//double getRegistrationAccuracy() const;
+
+	void addXml(QDomNode& dataNode);
+	void parseXml(QDomNode& dataNode);
+
+private:
+	std::string mUid;
+	ssc::Vector3D mCoord; ///< the landmarks coordinate
+	QDateTime mTimestamp; ///< the time at which the landmark was sample
+  //double mRegistrationAccuracy; ///< accuracy of registration relative to reference space
+};
+typedef std::map<std::string, Landmark> LandmarkMap;
+
+class LandmarkProperty
+{
+public:
+  explicit LandmarkProperty(const std::string& uid="", const std::string& name="", bool active=true);
+  bool getActive() const;
+  std::string getName() const;
+  std::string getUid() const;
+  void setName(const std::string& name);
+  void setActive(bool active);
+
+  void addXml(QDomNode& dataNode);
+  void parseXml(QDomNode& dataNode);
+
+private:
+  std::string mUid;
+  std::string mName;
+  bool mActive;
+
+  //std::map<space, coord>
+};
+typedef std::map<std::string, LandmarkProperty> LandmarkPropertyMap;
+
+
+} //namespace ssc
+
+#endif /* SSCLANDMARK_H_ */
