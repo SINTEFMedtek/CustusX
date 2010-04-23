fcb6d0005d6e9c7899728ab097c650dfc3e3d90b
Christian Askeland <Christian.Askeland@sintef.no>
2010-04-23 09:54:38 +0000

    more status info in tool properties.
    git-svn-id: svn+ssh://cxserver.sintef.no/svn/Repository/CustusX3@780 bf23512e-66b2-487e-abc1-aab7af62561f
    
    Former-commit-id: 8f889dd6ced3abf43a9c5297d2665a3c763d6195

--- source/gui/cxToolPropertiesWidget.cpp
+++ source/gui/cxToolPropertiesWidget.cpp
@@ -37,19 +37,54 @@ ToolPropertiesWidget::ToolPropertiesWidget(QWidget* parent) :
   QVBoxLayout* toptopLayout = new QVBoxLayout(this);
   //toptopLayout->setMargin(0);
 
+  QHBoxLayout* generalLayout = new QHBoxLayout;
+  mReferenceStatusLabel = new QLabel("Reference frame <undefined>", this);
+  generalLayout->addWidget(mReferenceStatusLabel);
+  mTrackingSystemStatusLabel = new QLabel("Tracking <undefined>", this);
+  generalLayout->addWidget(mTrackingSystemStatusLabel);
+
+  toptopLayout->addLayout(generalLayout);
+
+  QGroupBox* activeGroup = new QGroupBox(this);
+  activeGroup->setTitle("Active Tool");
+  toptopLayout->addWidget(activeGroup);
+  QVBoxLayout* activeGroupLayout = new QVBoxLayout;
+  activeGroup->setLayout(activeGroupLayout);
+
+  QHBoxLayout* activeToolLayout = new QHBoxLayout;
+  activeToolLayout->addWidget(new QLabel("Name:", this));
   mToolNameLabel = new QLabel(this);
-  toptopLayout->addWidget(mToolNameLabel);
+  activeToolLayout->addWidget(mToolNameLabel);
+  mActiveToolVisibleLabel = new QLabel("Visible: NA");
+  activeToolLayout->addWidget(mActiveToolVisibleLabel);
+  activeGroupLayout->addLayout(activeToolLayout);
 
 //  QGroupBox* group2D = new QGroupBox(this);
 //  group2D->setTitle("2D properties");
 //  toptopLayout->addWidget(group2D);
 
   QGridLayout* gridLayout = new QGridLayout;
-  toptopLayout->addLayout(gridLayout);
+  activeGroupLayout->addLayout(gridLayout);
 
   mToolOffsetWidget = new SliderGroupWidget(this, ssc::DoubleDataInterfacePtr(new DoubleDataInterfaceActiveToolOffset), gridLayout, 0);
 
+  void configured(); ///< signal emitted when the system is configured
+  void initialized(); ///< signal emitted when the system is initialized
+  void trackingStarted(); ///< signal emitted when the system starts tracking
+  void trackingStopped(); ///< signal emitted when the system stops tracking
+
+  connect(toolManager(), SIGNAL(trackingStarted()), this, SLOT(referenceToolChangedSlot()));
+  connect(toolManager(), SIGNAL(trackingStopped()), this, SLOT(referenceToolChangedSlot()));
+  connect(toolManager(), SIGNAL(dominantToolChanged(const std::string&)), this, SLOT(dominantToolChangedSlot()));
+
+  connect(toolManager(), SIGNAL(configured()), this, SLOT(updateSlot()));
+  connect(toolManager(), SIGNAL(initialized()), this, SLOT(updateSlot()));
+  connect(toolManager(), SIGNAL(trackingStarted()), this, SLOT(updateSlot()));
+  connect(toolManager(), SIGNAL(trackingStopped()), this, SLOT(updateSlot()));
   connect(toolManager(), SIGNAL(dominantToolChanged(const std::string&)), this, SLOT(updateSlot()));
+
+  dominantToolChangedSlot();
+  referenceToolChangedSlot();
   updateSlot();
 }
 
@@ -57,13 +92,61 @@ ToolPropertiesWidget::~ToolPropertiesWidget()
 {
 }
 
+void ToolPropertiesWidget::dominantToolChangedSlot()
+{
+  if (mActiveTool)
+    disconnect(mActiveTool.get(), SIGNAL(toolVisible(bool)), this, SLOT(updateSlot()));
+
+  mActiveTool = toolManager()->getDominantTool();
+
+  if (mActiveTool)
+    connect(mActiveTool.get(), SIGNAL(toolVisible(bool)), this, SLOT(updateSlot()));
+}
+
+void ToolPropertiesWidget::referenceToolChangedSlot()
+{
+  if (mReferenceTool)
+    disconnect(mReferenceTool.get(), SIGNAL(toolVisible(bool)), this, SLOT(updateSlot()));
+
+  mReferenceTool = toolManager()->getReferenceTool();
+
+  if (mReferenceTool)
+    connect(mReferenceTool.get(), SIGNAL(toolVisible(bool)), this, SLOT(updateSlot()));
+}
+
+
 void ToolPropertiesWidget::updateSlot()
 {
-  ssc::ToolPtr tool = toolManager()->getDominantTool();
-  if (tool)
+  if (mActiveTool)
   {
-    mToolNameLabel->setText(qstring_cast(tool->getName()));
+    mToolNameLabel->setText(qstring_cast(mActiveTool->getName()));
+    QString text = mActiveTool->getVisible() ? "Visible" : "Not Visible";
+    mActiveToolVisibleLabel->setText(text);
   }
+  else
+  {
+    mToolNameLabel->setText("none");
+    mActiveToolVisibleLabel->setText("");
+  }
+
+  if (mReferenceTool)
+  {
+    QString text = mReferenceTool->getVisible() ? "visible" : "not visible";
+    mReferenceStatusLabel->setText("Reference " + text);
+  }
+  else
+  {
+    mReferenceStatusLabel->setText("Reference not found");
+  }
+
+  QString status = "Unconfigured";
+  if (toolManager()->isConfigured())
+    status = "Configured";
+  if (toolManager()->isInitialized())
+    status = "Initialized";
+  if (toolManager()->isTracking())
+    status = "Tracking";
+  mTrackingSystemStatusLabel->setText("Tracking status: " + status);
 }
 
 void ToolPropertiesWidget::showEvent(QShowEvent* event)
