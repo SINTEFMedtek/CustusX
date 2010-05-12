dbd8c4d8a5aeba12912fa584b2d1ccc4ed1fc3b5
Christian Askeland <Christian.Askeland@sintef.no>
2010-05-12 13:01:48 +0000

    forgot to add new file in last commit
    git-svn-id: https://wush.net/svn/ssc@396 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: c69ebf9846e060cfd041984ae8e0b83b35b834d9

---
+++ Code/Utilities/sscDefinitionStrings.cpp
@@ -0,0 +1,35 @@
+/*
+ * sscDefinitions.cpp
+ *
+ *  Created on: May 11, 2010
+ *      Author: christiana
+ */
+
+#include "sscDefinitionStrings.h"
+
+SNW_DEFINE_ENUM_STRING_CONVERTERS_BEGIN(ssc, ORIENTATION_TYPE, otCOUNT)
+{
+  "Oblique",
+  "Orthogonal"
+}
+SNW_DEFINE_ENUM_STRING_CONVERTERS_END(ssc, ORIENTATION_TYPE, otCOUNT);
+
+SNW_DEFINE_ENUM_STRING_CONVERTERS_BEGIN(ssc, PLANE_TYPE, ptCOUNT)
+{
+  "NoPlane",
+  "Sagittal",
+  "Coronal",
+  "Axial",
+  "Any",
+  "Dual",
+  "Radial"
+}
+SNW_DEFINE_ENUM_STRING_CONVERTERS_END(ssc, PLANE_TYPE, ptCOUNT)
+
+SNW_DEFINE_ENUM_STRING_CONVERTERS_BEGIN(ssc, FOLLOW_TYPE, ftCOUNT)
+{
+  "Follow tool",
+  "Fixed center"
+}
+SNW_DEFINE_ENUM_STRING_CONVERTERS_END(ssc, FOLLOW_TYPE, ftCOUNT)
+
---
+++ Code/Utilities/sscDefinitionStrings.h
@@ -0,0 +1,18 @@
+#ifndef SSCDEFINITIONSTRINGS_H_
+#define SSCDEFINITIONSTRINGS_H_
+
+#include "sscDefinitions.h"
+#include "sscEnumConverter.h"
+
+// put compiler-intensive templates here instead of in the much used sscDefinitions.h
+
+namespace ssc
+{
+} //namespace end
+
+SNW_DECLARE_ENUM_STRING_CONVERTERS(ssc, ORIENTATION_TYPE);
+SNW_DECLARE_ENUM_STRING_CONVERTERS(ssc, PLANE_TYPE);
+SNW_DECLARE_ENUM_STRING_CONVERTERS(ssc, FOLLOW_TYPE);
+
+
+#endif /*SSCDEFINITIONSTRINGS_H_*/
