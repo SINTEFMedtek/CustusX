c10454d810808211f3db0987a7304937f0b49a09
Christian Askeland <Christian.Askeland@sintef.no>
2010-05-18 10:51:40 +0000

    moved patient data stuff from mainwindow to separate class
    git-svn-id: svn+ssh://cxserver.sintef.no/svn/Repository/CustusX3@839 bf23512e-66b2-487e-abc1-aab7af62561f
    
    Former-commit-id: f13f73485b5390dbf7f5d0b169ea79e1c39646f1

---
+++ source/gui/cxFileCopied.h
@@ -0,0 +1,42 @@
+#ifndef CXFILECOPIED_H_
+#define CXFILECOPIED_H_
+
+#include <string>
+#include <QObject>
+#include "sscForwardDeclarations.h"
+#include "sscData.h"
+
+namespace cx
+{
+
+/** \class FileCopied
+ *
+ * \brief This is a helper class for MainWindow::importDataSlot().
+ * Contain a slot that checks if a file is finished copying and updates the
+ * ssc:Image object
+ *
+ * \date Feb 8, 2010
+ * \author Ole Vegard Solberg, SINTEF
+ */
+class FileCopied : public QObject
+{
+  Q_OBJECT
+public:
+  FileCopied(const std::string& absolutefilePath,
+             const std::string& relativefilePath,
+             ssc::DataPtr data);///< Creates the object
+
+signals:
+  void fileCopiedCorrectly();///< Sends a signal when the copy is verified
+protected:
+  std::string mFilePath;
+  std::string mRelativeFilePath;
+  ssc::DataPtr mData;
+public slots:
+  void areFileCopiedSlot();///< Checks if the file is finished copying and updates the ssc::Image
+};
+
+} // namespace cx
+
+
+#endif /* CXFILECOPIED_H_ */
---
+++ source/gui/cxPatientData.h
@@ -0,0 +1,62 @@
+/*
+ * cxPatientData.h
+ *
+ *  Created on: May 18, 2010
+ *      Author: christiana
+ */
+
+#ifndef CXPATIENTDATA_H_
+#define CXPATIENTDATA_H_
+
+#include "boost/shared_ptr.hpp"
+#include <QString>
+#include <QObject>
+
+class QDomDocument;
+class QSettings;
+
+namespace cx
+{
+
+/**Functionality for storing patient data in a folder on the disk
+ * and access to these data.
+ * Keeps track of a single active patient (patient folder).
+ * Only one such patient can be active at a time, since the
+ * global managers are updated by this class.
+ */
+class PatientData : public QObject
+{
+  Q_OBJECT
+public:
+  PatientData(QObject* parent);
+  virtual ~PatientData() {}
+
+  QString getActivePatientFolder() const;
+
+public slots:
+  void newPatient(QString choosenDir);
+  void loadPatient(QString chosenDir);
+  void importData(QString fileName);
+  void savePatient();///< Save all application data to XML file
+
+private:
+  //patient
+  void setActivePatient(const QString& activePatientFolder); ///< set the activepatientfolder (relative to the globalPatientDataFolder)
+  void createPatientFolders(QString choosenDir); ///< Create patient folders and save xml for new patient and for load patient for a directory whitout xml file.
+
+  //saving/loading
+  void generateSaveDoc(QDomDocument& doc);
+  void readLoadDoc(QDomDocument& loadDoc);
+
+  //Patient
+  QString mActivePatientFolder; ///< Folder for storing the files for the active patient. Path relative to globalPatientDataFolder.
+  //Preferences
+  QSettings* mSettings; ///< Object for storing all program/user specific settings
+
+};
+
+typedef boost::shared_ptr<PatientData> PatientDataPtr;
+
+} // namespace cx
+
+#endif /* CXPATIENTDATA_H_ */
