358b631bf7307e4913abda67493a99b2ad87739d
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-06-21 13:02:45 +0000

    Added test for class ToolConfigurationParser
    git-svn-id: svn+ssh://cxserver.sintef.no/svn/Repository/CustusX3@958 bf23512e-66b2-487e-abc1-aab7af62561f
    
    Former-commit-id: e13df3a9a934595620aabe8839c549fca9acbdff

--- source/toolManager/testing/cxTest_ToolConfigurationParser.h
+++ source/toolManager/testing/cxTest_ToolConfigurationParser.h
@@ -5,24 +5,37 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <QString>
+
+namespace cx
+{
+class ToolConfigurationParser;
+}
+
 /**Unit tests that show the properties of the cxToolConfigurationParser class
  */
-class TestToolConfigurationParser : public CppUnit::TestFixture
+class TestToolConfigurationParser: public CppUnit::TestFixture
 {
 public:
-	void setUp();
-	void tearDown();
+  void setUp();
+  void tearDown();
 
-	void testConstructor();
+  void testConstructor();
+  void testGetTracker();
+  void testGetConfiguredTools();
 
 public:
-	CPPUNIT_TEST_SUITE( TestToolConfigurationParser );
-		CPPUNIT_TEST( testConstructor );
-	CPPUNIT_TEST_SUITE_END();
+CPPUNIT_TEST_SUITE( TestToolConfigurationParser );
+    CPPUNIT_TEST( testConstructor );
+    CPPUNIT_TEST( testGetTracker );
+    CPPUNIT_TEST( testGetConfiguredTools );
+  CPPUNIT_TEST_SUITE_END();
 private:
+
+  cx::ToolConfigurationParser* mConfigurationParser;
+  QString mXmlFilePath;
 
 };
 CPPUNIT_TEST_SUITE_REGISTRATION( TestToolConfigurationParser );
-
 
 #endif /*CXTEST_TOOLCONFIGURATIONPARSER_H_*/
