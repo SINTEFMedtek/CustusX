9972232381583a869d3608f832444c9ae8b5e9d1
Ole Vegard Solberg <Ole.V.Solberg@sintef.no>
2010-06-22 08:55:47 +0000

    Moved messageManager from cx to ssc
    git-svn-id: https://wush.net/svn/ssc@431 21e1a38d-980f-4db5-9c1c-a165da9a7262
    
    Former-commit-id: c42a62664a59b2d93b730888b3ace70d539540dd

---
+++ Code/Utilities/sscMessageManager.cpp
@@ -0,0 +1,55 @@
+#include "sscMessageManager.h"
+
+#include <QString>
+
+namespace ssc
+{
+MessageManager* MessageManager::mTheInstance = NULL;
+MessageManager* messageManager() { return MessageManager::getInstance(); }
+
+MessageManager* MessageManager::getInstance()
+{
+  if(mTheInstance == NULL)
+  {
+    mTheInstance = new MessageManager();
+  }
+  return mTheInstance;
+}
+
+void MessageManager::destroyInstance()
+{
+  delete mTheInstance;
+}
+
+void MessageManager::sendInfo(std::string info)
+{
+  QString qinfo(info.c_str());
+  QString message("INFO: ");
+  message.append(&qinfo);
+  this->sendMessage(message, 1500);
+}
+
+void MessageManager::sendWarning(std::string warning)
+{
+  QString qwarning(warning.c_str());
+  QString message("WARNING: ");
+  message.append(&qwarning);
+  this->sendMessage(message, 3000);
+}
+
+void MessageManager::sendError(std::string error)
+{
+  QString qerror(error.c_str());
+  QString message("ERROR: ");
+  message.append(&qerror);
+  this->sendMessage(message, 0);
+}
+
+void MessageManager::sendMessage(QString &message, int timeout)
+{
+  emit this->emittedMessage((const QString &)message, timeout);
+}
+
+
+
+} //End namespace ssc
---
+++ Code/Utilities/sscMessageManager.h
@@ -0,0 +1,57 @@
+#ifndef MESSAGEMANAGER_H_
+#define MESSAGEMANAGER_H_
+
+#include <QObject>
+
+class QString;
+class QDomNode;
+class QDomDocument;
+
+namespace ssc
+{
+/**
+ * \class MessageManager
+ *
+ * \brief This class handles delivering messages to a statusbar from all the other
+ * classes.
+ *
+ * \author Janne Beate Lervik Bakeng, SINTEF Health Research
+ * \date 16.10.2008
+ *
+ *TODO Sender should be added to the message
+ *TODO Enable/disable info, warnings and errors
+ *
+ */
+class MessageManager : public QObject
+{
+  Q_OBJECT
+
+public:
+  static MessageManager* getInstance(); ///< Returns a reference to the only MessageManager that exists.
+  static void destroyInstance(); ///< Should be called by the object that made the MessageManager.
+
+  void sendInfo(std::string info); ///< Used to report successful operations.
+  void sendWarning(std::string warning); ///< The program does not need to terminate, but the user might need to do something.
+  void sendError(std::string error); ///< The program (might) need to terminate
+
+signals:
+  void emittedMessage(const QString& message, int timeout); ///< The signal the owner of a statusbar should listen to.
+
+private:
+  MessageManager(){}; ///< Use getInstance().
+  ~MessageManager(){}; ///< Use destroyInstance().
+  MessageManager(const MessageManager&){this->getInstance();}; ///< Copycontructur.
+  MessageManager& operator=(const MessageManager&){return *this->getInstance();}; ///< Assignment operator.
+
+  void sendMessage(QString &message, int timeout); ///< Emits the signal that actually sends the message.
+
+  static MessageManager *mTheInstance; ///< The unique MessageManager.
+};
+
+/**Shortcut for accessing the message manager instance.
+ */
+MessageManager* messageManager();
+
+} //namespace ssc
+
+#endif /* MESSAGEMANAGER_H_ */
