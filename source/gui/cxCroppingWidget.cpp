f3673757b414e8b88440feabb1b2fe2cfa839fa7
Christian Askeland <Christian.Askeland@sintef.no>
2010-08-23 05:19:46 +0000

    cropping widget - test code
    git-svn-id: svn+ssh://cxserver.sintef.no/svn/Repository/CustusX3@1111 bf23512e-66b2-487e-abc1-aab7af62561f
    
    Former-commit-id: a99e5c0e69a6f63847161a4623aa60ca1491a548

--- source/gui/cxCroppingWidget.cpp
+++ source/gui/cxCroppingWidget.cpp
@@ -7,6 +7,11 @@
 
 #include "cxCroppingWidget.h"
 #include <QVBoxLayout>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QGroupBox>
+#include "sscStringDataAdapter.h"
+#include "sscStringWidgets.h"
 
 namespace cx
 {
@@ -17,6 +22,17 @@ CroppingWidget::CroppingWidget(QWidget* parent) : QWidget(parent)
   this->setWindowTitle("Crop");
 
   QVBoxLayout* layout = new QVBoxLayout(this);
+
+  QGroupBox* activeClipGroupBox = new QGroupBox("Clip Active Image");
+  layout->addWidget(activeClipGroupBox);
+  QVBoxLayout* activeClipLayout = new QVBoxLayout(activeClipGroupBox);
+
+  ssc::ComboGroupWidget* combo = new ssc::ComboGroupWidget(this, ssc::StringDataAdapterPtr(new ssc::StringDataAdapterNull()));
+  activeClipLayout->addWidget(new QCheckBox("Interactive clipping"));
+  activeClipLayout->addWidget(combo);
+  activeClipLayout->addWidget(new QCheckBox("Invert plane"));
+  activeClipLayout->addWidget(new QPushButton("Save clip plane"));
+  activeClipLayout->addWidget(new QPushButton("Clear saved planes"));
 }
 
 }
