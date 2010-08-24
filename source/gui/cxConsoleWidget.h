79703dd74bd7784e36510aa8a8a7e65bc47a70ab
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-08-24 10:53:29 +0200

    Started working on a console widget for messges from the messagemanager.
    Former-commit-id: f0100353e2e51a276fa4ce204dad7d9824cab5ce

---
+++ source/gui/cxConsoleWidget.h
@@ -0,0 +1,24 @@
+#ifndef CXCONSOLEWIDGET_H_
+#define CXCONSOLEWIDGET_H_
+
+#include <QWidget>
+
+namespace cx
+{
+/**
+ * \class ConsoleWidget
+ *
+ * \brief
+ *
+ * \date 24. aug. 2010
+ * \author: Janne Beate Bakeng
+ */
+class ConsoleWidget : public QWidget
+{
+  Q_OBJECT
+
+  ConsoleWidget(){};
+  ~ConsoleWidget(){};
+};
+} // namespace cx
+#endif /* CXCONSOLEWIDGET_H_ */
