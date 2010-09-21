a04b64a674320d44fab0dddc6e4f498fdd5bafc9
Christian Askeland <christian.askeland@sintef.no>
2010-09-21 23:33:10 +0200

    connected cout/cerr to message manager
    Former-commit-id: aee7efe0a044701de567a8f3cb1d78f55f8bcc86

--- source/gui/cxConsoleWidget.cpp
+++ source/gui/cxConsoleWidget.cpp
@@ -46,31 +46,43 @@ void ConsoleWidget::printMessage(Message message)
 
 void ConsoleWidget::createTextCharFormats()
 {
-  mInfoFormat.setForeground(Qt::black);
-  mWarningFormat.setForeground(QColor(255,140,0)); //dark orange
-  mErrorFormat.setForeground(Qt::red);
-  mDebugFormat.setForeground(QColor(135,206,250)); //sky blue
+  mFormat[ssc::mlINFO].setForeground(Qt::black);
+  mFormat[ssc::mlWARNING].setForeground(QColor(255,140,0));
+  mFormat[ssc::mlERROR].setForeground(Qt::red);
+  mFormat[ssc::mlDEBUG].setForeground(QColor(135,206,250));
+  mFormat[ssc::mlCERR].setForeground(Qt::red);
+  mFormat[ssc::mlCOUT].setForeground(QColor(80,255,80));
+
+//
+//  mInfoFormat.setForeground(Qt::black);
+//  mWarningFormat.setForeground(QColor(255,140,0)); //dark orange
+//  mErrorFormat.setForeground(Qt::red);
+//  mDebugFormat.setForeground(QColor(135,206,250)); //sky blue
 }
 
 void ConsoleWidget::format(Message& message)
 {
-  switch(message.getMessageLevel())
-  {
-  case ssc::mlINFO:
-    mTextBrowser->setCurrentCharFormat(mInfoFormat);
-    break;
-  case ssc::mlWARNING:
-    mTextBrowser->setCurrentCharFormat(mWarningFormat);
-    break;
-  case ssc::mlERROR:
-    mTextBrowser->setCurrentCharFormat(mErrorFormat);
-    break;
-  case ssc::mlDEBUG:
-    mTextBrowser->setCurrentCharFormat(mDebugFormat);
-    break;
-  default:
-    break;
-  }
+  if (!mFormat.count(message.getMessageLevel()))
+    return;
+  mTextBrowser->setCurrentCharFormat(mFormat[message.getMessageLevel()]);
+//
+//  switch(message.getMessageLevel())
+//  {
+//  case ssc::mlINFO:
+//    mTextBrowser->setCurrentCharFormat(mInfoFormat);
+//    break;
+//  case ssc::mlWARNING:
+//    mTextBrowser->setCurrentCharFormat(mWarningFormat);
+//    break;
+//  case ssc::mlERROR:
+//    mTextBrowser->setCurrentCharFormat(mErrorFormat);
+//    break;
+//  case ssc::mlDEBUG:
+//    mTextBrowser->setCurrentCharFormat(mDebugFormat);
+//    break;
+//  default:
+//    break;
+//  }
 }
 
 }//namespace cx
