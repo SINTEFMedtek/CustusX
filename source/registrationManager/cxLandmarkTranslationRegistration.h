28ff88dd6888447e86fd40d2591245be9a9215f1
Christian Askeland <christian.askeland@gmail.com>
2010-09-27 15:28:01 +0200

    added code for itk translation registration.
    Former-commit-id: dca5de6e753c1374bc6d314a029b58166c3d2f0a

---
+++ source/registrationManager/cxLandmarkTranslationRegistration.h
@@ -0,0 +1,29 @@
+/*
+ * cxLandmarkTranslationRegistration.h
+ *
+ *  Created on: Sep 27, 2010
+ *      Author: christiana
+ */
+
+#ifndef CXLANDMARKTRANSLATIONREGISTRATION_H_
+#define CXLANDMARKTRANSLATIONREGISTRATION_H_
+
+#include <vector>
+#include "sscTransform3D.h"
+
+namespace cx
+{
+
+/** Use ITK to perform a landmark registration in 3D with translation only.
+ *
+ * Code is based on  itk/Examples/Registration/IterativeClosestPoint1.cxx
+ *
+ */
+class LandmarkTranslationRegistration
+{
+  ssc::Transform3D registerPoints(std::vector<ssc::Vector3D> ref, std::vector<ssc::Vector3D> target);
+};
+
+}
+
+#endif /* CXLANDMARKTRANSLATIONREGISTRATION_H_ */
