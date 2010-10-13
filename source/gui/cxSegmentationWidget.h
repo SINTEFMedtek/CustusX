c5256a5e309483121f6e69351c080c1e241723d0
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-10-13 14:54:08 +0200

    Working on the segmentationwidget layout.
    Former-commit-id: 04ac0a46d8e8cec6c4139f2678331a5fdd6d132e

--- source/gui/cxSegmentationWidget.h
+++ source/gui/cxSegmentationWidget.h
@@ -6,6 +6,9 @@
 #include "cxWhatsThisWidget.h"
 #include <QString>
 
+class QGroupBox;
+class QWidget;
+
 namespace cx
 {
 
@@ -25,23 +28,36 @@ class SegmentationWidget : public WhatsThisWidget
 public:
   SegmentationWidget(QWidget* parent);
   virtual ~SegmentationWidget();
-
   virtual QString defaultWhatsThis() const;
 
 protected:
   virtual void showEvent(QShowEvent* event); ///<updates internal info before showing the widget
   virtual void hideEvent(QCloseEvent* event); ///<disconnects stuff
+
 private slots:
   void segmentSlot();
   void contourSlot();
+  void adjustSizeSlot();
 
 private:
   SegmentationWidget();
-  void fillImageCombobox();
+  QWidget* createSegmentationOptionsWidget();
+  QWidget* createCotourOptionsWidget();
+
+  int mThreshold; ///< the threshold value used when segmenting
+  bool mBinary; ///< whether or not the segmentation should create a binary volume
+  bool mUseSmothing; ///< whether or not the volume should be smoothed
+  double mSmoothSigma; ///< the value used for smoothing (if enabled)
 };
 
-
-
+/**
+ * \class CenterlineWidget
+ *
+ * \brief Widget for extracting the centerline from a segment.
+ *
+ * \date 12. okt. 2010
+ * \author: Janne Beate Bakeng
+ */
 class CenterlineWidget : public WhatsThisWidget
 {
   Q_OBJECT
@@ -50,6 +66,16 @@ public:
   CenterlineWidget(QWidget* parent);
   ~CenterlineWidget();
   virtual QString defaultWhatsThis() const;
+
+protected:
+  virtual void showEvent(QShowEvent* event); ///<updates internal info before showing the widget
+  virtual void hideEvent(QCloseEvent* event); ///<disconnects stuff
+
+private slots:
+  void findCenterlineSlot();
+
+private:
+  QPushButton* mFindCenterlineButton;///<Button for finding centerline in a segment
 };
 
 }//namespace cx
