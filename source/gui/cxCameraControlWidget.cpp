41455e665e5ff007f30973b190e2be032cdbc7ee
Christian Askeland <christian.askeland@gmail.com>
2010-10-15 14:18:31 +0200

    generalized camer views
    Former-commit-id: 768f9850bf52c0fe1788deb87ca627dec3f5429e

--- source/gui/cxCameraControlWidget.cpp
+++ source/gui/cxCameraControlWidget.cpp
@@ -20,6 +20,7 @@
 #include "cxViewManager.h"
 #include "cxView3D.h"
 #include "cxDataInterface.h"
+#include "cxCameraControl.h"
 
 namespace cx
 {
@@ -91,9 +92,8 @@ void MousePadWidget::paintEvent(QPaintEvent *)
 
     ssc::Vector3D center(this->width()/2, this->height()/2, 0);
     ssc::Vector3D delta = ssc::Vector3D(mLastPos.x(), mLastPos.y(), 0) - center;
-    double radius = center.length();// - delta.length();
+    double radius = center.length();
     QPoint qcenter(this->width()/2, this->height()/2);
-
 
     //QRadialGradient radialGrad(mLastPos, radius, qcenter);
     QRadialGradient radialGrad(qcenter, radius, mLastPos);
@@ -105,7 +105,6 @@ void MousePadWidget::paintEvent(QPaintEvent *)
 //    radialGrad.setColorAt(0.5, Qt::blue);
 //    radialGrad.setColorAt(1, Qt::green);
 
-//    p.drawImage(0, 0, m_shade);
     QColor color(146, 0, 146);
     QBrush brush(radialGrad);
 
@@ -113,65 +112,6 @@ void MousePadWidget::paintEvent(QPaintEvent *)
     p.setBrush(QColor(146, 0, 146));
     p.fillRect(0, 0, width() - 1, height() - 1, brush);
 }
-
-
-///** Interface to the tool offset of the dominant tool
-// */
-//class DoubleDataAdapterCamera3DControl : public ssc::DoubleDataAdapter
-//{
-//  Q_OBJECT
-//public:
-//  DoubleDataAdapterCamera3DControl();
-//  virtual ~DoubleDataAdapterCamera3DControl() {}
-//  virtual QString getValueName() const { return "Zoom"; }
-//  virtual double getValue() const;
-//  virtual bool setValue(double val);
-//  virtual void connectValueSignals(bool on) {}
-//  ssc::DoubleRange getValueRange() const;
-//
-//private slots:
-//protected:
-//  vtkCameraPtr getCamera() const;
-//};
-//
-//DoubleDataAdapterCamera3DControl::DoubleDataAdapterCamera3DControl()
-//{
-//}
-//
-//vtkCameraPtr DoubleDataAdapterCamera3DControl::getCamera() const
-//{
-//  return viewManager()->get3DView()->GetRenderer()->GetActiveCamera();
-//}
-//
-//double DoubleDataAdapterCamera3DControl::getValue() const
-//{
-//  if (!this->getCamera())
-//    return 1.0;
-////  this->getCamera()->();
-////  if (mTool)
-////    return mTool->getTooltipOffset();
-////  return 0.0;
-//}
-//
-//bool DoubleDataAdapterCamera3DControl::setValue(double val)
-//{
-//  if (!this->getCamera())
-//    return false;
-//  this->getCamera()->Dolly(1.1);
-//  return true;
-//}
-//
-//ssc::DoubleRange DoubleDataAdapterCamera3DControl::getValueRange() const
-//{
-//  double range = 10;
-//  return ssc::DoubleRange(0.1,range,range/1000.0);
-//}
-//
-
-
-//---------------------------------------------------------
-//---------------------------------------------------------
-//---------------------------------------------------------
 
 
 ///--------------------------------------------------------
@@ -182,9 +122,10 @@ void MousePadWidget::paintEvent(QPaintEvent *)
 CameraControlWidget::CameraControlWidget(QWidget* parent) :
     QWidget(parent)
 {
-//  this->viewport()->setAttribute(Qt::WA_AcceptTouchEvents);
   this->setObjectName("CameraControlWidget");
   this->setWindowTitle("Camera Control");
+
+  mCameraControl.reset(new CameraControl());
 
   mMinPadSize = QSize(50,50);
   mMinBarSize = QSize(20,50);
@@ -196,76 +137,16 @@ CameraControlWidget::CameraControlWidget(QWidget* parent) :
   this->createStandard3DViewActions();
   this->definePanLayout();
   this->defineRotateLayout();
-
-
-//  mTopLayout->addStretch();
-
-  //connect(ssc::dataManager(), SIGNAL(activeImageChanged(const std::string&)), this, SLOT(updateSlot()));
-  //updateSlot();
 }
 
 void CameraControlWidget::createStandard3DViewActions()
 {
-  QActionGroup* group = new QActionGroup(this);
-  this->addStandard3DViewAction("A", "Anterior View",   ssc::Vector3D( 0, 1, 0), group);
-  this->addStandard3DViewAction("P", "Posterior View",  ssc::Vector3D( 0,-1, 0), group);
-  this->addStandard3DViewAction("S", "Superior View",   ssc::Vector3D( 0, 0,-1), group);
-  this->addStandard3DViewAction("I", "Inferior View",   ssc::Vector3D( 0, 0, 1), group);
-  this->addStandard3DViewAction("L", "Left View",       ssc::Vector3D(-1, 0, 0), group);
-  this->addStandard3DViewAction("R", "Right View",      ssc::Vector3D( 1, 0, 0), group);
-  this->addStandard3DViewAction("O", "Orthogonal View", ssc::Vector3D(-1, 1,-1).normal(), group);
+  QActionGroup* group = mCameraControl->createStandard3DViewActions();
 
   QToolBar* toolBar = new QToolBar(this);
   mTopLayout->addWidget(toolBar);
   toolBar->addActions(group->actions());
   toolBar->addSeparator();
-}
-
-/** Add one layout as an action to the layout menu.
- */
-QAction* CameraControlWidget::addStandard3DViewAction(QString caption, QString help, ssc::Vector3D viewDirection, QActionGroup* group)
-{
-  QAction* action = new QAction(caption, group);
-  action->setStatusTip(help);
-  action->setWhatsThis(help);
-  QFont font;
-  font.setBold(true);
-  if (font.pointSize()>=0)
-    font.setPointSize(font.pointSize()*1.5);
-  action->setFont(font);
-  action->setData(QVariant(qstring_cast(viewDirection)));
-  connect(action, SIGNAL(triggered()), this, SLOT(setStandard3DViewActionSlot()));
-  return action;
-}
-
-void CameraControlWidget::setStandard3DViewActionSlot()
-{
-  QAction* action = dynamic_cast<QAction*>(sender());
-  if (!action)
-    return;
-  ssc::Vector3D viewDirection = ssc::Vector3D::fromString(action->data().toString());
-
-  vtkRendererPtr renderer = viewManager()->get3DView()->getRenderer();
-  vtkCameraPtr camera = renderer->GetActiveCamera();
-
-  renderer->ResetCamera();
-
-  ssc::Vector3D focus(camera->GetFocalPoint());
-  ssc::Vector3D pos = focus - 500 * viewDirection;
-  ssc::Vector3D vup(0,0,1);
-  //ssc::Vector3D dir = (focus-direction).normal();
-
-  ssc::Vector3D left = cross(vup, viewDirection);
-  if (ssc::similar(left.length(), 0.0))
-    left = ssc::Vector3D(1,0,0);
-  vup = cross(viewDirection, left).normal();
-
-  camera->SetPosition(pos.begin());
-  camera->SetViewUp(vup.begin());
-
-  renderer->ResetCamera(); // let vtk do the zooming base work
-  camera->Dolly(1.5); // zoom in a bit more than the default vtk value
-  renderer->ResetCameraClippingRange();
 }
 
 void CameraControlWidget::defineRotateLayout()
