212a71647c7e57f74225d60501b20ee0e1813242
Christian Askeland <christian.askeland@gmail.com>
2010-10-18 08:21:26 +0200

    added control panel as a secondary main window, forgot to add widget at last commit
    Former-commit-id: 793471eba8fbde575334d2605a912b84734bae13

---
+++ source/viewManager/cxCameraControl.h
@@ -0,0 +1,89 @@
+/*
+ * cxCameraControl.h
+ *
+ *  Created on: Oct 15, 2010
+ *      Author: christiana
+ */
+
+#ifndef CXCAMERACONTROL_H_
+#define CXCAMERACONTROL_H_
+
+#include <vector>
+#include "sscVector3D.h"
+#include "sscDoubleWidgets.h"
+#include "sscForwardDeclarations.h"
+#include "vtkSmartPointer.h"
+
+class QActionGroup;
+class QAction;
+class QDomNode;
+typedef vtkSmartPointer<class vtkCamera> vtkCameraPtr;
+typedef vtkSmartPointer<class vtkRenderer> vtkRendererPtr;
+
+
+namespace cx
+{
+
+typedef boost::shared_ptr<class CameraData> CameraDataPtr;
+
+/** Class encapsulating the view transform of a camera. Use with vtkCamera
+ */
+class CameraData
+{
+public:
+  CameraData();
+  static CameraDataPtr create() { return CameraDataPtr(new CameraData()); }
+//  void writeCamera(vtkCameraPtr camera);
+//  void readCamera(vtkCameraPtr camera);
+
+  void setCamera(vtkCameraPtr camera);
+  vtkCameraPtr getCamera() const;
+
+  void addXml(QDomNode dataNode) const; ///< store internal state info in dataNode
+  void parseXml(QDomNode dataNode);///< load internal state info from dataNode
+
+//  ssc::Vector3D mPosition;
+//  ssc::Vector3D mFocalPoint;
+//  ssc::Vector3D mViewUp;
+//  double mNearClip, mFarClip;
+//  double mParallelScale;
+private:
+  mutable vtkCameraPtr mCamera;
+  void addTextElement(QDomNode parentNode, QString name, QString value) const;
+};
+
+
+/**Utility class for 3D camera control.
+ *
+ */
+class CameraControl : public QObject
+{
+  Q_OBJECT
+
+public:
+  CameraControl(QObject* parent=NULL);
+  virtual ~CameraControl();
+
+  QActionGroup* createStandard3DViewActions();
+
+signals:
+
+protected slots:
+//  void dollySlot(QPointF delta);
+//  void panXZSlot(QPointF delta);
+//  void rotateYSlot(QPointF delta);
+//  void rotateXZSlot(QPointF delta);
+  void setStandard3DViewActionSlot();
+
+private:
+  vtkRendererPtr getRenderer() const;
+  vtkCameraPtr getCamera() const;
+  void defineRotateLayout();
+  void definePanLayout();
+
+  QAction* addStandard3DViewAction(QString caption, QString help, ssc::Vector3D viewDirection, QActionGroup* group);
+};
+
+}//end namespace cx
+
+#endif /* CXCAMERACONTROL_H_ */
