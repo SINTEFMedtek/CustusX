15767645d9076d49fecf82b67c15e58648f3ff87
Christian Askeland <christian.askeland@gmail.com>
2010-10-20 11:41:43 +0200

    added speed test code
    Former-commit-id: 78f2d2c84fc344a2a41bffcb5bf8e9f05c8045cb

--- source/testing/cxTestCustusXController.h
+++ source/testing/cxTestCustusXController.h
@@ -7,10 +7,8 @@
 #ifndef CXTESTCUSTUSXCONTROLLER_H_
 #define CXTESTCUSTUSXCONTROLLER_H_
 
-
 #include <QApplication>
 #include "cxMainWindow.h"
-#include "cxPatientData.h"
 
 /**Helper object for automated control of the CustusX application.
  *
@@ -23,38 +21,14 @@ public:
   cx::MainWindow* mMainWindow;
   QString mPatientFolder;
 
-  CustusXController(QObject* parent) : QObject(parent)
-  {
-    mMainWindow = NULL;
-  }
-  void start()
-  {
-    qApp->setOrganizationName("SINTEF");
-    qApp->setOrganizationDomain("test.sintef.no");
-    qApp->setApplicationName("CustusX3");
-    qApp->setWindowIcon(QIcon(":/icons/CustusX.png"));
-    qApp->setWindowIcon(QIcon(":/icons/.png"));
-
-    cx::MainWindow::initialize();
-
-    mMainWindow = new cx::MainWindow;
-    mMainWindow->show();
-  #ifdef __APPLE__ // needed on mac for bringing to front: does the opposite on linux
-    mMainWindow->activateWindow();
-  #endif
-    mMainWindow->raise();
-  }
-  void stop()
-  {
-    delete mMainWindow;
-    cx::MainWindow::shutdown(); // shutdown all global resources, _after_ gui is deleted.
-  }
+  CustusXController(QObject* parent);
+  void start();
+  void stop();
 
 public slots:
-  void loadPatientSlot()
-  {
-    cx::stateManager()->getPatientData()->loadPatient("/Users/christiana/Patients/Kaisa_Speed_Test.cx3");
-  }
+  void loadPatientSlot();
+  void beginCheckRenderSlot();
+  void endCheckRenderSlot();
 
 private:
 };
