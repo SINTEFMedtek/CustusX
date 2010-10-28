3a74348817d7848d74e8d556d0264962da687095
Christian Askeland <christian.askeland@gmail.com>
2010-10-28 13:54:26 +0200

    fine-tuned openigtlink thread
    Former-commit-id: 378d2c984792b076eca055b1458cb49a2c3f007b

--- source/gui/cxIGTLinkClient.h
+++ source/gui/cxIGTLinkClient.h
@@ -5,20 +5,32 @@
 #include <QtCore>
 #include "boost/shared_ptr.hpp"
 
+#include "igtlMessageHeader.h"
+#include "igtlClientSocket.h"
+
 namespace cx
 {
 typedef boost::shared_ptr<class IGTLinkClient> IGTLinkClientPtr;
 
 class IGTLinkClient : public QThread
 {
+  Q_OBJECT
 public:
   IGTLinkClient(QString address, int port, QObject* parent = NULL);
+  void stop();
+
 protected:
   virtual void run();
 
+private slots:
+  void tick();
+
 private:
+  bool mStopped;
   QString mAddress;
   int mPort;
+  igtl::ClientSocket::Pointer mSocket;
+  igtl::MessageHeader::Pointer mHeaderMsg;
 };
 
 }//end namespace cx
