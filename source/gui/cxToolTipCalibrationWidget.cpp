22ca3f2bb36ccf3a313c6775070904abe85e3475
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-11-08 12:57:43 +0100

    Refactored gui for tooltipcalibration and implemented calibrate functionality, needs testing, added support for reading the <reference_point> tag in a tool file.
    Former-commit-id: a6523d9b2d3bfe2933fd3676b2736d8cec9393b6

--- source/gui/cxToolTipCalibrationWidget.cpp
+++ source/gui/cxToolTipCalibrationWidget.cpp
@@ -3,6 +3,7 @@
 #include <QPushButton>
 #include <QTextStream>
 #include <QFileDialog>
+#include <QMessageBox>
 #include "sscTypeConversions.h"
 #include "sscMessageManager.h"
 #include "sscToolManager.h"
@@ -11,49 +12,32 @@
 #include "sscDefinitionStrings.h"
 #include "sscLabeledComboBoxWidget.h"
 #include "cxDataLocations.h"
+#include "cxTool.h"
 
 namespace cx
 {
 ToolTipCalibrationWidget::ToolTipCalibrationWidget(QWidget* parent) :
   WhatsThisWidget(parent),
-  mCalibrateButton(new QPushButton("Calibrate", this)),
-  mTestButton(new QPushButton("Test calibration", this)),
-  mSampleButton(new QPushButton("Sample")),
-  mSaveFileButton(new QPushButton("Save to..."))
+  mCalibrationWidget(new ToolTipCalibrateWidget(this)),
+  mSampleWidget(new ToolTipSampleWidget(this))
 {
   this->setObjectName("ToolTipCalibrationWidget");
   this->setWindowTitle("Tool Tip");
 
-  mSaveToFileNameLabel = new QLabel(DataLocations::getRootConfigPath()+"/SampledPoints.txt", this);
-
-  connect(mSaveFileButton, SIGNAL(clicked()), this, SLOT(saveFileSlot()));
-
-  mCoordinateSystems = SelectCoordinateSystemStringDataAdapter::New();
-  connect(mCoordinateSystems.get(), SIGNAL(changed()), this, SLOT(coordChangedSlot()));
-
-  mTools = SelectToolStringDataAdapter::New();
-  connect(mTools.get(), SIGNAL(changed()), this, SLOT(referenceObjectChanged()));
-
-  mData = SelectDataStringDataAdapter::New();
-  connect(mData.get(), SIGNAL(changed()), this, SLOT(referenceObjectChanged()));
-
-  mToCoordinateSystem.mId = ssc::csTOOL;
-  mToCoordinateSystem.mRefObject = ssc::toolManager()->getDominantTool()->getUid();
-
   QVBoxLayout* toptopLayout = new QVBoxLayout(this);
   QGridLayout* topLayout = new QGridLayout();
+
+  QGroupBox* calibrateGroupBox = new QGroupBox("Calibrate dominant tool", this);
+  calibrateGroupBox->setLayout(new QVBoxLayout());
+  calibrateGroupBox->layout()->addWidget(mCalibrationWidget);
+  QGroupBox* sampleGroupBox = new QGroupBox("Sample dominant tool", this);
+  sampleGroupBox->setLayout(new QVBoxLayout());
+  sampleGroupBox->layout()->addWidget(mSampleWidget);
+
   toptopLayout->addLayout(topLayout);
   toptopLayout->addStretch();
-
-  connect(mCalibrateButton, SIGNAL(clicked()), this, SLOT(calibrateSlot()));
-  connect(mTestButton, SIGNAL(clicked()), this, SLOT(testSlot()));
-  connect(mSampleButton, SIGNAL(clicked()), this, SLOT(sampleSlot()));
-
-  topLayout->addWidget(this->createCalibrateGroupBox(), 1,0);
-  topLayout->addWidget(this->createTestGroupBox(), 2,0);
-  topLayout->addWidget(this->createSampleGroupBox(), 3,0);
-
-  this->coordChangedSlot();
+  topLayout->addWidget(calibrateGroupBox, 1, 0);
+  topLayout->addWidget(sampleGroupBox, 2, 0);
 }
 
 ToolTipCalibrationWidget::~ToolTipCalibrationWidget()
@@ -64,7 +48,9 @@ QString ToolTipCalibrationWidget::defaultWhatsThis() const
 {
   return "<html>"
     "<h3>Tool tip calibration.</h3>"
-    "<p><i>Calibrates a tool by sampling it when pointing at a known point on another frame.</i></p>"
+    "<p><i>Calibrates a tool by sampling it when pointing at a known point on another frame.</i></br>"
+    "By using the test button you can test your calibration by pointing at a known reference point.</br>"
+    "You can sample the dominant(active) tools tooltip in any coordinate system and get the results written to file.</p>"
     "</html>";
 }
 
@@ -73,22 +59,203 @@ void ToolTipCalibrationWidget::showEvent(QShowEvent* event)
   QWidget::showEvent(event);
 }
 
-void ToolTipCalibrationWidget::hideEvent(QCloseEvent* event)
+void ToolTipCalibrationWidget::hideEvent(QHideEvent* event)
 {
-  QWidget::closeEvent(event);
+  QWidget::hideEvent(event);
+}
+//------------------------------------------------------------------------------
+ToolTipCalibrateWidget::ToolTipCalibrateWidget(QWidget* parent) :
+    WhatsThisWidget(parent),
+    mCalibrateButton(new QPushButton("Calibrate")),
+    mReferencePointLabel(new QLabel("Ref. point:")),
+    mTestButton(new QPushButton("Test calibration")),
+    mDeltaLabel(new QLabel("Delta:"))
+{
+  this->setObjectName("ToolTipCalibrateWidget");
+  this->setWindowTitle("Tool Tip Calibration");
+
+  QVBoxLayout* toplayout = new QVBoxLayout(this);
+
+  mTools = SelectToolStringDataAdapter::New();
+  mCalibrateToolComboBox = new ssc::LabeledComboBoxWidget(this, mTools);
+
+  toplayout->addWidget(new QLabel("<b>Select a tool with a known referece point:</b>"));
+  toplayout->addWidget(mCalibrateToolComboBox);
+  toplayout->addWidget(mReferencePointLabel);
+  toplayout->addWidget(mCalibrateButton);
+  toplayout->addWidget(this->createHorizontalLine());
+  toplayout->addWidget(mTestButton);
+  toplayout->addWidget(mDeltaLabel);
+  toplayout->addStretch();
+
+  connect(mCalibrateButton, SIGNAL(clicked()), this, SLOT(calibrateSlot()));
+  connect(mTestButton, SIGNAL(clicked()), this, SLOT(testCalibrationSlot()));
+
+  connect(mTools.get(), SIGNAL(changed()), this, SLOT(toolSelectedSlot()));
+
+  //setting default state
+  this->toolSelectedSlot();
 }
 
-void ToolTipCalibrationWidget::calibrateSlot()
+ToolTipCalibrateWidget::~ToolTipCalibrateWidget()
+{}
+
+QString ToolTipCalibrateWidget::defaultWhatsThis() const
 {
-  ssc::messageManager()->sendDebug("Calibration slot not implemented yet.");
+  return "<html>"
+      "<h3>Tool tip calibration.</h3>"
+      "<p><i>Calibrates a tool by sampling it when pointing at a known point on another frame.</i></br>"
+      "By using the test button you can test your calibration by pointing at a known reference point.</br></p>"
+      "</html>";
 }
 
-void ToolTipCalibrationWidget::testSlot()
+void ToolTipCalibrateWidget::showEvent(QShowEvent* event)
 {
-  ssc::messageManager()->sendDebug("Test slot not implemented yet.");
+  QWidget::showEvent(event);
 }
 
-void ToolTipCalibrationWidget::sampleSlot()
+void ToolTipCalibrateWidget::hideEvent(QHideEvent* event)
+{
+  QWidget::hideEvent(event);
+}
+
+void ToolTipCalibrateWidget::calibrateSlot()
+{
+  ssc::ToolPtr refTool = mTools->getTool();
+  if(!refTool || (ssc::similar(refTool->getReferencePoint(), ssc::Vector3D(0.000,0.000,0.000))))
+    return;
+
+  ssc::CoordinateSystem to = ssc::CoordinateSystemHelpers::getCoordinateSystem(refTool);
+  ssc::Vector3D sampledPoint = ssc::CoordinateSystemHelpers::getDominantToolTipPoint(to);
+
+  ssc::ToolPtr tool = ssc::toolManager()->getDominantTool();
+
+  ToolTipCalibrationCalculator calc(tool, refTool, sampledPoint);
+  ssc::Transform3D calibration = calc.get_calibration_sMt();
+
+  QMessageBox msgBox;
+  msgBox.setText("Do you want to overwrite "+tool->getName()+"s calibration file?");
+  msgBox.setInformativeText("This cannot be undone.");
+  msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+  msgBox.setDefaultButton(QMessageBox::Ok);
+  int ret = msgBox.exec();
+
+  if(ret == QMessageBox::Ok)
+    tool->setCalibration_sMt(calibration);
+
+  ssc::messageManager()->sendInfo("Calibration:\n"+qstring_cast(calibration));
+}
+
+void ToolTipCalibrateWidget::testCalibrationSlot()
+{
+  ssc::ToolPtr selectedTool = mTools->getTool();
+  if(!selectedTool || (ssc::similar(selectedTool->getReferencePoint(), ssc::Vector3D(0.000,0.000,0.000))))
+    return;
+
+  ssc::CoordinateSystem to = ssc::CoordinateSystemHelpers::getCoordinateSystem(selectedTool);
+  ssc::Vector3D sampledPoint = ssc::CoordinateSystemHelpers::getDominantToolTipPoint(to);
+
+  ToolTipCalibrationCalculator calc(ssc::toolManager()->getDominantTool(), selectedTool, sampledPoint);
+  ssc::Vector3D delta_selectedTool = calc.get_delta_ref();
+
+  ssc::messageManager()->sendInfo("Delta:\n"+qstring_cast(delta_selectedTool));
+
+  mDeltaLabel->setText("<b>Delta:</b> "+qstring_cast(delta_selectedTool)+" <br> <b>Length:</b>  "+qstring_cast(delta_selectedTool.length()));
+}
+
+void ToolTipCalibrateWidget::toolSelectedSlot()
+{
+  QString text("Ref. point: <UNDEFINED POINT>");
+  mCalibrateButton->setEnabled(false);
+  mTestButton->setEnabled(false);
+
+  if(mTools->getTool())
+  {
+    ToolPtr tool = boost::dynamic_pointer_cast<Tool>(mTools->getTool());
+    if(tool && !(ssc::similar(tool->getReferencePoint(), ssc::Vector3D(0.000,0.000,0.000))))
+    {
+      text = "Ref. point: "+qstring_cast(tool->getReferencePoint());
+      mCalibrateButton->setEnabled(true);
+      mTestButton->setEnabled(true);
+    }
+  }
+
+  mReferencePointLabel->setText(text);
+}
+  //------------------------------------------------------------------------------
+ToolTipSampleWidget::ToolTipSampleWidget(QWidget* parent) :
+    WhatsThisWidget(parent),
+    mSampleButton(new QPushButton("Sample")),
+    mSaveToFileNameLabel(new QLabel("<font color=red> No file selected </font>")),
+    mSaveFileButton(new QPushButton("Save to..."))
+{
+  this->setObjectName("ToolTipSampleWidget");
+  this->setWindowTitle("Tool Tip Sampling");
+
+  QVBoxLayout* toplayout = new QVBoxLayout(this);
+
+  mCoordinateSystems = SelectCoordinateSystemStringDataAdapter::New();
+  mTools = SelectToolStringDataAdapter::New();
+  mData = SelectDataStringDataAdapter::New();
+
+  mCoordinateSystemComboBox = new ssc::LabeledComboBoxWidget(this, mCoordinateSystems);
+  mToolComboBox = new ssc::LabeledComboBoxWidget(this, mTools);
+  mDataComboBox = new ssc::LabeledComboBoxWidget(this, mData);
+
+  toplayout->addWidget(mSaveFileButton);
+  toplayout->addWidget(mSaveToFileNameLabel);
+  toplayout->addWidget(this->createHorizontalLine());
+  toplayout->addWidget(new QLabel("<b>Select coordinate system to sample in: </b>"));
+  toplayout->addWidget(mCoordinateSystemComboBox);
+  toplayout->addWidget(mToolComboBox);
+  toplayout->addWidget(mDataComboBox);
+  toplayout->addWidget(this->createHorizontalLine());
+  toplayout->addWidget(mSampleButton);
+  toplayout->addStretch();
+
+  connect(mSaveFileButton, SIGNAL(clicked()), this, SLOT(saveFileSlot()));
+  connect(mSampleButton, SIGNAL(clicked()), this, SLOT(sampleSlot()));
+  connect(mCoordinateSystems.get(), SIGNAL(changed()), this, SLOT(coordinateSystemChanged()));
+
+  //setting initial state
+  this->coordinateSystemChanged();
+}
+
+ToolTipSampleWidget::~ToolTipSampleWidget()
+{}
+
+QString ToolTipSampleWidget::defaultWhatsThis() const
+{
+  return "<html>"
+     "<h3>Tool tip sampling.</h3>"
+     "<p>You can sample the dominant(active) tools tooltip in any coordinate system and get the results written to file.</p>"
+     "</html>";
+}
+
+void ToolTipSampleWidget::showEvent(QShowEvent* event)
+{
+  QWidget::showEvent(event);
+}
+
+void ToolTipSampleWidget::hideEvent(QHideEvent* event)
+{
+  QWidget::hideEvent(event);
+}
+
+void ToolTipSampleWidget::saveFileSlot()
+{
+  QString configPath = DataLocations::getRootConfigPath();
+
+  QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
+                             configPath+"/SampledPoints.txt",
+                             tr("Text (*.txt)"));
+  if(fileName.isEmpty())
+    return;
+
+  mSaveToFileNameLabel->setText(fileName);
+}
+
+void ToolTipSampleWidget::sampleSlot()
 {
   QFile samplingFile(mSaveToFileNameLabel->text());
 
@@ -98,23 +265,21 @@ void ToolTipCalibrationWidget::sampleSlot()
     return;
   }
 
-  ssc::Vector3D toolPoint = ssc::CoordinateSystemHelpers::getDominantToolTipPoint(mToCoordinateSystem, false);
+  ssc::CoordinateSystem to = this->getSelectedCoordinateSystem();
+  ssc::Vector3D toolPoint = ssc::CoordinateSystemHelpers::getDominantToolTipPoint(to, false);
 
   QString sampledPoint = qstring_cast(toolPoint);
 
   QTextStream streamer(&samplingFile);
-//  streamer << QDateTime::currentDateTime().toString();
   streamer << sampledPoint;
   streamer << endl;
 
-  ssc::messageManager()->sendInfo("Sampled point in "+qstring_cast(mToCoordinateSystem.mId)+" ("+mToCoordinateSystem.mRefObject+") space, result: "+sampledPoint);
+  ssc::messageManager()->sendInfo("Sampled point in "+qstring_cast(to.mId)+" ("+to.mRefObject+") space, result: "+sampledPoint);
 }
 
-void ToolTipCalibrationWidget::coordChangedSlot()
+void ToolTipSampleWidget::coordinateSystemChanged()
 {
-  mToCoordinateSystem.mId = string2enum<ssc::COORDINATE_SYSTEM>(mCoordinateSystems->getValue());
-
-  switch (mToCoordinateSystem.mId)
+  switch (string2enum<ssc::COORDINATE_SYSTEM>(mCoordinateSystems->getValue()))
   {
   case ssc::csDATA:
     mDataComboBox->show();
@@ -133,82 +298,79 @@ void ToolTipCalibrationWidget::coordChangedSlot()
     mToolComboBox->hide();
     break;
   };
-
-//  ssc::messageManager()->sendDebug(qstring_cast(mToCoordinateSystem.mId)+" space selected, with "+mToCoordinateSystem.mRefObject+" as reference object. (1)");
 }
 
-void ToolTipCalibrationWidget::referenceObjectChanged()
+ssc::CoordinateSystem ToolTipSampleWidget::getSelectedCoordinateSystem()
 {
-  QString uid = "";
-  if(static_cast<SelectToolStringDataAdapter*>(this->sender()) && mTools->getTool())
-    uid = mTools->getTool()->getUid();
+  ssc::CoordinateSystem retval;
 
-  if(static_cast<SelectDataStringDataAdapter*>(this->sender()) && mData->getData())
-    uid = mData->getData()->getUid();
+  retval.mId = string2enum<ssc::COORDINATE_SYSTEM>(mCoordinateSystems->getValue());
 
-  mToCoordinateSystem.mRefObject = uid;
-//  ssc::messageManager()->sendDebug(qstring_cast(mToCoordinateSystem.mId)+" space selected, with "+mToCoordinateSystem.mRefObject+" as reference object.(2)");
-}
-
-void ToolTipCalibrationWidget::saveFileSlot()
-{
-  QString configPath = DataLocations::getRootConfigPath();
-
-  QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
-                             configPath+"/SampledPoints.txt",
-                             tr("Text (*.txt)"));
-  if(fileName.isEmpty())
-    return;
-
-  mSaveToFileNameLabel->setText(fileName);
-}
-
-QGroupBox* ToolTipCalibrationWidget::createCalibrateGroupBox()
-{
-  QGroupBox* retval = new QGroupBox("Calibrate tool", this);
-  QVBoxLayout* toplayout = new QVBoxLayout(retval);
-
-  toplayout->addWidget(mCalibrateButton);
-  toplayout->addStretch();
+  switch (retval.mId)
+  {
+  case ssc::csDATA:
+    retval = ssc::CoordinateSystemHelpers::getCoordinateSystem(mData->getData());
+    break;
+  case ssc::csTOOL:
+    retval = ssc::CoordinateSystemHelpers::getCoordinateSystem(mTools->getTool());
+    break;
+  case ssc::csSENSOR:
+    retval = ssc::CoordinateSystemHelpers::getCoordinateSystem(mTools->getTool());
+    break;
+  default:
+    retval.mRefObject = "";
+    break;
+  };
 
   return retval;
 }
+//------------------------------------------------------------------------------
+ToolTipCalibrationCalculator::ToolTipCalibrationCalculator(ssc::ToolPtr tool, ssc::ToolPtr ref, ssc::Vector3D p_t) :
+    mTool(tool), mRef(ref), mP_t(p_t)
+{}
 
-QGroupBox* ToolTipCalibrationWidget::createTestGroupBox()
+ToolTipCalibrationCalculator::~ToolTipCalibrationCalculator()
+{}
+
+ssc::Vector3D ToolTipCalibrationCalculator::get_delta_ref()
 {
-  QGroupBox* retval = new QGroupBox("Test calibration", this);
-  QVBoxLayout* toplayout = new QVBoxLayout(retval);
-
-  toplayout->addWidget(mTestButton);
-  toplayout->addStretch();
-
-  return retval;
+  return get_sampledPoint_ref() - get_referencePoint_ref(); //or the other way around?
 }
 
-QGroupBox* ToolTipCalibrationWidget::createSampleGroupBox()
+ssc::Transform3D ToolTipCalibrationCalculator::get_calibration_sMt()
 {
-  QGroupBox* retval = new QGroupBox("Sample points", this);
-  QVBoxLayout* toplayout = new QVBoxLayout(retval);
-
-  mCoordinateSystemComboBox = new ssc::LabeledComboBoxWidget(this, mCoordinateSystems);
-  mToolComboBox = new ssc::LabeledComboBoxWidget(this, mTools);
-  mDataComboBox = new ssc::LabeledComboBoxWidget(this, mData);
-
-
-  toplayout->addWidget(mSaveFileButton);
-  toplayout->addWidget(mSaveToFileNameLabel);
-//  toplayout->addSpacing(15);
-  toplayout->addWidget(this->createHorizontalLine());
-  toplayout->addWidget(new QLabel("<b>Select coordinate system to sample in: </b>", retval));
-  toplayout->addWidget(mCoordinateSystemComboBox);
-  toplayout->addWidget(mToolComboBox);
-  toplayout->addWidget(mDataComboBox);
-//  toplayout->addSpacing(15);
-  toplayout->addWidget(this->createHorizontalLine());
-  toplayout->addWidget(mSampleButton);
-  toplayout->addStretch();
-
-  return retval;
+  return this->get_sMt_new();
 }
 
+ssc::Vector3D ToolTipCalibrationCalculator::get_sampledPoint_t()
+{
+  return mP_t;
+}
+
+ssc::Vector3D ToolTipCalibrationCalculator::get_sampledPoint_ref()
+{
+  ssc::CoordinateSystem csT = ssc::CoordinateSystemHelpers::getCoordinateSystem(mTool);
+  ssc::CoordinateSystem csRef = ssc::CoordinateSystemHelpers::getCoordinateSystem(mRef);
+
+  ssc::Transform3D refMt = ssc::CoordinateSystemHelpers::get_toMfrom(csT, csRef);
+
+  ssc::Vector3D P_ref = refMt.coord(mP_t);
+
+  return P_ref;
+}
+
+ssc::Vector3D ToolTipCalibrationCalculator::get_referencePoint_ref()
+{
+  return mRef->getReferencePoint();
+}
+
+ssc::Transform3D ToolTipCalibrationCalculator::get_sMt_new()
+{
+  ssc::Transform3D delta_ref = ssc::createTransformTranslate(get_delta_ref());
+  ssc::Transform3D sMt_old = mTool->getCalibration_sMt();
+
+  return sMt_old * delta_ref;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 }//namespace cx
