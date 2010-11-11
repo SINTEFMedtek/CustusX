6b1ded93221ab822fc532119ecc825d4e87e8702
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-11-11 14:54:01 +0100

    Added functionality to the plateregistrationwidget.
    Former-commit-id: d700b1c57c14f9ba3bacc1ec3310b47b7cae07b7

--- source/gui/cxPlateRegistrationWidget.cpp
+++ source/gui/cxPlateRegistrationWidget.cpp
@@ -1,6 +1,9 @@
 #include "cxPlateRegistrationWidget.h"
 
 #include <QPushButton>
+#include "sscTypeConversions.h"
+#include "sscToolManager.h"
+#include "sscMessageManager.h"
 
 namespace cx
 {
@@ -11,7 +14,16 @@ PlateRegistrationWidget::PlateRegistrationWidget(QWidget* parent) :
   this->setObjectName("PlateRegistrationWidget");
   this->setWindowTitle("Fast Image Registration");
 
-  //connect(mPlateRegistrationButton, SIGNAL(clicked()), );
+  connect(mPlateRegistrationButton, SIGNAL(clicked()), this, SLOT(plateRegistrationSlot()));
+
+  QVBoxLayout* toptopLayout = new QVBoxLayout(this);
+  //QGridLayout* topLayout = new QGridLayout();
+
+//  toptopLayout->addLayout(topLayout);
+  toptopLayout->addWidget(mPlateRegistrationButton);
+  toptopLayout->addStretch();
+//  topLayout->addWidget(mPlateRegistrationButton, 1, 0);
+//  topLayout->addWidget(sampleGroupBox, 2, 0);
 }
 
 PlateRegistrationWidget::~PlateRegistrationWidget()
@@ -26,6 +38,30 @@ QString PlateRegistrationWidget::defaultWhatsThis() const
       "<p>Internally register the reference plates reference points as landmarks.</p>"
       "<p><i>Click register to add landmarks.</i></p>"
       "</html>";
+}
+
+void PlateRegistrationWidget::plateRegistrationSlot()
+{
+  //TODO clear toolmanagers landmarks
+
+  ssc::ToolPtr refTool = ssc::toolManager()->getReferenceTool();
+  if(!refTool)//cannot register without a reference tool
+  {
+    ssc::messageManager()->sendDebug("No refTool");
+    return;
+  }
+  std::map<int, ssc::Vector3D> referencePoints = refTool->getReferencePoints();
+  if(referencePoints.empty()) //cannot register without at least 1 reference point
+  {
+    ssc::messageManager()->sendDebug("No referenceppoints in reftool "+refTool->getName());
+    return;
+  }
+
+  std::map<int, ssc::Vector3D>::iterator it = referencePoints.begin();
+  for(; it != referencePoints.end(); ++it)
+  {
+    ssc::toolManager()->setLandmark(ssc::Landmark(qstring_cast(it->first), it->second));
+  }
 }
 
 }//namespace cx
