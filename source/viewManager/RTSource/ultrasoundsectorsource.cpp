457be31c7f92e97213ae4fa540f31dc7cb7ecb68
Christian Askeland <christian.askeland@gmail.com>
2010-11-18 14:36:53 +0100

    experimental but working version of cx3 texture mask in us rt rep
    Former-commit-id: bbe1ec7e272739a5b75d21b893be7046f0a3b746

--- source/viewManager/RTSource/ultrasoundsectorsource.cpp
+++ source/viewManager/RTSource/ultrasoundsectorsource.cpp
@@ -10,153 +10,22 @@
 
 #include "sscVector3D.h"
 #include "sscTransform3D.h"
-using ssc::Vector3D;
-using ssc::Transform3D;
-using ssc::unitVector;
+#include "sscBoundingBox3D.h"
 
-//#include "../datahandling/ultrasoundSector.h"
-//#include "../toolbox.h"
-//#include "../math/suvecmat.h"
+using namespace ssc;
 
-vtkCxxRevisionMacro(UltrasoundSectorSource, "$Revision: 1.1 $")
+vtkCxxRevisionMacro(UltrasoundSectorSource, "$Revision: 3.0 $")
 ;
 vtkStandardNewMacro(UltrasoundSectorSource)
 ;
 
 UltrasoundSectorSource::UltrasoundSectorSource()
 {
-  this->InnerRadius = 10;
-  this->OuterRadius = 30;
-  this->RadialResolution = 1;
-  this->CircumferentialResolution = 4;
 }
 
 void UltrasoundSectorSource::Execute()
 {
-  vtkIdType numPolys, numPts;
-  double x[3], tc[2];
-  int i, j;
-  //vtkIdType pts[4];
-  double theta, deltaRadius;
-  double cosTheta, sinTheta;
-  vtkPoints *newPoints;
-  vtkFloatArray *newNormals;
-  vtkFloatArray *newTCoords;
-  vtkCellArray *newPolys;
-  vtkPolyData *output = this->GetOutput();
-  vtkCellArray* strips = vtkCellArray::New();
-
-  //printf("***UltrasoundSectorSource::Execute()\n");
-  // Ultrasound sector data
-  float openingAngle, startAngle, incrementAngle, depthCm, resolution;
-  int sectorStart;
-  /// values hardcoded:
-  // usSectorPixel sectorOrigin;
-  int probeCalibrated = true;
-  openingAngle = 90;
-  depthCm = 25;
-  resolution = 20;
-  sectorStart = 50;
-  double sectorOrigin_x = 250;
-  double sectorOrigin_y = 0;
-  //  sectorOrigin = ssc::Vector3D(,,);
-  ///
-
-
-  //  if(Toolbox::GetBookkeeper()->GetUSsectorData() == NULL || Toolbox::GetBookkeeper() == NULL)
-  //    goto returnLabel;
-
-  if (probeCalibrated)
-  {
-    this->InnerRadius = ((double) sectorStart - sectorOrigin_y) * (double) resolution;
-    this->OuterRadius = this->InnerRadius + (double) depthCm * 10.0;
-
-    startAngle = 90.0 - (openingAngle / 2.0);
-    incrementAngle = openingAngle / this->CircumferentialResolution;
-  }
-  // Set things up; allocate memory
-  //
-  double normal[3];
-  normal[0] = -1.0;
-  normal[1] = 0;
-  normal[2] = 0;
-
-  numPts = (this->RadialResolution + 1) * (this->CircumferentialResolution + 1);
-  newPoints = vtkPoints::New();
-  newPoints->Allocate(numPts);
-  //newNormals = vtkFloatArray::New();
-  //newNormals->SetNumberOfComponents(3);
-  //newNormals->Allocate(3*numPts);
-  newTCoords = vtkFloatArray::New();
-  newTCoords->SetNumberOfComponents(2);
-  newTCoords->Allocate(2 * numPts);
-
-  bool useCAWay = true;
-  if (useCAWay)
-  {
-    updateSector();
-    return;
-  }
-  abort();
-
-  if (probeCalibrated)
-  {
-    for (i = 0; i < (this->CircumferentialResolution); i++)
-    {
-      theta = startAngle + i * incrementAngle;
-      theta = theta * vtkMath::DegreesToRadians();
-      deltaRadius = (this->OuterRadius - this->InnerRadius) / ((double) this->RadialResolution);
-      //printf("UltrasoundSectorSource i : %d - theta : %f\n",i,(theta*vtkMath::RadiansToDegrees()));
-
-      cosTheta = cos((double) theta);
-      sinTheta = sin((double) theta);
-      for (j = 0; j <= this->RadialResolution; j++)
-      {
-        // Vertex coordinates
-        x[0] = ((this->InnerRadius + j * deltaRadius) * cosTheta);// + horizontalOffset;
-        x[1] = (this->InnerRadius + j * deltaRadius) * sinTheta;
-        x[2] = 0.0;
-        // Texture coordinates
-
-        if (j == 0)
-        {
-          tc[0] = (sectorOrigin_x - cosTheta * this->InnerRadius) / TEXTURESIZE;
-          tc[1] = (sectorOrigin_x - sinTheta * this->InnerRadius) / TEXTURESIZE;
-        }
-        else
-        {
-          tc[0] = (sectorOrigin_x - cosTheta * this->OuterRadius);// / TEXTURESIZE;
-          tc[1] = (sectorOrigin_x - sinTheta * this->OuterRadius);// / TEXTURESIZE;
-        }
-
-        newPoints->InsertNextPoint(x);
-        newTCoords->InsertTuple((i + j), tc);
-        //newNormals->InsertTuple(i++,normal);
-      }
-    }
-
-    //  Create connectivity
-    //
-    // VTK_TRIANGLE_STRIP
-    strips->InsertNextCell(this->CircumferentialResolution * 2);
-    for (i = 0; i < (this->CircumferentialResolution * 2); i++)
-    {
-      strips->InsertCellPoint(i);
-    }
-  }
-  // Update ourselves and release memory
-  //
-  output->SetPoints(newPoints);
-  output->SetStrips(strips);
-  newPoints->Delete();
-
-  //output->GetPointData()->SetNormals(newNormals);
-  //newNormals->Delete();
-
-  output->GetPointData()->SetTCoords(newTCoords);
-  newTCoords->Delete();
-
-  returnLabel: i = 0;
+    this->updateSector();
 }
 
 void UltrasoundSectorSource::setProbeData(const ssc::ProbeSector& data)
@@ -165,13 +34,27 @@ void UltrasoundSectorSource::setProbeData(const ssc::ProbeSector& data)
   this->Modified();
 }
 
+Transform3D UltrasoundSectorSource::get_tMu() const
+{
+  Transform3D Rx = ssc::createTransformRotateX(-M_PI/2.0);
+  Transform3D Rz = ssc::createTransformRotateY(M_PI/2.0);
+  ssc::Transform3D R = (Rx*Rz);
+  ssc::Transform3D T = ssc::createTransformTranslate(-mData.mImage.mOrigin_u);
+
+  ssc::Transform3D tMu = R*T;
+  return tMu;
+}
+
 void UltrasoundSectorSource::updateSector()
 {
   vtkPolyData *mPolyData = this->GetOutput();
 //  double mDepthStart = 30;
 //  double mDepthEnd = 150;
 //  double mWidth = M_PI_2;
-  ssc::Vector3D tex(100,100,1);
+  //ssc::Vector3D tex(255.5,255.5,1);
+  ssc::Vector3D bounds = ssc::Vector3D(mData.mImage.mSize.width(), mData.mImage.mSize.height(), 1);
+  bounds = multiply_elems(bounds, mData.mImage.mSpacing);
+
 
   vtkFloatArray *newTCoords;
 
@@ -185,8 +68,12 @@ void UltrasoundSectorSource::updateSector()
   // after that, transform into yz-plane because thats the tool plane (probe point towards positive z)
   // then transform to global space.
   Transform3D tMl = createTransformIJC(Vector3D(0,1,0), Vector3D(0,0,1), Vector3D(0,0,0));
+  Transform3D texMu = createTransformNormalize(DoubleBoundingBox3D(0, bounds[0], 0, bounds[1], 0, 1), DoubleBoundingBox3D(0,1,0,1,0,1));
+  Transform3D uMt = this->get_tMu().inv();
+  Transform3D texMl = texMu * uMt * tMl;
+  Transform3D uMl = uMt * tMl;
 
-  Transform3D M;// = tMl;
+  Transform3D M = tMl;
   Vector3D e_x = unitVector(0);
   Vector3D e_y = unitVector(M_PI_2);
   Vector3D e_z(0,0,1);
@@ -237,17 +124,17 @@ void UltrasoundSectorSource::updateSector()
     {
       double theta = startAngle + i*angleIncrement;
       Vector3D startTheta = c + mData.mDepthStart * unitVector(theta);
-      newTCoords->InsertNextTuple(divide_elems(startTheta, tex).begin());
-      startTheta = M.coord(startTheta);
-      points->InsertNextPoint(startTheta.begin());
+      newTCoords->InsertNextTuple(texMl.coord(startTheta).begin());
+      //startTheta = M.coord(startTheta);
+      points->InsertNextPoint(uMl.coord(startTheta).begin());
     }
     for(int i = 0; i <= arcRes; i++)
     {
       double theta = stopAngle - i*angleIncrement;
       Vector3D endTheta = c + mData.mDepthEnd * unitVector(theta);
-      newTCoords->InsertNextTuple(divide_elems(endTheta, tex).begin());
-      endTheta = M.coord(endTheta);
-      points->InsertNextPoint(endTheta.begin());
+      newTCoords->InsertNextTuple(texMl.coord(endTheta).begin());
+//      endTheta = M.coord(endTheta);
+      points->InsertNextPoint(uMl.coord(endTheta).begin());
     }
 
     sides->InsertNextCell(arcRes*2+2+1);
@@ -273,11 +160,7 @@ void UltrasoundSectorSource::updateSector()
 void UltrasoundSectorSource::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-
-  os << indent << "InnerRadius: " << this->InnerRadius << "\n";
-  os << indent << "OuterRadius: " << this->OuterRadius << "\n";
-  os << indent << "RadialResolution: " << this->RadialResolution << "\n";
-  os << indent << "CircumferentialResolution: " << this->CircumferentialResolution << "\n";
+//  os << indent << "InnerRadius: " << this->InnerRadius << "\n";
 }
 
 /*
