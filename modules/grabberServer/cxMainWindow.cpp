33c97d3209d38d0fbcbf05fe0bb82868df0930fc
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-11-26 11:59:11 +0100

    Working on the grabber server and moved consolewidget into ssc.
    Former-commit-id: f97610169ee2814f2667868384ee8a89eac4e63d

--- modules/grabberServer/cxMainWindow.cpp
+++ modules/grabberServer/cxMainWindow.cpp
@@ -1,18 +1,25 @@
 #include "cxMainWindow.h"
 
 #include "cxGrabberServerWidget.h"
+#include <QDockWidget>
 
 namespace cx
 {
 
 MainWindow::MainWindow() :
-  QMainWindow(0)
+  QMainWindow(0),
+  mConsoleWidget(new ssc::ConsoleWidget(this))
 {
   this->setCentralWidget(new QWidget());
   mGrabberServerWidget = new GrabberServerWidget(this->centralWidget());
   this->setCentralWidget(mGrabberServerWidget);
-  //this->layout()->addWidget(mGrabberServerWidget);
   
+  QDockWidget* dockWidget = new QDockWidget(mConsoleWidget->windowTitle(), this);
+  dockWidget->setObjectName(mConsoleWidget->objectName() + "DockWidget");
+  dockWidget->setWidget(mConsoleWidget);
+  this->addDockWidget(Qt::BottomDockWidgetArea, dockWidget);
+  dockWidget->setVisible(true); // default visibility
+
   this->setMinimumSize(800,600);
 }
 
