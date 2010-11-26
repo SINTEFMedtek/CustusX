9685e01cabbdfbe3339bb0adad11c8ffca78ef8f
Janne Beate Bakeng <Janne.Beate.Bakeng@sintef.no>
2010-11-26 14:03:30 +0100

    Registered ssc::Message as a MetaType and registered it.
    Former-commit-id: 53be51bda85e749df0b0abe63df1e50019259b10

---
+++ Code/Widgets/sscConsoleWidget.h
@@ -0,0 +1,48 @@
+#ifndef SSCCONSOLEWIDGET_H_
+#define SSCCONSOLEWIDGET_H_
+
+#include <QTextBrowser>
+#include <QTextCharFormat>
+#include "sscMessageManager.h"
+
+class QContextMenuEvent;
+class QAction;
+
+namespace ssc
+{
+/**
+ * \class ConsoleWidget
+ *
+ * \brief
+ *
+ * \date 24. aug. 2010
+ * \author: Janne Beate Bakeng, SINTEF Medisinsk Teknologi
+ */
+class ConsoleWidget : public QTextBrowser
+{
+  typedef ssc::Message Message;
+
+  Q_OBJECT
+
+public:
+  ConsoleWidget(QWidget* parent);
+  ~ConsoleWidget();
+  virtual QString defaultWhatsThis() const;
+
+protected slots:
+  void contextMenuEvent(QContextMenuEvent* event);
+
+private slots:
+  void printMessage(Message message); ///< prints the message into the console
+  void lineWrappingSlot(bool checked);
+
+private:
+  void createTextCharFormats(); ///< sets up the formating rules for the message levels
+  void format(Message& message); ///< formats the text to suit the message level
+
+  QAction* mLineWrappingAction;
+
+  std::map<ssc::MESSAGE_LEVEL, QTextCharFormat> mFormat;
+};
+} // namespace ssc
+#endif /* SSCCONSOLEWIDGET_H_ */
