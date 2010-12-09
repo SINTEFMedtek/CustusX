dd198ae65c38f32facbb85e8dc6b96a468a11ea9
Christian Askeland <christian.askeland@gmail.com>
2010-12-09 09:24:52 +0100

    extended position storage file format
    Former-commit-id: 239f52fdb5bea169976cd911b538837642c2d7b0

--- Code/Utilities/sscPositionStorageFile.cpp
+++ Code/Utilities/sscPositionStorageFile.cpp
@@ -2,7 +2,139 @@
 #include <QDateTime>
 #include <boost/cstdint.hpp>
 #include "sscFrame3D.h"
-namespace ssc {
+
+
+namespace ssc
+{
+
+
+PositionStorageReader::PositionStorageReader(QString filename) : positions(filename)
+{
+  positions.open(QIODevice::ReadOnly);
+  stream.setDevice(&positions);
+  stream.setByteOrder(QDataStream::LittleEndian);
+
+  char header[6] = "     ";
+  stream.readRawData(header, 6);
+  quint8 version = 0;
+  stream >> version;
+
+  if (QString(header)!="SNWPOS" || version<1)
+  {
+    std::cout << "Error in header for file [" << filename.toStdString() << "]" << std::endl;
+    positions.close();
+  }
+}
+
+PositionStorageReader::~PositionStorageReader()
+{
+
+}
+
+bool PositionStorageReader::read(Transform3D* matrix, double* timestamp, int* toolIndex)
+{
+  if (atEnd())
+    return false;
+
+  quint8 type;
+  quint8 size;
+  quint64 ts;
+  quint8 tool;
+  ssc::Frame3D frame;
+
+  stream >> type;
+  stream >> size;
+  stream >> ts;
+  stream >> tool;
+  stream >> frame.mThetaXY >> frame.mThetaZ >> frame.mPhi;
+  stream >> frame.mPos[0] >> frame.mPos[1] >> frame.mPos[2];
+
+  *matrix = frame.transform();
+  *timestamp = ts;
+  *toolIndex = tool;
+
+  return (type==1) && (size==15);
+}
+
+bool PositionStorageReader::read(Transform3D* matrix, double* timestamp, QString* toolUid)
+{
+  if (this->atEnd())
+    return false;
+
+  quint8 type;
+  quint8 size;
+
+  if (type==2) // change tool format
+  {
+    stream >> size;
+    char* data = NULL;
+    uint isize = 0;
+    stream.readBytes(data, isize);
+    mCurrentToolUid = QString(QByteArray(data, size));
+    delete[] data;
+
+    stream >> type; // read type and make ready for a new read below
+  }
+
+  if (type==1) // tool-on-line format
+  {
+    quint64 ts;
+    quint8 tool;
+    ssc::Frame3D frame;
+
+    stream >> size;
+    stream >> ts;
+    stream >> tool;
+    stream >> frame.mThetaXY >> frame.mThetaZ >> frame.mPhi;
+    stream >> frame.mPos[0] >> frame.mPos[1] >> frame.mPos[2];
+
+    *matrix = frame.transform();
+    *timestamp = ts;
+    *toolUid = QString::number(tool);
+    return (size==15);
+  }
+  else if (type==3) // position only format
+  {
+    quint64 ts;
+    ssc::Frame3D frame;
+
+    stream >> size;
+    stream >> ts;
+    stream >> frame.mThetaXY >> frame.mThetaZ >> frame.mPhi;
+    stream >> frame.mPos[0] >> frame.mPos[1] >> frame.mPos[2];
+
+    *matrix = frame.transform();
+    *timestamp = ts;
+    *toolUid = mCurrentToolUid;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+
+  return false;
+}
+
+bool PositionStorageReader::atEnd() const
+{
+  return !positions.isReadable() || stream.atEnd();
+}
+
+QString PositionStorageReader::timestampToString(double timestamp)
+{
+  QDateTime retval;
+  boost::uint64_t ts = static_cast<boost::uint64_t>(timestamp);
+  retval.setTime_t(ts/1000);
+  retval = retval.addMSecs(ts%1000);
+  return retval.toString("yyyyMMddhhmmss.zzz");
+}
+
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
+
 
 PositionStorageWriter::PositionStorageWriter(QString filename) : positions(filename)
 {
@@ -12,7 +144,7 @@ PositionStorageWriter::PositionStorageWriter(QString filename) : positions(filen
 	if (positions.size() == 0)
 	{
 		stream.writeRawData("SNWPOS", 6);
-		stream << (quint8)1;
+		stream << (quint8)2;
 	}
 }
 
@@ -38,70 +170,33 @@ void PositionStorageWriter::write(Transform3D matrix, double timestamp, int tool
 	stream << (double)frame.mPos[2];
 }
 
-//---------------------------------------------------------
-//---------------------------------------------------------
-//---------------------------------------------------------
-
-PositionStorageReader::PositionStorageReader(QString filename) : positions(filename)
+void PositionStorageWriter::write(Transform3D matrix, double timestamp, QString toolUid)
 {
-	positions.open(QIODevice::ReadOnly);
-	stream.setDevice(&positions);
-	stream.setByteOrder(QDataStream::LittleEndian);
-	
-	char header[6] = "     ";
-	stream.readRawData(header, 6);
-	quint8 version = 0;
-	stream >> version;
-	
-	if (QString(header)!="SNWPOS" || version<1)
-	{
-		std::cout << "Error in header for file [" << filename.toStdString() << "]" << std::endl;
-		positions.close();
-	}
+  if (toolUid!=mCurrentToolUid)
+  {
+    QByteArray name = toolUid.toAscii();
+
+    stream << (quint8)2;  // Type - tool change
+    stream << (quint8)(name.size()+4); // Size of data following this point
+    stream.writeBytes(name.data(), name.size());
+    mCurrentToolUid = toolUid;
+  }
+  else
+  {
+    ssc::Frame3D frame = ssc::Frame3D::create(matrix);
+
+    stream << (quint8)3;  // Type -
+    stream << (quint8)15; // Size of data following this point
+    stream << (quint64)timestamp; // Microseconds since Epoch
+    stream << (double)frame.mThetaXY;
+    stream << (double)frame.mThetaZ;
+    stream << (double)frame.mPhi;
+    stream << (double)frame.mPos[0];
+    stream << (double)frame.mPos[1];
+    stream << (double)frame.mPos[2];
+  }
+
 }
 
-PositionStorageReader::~PositionStorageReader()
-{
-	
-}
-
-bool PositionStorageReader::read(Transform3D* matrix, double* timestamp, int* toolIndex)
-{
-	if (atEnd())
-		return false;
-	
-	quint8 type;
-	quint8 size;
-	quint64 ts;
-	quint8 tool;
-	ssc::Frame3D frame;
-	
-	stream >> type;
-	stream >> size;
-	stream >> ts;
-	stream >> tool;
-	stream >> frame.mThetaXY >> frame.mThetaZ >> frame.mPhi;
-	stream >> frame.mPos[0] >> frame.mPos[1] >> frame.mPos[2];
-
-	*matrix = frame.transform();
-	*timestamp = ts;
-	*toolIndex = tool;
-	
-	return (type==1) && (size==15);
-}
-
-bool PositionStorageReader::atEnd() const
-{
-	return !positions.isReadable() || stream.atEnd();	
-}
-
-QString PositionStorageReader::timestampToString(double timestamp)
-{
-	QDateTime retval;
-	boost::uint64_t ts = static_cast<boost::uint64_t>(timestamp);
-	retval.setTime_t(ts/1000);
-	retval = retval.addMSecs(ts%1000);
-	return retval.toString("yyyyMMddhhmmss.zzz");
-}
 
 } // namespace ssc 
