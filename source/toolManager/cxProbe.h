510fde94d2457112cad5ef8380dae7cb4b4997e0
Christian Askeland <christian.askeland@sintef.no>
2011-02-11 11:33:37 +0100

    refacatoring of probe classes
    Former-commit-id: 14d7a145b0fbe08df22742df1e85b7ed8774b19a

--- source/toolManager/cxProbe.h
+++ source/toolManager/cxProbe.h
@@ -20,10 +20,10 @@ class Probe : public ssc::Probe
 public:
   virtual ~Probe() {}
   virtual bool isValid() const;
-  virtual ssc::ProbeSector getSector() const;
+  virtual ssc::ProbeData getData() const;
   virtual ssc::RealTimeStreamSourcePtr getRealTimeStreamSource() const;
+  virtual ssc::ProbeSectorPtr getSector();
 
-//  Probe(ToolWeakPtr tool, QString instrumentUid, QString scannerUid);
   Probe(QString instrumentUid, QString scannerUid);
 
   virtual void addXml(QDomNode& dataNode);
@@ -35,18 +35,20 @@ public:
   virtual QString getConfigurationPath() const;
 
   virtual void setConfigId(QString uid);
+  virtual void setTemporalCalibration(double val);
+  virtual void setSoundSpeedCompensationFactor(double val);
 
   // non-inherited methods
   void setRealTimeStreamSource(ssc::RealTimeStreamSourcePtr source);
   ProbeXmlConfigParser::Configuration getConfiguration() const;
 
 private:
-  void setUSProbeSector(ssc::ProbeSector probeSector);
+  void setUSProbeSector(ssc::ProbeData probeSector);
   ProbeXmlConfigParser::Configuration getConfiguration(QString uid) const;
   QString getInstrumentId() const;
   QString getInstrumentScannerId() const;
 
-  ssc::ProbeSector mSector; ///< Probe sector information
+  ssc::ProbeData mSector; ///< Probe sector information
   ssc::RealTimeStreamSourcePtr mSource;
 
   QString mInstrumentUid;
