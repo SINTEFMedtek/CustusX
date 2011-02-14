7787cc2fae413a88cb832482793564e80b5bb63e
Janne Beate Lervik Bakeng <Janne.Beate.Bakeng@sintef.no>
2011-02-14 15:26:00 +0100

    Moved soundspeedconversionwidget into own file.w
    Former-commit-id: a0e9fb685b23160a1d557de7315465cb0fc1748d

--- source/gui/cxRecordBaseWidget.h
+++ source/gui/cxRecordBaseWidget.h
@@ -164,36 +164,5 @@ private:
   QFutureWatcher<QString> mFileMakerFutureWatcher;
   UsReconstructionFileMakerPtr mFileMaker;
 };
-
-class SoundSpeedConverterWidget : public QWidget
-{
-  Q_OBJECT
-
-public:
-  SoundSpeedConverterWidget(QWidget* parent);
-  ~SoundSpeedConverterWidget();
-
-  double getSoundSpeedCompensationFactor(); ///< calculates the sound speed conversion factor
-  double getWaterSoundSpeed(); ///< the sound speed in water given a temperatur
-
-  void setProbe(ToolPtr probe);
-
-public slots:
-  void applySoundSpeedCompensationFactorSlot(); ///< sets the sounds speed conversion factor on the rt source
-
-private slots:
-  void waterSoundSpeedChangedSlot();
-  void waterDegreeChangedSlot();
-
-private:
-  const double mFromSoundSpeed; //m/s
-  double mToSoundSpeed; //m/s
-
-  ToolPtr mProbe;
-
-  QPushButton*    mApplyButton; //applies the compensation on the rt source
-  QDoubleSpinBox* mSoundSpeedSpinBox; //m/s
-  QDoubleSpinBox* mWaterDegreeSpinBox; //celsius
-};
 }//namespace cx
 #endif /* CXRECORDBASEWIDGET_H_ */
