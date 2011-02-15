3e190fd8fb38d5ed7f83007260b0508fb3b4cabb
Developer User <christian.askeland@sintef.no>
2011-02-15 12:15:43 +0100

    Refactored USAcqusitionWidget to conform to the new probe interface, removed some cout, made SoundSpeedConverterWidget more robust.
    Former-commit-id: da2547ff752295b331ae30c1bc47b7d8e59572d2

--- source/gui/cxSoundSpeedConversionWidget.h
+++ source/gui/cxSoundSpeedConversionWidget.h
@@ -28,22 +28,29 @@ public:
   double getSoundSpeedCompensationFactor(); ///< calculates the sound speed conversion factor
   double getWaterSoundSpeed(); ///< the sound speed in water given a temperatur
 
-  void setProbe(ToolPtr probe);
+  void setProbe(ssc::ProbePtr probe);
 
 public slots:
   void applySoundSpeedCompensationFactorSlot(); ///< sets the sounds speed conversion factor on the rt source
+  void setToolSlot(const QString& uid); ///< convenient slot for connecting to the toolmanagers dominantToolChanged signal
 
 private slots:
   void waterSoundSpeedChangedSlot();
   void waterDegreeChangedSlot();
+  void resetSlot();
 
 private:
-  const double mFromSoundSpeed; //m/s
+  void setSoundSpeed(double soundspeed);
+  void setWaterDegree(double degree);
+  void updateButtons();
+
+  const double mScannerSoundSpeed; //m/s
   double mToSoundSpeed; //m/s
 
-  ToolPtr mProbe;
+  ssc::ProbePtr mProbe;
 
-  QPushButton*    mApplyButton; //applies the compensation on the rt source
+  QPushButton*    mApplyButton; ///< applies the compensation on the rt source
+  QPushButton*    mResetButton; ///< resets the sound speed to scanner sound speed
   QDoubleSpinBox* mSoundSpeedSpinBox; //m/s
   QDoubleSpinBox* mWaterDegreeSpinBox; //celsius
 };
