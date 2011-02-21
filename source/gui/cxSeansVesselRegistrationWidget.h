004a1b70909c5271cec7e67a8d27c0c1af2453d1
Janne Beate Lervik Bakeng <Janne.Beate.Bakeng@sintef.no>
2011-02-21 16:06:45 +0100

    Fixed: 0000220: Gui for controlling parameter (lts_ration) in vessel reg algorithm.
    Former-commit-id: bc399fe4693d4d3cb1c4c4426306d1386c47241b

---
+++ source/gui/cxSeansVesselRegistrationWidget.h
@@ -0,0 +1,46 @@
+#ifndef CXSEANSVESSELREGISTRATIONWIDGET_H_
+#define CXSEANSVESSELREGISTRATIONWIDGET_H_
+
+#include "cxWhatsThisWidget.h"
+
+class QSpinBox;
+class QPushButton;
+class QLabel;
+
+namespace cx
+{
+/**
+ * SeansVesselRegistrationWidget.h
+ *
+ * \brief Widget for controlling input to
+ * Seans Vessel Registration
+ *
+ * \date Feb 21, 2011
+ * \author Janne Beate Bakeng, SINTEF
+ */
+
+class SeansVesselRegistrationWidget : public WhatsThisWidget
+{
+  Q_OBJECT
+public:
+  SeansVesselRegistrationWidget(QWidget* parent);
+  virtual ~SeansVesselRegistrationWidget();
+  virtual QString defaultWhatsThis() const;
+
+public slots:
+  void fixedImageSlot(QString uid);
+  void movingImageSlot(QString uid);
+
+private slots:
+  void registerSlot();
+
+private:
+  QSpinBox* mLTSRatioSpinBox;
+  QPushButton* mRegisterButton;
+  QLabel* mFixedImageLabel;
+  QLabel* mMovingImageLabel;
+};
+
+}//namespace cx
+
+#endif /* CXSEANSVESSELREGISTRATIONWIDGET_H_ */
