f2da9fb87a2daf49365ac083030405499fc5fb75
Developer User <christian.askeland@sintef.no>
2011-03-07 16:33:56 +0100

    stabilized tool view. now works between tools
    Former-commit-id: 80458429dc7acd67704e4e480f5d6a803462708e

--- source/viewManager/cxView3D.cpp
+++ source/viewManager/cxView3D.cpp
@@ -32,14 +32,17 @@ View3D::View3D(const QString& uid, const QString& name, QWidget *parent, Qt::WFl
   this->setContextMenuPolicy(Qt::CustomContextMenu);
 
   mRenderer->GetActiveCamera()->SetClippingRange(1, 2000);
+  connect(ssc::toolManager(), SIGNAL(dominantToolChanged(const QString&)), this, SLOT(dominantToolChangedSlot()));
+  dominantToolChangedSlot();
 }
+
 View3D::~View3D()
 {
 }
 
 void View3D::setCameraStyle(View3D::CameraStyle style, int offset)
 {
-  ssc::messageManager()->sendDebug("View3D is trying to set the camerastyle.");
+//  ssc::messageManager()->sendDebug("View3D is trying to set the camerastyle.");
 
   if(mCameraStyle == style)
     return;
@@ -47,8 +50,6 @@ void View3D::setCameraStyle(View3D::CameraStyle style, int offset)
   switch(style)
   {
   case View3D::DEFAULT_STYLE:
-    if(mCameraStyle == View3D::TOOL_STYLE)
-      this->deactivateCameraToolStyle(); //need to disconnect some signals
     this->activateCameraDefaultStyle();
     break;
   case View3D::TOOL_STYLE:
@@ -58,122 +59,101 @@ void View3D::setCameraStyle(View3D::CameraStyle style, int offset)
     break;
   };
 }
+
 void View3D::setCameraOffsetSlot(int offset)
 {
-  mCameraOffset = offset;
+  if(offset != -1)
+    mCameraOffset = std::max<int>(offset, 1.0);
 }
+
 void View3D::moveCameraToolStyleSlot(Transform3D prMt, double timestamp)
 {
-//  ssc::Vector3D cameraPoint_t = ssc::Vector3D(0,0,-mCameraOffset); //the camerapoint in tool space
-//  ssc::Transform3D cameraTransform_pr = prMt*ssc::createTransformTranslate(cameraPoint_t);
-//  ssc::Vector3D cameraPoint_pr = ssc::Vector3D(cameraTransform_pr[0][3], cameraTransform_pr[1][3], cameraTransform_pr[2][3]); //the camera point in patient ref space
-
   ssc::Transform3D rMpr = *ssc::toolManager()->get_rMpr();
-//  ssc::Transform3D cameraPoint_r = rMpr*ssc::createTransformTranslate(cameraPoint_pr);
 
   ssc::Transform3D rMt = rMpr * prMt;
-
-//  double xCameraPos = cameraPoint_r[0][3];
-//  double yCameraPos = cameraPoint_r[1][3];
-//  double zCameraPos = cameraPoint_r[2][3];
-//  double xFocalPos = rMt[0][3];
-//  double yFocalPos = rMt[1][3];
-//  double zFocalPos = rMt[2][3];
 
   ssc::Vector3D camera_r = rMt.coord(ssc::Vector3D(0,0,-mCameraOffset));
   ssc::Vector3D focus_r = rMt.coord(ssc::Vector3D(0,0,0));
   ssc::Vector3D vup_r = rMt.vector(ssc::Vector3D(-1,0,0));
-  
-  //std::cout << rMt_withOffset << std::endl;
-  //std::cout << (*rMt) << std::endl;
 
   vtkCamera* camera = mRenderer->GetActiveCamera();
   camera->SetPosition(camera_r.begin());
   camera->SetFocalPoint(focus_r.begin());
   camera->SetViewUp(vup_r.begin());
 
-//  camera->SetPosition(xCameraPos, yCameraPos, zCameraPos);
-//  camera->SetFocalPoint(xFocalPos, yFocalPos, zFocalPos);
-
   camera->SetClippingRange(1, 2000);
 }
+
 void View3D::activateCameraDefaultStyle()
 {
+  this->disconnectTool();
+
   mRenderer->ResetCamera();
   mRenderer->GetActiveCamera()->SetClippingRange(1, 2000);
 
   mCameraStyle = View3D::DEFAULT_STYLE;
 
-  ssc::messageManager()->sendDebug("Default camera style activated.");
+  ssc::messageManager()->sendInfo("Default camera style activated.");
 }
+
 void View3D::activateCameraToolStyle(int offset)
 {
-  if(offset != -1)
-    mCameraOffset = offset;
+  this->setCameraOffsetSlot(offset);
+  mCameraStyle = View3D::TOOL_STYLE;
+  this->connectTool();
+  ssc::messageManager()->sendInfo("Tool camera style activated.");
+}
 
-  ssc::ToolPtr dominantToolPtr = ssc::toolManager()->getDominantTool();
-  if(!dominantToolPtr)
+void View3D::dominantToolChangedSlot()
+{
+  ssc::ToolPtr newTool = ssc::toolManager()->getDominantTool();
+  if (newTool==mFollowingTool)
+    return;
+  if (mCameraStyle!=View3D::TOOL_STYLE)
     return;
 
-  //Need the toolrep to get the direction the camera should point in
-  ssc::ToolRep3DPtr dominantToolRepPtr = repManager()->findFirstRep<ssc::ToolRep3D>(this->getReps(), dominantToolPtr);
+  this->disconnectTool();
+  this->connectTool();
+}
 
-//  ToolRep3DMap* toolRep3DMap = repManager()->getToolRep3DReps();
-//  ToolRep3DMap::iterator it = toolRep3DMap->begin();
-//  while(it != toolRep3DMap->end())
-//  {
-//    if(it->second->hasTool(dominantToolPtr) && //there must be a rep for the dominant tool
-//       this->hasRep(it->second)) //and this view must have it
-//    {
-//      dominantToolRepPtr = it->second;
-//    }
-//    it++;
-//  }
-  if(!dominantToolRepPtr)
+void View3D::connectTool()
+{
+  mFollowingTool = ssc::toolManager()->getDominantTool();
+
+  if (mFollowingTool)
   {
-    ssc::messageManager()->sendWarning("The tool you are requesting to follow does not have a rep associated with it.");
-    return; //cannot set the camera to follow a tool if that tool dose not have a rep
+
+    //Need the toolrep to get the direction the camera should point in
+    ssc::ToolRep3DPtr rep = repManager()->findFirstRep<ssc::ToolRep3D>(this->getReps(), mFollowingTool);
+
+    if(!rep)
+    {
+//      ssc::messageManager()->sendWarning("The tool you are requesting to follow does not have a rep associated with it.");
+      return; //cannot set the camera to follow a tool if that tool dose not have a rep
+    }
+
+    connect(mFollowingTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)), this, SLOT(moveCameraToolStyleSlot(Transform3D, double)));
+
+    rep->setOffsetPointVisibleAtZeroOffset(true);
+    rep->setStayHiddenAfterVisible(true);
+
+    ssc::messageManager()->sendInfo("Tool camera style: Following tool with uid: "+mFollowingTool->getUid());
+  }
+}
+
+void View3D::disconnectTool()
+{
+  if (mFollowingTool)
+  {
+    disconnect(mFollowingTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)), this, SLOT(moveCameraToolStyleSlot(Transform3D, double)));
+
+    ssc::ToolRep3DPtr toolRep = repManager()->findFirstRep<ssc::ToolRep3D>(this->getReps(), mFollowingTool);
+    if(toolRep)
+      toolRep->setStayHiddenAfterVisible(false);
   }
 
-  mFollowingTool = dominantToolPtr;
-  connect(mFollowingTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)),
-          this, SLOT(moveCameraToolStyleSlot(Transform3D, double)));
-
-//  std::cout << "dominant: " << dominantToolRepPtr->getTool()->getName() << std::endl;
-  dominantToolRepPtr->setOffsetPointVisibleAtZeroOffset(true);
-  dominantToolRepPtr->setStayHiddenAfterVisible(true);
-
-  mCameraStyle = View3D::TOOL_STYLE;
-  ssc::messageManager()->sendDebug("Tool camera style activated. Following tool with uid: "+mFollowingTool->getUid());
+  mFollowingTool.reset();
 }
-void View3D::deactivateCameraToolStyle()
-{
-  //make sure we disconnect from the right tool, which might not be the current
-  //dominant tool
-  if(!mFollowingTool)
-    return;
-
-  disconnect(mFollowingTool.get(), SIGNAL(toolTransformAndTimestamp(Transform3D, double)),
-      this, SLOT(moveCameraToolStyleSlot(Transform3D, double)));
-
-  ssc::ToolRep3DPtr toolRep = repManager()->findFirstRep<ssc::ToolRep3D>(this->getReps(), mFollowingTool);
-  if(toolRep)
-    toolRep->setStayHiddenAfterVisible(false);
 
 
-//  ToolRep3DMap* toolRep3DMap = repManager()->getToolRep3DReps();
-//  ToolRep3DMap::iterator it = toolRep3DMap->begin();
-//  while(it != toolRep3DMap->end())
-//  {
-//    if(it->second->hasTool(mFollowingTool))
-//    {
-//      //it->second->setOffsetPointVisibleAtZeroOffset(false);
-//      it->second->setStayHiddenAfterVisible(false);
-//    }
-//    ++it;
-//  }
-
-
-  ssc::messageManager()->sendDebug("Default camera style deactivated.");
-}
 }//namespace cx
