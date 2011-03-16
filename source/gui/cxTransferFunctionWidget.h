ed23db2dd63d1d05b191905aedc21f0486a62766
Christian Askeland <christian.askeland@sintef.no>
2011-03-16 08:46:25 +0100

    more work on tf2d
    Former-commit-id: 241c8a036cddb36c4e8e37563dcf5f492efa07ba

--- source/gui/cxTransferFunctionWidget.h
+++ source/gui/cxTransferFunctionWidget.h
@@ -16,6 +16,50 @@ namespace cx
 class TransferFunctionAlphaWidget;
 class TransferFunctionColorWidget;
 
+//ShadingWidget
+
+/*
+ *
+ */
+class TransferFunction3DWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  TransferFunction3DWidget(QWidget* parent);
+  virtual ~TransferFunction3DWidget();
+
+public slots:
+  void activeImageChangedSlot();
+
+protected:
+  void init();
+  QVBoxLayout* mLayout;
+  TransferFunctionAlphaWidget* mTransferFunctionAlphaWidget;
+  TransferFunctionColorWidget* mTransferFunctionColorWidget;
+};
+
+/*
+ *
+ */
+class TransferFunction2DWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  TransferFunction2DWidget(QWidget* parent);
+  virtual ~TransferFunction2DWidget();
+
+public slots:
+  void activeImageChangedSlot();
+
+protected:
+  void init();
+  QVBoxLayout* mLayout;
+  TransferFunctionAlphaWidget* mTransferFunctionAlphaWidget;
+  TransferFunctionColorWidget* mTransferFunctionColorWidget;
+};
+
 /**
  * \class TransferFunctionWidget
  *
@@ -31,26 +75,29 @@ class TransferFunctionWidget : public QWidget
 
 public:
   TransferFunctionWidget(QWidget* parent);
-  ~TransferFunctionWidget();
+  virtual ~TransferFunctionWidget();
 
 public slots:
   void presetsBoxChangedSlot(const QString& presetName);
   void resetSlot();
   void saveSlot();
-  void activeImageChangedSlot();
+//  void activeImageChangedSlot();
 
 protected:
   void init();///< Initialize TransferFunctionWidget. Create members
   //gui
   QVBoxLayout* mLayout;
-  TransferFunctionAlphaWidget* mTransferFunctionAlphaWidget;
-  TransferFunctionColorWidget* mTransferFunctionColorWidget;
+//  TransferFunctionAlphaWidget* mTransferFunctionAlphaWidget;
+//  TransferFunctionColorWidget* mTransferFunctionColorWidget;
   QComboBox* mPresetsComboBox;
+  TransferFunction3DWidget* mTF3DWidget;
+  TransferFunction2DWidget* mTF2DWidget;
 
   PresetTransferFunctions3D mPresets;
 
-	bool mInitialized;///< Is TransferFunctionWidget initialized
+//	bool mInitialized;///< Is TransferFunctionWidget initialized
 };
+
 }
 
 #endif /* CXTRANSFERFUNCTIONWIDGET_H_ */
