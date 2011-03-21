150c8cf10c801880987cef6c94bc79f73ab559e9
Janne Beate Lervik Bakeng <Janne.Beate.Bakeng@sintef.no>
2011-03-21 15:07:51 +0100

    Refactored the toolmanager to operate in its own thread, also did some cleaning up.
    Former-commit-id: 197afce43ce2b933bfbf24d1d37921adc544505c

---
+++ source/toolManager/cxIgstkToolManager.h
@@ -0,0 +1,75 @@
+#ifndef CXIGSTKTOOLMANAGER_H_
+#define CXIGSTKTOOLMANAGER_H_
+
+#include <map>
+#include <vector>
+#include <QThread>
+#include <QTimer>
+#include <QMutex>
+#include "cxIgstkTracker.h"
+#include "cxTool.h"
+#include "cxIgstkTool.h"
+
+/**
+ * cxIgstkToolManager.h
+ *
+ * \brief
+ *
+ * \date Mar 17, 2011
+ * \author jbake
+ */
+namespace cx
+{
+
+/**
+ * cxIgstkToolManager.h
+ *
+ * \brief
+ *
+ * \date Mar 16, 2011
+ * \author Janne Beate Bakeng, SINTEF
+ * \author Christian Askeland, SINTEF
+ */
+class IgstkToolManager: public QObject
+{
+  Q_OBJECT
+
+public:
+  IgstkToolManager(IgstkTracker::InternalStructure trackerStructure, std::vector<Tool::InternalStructure> toolStructures);
+  virtual ~IgstkToolManager();
+
+  std::map<QString, IgstkToolPtr> getTools(); ///< ThreadSafe
+
+signals:
+  void initialized(bool on); ///< trackers and tools are initialized
+  void tracking(bool on);
+
+public slots:
+  void initializeSlot(bool on); ///< connects to the hardware
+  void trackSlot(bool on); ///< tracking on or off
+
+private slots:
+  void trackerTrackingSlot(bool);
+  void checkTimeoutsAndRequestTransformSlot();
+  void deviceInitializedSlot(bool);
+
+private:
+  void createTracker(IgstkTracker::InternalStructure trackerStructure);
+  void createTools(std::vector<Tool::InternalStructure> toolStructures);
+    void setReference();
+
+  TrackerPtr mTracker;
+  QMutex mToolMutex; ///< protects mTools
+  std::map<QString, IgstkToolPtr> mTools;
+  int mInitAnsweres; ///< keeps track of how many tools and trackers have gotten an answer from the hardware
+  IgstkToolPtr mReferenceTool;
+
+  QTimer* mTimer; ///< timer controlling the demand of transforms
+  igstk::PulseGenerator::Pointer mPulseGenerator;
+
+};
+typedef boost::shared_ptr<IgstkToolManager> IgstkToolManagerPtr;
+
+}
+
+#endif /* CXIGSTKTOOLMANAGER_H_ */
