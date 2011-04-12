56b6d70945ab16a72dd2e91c0183c2a7730d3894
Christian Askeland <christian.askeland@sintef.no>
2011-04-12 15:52:20 +0200

    light greeen color for dominant tool
    Former-commit-id: 6f838d3f11687bb3da062fcbdefc9ef812f88daf

--- source/gui/cxCustomStatusBar.cpp
+++ source/gui/cxCustomStatusBar.cpp
@@ -20,6 +20,7 @@ CustomStatusBar::CustomStatusBar() :
   connect(ssc::toolManager(), SIGNAL(trackingStarted()), this, SLOT(connectToToolSignals()));
   connect(ssc::toolManager(), SIGNAL(trackingStopped()), this, SLOT(disconnectFromToolSignals()));
   connect(ssc::toolManager(), SIGNAL(tps(int)), this, SLOT(tpsSlot(int)));
+  connect(ssc::toolManager(), SIGNAL(dominantToolChanged(const QString&)), this, SLOT(receiveToolDominant()));
   connect(viewManager(), SIGNAL(fps(int)),this, SLOT(fpsSlot(int)));
   
   this->addPermanentWidget(mFpsLabel);
@@ -39,12 +40,12 @@ void CustomStatusBar::connectToToolSignals()
     ssc::ToolPtr tool = it->second;
     if(tool->getType() == ssc::Tool::TOOL_MANUAL)
       continue;
-    connect(tool.get(), SIGNAL(toolVisible(bool)), this, SLOT(receiveToolVisible(bool)));
+    connect(tool.get(), SIGNAL(toolVisible(bool)), this, SLOT(receiveToolVisible()));
 
     QString toolName = tool->getName();
 
     QLabel* toolLabel = new QLabel(toolName);
-    this->setToolLabelColor(toolLabel, tool->getVisible());
+    this->setToolLabelColor(toolLabel, tool->getVisible(), ssc::toolManager()->getDominantTool()==tool);
     this->addPermanentWidget(toolLabel);
     mToolLabels.push_back(toolLabel);
   }
@@ -57,7 +58,7 @@ void CustomStatusBar::disconnectFromToolSignals()
   ssc::ToolManager::ToolMap::iterator toolIt = initializedTools->begin();
   for(;toolIt != initializedTools->end(); ++toolIt)
   {
-    disconnect(toolIt->second.get(), SIGNAL(toolVisible(bool)), this, SLOT(receiveToolVisible(bool)));
+    disconnect(toolIt->second.get(), SIGNAL(toolVisible(bool)), this, SLOT(receiveToolVisible()));
   }
   for(unsigned i=0; i<mToolLabels.size(); ++i)
   {
@@ -68,9 +69,19 @@ void CustomStatusBar::disconnectFromToolSignals()
   mToolLabels.clear();
 }
 
-void CustomStatusBar::receiveToolVisible(bool visible)
+void CustomStatusBar::receiveToolVisible()
 {
   ssc::Tool* tool = dynamic_cast<ssc::Tool*>(this->sender());
+  this->colorTool(tool);
+}
+
+void CustomStatusBar::receiveToolDominant()
+{
+  this->colorTool(ssc::toolManager()->getDominantTool().get());
+}
+
+void CustomStatusBar::colorTool(ssc::Tool* tool)
+{
   if(!tool)
   {
     ssc::messageManager()->sendWarning("Could not determine which tool changed visibility.");
@@ -82,15 +93,20 @@ void CustomStatusBar::receiveToolVisible(bool visible)
   {
     QLabel* toolLabel = mToolLabels[i];
     if(toolLabel->text().compare(name, Qt::CaseInsensitive) == 0)
-      this->setToolLabelColor(toolLabel, tool->getVisible());
+      this->setToolLabelColor(toolLabel, tool->getVisible(), ssc::toolManager()->getDominantTool().get()==tool);
   }
 }
 
-void CustomStatusBar::setToolLabelColor(QLabel* label, bool visible)
+void CustomStatusBar::setToolLabelColor(QLabel* label, bool visible, bool dominant)
 {
   QString color;
   if(visible)
-    color = QString("QLabel { background-color: green }");
+  {
+    if (dominant)
+      color = QString("QLabel { background-color: lightgreen }");
+    else
+      color = QString("QLabel { background-color: green }");
+  }
   else
     color = QString("QLabel { background-color: red }");
 
