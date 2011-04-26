9cf0a2a7fc19864a035fab41b559b25ab2d08964
Janne Beate Lervik Bakeng <Janne.Beate.Bakeng@sintef.no>
2011-04-26 11:50:26 +0200

    Moved adding of tabs in method hierarchy to respective classes.
    Former-commit-id: 7b678b659b61d45cd81b4ffb364aa0526b343459

--- source/gui/cxTabbedWidget.cpp
+++ source/gui/cxTabbedWidget.cpp
@@ -4,6 +4,23 @@
 #include <QTabWidget>
 #include <QVBoxLayout>
 
+//segmentation
+#include "cxConnectedThresholdImageFilterWidget.h"
+#include "cxBinaryThresholdImageFilterWidget.h"
+
+//registration
+#include "cxImageRegistrationWidget.h"
+#include "cxPatientRegistrationWidget.h"
+#include "cxFastImageRegistrationWidget.h"
+#include "cxFastPatientRegistrationWidget.h"
+#include "cxFastOrientationRegistrationWidget.h"
+#include "cxImageSegmentationAndCenterlineWidget.h"
+#include "cxPlateRegistrationWidget.h"
+#include "cxManualRegistrationOffsetWidget.h"
+
+//calibration
+#include "cxToolTipCalibrationWidget.h"
+
 namespace cx
 {
 //------------------------------------------------------------------------------
@@ -33,65 +50,6 @@ QString TabbedWidget::defaultWhatsThis() const
       "<h3>Tabbed widget.</h3>"
       "<p>This is a tabbed widget, used for creating a hierarchy of tabbed widgets.</p>"
       "<p><i>If you see this whats this message, something's wrong!</i></p>"
-      "</html>";
-}
-
-//------------------------------------------------------------------------------
-
-RegistrationMethodsWidget::RegistrationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
-  TabbedWidget(parent, objectName, windowTitle)
-{
-}
-
-QString RegistrationMethodsWidget::defaultWhatsThis() const
-{
-  return"<html>"
-      "<h3>Registration methods.</h3>"
-      "<p>This is a way to tell the system how to display data and tools in the same world.</p>"
-      "<p><i>Choose a method.</i></p>"
-      "</html>";
-}
-
-//------------------------------------------------------------------------------
-SegmentationMethodsWidget::SegmentationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
-  TabbedWidget(parent, objectName, windowTitle)
-{
-}
-
-QString SegmentationMethodsWidget::defaultWhatsThis() const
-{
-  return"<html>"
-      "<h3>Segmentation methods.</h3>"
-      "<p>Segmentation methods are used to extract parts of a volume.</p>"
-      "<p><i>Choose a method.</i></p>"
-      "</html>";
-}
-//------------------------------------------------------------------------------
-VisualizationMethodsWidget::VisualizationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
-  TabbedWidget(parent, objectName, windowTitle)
-{
-}
-
-QString VisualizationMethodsWidget::defaultWhatsThis() const
-{
-  return"<html>"
-      "<h3>Visualization methods.</h3>"
-      "<p>These methods creates data structures that can be use in visualization.</p>"
-      "<p><i>Choose a method.</i></p>"
-      "</html>";
-}
-//------------------------------------------------------------------------------
-CalibrationMethodsWidget::CalibrationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
-  TabbedWidget(parent, objectName, windowTitle)
-{
-}
-
-QString CalibrationMethodsWidget::defaultWhatsThis() const
-{
-  return"<html>"
-      "<h3>Calibration methods.</h3>"
-      "<p>These methods creates data structures that can be use in visualization.</p>"
-      "<p><i>Choose a method.</i></p>"
       "</html>";
 }
 //------------------------------------------------------------------------------
@@ -155,6 +113,109 @@ QString Image2PlateRegistrationWidget::defaultWhatsThis() const
       "<h3>Image to plate registration.</h3>"
       "<p>This is a method used to registrate one image to a specific aurora tool (a plastic plate with ct markers).</p>"
       "<p><i>Choose a step to continue.</i></p>"
+      "</html>";
+}
+
+//------------------------------------------------------------------------------
+
+RegistrationMethodsWidget::RegistrationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
+  TabbedWidget(parent, objectName, windowTitle)
+{
+  //landmark
+  LandmarkRegistrationsWidget* landmarkRegistrationsWidget = new LandmarkRegistrationsWidget(this, "LandmarkRegistrationWidget", "Landmark Registrations");
+  ImageRegistrationWidget* imageRegistrationWidget = new ImageRegistrationWidget(landmarkRegistrationsWidget, "ImageRegistrationWidget", "Image Registration");
+  PatientRegistrationWidget* patientRegistrationWidget = new PatientRegistrationWidget(landmarkRegistrationsWidget, "PatientRegistrationWidget", "Patient Registration");
+  landmarkRegistrationsWidget->addTab(imageRegistrationWidget, "Image");
+  landmarkRegistrationsWidget->addTab(patientRegistrationWidget, "Patient");
+
+  //fast
+  FastRegistrationsWidget* fastRegistrationsWidget = new FastRegistrationsWidget(this, "FastRegistrationWidget", "Fast Registrations");
+  FastOrientationRegistrationWidget* fastOrientationRegistrationWidget = new FastOrientationRegistrationWidget(fastRegistrationsWidget);
+  FastImageRegistrationWidget* fastImageRegistrationWidget = new FastImageRegistrationWidget(fastRegistrationsWidget, "FastImageRegistrationWidget", "Fast Image Registration");
+  FastPatientRegistrationWidget* fastPatientRegistrationWidget = new FastPatientRegistrationWidget(fastRegistrationsWidget);
+  fastRegistrationsWidget->addTab(fastOrientationRegistrationWidget, "Orientation");
+  fastRegistrationsWidget->addTab(fastImageRegistrationWidget, "Image");
+  fastRegistrationsWidget->addTab(fastPatientRegistrationWidget, "Patient");
+
+  //vessel based image to image
+  Image2ImageRegistrationWidget* image2imageWidget = new Image2ImageRegistrationWidget(this, "Image2ImageRegistrationWidget", "Image 2 Image Registration");
+  FixedImage2ImageWidget* fixedRegistrationWidget = new FixedImage2ImageWidget(image2imageWidget);
+  MovingImage2ImageWidget* movingRegistrationWidget = new MovingImage2ImageWidget(image2imageWidget);
+
+  image2imageWidget->addTab(movingRegistrationWidget, "Moving"); //should be application specific
+  image2imageWidget->addTab(fixedRegistrationWidget, "Fixed"); //should be application specific
+  image2imageWidget->addTab(new RegisterI2IWidget(image2imageWidget),"Register");
+
+  //manual offset
+  ManualRegistrationOffsetWidget* landmarkManualRegistrationOffsetWidget = new ManualRegistrationOffsetWidget(this);
+
+  //plate
+  Image2PlateRegistrationWidget* imageAndPlateRegistrationWidget = new Image2PlateRegistrationWidget(this, "PlateRegistrationWidget", "Plate");
+  PlateImageRegistrationWidget* platesImageRegistrationWidget = new PlateImageRegistrationWidget(imageAndPlateRegistrationWidget);
+  PlateRegistrationWidget* plateRegistrationWidget = new PlateRegistrationWidget(imageAndPlateRegistrationWidget);
+  imageAndPlateRegistrationWidget->addTab(plateRegistrationWidget, "Plate");
+  imageAndPlateRegistrationWidget->addTab(platesImageRegistrationWidget, "Image");
+
+  this->addTab(landmarkRegistrationsWidget, "Landmark");
+  this->addTab(fastRegistrationsWidget, "Fast");
+  this->addTab(landmarkManualRegistrationOffsetWidget, "Manual");
+  this->addTab(image2imageWidget, "Image2Image");
+  this->addTab(imageAndPlateRegistrationWidget, "Plate");
+}
+
+QString RegistrationMethodsWidget::defaultWhatsThis() const
+{
+  return"<html>"
+      "<h3>Registration methods.</h3>"
+      "<p>This is a way to tell the system how to display data and tools in the same world.</p>"
+      "<p><i>Choose a method.</i></p>"
+      "</html>";
+}
+
+//------------------------------------------------------------------------------
+SegmentationMethodsWidget::SegmentationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
+  TabbedWidget(parent, objectName, windowTitle)
+{
+  this->addTab(new BinaryThresholdImageFilterWidget(this), "Binary Threshold");
+  this->addTab(new ConnectedThresholdImageFilterWidget(this), "Connected Threshold");
+}
+
+QString SegmentationMethodsWidget::defaultWhatsThis() const
+{
+  return"<html>"
+      "<h3>Segmentation methods.</h3>"
+      "<p>Segmentation methods are used to extract parts of a volume.</p>"
+      "<p><i>Choose a method.</i></p>"
+      "</html>";
+}
+//------------------------------------------------------------------------------
+VisualizationMethodsWidget::VisualizationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
+  TabbedWidget(parent, objectName, windowTitle)
+{
+  this->addTab(new SurfaceWidget(this), "Surface");
+}
+
+QString VisualizationMethodsWidget::defaultWhatsThis() const
+{
+  return"<html>"
+      "<h3>Visualization methods.</h3>"
+      "<p>These methods creates data structures that can be use in visualization.</p>"
+      "<p><i>Choose a method.</i></p>"
+      "</html>";
+}
+//------------------------------------------------------------------------------
+CalibrationMethodsWidget::CalibrationMethodsWidget(QWidget* parent, QString objectName, QString windowTitle) :
+  TabbedWidget(parent, objectName, windowTitle)
+{
+  this->addTab(new ToolTipCalibrationWidget(this), "Tool Tip");
+}
+
+QString CalibrationMethodsWidget::defaultWhatsThis() const
+{
+  return"<html>"
+      "<h3>Calibration methods.</h3>"
+      "<p>These methods creates data structures that can be use in visualization.</p>"
+      "<p><i>Choose a method.</i></p>"
       "</html>";
 }
 
