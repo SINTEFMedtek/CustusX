862d98353a4caabf59daeb747dac15fd4421ebf6
Janne Beate Lervik Bakeng <Janne.Beate.Bakeng@sintef.no>
2011-04-26 14:39:13 +0200

    Implemented use of the ConnectedThresholdImageFilter.
    Former-commit-id: a9b12b30208275b73cc5d39bc35fa2facc95bf07

--- source/algorithms/cxConnectedThresholdImageFilter.cpp
+++ source/algorithms/cxConnectedThresholdImageFilter.cpp
@@ -1,4 +1,12 @@
-#include <cxConnectedThresholdImageFilter.h>
+#include "cxConnectedThresholdImageFilter.h"
+
+#include "itkConnectedThresholdImageFilter.h"
+
+#include "sscTypeConversions.h"
+#include "sscMessageManager.h"
+#include "sscDataManager.h"
+#include "sscRegistrationTransform.h"
+
 
 namespace cx
 {
@@ -13,13 +21,116 @@ ConnectedThresholdImageFilter::~ConnectedThresholdImageFilter()
 {
 }
 
+void ConnectedThresholdImageFilter::setInput(ssc::ImagePtr image, QString outputBasePath, float lowerThreshold, float upperThreshold, int replaceValue, itkImageType::IndexType seed)
+{
+  mInput = image;
+  mOutputBasePath = outputBasePath;
+
+  mLowerThreshold = lowerThreshold;
+  mUpperTheshold = upperThreshold;
+  mReplaceValue = replaceValue;
+  mSeed = seed;
+
+  this->generate();
+}
+
+ssc::ImagePtr ConnectedThresholdImageFilter::getOutput()
+{
+  return mOutput;
+}
+
 void ConnectedThresholdImageFilter::postProcessingSlot()
 {
+  vtkImageDataPtr rawResult = this->getResult();
+
+  QString uid = mInput->getUid() + "_seg%1";
+  QString name = mInput->getName()+" seg%1";
+  mOutput = ssc::dataManager()->createImage(rawResult,uid, name);
+  if(!mOutput)
+  {
+    ssc::messageManager()->sendError("Segmentation failed.");
+    return;
+  }
+
+  mOutput->get_rMd_History()->setRegistration(mInput->get_rMd());
+  mOutput->get_rMd_History()->setParentFrame(mInput->getUid());
+  ssc::dataManager()->loadData(mOutput);
+  ssc::dataManager()->saveImage(mOutput, mOutputBasePath);
+
+  ssc::messageManager()->sendSuccess("Done segmenting: \"" + mOutput->getName()+"\"");
+
+  emit finished();
 }
 
 vtkImageDataPtr ConnectedThresholdImageFilter::calculate()
 {
-  return vtkImageDataPtr();
+  itkImageType::ConstPointer itkImage = AlgorithmHelper::getITKfromSSCImage(mInput);
+
+  //Connected Thresholding
+
+  //Documentation:
+  // http://www.na-mic.org/svn/Slicer3-lib-mirrors/trunk/Insight/Examples/Segmentation/ConnectedThresholdImageFilter.cxx
+
+  //  The ConnectedThresholdImageFilter has two main parameters to be
+  //  defined. They are the lower and upper thresholds of the interval in
+  //  which intensity values should fall in order to be included in the
+  //  region. Setting these two values too close will not allow enough
+  //  flexibility for the region to grow. Setting them too far apart will
+  //  result in a region that engulfs the image.
+
+  //  The output of this filter is a binary image with zero-value pixels
+  //  everywhere except on the extracted region. The intensity value set
+  //  inside the region is selected with the method SetReplaceValue()
+
+  //  The initialization of the algorithm requires the user to provide a seed
+  //  point. It is convenient to select this point to be placed in a
+  //  typical region of the anatomical structure to be segmented. The
+  //  seed is passed in the form of a IndexType to the SetSeed()
+  //  method.
+
+  //  Another option for segmenting regions is to take advantage of the
+  //  functionality provided by the ConnectedThresholdImageFilter for
+  //  managing multiple seeds. The seeds can be passed one by one to the
+  //  filter using the AddSeed() method. You could imagine a user
+  //  interface in which an operator clicks on multiple points of the object
+  //  to be segmented and each selected point is passed as a seed to this
+  //  filter.
+
+  typedef itk::ConnectedThresholdImageFilter<itkImageType, itkImageType> thresholdFilterType;
+  thresholdFilterType::Pointer thresholdFilter = thresholdFilterType::New();
+  thresholdFilter->SetInput(itkImage);
+
+  //set thresholds
+  thresholdFilter->SetLower(mLowerThreshold);
+  thresholdFilter->SetUpper(mUpperTheshold);
+  thresholdFilter->SetReplaceValue(mReplaceValue);
+
+  //set seeds
+  thresholdFilter->SetSeed(mSeed);
+
+  //calculate
+  try
+  {
+    thresholdFilter->Update();
+  }
+  catch( itk::ExceptionObject & excep )
+  {
+    ssc::messageManager()->sendError("Error when setting seed for Connected Threshold Image Filter:");
+    ssc::messageManager()->sendError(qstring_cast(excep.GetDescription()), true);
+  }
+
+  itkImage = thresholdFilter->GetOutput();
+
+  //Convert ITK to VTK
+  itkToVtkFilterType::Pointer itkToVtkFilter = itkToVtkFilterType::New();
+  itkToVtkFilter->SetInput(itkImage);
+  itkToVtkFilter->Update();
+
+  vtkImageDataPtr rawResult = vtkImageDataPtr::New();
+  rawResult->DeepCopy(itkToVtkFilter->GetOutput());
+  // TODO: possible memory problem here - check debug mem system of itk/vtk
+
+  return rawResult;
 }
 
 }
