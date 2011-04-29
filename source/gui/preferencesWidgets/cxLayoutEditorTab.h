e7b31f09d1cd5d522c7f2dbe08982c86acc68f5b
Janne Beate Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2011-04-29 13:27:47 +0200

    Added new class LayoutEditorTab
    Former-commit-id: 9d7a71593f449858e9cf831c1064b273baa411ab

---
+++ source/gui/preferencesWidgets/cxLayoutEditorTab.h
@@ -0,0 +1,25 @@
+#ifndef cxLayoutEditorTab_h__
+#define cxLayoutEditorTab_h__
+
+#include "cxPreferencesDialog.h"
+
+namespace cx
+{
+/**
+* \class LayoutEditorTab
+*
+* \brief
+*
+* \date Apr 29, 2011
+* \author Janne Beate Bakeng, SINTEF
+*/
+class LayoutEditorTab : public PreferencesTab
+{
+  Q_OBJECT
+public:
+  LayoutEditorTab();
+  virtual ~LayoutEditorTab();
+};
+
+}//namespace cx
+#endif // cxLayoutEditorTab_h__
