1cec8c9e7bb0e434aef5cb0e9e5846c9af9ce01f
Christian Askeland <christian.askeland@sintef.no>
2011-05-02 14:05:20 +0200

    completed refactor of v2v gui
    Former-commit-id: 7f877c3c38af57959569efe371bf52264885ff05

--- source/gui/registrationWidgets/cxImageSegmentationAndCenterlineWidget.h
+++ source/gui/registrationWidgets/cxImageSegmentationAndCenterlineWidget.h
@@ -26,53 +26,32 @@ class ImageSegmentationAndCenterlineWidget : public BaseWidget
   Q_OBJECT
 
 public:
-//  ImageSegmentationAndCenterlineWidget(QWidget* parent, QString objectName, QString windowTitle);
   ImageSegmentationAndCenterlineWidget(QWidget* parent);
   virtual ~ImageSegmentationAndCenterlineWidget();
   virtual QString defaultWhatsThis() const;
 
 protected:
   QVBoxLayout* mLayout;
-  ssc::StringDataAdapterPtr mOutput;
 
 private slots:
   void setImageSlot(QString uid);
   void resampleOutputArrived(QString uid);
   void segmentationOutputArrived(QString uid);
-//  void surfaceOutputArrived(QString uid);
   void centerlineOutputArrived(QString uid);
+
+  void setColorSlot(QColor color);
+  void toMovingSlot();
+  void toFixedSlot();
 
 protected:
   class ResampleWidget*     mResampleWidget;
   class BinaryThresholdImageFilterWidget* mSegmentationWidget;
-//  class SurfaceWidget*      mSurfaceWidget;
   class CenterlineWidget*   mCenterlineWidget;
-
   SelectImageStringDataAdapterPtr mResampleOutput;
   SelectImageStringDataAdapterPtr mSegmentationOutput;
-//  SelectMeshStringDataAdapterPtr mSurfaceOutput;
   SelectDataStringDataAdapterPtr mCenterlineOutput;
 };
 
-//class FixedImage2ImageWidget : public ImageSegmentationAndCenterlineWidget
-//{
-//public:
-//  FixedImage2ImageWidget(QWidget* parent);
-//  virtual ~FixedImage2ImageWidget();
-//  virtual QString defaultWhatsThis() const;
-//
-//private:
-//};
-//
-//class MovingImage2ImageWidget : public ImageSegmentationAndCenterlineWidget
-//{
-//public:
-//  MovingImage2ImageWidget(QWidget* parent);
-//  virtual ~MovingImage2ImageWidget();
-//  virtual QString defaultWhatsThis() const;
-//
-//private:
-//};
 
 }//namespace cx
 #endif /* CXIMAGESEGMENTATIONANDCENTERLINEWIDGET_H_ */
