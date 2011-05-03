31fd91b3219b905a03430759e2dc08d8eb4ffe8f
Janne Beate Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2011-05-03 11:35:18 +0200

    Added new resource icon, implemented layouteditortab functionality for deleting custom layouts, know issue is that save is not smart enough.
    Former-commit-id: e0152281065413210e7b3ad1b694f7b1156850f0

--- source/gui/preferencesWidgets/cxLayoutEditorTab.cpp
+++ source/gui/preferencesWidgets/cxLayoutEditorTab.cpp
@@ -3,6 +3,7 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <QComboBox>
+#include <QPushButton>
 #include <QActionGroup>
 #include "cxViewManager.h"
 
@@ -13,17 +14,23 @@ LayoutEditorTab::LayoutEditorTab(QWidget* parent) :
   PreferencesTab(parent),
   mLayoutEditorWidget(new LayoutEditorWidget(this)),
   mLayoutLabel(new QLabel("Custom layouts: ")),
-  mLayoutList(new QComboBox())
+  mLayoutList(new QComboBox()),
+  mDeleteButton(new QPushButton(QIcon(":/icons/open_icon_library/png/64x64/actions/edit-delete-2.png"), ""))
 {
+  mLayoutList->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+  mDeleteButton->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+
   connect(mLayoutList, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(layoutToEditChangedSlot(const QString&)));
+  connect(mDeleteButton, SIGNAL(clicked()), this, SLOT(deleteCustomLayoutSlot()));
 }
 
 LayoutEditorTab::~LayoutEditorTab()
-{
-}
+{}
 
 void LayoutEditorTab::saveParametersSlot()
 {
+  //TODO when do we want to save???
+
   LayoutData data = mLayoutEditorWidget->getLayoutData();
   if (data.getUid().isEmpty())
     return;
@@ -38,7 +45,8 @@ void LayoutEditorTab::init()
   QGridLayout* layout = new QGridLayout;
   layout->addWidget(mLayoutLabel, 0, 0);
   layout->addWidget(mLayoutList, 0, 1);
-  layout->addWidget(mLayoutEditorWidget, 1, 0, 1, 2);
+  layout->addWidget(mDeleteButton, 0, 2);
+  layout->addWidget(mLayoutEditorWidget, 1, 0, 1, 3);
   mTopLayout->addLayout(layout);
 
   LayoutData data = viewManager()->getLayoutData(viewManager()->getActiveLayout());
@@ -49,6 +57,10 @@ void LayoutEditorTab::populateEditableLayouts(QString selectedLayoutUid)
 {
   mLayoutList->clear();
 
+  QString newName = "New layout";
+  QString newUid = newName;
+  this->addCustomLayoutToDisplayList(newName, newUid);
+
   std::vector<QString> layoutUids = viewManager()->getAvailableLayouts();
   for(int i=0; i<layoutUids.size();++i)
   {
@@ -56,40 +68,79 @@ void LayoutEditorTab::populateEditableLayouts(QString selectedLayoutUid)
     if(viewManager()->isCustomLayout(uid))
     {
       QString name = viewManager()->getLayoutData(uid).getName();
-      mLayoutList->addItem(name);
+      this->addCustomLayoutToDisplayList(name, uid);
     }
   }
 
-  int findIndex = mLayoutList->findText(selectedLayoutUid);
-  if(findIndex != -1)
-    mLayoutList->setCurrentIndex(findIndex);
-  else
-    this->layoutToEditChangedSlot(viewManager()->getActiveLayout());
+  int findIndex = mLayoutList->findData(selectedLayoutUid, Qt::ToolTipRole);
+  if(findIndex == -1)
+    findIndex = mLayoutList->findData(newUid, Qt::ToolTipRole);
+  
+  mLayoutList->setCurrentIndex(findIndex);
+
 }
 
-void LayoutEditorTab::layoutToEditChangedSlot(const QString& uid)
+void LayoutEditorTab::layoutToEditChangedSlot(const QString& name)
 {
+  int index = mLayoutList->findText(name);
+  QString uid = mLayoutList->itemData(index, Qt::ToolTipRole).toString();
+
   LayoutData data;
 
+  //std::cout << "================ " << name << " =================" << std::endl;
   std::vector<QString> layoutUids = viewManager()->getAvailableLayouts();
   std::vector<QString>::iterator it = std::find(layoutUids.begin(), layoutUids.end(), uid);
   if(it != layoutUids.end())
   {
-    std::cout << "Found layout with uid: " << uid << std::endl;
+    //std::cout << "Found layout in viewmanager: " << *it << std::endl;
     data = viewManager()->getLayoutData(uid);
   }
   else
   {
-    std::cout << "Did not find layout with uid: " << uid << std::endl;
+    //std::cout << "Did not find layout in viewmanager: " << name << std::endl;
     data = viewManager()->getLayoutData(viewManager()->getActiveLayout());
   }
   
-  if(!viewManager()->isCustomLayout(uid))
+  if(!viewManager()->isCustomLayout(data.getUid()))
+  {
+    //std::cout << "It is not a custom layout. New layout mode." << std::endl;
     data.resetUid(viewManager()->generateLayoutUid()); //for adding a new layout
+  }
   else
-   data = viewManager()->getLayoutData(uid); //for editing
+  {
+    //std::cout << "It is a custom layout. Editing mode." << std::endl;
+   data = viewManager()->getLayoutData(data.getUid()); //for editing
+  }
+  //std::cout << "=================================" << std::endl;
   
   mLayoutEditorWidget->setLayoutData(data);
+  viewManager()->setActiveLayout(data.getUid());
+}
+
+void LayoutEditorTab::deleteCustomLayoutSlot()
+{
+  int currentIndex = mLayoutList->currentIndex();
+  QString selectedLayoutName = mLayoutList->itemText(currentIndex);
+  QString selectedLayoutUid = mLayoutList->itemData(currentIndex, Qt::ToolTipRole).toString();
+  if(!viewManager()->isCustomLayout(selectedLayoutUid))
+    return;
+
+  //std::cout << "Going to erase " << selectedLayoutUid << std::endl;
+
+  viewManager()->deleteLayoutData(selectedLayoutUid);
+  viewManager()->setActiveLayout(viewManager()->getAvailableLayouts().front()); // revert to existing state
+
+  QString uid = viewManager()->getActiveLayout();
+  //std::cout << "Active layout uid is now " << uid << std::endl;
+  LayoutData activeLayoutData = viewManager()->getLayoutData(uid);
+  this->populateEditableLayouts(activeLayoutData.getUid());
+}
+
+void LayoutEditorTab::addCustomLayoutToDisplayList( QString displayName, QString uid )
+{
+  mLayoutList->addItem(displayName);
+  int index = mLayoutList->findText(displayName);
+  mLayoutList->setItemData(index, uid, Qt::ToolTipRole);
 }
 
 }//namespace cx
