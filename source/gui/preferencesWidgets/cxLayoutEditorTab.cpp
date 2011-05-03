1c2d58d8480323bd29a81804ca9ffc3d70c08686
Janne Beate Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2011-05-03 14:22:24 +0200

    Added a save layout button for saving layouts in the preference dialog instead of autosave on Ok.
    Former-commit-id: 2c3e451353dc4ea7cd1526eee24a54800438afeb

--- source/gui/preferencesWidgets/cxLayoutEditorTab.cpp
+++ source/gui/preferencesWidgets/cxLayoutEditorTab.cpp
@@ -15,13 +15,15 @@ LayoutEditorTab::LayoutEditorTab(QWidget* parent) :
   mLayoutEditorWidget(new LayoutEditorWidget(this)),
   mLayoutLabel(new QLabel("Custom layouts: ")),
   mLayoutList(new QComboBox()),
-  mDeleteButton(new QPushButton(QIcon(":/icons/open_icon_library/png/64x64/actions/edit-delete-2.png"), ""))
+  mDeleteButton(new QPushButton(QIcon(":/icons/open_icon_library/png/64x64/actions/edit-delete-2.png"), "")),
+  mSaveLayoutButton(new QPushButton("Save layout"))
 {
   mLayoutList->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
   mDeleteButton->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
 
   connect(mLayoutList, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(layoutToEditChangedSlot(const QString&)));
   connect(mDeleteButton, SIGNAL(clicked()), this, SLOT(deleteCustomLayoutSlot()));
+  connect(mSaveLayoutButton, SIGNAL(clicked()), this, SLOT(saveLayoutSlot()));
 }
 
 LayoutEditorTab::~LayoutEditorTab()
@@ -29,15 +31,6 @@ LayoutEditorTab::~LayoutEditorTab()
 
 void LayoutEditorTab::saveParametersSlot()
 {
-  //TODO when do we want to save???
-
-  LayoutData data = mLayoutEditorWidget->getLayoutData();
-  if (data.getUid().isEmpty())
-    return;
-  viewManager()->setLayoutData(data);
-
-  if(viewManager()->getActiveLayout() != data.getUid())
-   viewManager()->setActiveLayout(data.getUid());
 }
 
 void LayoutEditorTab::init()
@@ -47,6 +40,7 @@ void LayoutEditorTab::init()
   layout->addWidget(mLayoutList, 0, 1);
   layout->addWidget(mDeleteButton, 0, 2);
   layout->addWidget(mLayoutEditorWidget, 1, 0, 1, 3);
+  layout->addWidget(mSaveLayoutButton, 2, 2, 1, 1);
   mTopLayout->addLayout(layout);
 
   LayoutData data = viewManager()->getLayoutData(viewManager()->getActiveLayout());
@@ -82,6 +76,8 @@ void LayoutEditorTab::populateEditableLayouts(QString selectedLayoutUid)
 
 void LayoutEditorTab::layoutToEditChangedSlot(const QString& name)
 {
+  mSaveLayoutButton->setEnabled(name == "New layout");
+
   int index = mLayoutList->findText(name);
   QString uid = mLayoutList->itemData(index, Qt::ToolTipRole).toString();
 
@@ -120,20 +116,15 @@ void LayoutEditorTab::layoutToEditChangedSlot(const QString& name)
 void LayoutEditorTab::deleteCustomLayoutSlot()
 {
   int currentIndex = mLayoutList->currentIndex();
-  QString selectedLayoutName = mLayoutList->itemText(currentIndex);
   QString selectedLayoutUid = mLayoutList->itemData(currentIndex, Qt::ToolTipRole).toString();
   if(!viewManager()->isCustomLayout(selectedLayoutUid))
     return;
 
-  //std::cout << "Going to erase " << selectedLayoutUid << std::endl;
-
   viewManager()->deleteLayoutData(selectedLayoutUid);
   viewManager()->setActiveLayout(viewManager()->getAvailableLayouts().front()); // revert to existing state
 
-  QString uid = viewManager()->getActiveLayout();
-  //std::cout << "Active layout uid is now " << uid << std::endl;
-  LayoutData activeLayoutData = viewManager()->getLayoutData(uid);
-  this->populateEditableLayouts(activeLayoutData.getUid());
+  this->setToActiveLayout();
+
 }
 
 void LayoutEditorTab::addCustomLayoutToDisplayList( QString displayName, QString uid )
@@ -141,6 +132,26 @@ void LayoutEditorTab::addCustomLayoutToDisplayList( QString displayName, QString
   mLayoutList->addItem(displayName);
   int index = mLayoutList->findText(displayName);
   mLayoutList->setItemData(index, uid, Qt::ToolTipRole);
+}
+
+void LayoutEditorTab::saveLayoutSlot()
+{
+  LayoutData data = mLayoutEditorWidget->getLayoutData();
+  if (data.getUid().isEmpty())
+    return;
+  viewManager()->setLayoutData(data);
+
+  if(viewManager()->getActiveLayout() != data.getUid())
+    viewManager()->setActiveLayout(data.getUid());
+
+  this->setToActiveLayout();
+}
+
+void LayoutEditorTab::setToActiveLayout()
+{
+  QString uid = viewManager()->getActiveLayout();
+  LayoutData activeLayoutData = viewManager()->getLayoutData(uid);
+  this->populateEditableLayouts(activeLayoutData.getUid());
 }
 
 }//namespace cx
