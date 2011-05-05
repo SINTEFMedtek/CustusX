da73a6dc76709f8a0f5137714e2fbb3d77140121
Christian Askeland <christian.askeland@sintef.no>
2011-05-05 14:31:51 +0200

    split data adapter into separate file.
    Former-commit-id: 7ccf153b6abefa3d1358ab9085f572b4501f7cf3

--- source/gui/recordingWidgets/cxRecordBaseWidget.cpp
+++ source/gui/recordingWidgets/cxRecordBaseWidget.cpp
@@ -22,65 +22,7 @@
 namespace cx
 {
 
-DoubleDataAdapterTimeCalibration::DoubleDataAdapterTimeCalibration()
-{
-  connect(ssc::toolManager(), SIGNAL(dominantToolChanged(const QString&)), this, SLOT(dominantToolChanged()));
-  this->dominantToolChanged();
-}
 
-void DoubleDataAdapterTimeCalibration::dominantToolChanged()
-{
-//  std::cout << "DoubleDataAdapterTimeCalibration::dominantToolChanged()" << std::endl;
-
-  // ignore tool changes to something non-probeish.
-  // This gives the user a chance to use the widget without having to show the probe.
-  ssc::ToolPtr newTool = ssc::toolManager()->getDominantTool();
-  if (!newTool || newTool->getProbeSector().mType==ssc::ProbeData::tNONE)
-    return;
-
-  if (mTool)
-    disconnect(mTool->getProbe().get(), SIGNAL(sectorChanged()), this, SIGNAL(changed()));
-
-  mTool = ssc::toolManager()->getDominantTool();
-
-  if (mTool)
-    connect(mTool->getProbe().get(), SIGNAL(sectorChanged()), this, SIGNAL(changed()));
-
-//  std::cout << "DoubleDataAdapterTimeCalibration::dominantToolChanged() .. " << mTool.get() << std::endl;
-  emit changed();
-}
-
-ssc::DoubleDataAdapterPtr DoubleDataAdapterTimeCalibration::New()
-{
-  return ssc::DoubleDataAdapterPtr(new DoubleDataAdapterTimeCalibration());
-}
-
-double DoubleDataAdapterTimeCalibration::getValue() const
-{
-//  std::cout << "DoubleDataAdapterTimeCalibration::getValue()" << std::endl;
-  if (!mTool)
-    return 0;
-//  std::cout << "mTool->getProbe()->getData().mTemporalCalibration " << mTool->getProbe()->getData().mTemporalCalibration << std::endl;
-  return mTool->getProbe()->getData().mTemporalCalibration;
-}
-
-QString DoubleDataAdapterTimeCalibration::getHelp() const
-{
-  return "Set a temporal shift to add to input probe frames. Will NOT be saved.";
-}
-
-bool DoubleDataAdapterTimeCalibration::setValue(double val)
-{
-  if (!mTool)
-    return 0;
-  mTool->getProbe()->setTemporalCalibration(val);
-  return true;
-}
-
-ssc::DoubleRange DoubleDataAdapterTimeCalibration::getValueRange() const
-{
-  return ssc::DoubleRange(-1000,1000,1);
-}
 
 //----------------------------------------------------------------------------------------------------------------------
 RecordBaseWidget::RecordBaseWidget(QWidget* parent, QString description):
