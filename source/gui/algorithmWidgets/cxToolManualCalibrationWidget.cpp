77ffe94ba4f9195e8298204d486db1e2dc25b600
Christian Askeland <christian.askeland@sintef.no>
2011-05-05 14:22:35 +0200

    implemented (not tested) tool manual calibration widget.
    Former-commit-id: c09775e4b8492587c39f2c8571c579306edd9aa3

--- source/gui/algorithmWidgets/cxToolManualCalibrationWidget.cpp
+++ source/gui/algorithmWidgets/cxToolManualCalibrationWidget.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <cxToolManualCalibrationWidget.h>
+#include "cxActiveToolWidget.h"
+#include "sscToolManager.h"
 
 namespace cx
 {
@@ -13,7 +15,32 @@ namespace cx
 ToolManualCalibrationWidget::ToolManualCalibrationWidget(QWidget* parent) :
     BaseWidget(parent, "TemporalCalibrationWidget", "Temporal Calibrate")
 {
-  QVBoxLayout* toplayout = new QVBoxLayout(this);
+  //layout
+  QVBoxLayout* mToptopLayout = new QVBoxLayout(this);
+  //toptopLayout->setMargin(0);
+
+  mToptopLayout->addWidget(new ActiveToolWidget(this));
+
+  mGroup = new QGroupBox(this);
+  mGroup->setTitle("Calibration matrix sMt");
+  mToptopLayout->addWidget(mGroup);
+  QVBoxLayout* groupLayout = new QVBoxLayout;
+  mGroup->setLayout(groupLayout);
+  groupLayout->setMargin(0);
+  mMatrixWidget = new Transform3DWidget(mGroup);
+  groupLayout->addWidget(mMatrixWidget);
+  connect(mMatrixWidget, SIGNAL(changed()), this, SLOT(matrixWidgetChanged()));
+  connect(ssc::toolManager(), SIGNAL(dominantToolChanged(const QString&)), this, SLOT(toolCalibrationChanged()));
+
+  this->toolCalibrationChanged();
+  mMatrixWidget->setEditable(true);
+
+  mToptopLayout->addStretch();
+
+  connect(ssc::toolManager(), SIGNAL(configured()), this, SLOT(toolCalibrationChanged()));
+  connect(ssc::toolManager(), SIGNAL(initialized()), this, SLOT(toolCalibrationChanged()));
+  connect(ssc::toolManager(), SIGNAL(trackingStarted()), this, SLOT(toolCalibrationChanged()));
+  connect(ssc::toolManager(), SIGNAL(trackingStopped()), this, SLOT(toolCalibrationChanged()));
 }
 
 
@@ -24,6 +51,30 @@ QString ToolManualCalibrationWidget::defaultWhatsThis() const
       "<p><i>Manipulate the tool calibration matrix sMt directly, using the matrix manipulation interface.</i></br>"
       "</html>";
 }
+
+
+void ToolManualCalibrationWidget::toolCalibrationChanged()
+{
+  ssc::ToolPtr tool = ssc::toolManager()->getDominantTool();
+  if (!tool)
+    return;
+
+//  mManualGroup->setVisible(tool->getVisible());
+  mMatrixWidget->blockSignals(true);
+  mMatrixWidget->setMatrix(tool->getCalibration_sMt());
+  mMatrixWidget->blockSignals(false);
+}
+
+void ToolManualCalibrationWidget::matrixWidgetChanged()
+{
+  ssc::ToolPtr tool = ssc::toolManager()->getDominantTool();
+    if (!tool)
+      return;
+
+  ssc::Transform3D M = mMatrixWidget->getMatrix();
+  tool->setCalibration_sMt(M);
+}
+
 
 
 }
