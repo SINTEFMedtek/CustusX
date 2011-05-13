50e8b77ed78ee248e1fe5083ada284a61a98d7e3
Christian Askeland <christian.askeland@sintef.no>
2011-05-13 11:35:11 +0200

    replaces correlation algo in temp cal with LS fitting.
    Former-commit-id: febbd2bbd0e64b3206c928a130b1bf26759461f6

--- source/algorithms/testing/cxTestAlgorithms.cpp
+++ source/algorithms/testing/cxTestAlgorithms.cpp
@@ -8,7 +8,7 @@
 void TestAlgorithms::testTemporalCalibration()
 {
   cx::TemporalCalibration calibrator;
-  QString filename = "/home/christiana/Patients/20110511T092103_temporal_calib.cx3/US_Acq/US-Acq_01_20110511T092317/US-Acq_01_20110511T092317.mhd";
+  QString filename = "/home/christiana/Patients/20110511T092103_temporal_calib_mac.cx3/US_Acq/US-Acq_01_20110511T092317/US-Acq_01_20110511T092317.mhd";
 //  QString filename = cx::DataLocations::getTestDataPath() + "/testing/temporal_calib.cx3/US_Acq/temporal_cal_01_20110505T155645/temporal_cal_01_20110505T155645.mhd";
   calibrator.selectData(filename);
   calibrator.setDebugFolder("/home/christiana/christiana/");
