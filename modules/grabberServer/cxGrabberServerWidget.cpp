d7f4c0dccc248d331bed44ea92dde0d22b83322f
Janne Beate Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2011-06-06 14:44:47 +0200

    Refactored grabberserver to compile on both mac32 and windows.
    Former-commit-id: b04b8ed8751d89941844c265ed6d736165e5d330

--- modules/grabberServer/cxGrabberServerWidget.cpp
+++ modules/grabberServer/cxGrabberServerWidget.cpp
@@ -4,7 +4,6 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QVBoxLayout>
-#include <QMacCocoaViewContainer>
 #include "sscTypeConversions.h"
 
 namespace cx
@@ -71,26 +70,5 @@ void GrabberServerWidget::grabberServerReadySlot(bool ready)
     mStartButton->setChecked(false);
   }
 }
-//=============================================================================
-MacGrabberServerWidget::MacGrabberServerWidget(QWidget* parent) :
-    GrabberServerWidget(parent)
-{
-  this->setObjectName("MacGrabberServerWidget");
-  this->setWindowTitle("Mac Grabber Server Widget");
-
-  this->connectGrabberServer();
-  connect(mGrabberServer.get(), SIGNAL(ready(bool)), this, SLOT(grabberServerReadySlot(bool)));
-  this->portChangedSlot(qstring_cast(mGrabberServer->getPort()));
-}
-
-void MacGrabberServerWidget::connectGrabberServer()
-{
-  mGrabberServer = GrabberServerPtr(new MacGrabberServer());
-  this->portChangedSlot(qstring_cast(mGrabberServer->getPort()));
-
-  connect(mGrabberServer.get(), SIGNAL(queueInfo(int, int)), this, SIGNAL(queueInfo(int, int)));
-}
-
-//=============================================================================
 
 }//Namespace cx
