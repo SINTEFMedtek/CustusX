0bfabb920b4b660d3b5150d66be50fcfb16e642b
Christian Askeland <christian.askeland@sintef.no>
2011-06-22 00:03:52 +0200

    added openCV support to OpenIGTLinkServer
    Former-commit-id: d2d9a8f75308615682f790039c0565ab63c27eee

---
+++ modules/OpenIGTLinkServer/cxImageSenderFile.h
@@ -0,0 +1,55 @@
+/*
+ * cxImageSenderFile.h
+ *
+ *  Created on: Jun 21, 2011
+ *      Author: christiana
+ */
+
+#ifndef CXIMAGESENDERFILE_H_
+#define CXIMAGESENDERFILE_H_
+
+#include "boost/shared_ptr.hpp"
+#include <QTcpSocket>
+#include <QThread>
+class QTimer;
+#include "igtlImageMessage.h"
+
+#include "vtkSmartPointer.h"
+typedef vtkSmartPointer<class vtkImageData> vtkImageDataPtr;
+typedef vtkSmartPointer<class vtkImageImport> vtkImageImportPtr;
+
+
+namespace cx
+{
+
+/**An object sending images out on an ip port.
+ * In order to operate within a nongui thread,
+ * it must be created within the run() method
+ * of a qthread.
+ */
+class ImageSender : public QObject
+{
+  Q_OBJECT
+
+public:
+  ImageSender(QTcpSocket* socket, QString imageFileDir, QObject* parent = NULL);
+  virtual ~ImageSender() {}
+protected:
+private:
+  QTcpSocket* mSocket;
+  QTimer* mTimer;
+  int mCounter;
+  vtkImageDataPtr mImageData;
+  QString mImageFileDir;
+  std::vector<unsigned char> mTestData;
+  vtkImageImportPtr mImageImport;
+  void setTestImage();
+private slots:
+  void tick();
+
+};
+
+
+}
+
+#endif /* CXIMAGESENDERFILE_H_ */
