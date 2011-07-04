5ac25a51847b45912e7fe412a2ac312a20a571eb
Christian Askeland <christian.askeland@sintef.no>
2011-07-04 15:20:42 +0200

    test code for new metrics system
    Former-commit-id: b961c92956740a69ba9063b32aa225e207772515

---
+++ source/service/patient/cxDistanceMetric.cpp
@@ -0,0 +1,70 @@
+/*
+ * cxDistanceMetric.cpp
+ *
+ *  Created on: Jul 4, 2011
+ *      Author: christiana
+ */
+
+#include <cxDistanceMetric.h>
+#include "sscBoundingBox3D.h"
+
+namespace cx
+{
+
+DistanceMetric::DistanceMetric(const QString& uid, const QString& name) :
+			ssc::Data(uid, name)
+{
+}
+
+DistanceMetric::~DistanceMetric()
+{
+}
+
+void DistanceMetric::setPoint(int index, PointMetricPtr p)
+{
+	mPoint[index] = p;
+}
+
+PointMetricPtr DistanceMetric::getPoint(int index)
+{
+	return mPoint[index];
+}
+
+void DistanceMetric::addXml(QDomNode& dataNode)
+{
+
+}
+
+void DistanceMetric::parseXml(QDomNode& dataNode)
+{
+
+}
+
+double DistanceMetric::getDistance() const
+{
+	// convert both inputs to r space
+	ssc::Transform3D rM0 = ssc::SpaceHelpers::get_toMfrom(mPoint[0]->getFrame(), ssc::CoordinateSystem(ssc::csREF));
+	ssc::Vector3D p0_r = rM0.coord(mPoint[0]->getCoordinate());
+
+	ssc::Transform3D rM1 = ssc::SpaceHelpers::get_toMfrom(mPoint[1]->getFrame(), ssc::CoordinateSystem(ssc::csREF));
+	ssc::Vector3D p1_r = rM1.coord(mPoint[1]->getCoordinate());
+
+
+	std::cout << "p0: " << p0_r << ", p1: " << p1_r << ", d=" << (p0_r - p1_r).length() << std::endl;
+
+	return (p0_r - p1_r).length();
+}
+
+ssc::DoubleBoundingBox3D DistanceMetric::boundingBox() const
+{
+	// convert both inputs to r space
+	ssc::Transform3D rM0 = ssc::SpaceHelpers::get_toMfrom(mPoint[0]->getFrame(), ssc::CoordinateSystem(ssc::csREF));
+	ssc::Vector3D p0_r = rM0.coord(mPoint[0]->getCoordinate());
+
+	ssc::Transform3D rM1 = ssc::SpaceHelpers::get_toMfrom(mPoint[1]->getFrame(), ssc::CoordinateSystem(ssc::csREF));
+	ssc::Vector3D p1_r = rM1.coord(mPoint[1]->getCoordinate());
+
+	return ssc::DoubleBoundingBox3D(p0_r, p1_r);
+}
+
+}
