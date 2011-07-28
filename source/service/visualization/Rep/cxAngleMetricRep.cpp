54193ccd9a82ebf8f1f132b09c133a6aa72ef13a
Christian Askeland <christian.askeland@sintef.no>
2011-07-28 10:02:04 +0200

    debugged metrics
    Former-commit-id: 54e0b815505d34ce86136e9fbec179144d4107ee

--- source/service/visualization/Rep/cxAngleMetricRep.cpp
+++ source/service/visualization/Rep/cxAngleMetricRep.cpp
@@ -21,20 +21,6 @@
 namespace cx
 {
 
-class AngleMetricCallback : public vtkCommand
-{
-public:
-  AngleMetricCallback() {}
-  static AngleMetricCallback* New() {return new AngleMetricCallback;}
-  void SetRep(AngleMetricRep* rep) {mRep = rep;}
-  virtual void Execute(vtkObject* caller, unsigned long, void*)
-  {
-    if (mRep)
-      mRep->scaleText();
-  }
-  AngleMetricRep* mRep;
-};
-
 AngleMetricRepPtr AngleMetricRep::New(const QString& uid, const QString& name)
 {
   AngleMetricRepPtr retval(new AngleMetricRep(uid,name));
@@ -62,6 +48,7 @@ void AngleMetricRep::setMetric(AngleMetricPtr point)
   mLine0.reset();
   mLine1.reset();
   mArc.reset();
+  mText.reset();
   this->changedSlot();
 }
 
@@ -72,13 +59,7 @@ void AngleMetricRep::addRepActorsToViewRenderer(ssc::View* view)
   mLine0.reset();
   mLine1.reset();
   mArc.reset();
-
-  mMetricCallback = AngleMetricCallbackPtr::New();
-  mMetricCallback->SetRep(this);
-//  mView->getRenderer()->GetActiveCamera()->AddObserver(vtkCommand::ModifiedEvent, DistanceMetricCallback::New());
-  mView->getRenderer()->AddObserver(vtkCommand::ModifiedEvent, mMetricCallback);
-  mView->getRenderer()->AddObserver(vtkCommand::ActiveCameraEvent, mMetricCallback);
-  mView->getRenderer()->AddObserver(vtkCommand::ResetCameraEvent, mMetricCallback);
+  mText.reset();
 
   this->changedSlot();
 }
@@ -90,14 +71,12 @@ void AngleMetricRep::removeRepActorsFromViewRenderer(ssc::View* view)
   mLine0.reset();
   mLine1.reset();
   mArc.reset();
-
-  mMetricCallback->SetRep(NULL);
-  mView->getRenderer()->RemoveObserver(mMetricCallback);
+  mText.reset();
 }
 
 void AngleMetricRep::changedSlot()
 {
-  if (!mMetric->getPoint(0) || !mMetric->getPoint(1))
+  if (!mMetric->isValid())
     return;
 
   if (!mLine0 && !mLine1 && mView && mMetric)
@@ -105,26 +84,11 @@ void AngleMetricRep::changedSlot()
     mLine0.reset(new ssc::GraphicalLine3D(mView->getRenderer()));
     mLine1.reset(new ssc::GraphicalLine3D(mView->getRenderer()));
     mArc.reset(new ssc::GraphicalArc3D(mView->getRenderer()));
+    mText.reset(new ssc::FollowerText3D(mView->getRenderer()));
   }
 
-  if (!mLine0 && !mLine1)
-    return;
-
-  if (!mText)
-  {
-    mText = vtkVectorText::New();
-    vtkPolyDataMapperPtr textMapper = vtkPolyDataMapperPtr::New();
-    textMapper->SetInput(mText->GetOutput());
-    mFollower = vtkFollower::New();
-    mFollower->SetMapper(textMapper);
-    mFollower->SetCamera(mView->getRenderer()->GetActiveCamera());
-//    mFollower->SetVisibility(true);
-    ssc::Vector3D mTextScale(2,2,2);
-    mFollower->SetScale(mTextScale.begin());
-    mFollower->GetProperty()->SetColor(mColor.begin());
-
-    mView->getRenderer()->AddActor(mFollower);
-  }
+  if (!mLine0)
+  	return;
 
   std::vector<ssc::Vector3D> p(4);
   for (unsigned i=0; i<4; ++i)
@@ -148,46 +112,18 @@ void AngleMetricRep::changedSlot()
   double d = (l0.length() + l1.length())/2 * 0.5;
   ssc::Vector3D a_start = a_center + l0.normalized() * d;
   ssc::Vector3D a_end = a_center + l1.normalized() * d;
-
   mArc->setValue(a_start, a_end, a_center);
 
+  ssc::Vector3D a_text = (a_center + a_start + a_end)/3;
+
   QString text = QString("%1*").arg(mMetric->getAngle()/M_PI*180, 0, 'f', 1);
-  mText->SetText(cstring_cast(text));
-  ssc::Vector3D p_mean = (p[0]+p[1]+p[2]+p[3])/4;
-  mFollower->SetPosition(p_mean.begin());
-
-  this->scaleText();
+//  ssc::Vector3D p_mean = (p[0]+p[1]+p[2]+p[3])/4;
+  mText->setColor(mColor);
+  mText->setText(text);
+  mText->setPosition(a_text);
+  mText->setSizeInNormalizedViewport(true, 0.025);
 }
 
-/**Note: Internal method!
- *
- * Scale the text to be a constant fraction of the viewport height
- * Called from a vtk camera observer
- *
- */
-void AngleMetricRep::scaleText()
-{
-  double targetSize = 0.025;// relative to vp height
-
-  //  use the focal point and focal point + vup.
-  //  Transform both to view space and remove z-coord.
-  //  The distance between then in the view plane can
-  //  be used to rescale the text.
-  vtkCameraPtr camera = mView->getRenderer()->GetActiveCamera();
-  ssc::Vector3D p_f(camera->GetFocalPoint());
-  ssc::Vector3D vup(camera->GetViewUp());
-  ssc::Vector3D p_fup = p_f+vup;
-  mView->getRenderer()->WorldToView(p_f[0],p_f[1],p_f[2]);
-  mView->getRenderer()->WorldToView(p_fup[0],p_fup[1],p_fup[2]);
-  p_f[2] = 0;
-  p_fup[2] = 0;
-  double size = (p_f - p_fup).length()/2;
-  double scale = targetSize/size;
-//  std::cout << "s= " << size << "  ,scale= " << scale << std::endl;
-  ssc::Vector3D mTextScale(scale,scale,scale);
-  if (mFollower)
-    mFollower->SetScale(mTextScale.begin());
-}
 
 
 }
