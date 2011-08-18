0b0e5916d9897302ab1e67aa5bcd9909c8247f68
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2011-08-18 13:52:38 +0200

    Initialize values from cxFrame
    Former-commit-id: 6553e367ecadce34bc1f27157118267074ad5383

--- source/ThirdParty/ultrasonix/cxFrame.h
+++ source/ThirdParty/ultrasonix/cxFrame.h
@@ -3,6 +3,7 @@ namespace cx
 class Frame
 {
 public:
+  bool mNewStatus;
   double mTimestamp; ///< Timestamp in seconds since 1/1/1970 (epoch)
   int mWidth; ///< Width in pixels
   int mHeight; ///< Height in pixels
@@ -10,5 +11,12 @@ public:
   unsigned char* mFirstPixel; ///< Pointer to first pixel in frame
   double mSpacing[2];
   double mOrigin[2];
+
+  Frame():
+    mNewStatus(false),
+    mTimestamp(0.0),
+    mWidth(0),
+    mHeight(0),
+    mPixelFormat(0) {};
 };
 }//namespace cx
