a38062409bfa61b40eb8e95ec3c46531dc217bb0
Christian <christian.askeland@sintef.no>
2011-09-12 10:34:58 +0200

    fixed bugs #369 and #371 transfer function trouble
    Former-commit-id: 61318e870617d9783136440914f05dea82edb032

--- source/service/patient/cxPresetTransferFunctions3D.h
+++ source/service/patient/cxPresetTransferFunctions3D.h
@@ -30,22 +30,16 @@ public:
   void load(QString name, ssc::ImagePtr image);
 
   QStringList getPresetList(); ///< returns a list of the preset names
-  //ssc::Image::ShadingStruct getShadingPresets(const QString& presetName);///< Returns a struct with specific shading presets
-  //void save(QString name, ssc::ImagePtr image);
 
 private:
-//  QDomElement& getPresetDomElement(const QString& presetName); ///< returns a specific presets QDomElement representation
   QStringList generatePresetList(); ///< internally generate the preset list
-//  void initializeDomDocument(); ///< internally populate the dom document with the available presets
-//  QDomElement& getDefaultPresetDomElement(); ///< returns a default preset
   ssc::XmlOptionFile getPresetNode(const QString& presetName);
   ssc::XmlOptionFile getCustomFile();
 
-  //QDomDocument* mPresetDomDocument; ///< the xml document with the presets
-  std::map<QString, ssc::Image::ShadingStruct> mShadings;// Map with the shading presets
   ssc::XmlOptionFile mPresetFile;
   QDomElement mLastReturnedPreset;
 };
+
 }//namespace cx
 
 #endif /* PRESETTRANSFERFUNCTIONS3D_H_ */
