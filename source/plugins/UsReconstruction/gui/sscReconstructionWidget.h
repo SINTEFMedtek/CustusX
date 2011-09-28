99241c8bbfe9a173a27456b55113f4b7f20c2147
Christian <christian.askeland@sintef.no>
2011-09-28 09:32:01 +0200

    changed formatting
    Former-commit-id: cbb8fe199da5156de62756ece37d0fd9c46cce18

--- source/plugins/UsReconstruction/gui/sscReconstructionWidget.h
+++ source/plugins/UsReconstruction/gui/sscReconstructionWidget.h
@@ -12,47 +12,50 @@ namespace ssc
 {
 
 /**
-*  sscReconstructionWidget.h
-*
-*  Created by Ole Vegard Solberg on 5/4/10.
-*
-*/
-class ReconstructionWidget : public QWidget
+ *  sscReconstructionWidget.h
+ *
+ *  Created by Ole Vegard Solberg on 5/4/10.
+ *
+ */
+class ReconstructionWidget: public QWidget
 {
-  Q_OBJECT
+Q_OBJECT
 public:
-  ReconstructionWidget(QWidget* parent, ReconstructerPtr reconstructer);
-  ReconstructerPtr reconstructer() {  return mReconstructer; }
+	ReconstructionWidget(QWidget* parent, ReconstructerPtr reconstructer);
+	ReconstructerPtr reconstructer()
+	{
+		return mReconstructer;
+	}
 
 public slots:
-  void selectData(QString inputfile);
-  void reconstruct();
-  void reload();
-  void paramsChangedSlot();
+	void selectData(QString inputfile);
+	void reconstruct();
+	void reload();
+	void paramsChangedSlot();
 private slots:
-  void inputDataSelected(QString mhdFileName);
+	void inputDataSelected(QString mhdFileName);
 
 private:
-  ReconstructerPtr mReconstructer;
-  
-  FileSelectWidget* mFileSelectWidget;
-  QPushButton* mReconstructButton;
-  QPushButton* mReloadButton;
-  QLineEdit* mExtentLineEdit;
-  QLineEdit* mInputSpacingLineEdit;
-  ssc::SpinBoxGroupWidget* mMaxVolSizeWidget;
-  ssc::SpinBoxGroupWidget* mSpacingWidget;
-  ssc::SpinBoxGroupWidget* mDimXWidget;
-  ssc::SpinBoxGroupWidget* mDimYWidget;
-  ssc::SpinBoxGroupWidget* mDimZWidget;
-  
-  QGroupBox* mAlgorithmGroup;
-  QStackedLayout* mAlgoLayout;
-  std::vector<QWidget*> mAlgoWidgets;
-  
-  QString getCurrentPath();
-  void updateComboBox();
-  void repopulateAlgorithmGroup();
+	ReconstructerPtr mReconstructer;
+
+	FileSelectWidget* mFileSelectWidget;
+	QPushButton* mReconstructButton;
+	QPushButton* mReloadButton;
+	QLineEdit* mExtentLineEdit;
+	QLineEdit* mInputSpacingLineEdit;
+	ssc::SpinBoxGroupWidget* mMaxVolSizeWidget;
+	ssc::SpinBoxGroupWidget* mSpacingWidget;
+	ssc::SpinBoxGroupWidget* mDimXWidget;
+	ssc::SpinBoxGroupWidget* mDimYWidget;
+	ssc::SpinBoxGroupWidget* mDimZWidget;
+
+	QGroupBox* mAlgorithmGroup;
+	QStackedLayout* mAlgoLayout;
+	std::vector<QWidget*> mAlgoWidgets;
+
+	QString getCurrentPath();
+	void updateComboBox();
+	void repopulateAlgorithmGroup();
 };
 
 }//namespace
