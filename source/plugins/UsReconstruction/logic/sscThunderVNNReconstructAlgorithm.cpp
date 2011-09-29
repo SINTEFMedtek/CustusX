3ade5be77c7b7e431b40d031d8d2d052b250129f
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2011-09-29 11:13:49 +0200

    Added ThunderGPUReconstruction test
    Former-commit-id: 99fac527461a6f4572bc50bcf1a27064b8a8c78e

--- source/plugins/UsReconstruction/logic/sscThunderVNNReconstructAlgorithm.cpp
+++ source/plugins/UsReconstruction/logic/sscThunderVNNReconstructAlgorithm.cpp
@@ -38,7 +38,7 @@ std::vector<DataAdapterPtr> ThunderVNNReconstructAlgorithm::getSettings(QDomElem
 		      "Which processor to use when reconstructing",
 		      "CPU", QString("CPU GPU").split(" "),
 		      root);
-	mDistanceOption = DoubleDataAdapterXml::initialize("Distance", "",
+	mDistanceOption = DoubleDataAdapterXml::initialize("Distance (mm)", "",
 		      "Max distance from frame to voxel when filling output volume. mm.",
 		      1, ssc::DoubleRange(0.1, 10, 0.01), 0,
 		      root);
