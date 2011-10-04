fc841b690ed3645298cac464876b589ae1b9529a
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2011-10-04 11:02:09 +0200

    Reconstruction tests now tests volume values, both for angio reconstruction and GPU reconstruction.
    Former-commit-id: 42745719366affc1ebf566a5b9a4bd5c53183855

--- source/plugins/UsReconstruction/testing/cxTest_UsReconstruction.cpp
+++ source/plugins/UsReconstruction/testing/cxTest_UsReconstruction.cpp
@@ -1,8 +1,10 @@
 #include "cxTest_UsReconstruction.h"
 
+#include <vtkImageData.h>
 #include "sscReconstructer.h"
 #include "sscImage.h"
 #include "sscThunderVNNReconstructAlgorithm.h"
+#include "sscPNNReconstructAlgorithm.h"
 #include "cxDataLocations.h"
 
 //#include "cxToolConfigurationParser.h"
@@ -26,16 +28,38 @@ void TestUsReconstruction::testAngioReconstruction()
 {
 	std::cout << "testAngioReconstruction running" << std::endl;
   ssc::ReconstructerPtr reconstructer(new ssc::Reconstructer(ssc::XmlOptionFile(),""));
-  //QString filename = cx::DataLocations::getTestDataPath() + "/testing/USAngioTest.cx3/US_Acq/US-Acq_02_20110222T120553/US-Acq_02_20110222T120553.mhd";
-  //QString filename = "/Users/olevs/Patients/USAngioTest.cx3/US_Acq/US-Acq_02_20110222T120553/US-Acq_02_20110222T120553.mhd";
   QString filename = cx::DataLocations::getTestDataPath() + "/testing/USAngioTest.cx3/US_Acq/US-Acq_01_20110520T121038/US-Acq_01_20110520T121038.mhd";
+  //reconstructer->mAlgorithmAdapter->setValue("PNN");//default
   reconstructer->mAngioAdapter->setValue(true);
+
+  CPPUNIT_ASSERT(boost::shared_dynamic_cast<ssc::PNNReconstructAlgorithm>(reconstructer->mAlgorithm));// Check if we got the PNN algorithm
+  boost::shared_dynamic_cast<ssc::PNNReconstructAlgorithm>(reconstructer->mAlgorithm)->mInterpolationStepsOption->setValue(1);
+
   reconstructer->selectData(filename);
   reconstructer->reconstruct();
 
   ssc::ImagePtr output = reconstructer->getOutput();
 
   CPPUNIT_ASSERT( output->getRange() != 0);//Just check if the output volume is empty
+
+  vtkImageDataPtr volume = output->getGrayScaleBaseVtkImageData();
+  unsigned char* volumePtr = reinterpret_cast<unsigned char*>(volume->GetScalarPointer());
+  CPPUNIT_ASSERT(volumePtr); //Check if the pointer != NULL
+
+  int* dimensions = volume->GetDimensions();
+  // inside angio area
+  int x = 133;
+  int y = 130;
+  int z = 81;
+  int position = x + y * dimensions[0] + z * dimensions[0]*dimensions[1];
+  CPPUNIT_ASSERT( volumePtr[position] !=0 );//Check if the voxel value is != zero inside the angio area
+
+  // outside angio
+  x = 60;
+  y = 60;
+  z = 146;
+  position = x + y * dimensions[0] + z * dimensions[0]*dimensions[1];
+  CPPUNIT_ASSERT( volumePtr[position] ==0 );//Check if the voxel value is zero outside the angio area
 }
 
 void TestUsReconstruction::testThunderGPUReconstruction()
@@ -44,12 +68,33 @@ void TestUsReconstruction::testThunderGPUReconstruction()
   QString filename = cx::DataLocations::getTestDataPath() + "/testing/USAngioTest.cx3/US_Acq/US-Acq_01_20110520T121038/US-Acq_01_20110520T121038.mhd";
   reconstructer->mAlgorithmAdapter->setValue("ThunderVNN");
   CPPUNIT_ASSERT(boost::shared_dynamic_cast<ssc::ThunderVNNReconstructAlgorithm>(reconstructer->mAlgorithm));
-  boost::shared_dynamic_cast<ssc::ThunderVNNReconstructAlgorithm>(reconstructer->mAlgorithm)->mProcessorOption->setValue("GPU");
+  boost::shared_dynamic_cast<ssc::ThunderVNNReconstructAlgorithm>(reconstructer->mAlgorithm)->mProcessorOption->setValue("GPU"); // Fails for AMD (most macs)
+//  boost::shared_dynamic_cast<ssc::ThunderVNNReconstructAlgorithm>(reconstructer->mAlgorithm)->mProcessorOption->setValue("CPU");
   reconstructer->selectData(filename);
   reconstructer->reconstruct();
 
   ssc::ImagePtr output = reconstructer->getOutput();
 
   CPPUNIT_ASSERT( output->getRange() != 0);//Just check if the output volume is empty
+
+  // Check two voxel values
+  vtkImageDataPtr volume = output->getGrayScaleBaseVtkImageData();
+  unsigned char* volumePtr = reinterpret_cast<unsigned char*>(volume->GetScalarPointer());
+  CPPUNIT_ASSERT(volumePtr); //Check if the pointer != NULL
+
+  int* dimensions = volume->GetDimensions();
+  // outside angio
+  int x = 60;
+  int y = 60;
+  int z = 146;
+  int position = x + y * dimensions[0] + z * dimensions[0]*dimensions[1];
+  CPPUNIT_ASSERT( volumePtr[position] !=0 );//Check if the voxel value is != zero outside the angio area
+
+  // inside angio
+  x = 133;
+  y = 130;
+  z = 81;
+  position = x + y * dimensions[0] + z * dimensions[0]*dimensions[1];
+  CPPUNIT_ASSERT( volumePtr[position] !=0 );//Check if the voxel value is != zero inside the angio area
 }
 
