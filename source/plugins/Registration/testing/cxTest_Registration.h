ef1953042c5020614f0946f6c72d2cd3ef3d0669
Christian <christian.askeland@sintef.no>
2011-10-25 13:48:31 +0200

    added centerline test
    Former-commit-id: 2d51d80179c86828886902e8b0b462ffadd7437a

---
+++ source/plugins/Registration/testing/cxTest_Registration.h
@@ -0,0 +1,38 @@
+/*
+ * cxTestRegistration.h
+ *
+ *  Created on: Oct 24, 2011
+ *      Author: christiana
+ */
+
+#ifndef CXTEST_REGISTRATION_H_
+#define CXTEST_REGISTRATION_H_
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Message.h>
+
+#include "sscTransform3D.h"
+
+/**
+ */
+class TestRegistration : public CppUnit::TestFixture
+{
+public:
+	void setUp();
+	void tearDown();
+
+	void testVessel2VesselRegistration();
+
+public:
+	CPPUNIT_TEST_SUITE( TestRegistration );
+		CPPUNIT_TEST( testVessel2VesselRegistration );
+	CPPUNIT_TEST_SUITE_END();
+private:
+	void doTestVessel2VesselRegistration(ssc::Transform3D perturbation, QString filenameSource, QString filenameTarget, double tol_dist, double tol_angle);
+};
+CPPUNIT_TEST_SUITE_REGISTRATION( TestRegistration );
+
+
+#endif /* CXTEST_REGISTRATION_H_ */
