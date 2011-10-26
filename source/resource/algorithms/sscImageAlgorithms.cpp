738e971e1eeb1ce47c7890b4c58fa9aed6129f9c
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2011-10-26 09:45:49 +0200

    Fixed bug #411: Keep modality when creating a derived image
    Former-commit-id: 30aa01597d9649a568498464cf8af628017389fd

--- source/resource/algorithms/sscImageAlgorithms.cpp
+++ source/resource/algorithms/sscImageAlgorithms.cpp
@@ -44,14 +44,12 @@ ssc::ImagePtr resampleImage(ssc::ImagePtr image, ssc::Transform3D qMd)
 
   QString uid = image->getUid() + "_or%1";
   QString name = image->getName()+" or%1";
-  ssc::ImagePtr oriented = ssc::dataManager()->createImage(rawResult, uid, name);
+  ssc::ImagePtr oriented = ssc::dataManager()->createDerivedImage(rawResult, uid, name, image);
   //oriented->get_rMd_History()->setRegistration(reference->get_rMd());
   oriented->get_rMd_History()->setRegistration(image->get_rMd() * qMd.inv());
 //  std::cout << "rMd pre merge oriented\n" << oriented->get_rMd() << std::endl;
   oriented->mergevtkSettingsIntosscTransform();
 //  std::cout << "rMd finished oriented\n" << oriented->get_rMd() << std::endl;
-  oriented->resetTransferFunction(image->getTransferFunctions3D()->createCopy(), image->getLookupTable2D()->createCopy());
-  oriented->get_rMd_History()->setParentSpace(image->getUid());
 
   return oriented;
 }
@@ -77,10 +75,7 @@ ImagePtr resampleImage(ssc::ImagePtr image, const Vector3D spacing, QString uid,
     uid = image->getUid() + "_res%1";
     name = image->getName()+" res%1";
   }
-  ssc::ImagePtr retval = ssc::dataManager()->createImage(rawResult, uid, name);
-  retval->get_rMd_History()->setRegistration(image->get_rMd());
-  retval->resetTransferFunction(image->getTransferFunctions3D()->createCopy(), image->getLookupTable2D()->createCopy());
-  retval->get_rMd_History()->setParentSpace(image->getUid());
+  ssc::ImagePtr retval = ssc::dataManager()->createDerivedImage(rawResult, uid, name, image);
 
   //ssc::dataManager()->loadData(retval);
   //ssc::dataManager()->saveImage(retval, outputBasePath);
@@ -121,11 +116,7 @@ ImagePtr cropImage(ImagePtr image)
 
   QString uid = image->getUid() + "_crop%1";
   QString name = image->getName()+" crop%1";
-  ImagePtr result = dataManager()->createImage(rawResult,uid, name);
-  result->get_rMd_History()->setRegistration(image->get_rMd());
-  result->mergevtkSettingsIntosscTransform();
-  result->resetTransferFunction(image->getTransferFunctions3D()->createCopy(), image->getLookupTable2D()->createCopy());
-  result->get_rMd_History()->setParentSpace(image->getUid());
+  ImagePtr result = dataManager()->createDerivedImage(rawResult,uid, name, image);
   //messageManager()->sendInfo("Created volume " + result->getName());
 
 //  dataManager()->loadData(result);
