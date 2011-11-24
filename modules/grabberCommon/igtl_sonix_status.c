ac95bea7521206f89d4b962feae4e3266a1512a4
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2011-11-24 09:21:41 +0100

    Fixed bugs with Sonix
    Former-commit-id: 59f6f37842b59522c8b58703c2a8c1bbad280ee1

--- modules/grabberCommon/igtl_sonix_status.c
+++ modules/grabberCommon/igtl_sonix_status.c
@@ -8,18 +8,19 @@
 #include "igtl_sonix_status.h"
 #include "igtl_util.h"
 
-
 void igtl_export igtl_sonix_status_convert_byte_order(igtl_sonix_status_message * message)
 {
-
 	igtl_uint64* tmp = (igtl_uint64*)message;
+	igtl_uint32* tmp32 = (igtl_uint32*)message;
 
-  if (igtl_is_little_endian())
-  {
-  	int i;
-  	for (i = 0; i < 3; i++)//Update number when adding more variables to message
-  		tmp[i] = BYTE_SWAP_INT64(tmp[i]);
-  }
+	if (igtl_is_little_endian())
+	{
+		int i;
+		for (i = 0; i < 3; i++)//Update number when adding more variables to message
+			tmp[i] = BYTE_SWAP_INT64(tmp[i]);
+	}
+	
+	tmp32[12] = BYTE_SWAP_INT32(tmp32[12]);
 }
 
 igtl_uint64 igtl_export igtl_sonix_status_get_crc(igtl_sonix_status_message * message)
