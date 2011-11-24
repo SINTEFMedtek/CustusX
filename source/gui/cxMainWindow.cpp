1a68ef43d9bfe06fe2b434f9caa2c9648c5f55a3
Christian Askeland <christian.askeland@sintef.no>
2011-11-24 10:12:13 +0100

    DICOM widget
    Former-commit-id: 1836fabc5c1d4cb8ae06c2e894f432b996b4689c

--- source/gui/cxMainWindow.cpp
+++ source/gui/cxMainWindow.cpp
@@ -45,6 +45,7 @@
 #include "cxPatientService.h"
 #include "cxMetricWidget.h"
 #include "cxViewWrapper.h"
+#include "sscDICOMWidget.h"
 
 namespace cx
 {
@@ -91,6 +92,7 @@ MainWindow::MainWindow(std::vector<PluginBasePtr> plugins) :
 	this->addAsDockWidget(new ImagePropertiesWidget(this), "Properties");
 	this->addAsDockWidget(new VolumePropertiesWidget(this), "Properties");
 	this->addAsDockWidget(new MeshPropertiesWidget(this), "Properties");
+	this->addAsDockWidget(new ssc::DICOMWidget(this), "Utility");
 	this->addAsDockWidget(new TrackPadWidget(this), "Utility");
 	this->addAsDockWidget(new ToolPropertiesWidget(this), "Properties");
 	this->addAsDockWidget(new NavigationWidget(this), "Properties");
