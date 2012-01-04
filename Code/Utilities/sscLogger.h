73b05f9be036814229d5a11b5313b5f12f4c35a0
Christian <christian.askeland@sintef.no>
2012-01-04 15:31:54 +0100

    started adding license and doxy doc to ssc
    Former-commit-id: b96b9aeaf8ed726477bef8b17a094c37ce84571a

--- Code/Utilities/sscLogger.h
+++ Code/Utilities/sscLogger.h
@@ -1,3 +1,22 @@
+// This file is part of SSC,
+// a C++ Library supporting Image Guided Therapy Applications.
+//
+// Copyright (C) 2008- SINTEF Medical Technology
+// Copyright (C) 2008- Sonowand AS
+//
+// SSC is owned by SINTEF Medical Technology and Sonowand AS,
+// hereafter named the owners. Each particular piece of code
+// is owned by the part that added it to the library.
+// SSC source code and binaries can only be used by the owners
+// and those with explicit permission from the owners.
+// SSC shall not be distributed to anyone else.
+//
+// SSC is distributed WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// See sscLicense.txt for more information.
+
 #ifndef _SSCLOGGER_H_
 #define _SSCLOGGER_H_
 
@@ -18,15 +37,11 @@ extern "C"
 #include "misc.h"
 
 /**
- * @mainpage
- * This is the low-level code documentation for SonoWand 2.0. The  <a href="modules.html">module list</a> is a recommended starting point.
+ * \file
  *
- * The file sscLogger contains the top-level, most basic project functions that are used by all the code.
+ * The file sscLogger contains c-style logging functions and macros.
  *
- * @defgroup SonoWand SonoWand Utility Functions
- *
- * This module contains the most basic shared code for all modules. It implements error logging, session handling, and path
- * resolving.
+ * \addtogroup sscUtility
  *
  * @{
  */
@@ -36,19 +51,22 @@ int SSC_Logging_Init_Default(const char *busName);	///< Initialize SonoWand logg
 int SSC_Logging_Init(const char *busName, const char* applicationPath);	///< Initialize SonoWand logging system and databus connection.
 void SSC_Logging_Done( void );			///< Properly close down connection to databus.
 void SSC_Logging( bool on );		///< Turn logging on or off.
-void SSC_Log(     const char *file, int line, const char *function, const char *format, ... ) sw__attribute((format (printf, 4, 5)));
-void SSC_Error(   const char *file, int line, const char *function, const char *format, ...)  sw__attribute((format (printf, 4, 5)));
-void SSC_Warning( const char *file, int line, const char *function, const char *format, ...)  sw__attribute((format (printf, 4, 5)));
 
 /** General low-level log function. */
-#define SSC_LOG( ... ) SSC_Log(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ )
-#define SSC_WARNING( ... ) SSC_Warning(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ );
-#define SSC_ERROR( ... ) SSC_Error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ );
+#define SSC_LOG( ... ) SSC_Log(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ ) ///< log using printf-style. Example: SSC_LOG(s%, "Hello world!");
+#define SSC_WARNING( ... ) SSC_Warning(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ ); ///< log using printf-style. Example: SSC_WARNING(s%, "Hello world!");
+#define SSC_ERROR( ... ) SSC_Error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__ ); ///< log using printf-style. Example: SSC_ERROR(s%, "Hello world!");
+
+/**
+ *  @}
+ */
+
+void SSC_Log(     const char *file, int line, const char *function, const char *format, ... ) sw__attribute((format (printf, 4, 5))); ///< internal function. use SSC_LOG
+void SSC_Error(   const char *file, int line, const char *function, const char *format, ...)  sw__attribute((format (printf, 4, 5))); ///< internal function. use SSC_ERROR
+void SSC_Warning( const char *file, int line, const char *function, const char *format, ...)  sw__attribute((format (printf, 4, 5))); ///< internal function. use SSC_WARNING
 
 #ifdef __cplusplus
 }
 #endif
-
-/* @} */
 
 #endif
--- Code/ssc.h
+++ Code/ssc.h
@@ -4,17 +4,68 @@
 namespace ssc
 {
 
+/**
+ * \mainpage
+ * \brief An IGT library used by Sonowand and Sintef Medical Technology.
+ *
+ *
+ * The <a href="modules.html">module list</a> is a recommended starting point.
+ *
+ * ssc is short for Sonowand / Sintef Medical Technology Cooperation.
+ *
+ * The library contains a common set of interfaces that enables the
+ * two companies to share software components.
+ */
+
+
 /**\namespace ssc
- * ssc, or Sonowand Sintef(Health Reseach) Cooperation,
- * contains a common set of interfaces that enables the
+ * \brief An IGT library used by Sonowand and Sintef Medical Technology.
+ *
+ * ssc is short for Sonowand / Sintef Medical Technology Cooperation.
+ *
+ * The library contains a common set of interfaces that enables the
  * two companies to share software components.
  *
  * The library is based on vtk and Qt, and uses code standards
  * defined by Sonowand.
  *
- *
- * Definition of all coordinate systems:
+ * Definition of all coordinate systems: TBD
  */
+
+
+/**
+ * \defgroup sscUtility SSC Utility
+ * \brief Defines and miscellaneous utilities for the other parts of SSC.
+ *
+ */
+
+
+/**
+ * \defgroup sscData SSC Data
+ * \brief A model of the patient.
+ *
+ * Data sets of various modalities, fiducials and labels are described in relation to each other.
+ *
+ *
+ */
+
+
+/**
+ * \defgroup sscRep SSC Rep
+ * \brief Representations that visualize Data entities in a View.
+ *
+ * Visualization of underlying structures assembled in Representations that are
+ * easily configurable and insertible into views. View widgets that can be added
+ * directly to a Qt layout.
+ *
+ * The representations are the core part of ssc. Each Rep contains one kind of
+ * visualization. The point is to hide the vtk/OpenGL complexity and create a
+ * toolkit where Data, Tools, Reps and Views can be assembled.
+ *
+ */
+
+
+
 
 } // namespace ssc
 
