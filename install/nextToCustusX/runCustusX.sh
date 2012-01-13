0fa6baba8fe39adb8874b9b553a6badf42053633
Christian <christian.askeland@sintef.no>
2012-01-13 13:40:41 +0100

    debugged linux installer for cmake 2.8.4 (space in filenames bugs)
    Former-commit-id: 0c13da3598e6c99164e3205b122f4152d62090ba

--- install/nextToCustusX/runCustusX.sh
+++ install/nextToCustusX/runCustusX.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #####################################################
-# Runc script for CustusX
+# Run script for CustusX
 # Author: Christian Askeland, SINTEF Medical Technology
 # Date:   2012.01.12
 #
@@ -17,5 +17,11 @@
 #
 #####################################################
 
-export LD_LIBRARY_PATH=bin:.:$LD_LIBRARY_PATH
-./bin/CustusX
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "$0" )" && pwd )" 
+
+# enable linking to bin folder where all the dynamic libs are
+export LD_LIBRARY_PATH=$DIR/bin:$DIR:$LD_LIBRARY_PATH
+
+# run CustusX
+$DIR/bin/CustusX
