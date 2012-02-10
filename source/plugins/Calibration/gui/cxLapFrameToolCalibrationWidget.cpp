10b61432cebbfd45cd6f2bac66275871fb32c1f5
Developer User <christian.askeland@sintef.no>
2012-02-10 12:55:33 +0100

    tested ok the lap tool calibration
    Former-commit-id: 0252baa5e4063c9e1087ede7c9d1b23bd1940f56

--- source/plugins/Calibration/gui/cxLapFrameToolCalibrationWidget.cpp
+++ source/plugins/Calibration/gui/cxLapFrameToolCalibrationWidget.cpp
@@ -31,6 +31,7 @@
 #include "sscToolManager.h"
 #include "cxPatientData.h"
 #include <cxActiveToolWidget.h>
+#include "sscDoubleWidgets.h"
 
 namespace cx
 {
@@ -46,16 +47,27 @@ LapFrameToolCalibrationWidget::LapFrameToolCalibrationWidget(QWidget* parent) :
 {
   QVBoxLayout* toplayout = new QVBoxLayout(this);
 
-  mTools = SelectToolStringDataAdapter::New();
-  mTools->setValueName("Calibration Frame");
-  mTools->setHelp("Select Calibration Reference Frame");
-  mCalibrateToolComboBox = new ssc::LabeledComboBoxWidget(this, mTools);
+  mCameraAngleAdapter = ssc::DoubleDataAdapterXml::initialize("Camera Angle", "",
+      "Additional tilt of calibration around tool y-axis,\nfor use with cameras tilted relative to tool direction",
+      0.0, ssc::DoubleRange(-M_PI_2, M_PI_2, M_PI/180), 0);
+  mCameraAngleAdapter->setInternal2Display(180.0/M_PI);
+
+  mCalibRefTool = SelectToolStringDataAdapter::New();
+  mCalibRefTool->setValueName("Calibration Frame");
+  mCalibRefTool->setHelp("Select Calibration Reference Frame");
+
+  mCalibratingTool = SelectToolStringDataAdapter::New();
+  mCalibratingTool->setValueName("Tool");
+  mCalibratingTool->setHelp("Select which Tool to calibrate");
+
   this->setToolTip(this->defaultWhatsThis());
 
 //  toplayout->addWidget(new QLabel("<b>Select a tool with a known reference point:</b>"));
-  toplayout->addWidget(mCalibrateToolComboBox);
+  toplayout->addWidget(new ssc::LabeledComboBoxWidget(this, mCalibRefTool));
   toplayout->addWidget(mReferencePointLabel);
-  toplayout->addWidget(new ActiveToolWidget(this));
+  toplayout->addWidget(new ssc::LabeledComboBoxWidget(this, mCalibratingTool));
+//  toplayout->addWidget(new ActiveToolWidget(this));
+  toplayout->addWidget(new ssc::SpinBoxAndSliderGroupWidget(this, mCameraAngleAdapter));
   toplayout->addWidget(mCalibrateButton);
   toplayout->addWidget(mCalibrationLabel);
   toplayout->addWidget(this->createHorizontalLine());
@@ -69,7 +81,7 @@ LapFrameToolCalibrationWidget::LapFrameToolCalibrationWidget(QWidget* parent) :
   connect(mCalibrateButton, SIGNAL(clicked()), this, SLOT(calibrateSlot()));
   connect(mTestButton, SIGNAL(clicked()), this, SLOT(testCalibrationSlot()));
 
-  connect(mTools.get(), SIGNAL(changed()), this, SLOT(toolSelectedSlot()));
+  connect(mCalibRefTool.get(), SIGNAL(changed()), this, SLOT(toolSelectedSlot()));
 
   //setting default state
   this->toolSelectedSlot();
@@ -85,27 +97,28 @@ QString LapFrameToolCalibrationWidget::defaultWhatsThis() const
   return "<html>"
       "<h3>Laparascopic tool calibration</h3>"
       "<p><i>Calibrates a tool by sampling it when it is inserted into the slot of the custom-made calibration frame.</i></br>"
-      "<p><i>The calibration frame position is set to a fixed value by CustusX, thus you are responsible for using "
-      "the correct frame.</i></br>"
+      "<p><i>The tool to be calibrated will have its calibration set in such a way that the calibrating and calibrated tools"
+      "are in the same position.</i></br>"
       "<p>By using the test button you can test your calibration by pointing at a known reference point.</br></p>"
       "</html>";
 }
 
 void LapFrameToolCalibrationWidget::calibrateSlot()
 {
-  ssc::ToolPtr refTool = mTools->getTool();
+  ssc::ToolPtr refTool = mCalibRefTool->getTool();
+  ssc::ToolPtr tool = mCalibratingTool->getTool();
+  double cameraAngle = mCameraAngleAdapter->getValue();
   //Todo, we only allow the reference point with id 1 to be used to calibrate
   //this could be done more dynamic.
-  if(!refTool || !refTool->hasReferencePointWithId(1))
+  if(!refTool || !tool || !refTool->hasReferencePointWithId(1))
     return;
 
-  ssc::ToolPtr tool = ssc::toolManager()->getDominantTool();
 
-  LapFrameToolCalibrationCalculator calc(tool, refTool);
+  LapFrameToolCalibrationCalculator calc(tool, refTool, cameraAngle);
   ssc::Transform3D calibration = calc.get_calibration_sMt();
 
   QMessageBox msgBox;
-  msgBox.setText("Do you want to overwrite "+tool->getName()+"s calibration file?");
+  msgBox.setText("Do you want to overwrite "+tool->getName()+"'s calibration file?");
   msgBox.setInformativeText("This cannot be undone.");
   msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
   msgBox.setDefaultButton(QMessageBox::Ok);
@@ -114,23 +127,28 @@ void LapFrameToolCalibrationWidget::calibrateSlot()
   if(ret == QMessageBox::Ok)
   {
     tool->setCalibration_sMt(calibration);
-    mCalibrationLabel->setText("Calibration:\n"+qstring_cast(calibration));
+//    std::stringstream ss;
+//    ss << calibration.matrix().format(Eigen::IOFormat()) << std::endl;
+    mCalibrationLabel->setText(QString("Calibration matrix for %1:\n%2").arg(tool->getName(), qstring_cast(calibration)));
   }
 }
 
 void LapFrameToolCalibrationWidget::testCalibrationSlot()
 {
-  ssc::ToolPtr selectedTool = mTools->getTool();
-  if(!selectedTool || !selectedTool->hasReferencePointWithId(1))
+  ssc::ToolPtr refTool = mCalibRefTool->getTool();
+  ssc::ToolPtr tool = mCalibratingTool->getTool();
+  double cameraAngle = mCameraAngleAdapter->getValue();
+
+  if(!refTool || !tool || !refTool->hasReferencePointWithId(1))
     return;
 
-  LapFrameToolCalibrationCalculator calc(ssc::toolManager()->getDominantTool(), selectedTool);
+  LapFrameToolCalibrationCalculator calc(tool, refTool, cameraAngle);
   ssc::Vector3D delta_selectedTool = calc.get_delta_ref();
 
   QString delta = QString("%1 mm").arg(delta_selectedTool.length(), 6, 'g', 1);
-  mDeltaLabel->setText("<b>Delta:</b> "+qstring_cast(delta_selectedTool)+" <br> <b>Accuracy:</b>  " + delta);
+  mDeltaLabel->setText("<b>Delta "+tool->getName()+":</b> "+qstring_cast(delta_selectedTool)+" <br> <b>Accuracy:</b>  " + delta);
 
-  ssc::messageManager()->sendInfo("Delta: "+qstring_cast(delta_selectedTool)+" Length:   "+ delta);
+  ssc::messageManager()->sendInfo("Delta "+tool->getName()+": "+qstring_cast(delta_selectedTool)+" Length:   "+ delta);
 
 
 }
@@ -141,9 +159,11 @@ void LapFrameToolCalibrationWidget::toolSelectedSlot()
 	mCalibrateButton->setEnabled(false);
 	mTestButton->setEnabled(false);
 
-	if (mTools->getTool())
+	if (mCalibRefTool->getTool())
 	{
-		mCalibrationLabel->setText("Calibration:\n" + qstring_cast(mTools->getTool()->getCalibration_sMt()));
+//		mCalibrationLabel->setText("Calibration:\n" + qstring_cast(mCalibratingTool->getTool()->getCalibration_sMt()));
+//    ssc::Transform3D calibration = mCalibratingTool->getTool()->getCalibration_sMt();
+//    mCalibrationLabel->setText(QString("Calibration matrix for %1:\n%2").arg(tool->getName(), qstring_cast(calibration)));
 		mCalibrateButton->setEnabled(true);
 		mTestButton->setEnabled(true);
 	}
@@ -154,9 +174,9 @@ void LapFrameToolCalibrationWidget::toolSelectedSlot()
 
 void LapFrameToolCalibrationWidget::trackingStartedSlot()
 {
-	ssc::ToolPtr ref = ssc::toolManager()->getReferenceTool();
+	ssc::ToolPtr ref = ssc::toolManager()->getTool("calibration_tool");
 	if (ref)
-		mTools->setValueName(ref->getUid());
+		mCalibRefTool->setValue(ref->getUid());
 }
 
 //------------------------------------------------------------------------------
@@ -166,16 +186,17 @@ void LapFrameToolCalibrationWidget::trackingStartedSlot()
 
 
 
-LapFrameToolCalibrationCalculator::LapFrameToolCalibrationCalculator(ssc::ToolPtr tool, ssc::ToolPtr calRef) :
-    mTool(tool), mCalibrationRef(calRef)
+LapFrameToolCalibrationCalculator::LapFrameToolCalibrationCalculator(ssc::ToolPtr tool, ssc::ToolPtr calRef, double cameraAngle) :
+    mTool(tool), mCalibrationRef(calRef), mCameraAngle(cameraAngle)
 {
 	m_sMpr = mTool->getCalibration_sMt() * mTool->get_prMt().inv();
 
-	m_qMcr = ssc::Transform3D::fromString(" 0.0,  0.0, -1.0, -71.5,"
-		                                  " 0.0, -1.0,  0.0,  -8.0,"
-	                                      "-1.0,  0.0,  0.0,  -8.8,"
-	                                      " 0.0,  0.0,  0.0,   1.0");
+//	m_qMcr = ssc::Transform3D::fromString(" 0.0,  0.0, -1.0, -71.5,"
+//		                                  " 0.0, -1.0,  0.0,  -8.0,"
+//	                                      "-1.0,  0.0,  0.0,  -8.8,"
+//	                                      " 0.0,  0.0,  0.0,   1.0");
 
+	m_qMcr = ssc::Transform3D::Identity();
 	m_qMpr = m_qMcr * mCalibrationRef->get_prMt().inv();
 }
 
@@ -191,7 +212,7 @@ ssc::Vector3D LapFrameToolCalibrationCalculator::get_delta_ref()
 
 ssc::Transform3D LapFrameToolCalibrationCalculator::get_calibration_sMt()
 {
-	return m_sMpr * m_qMpr.inv();
+	return m_sMpr * m_qMpr.inv() * ssc::createTransformRotateY(mCameraAngle);
 }
 
 
