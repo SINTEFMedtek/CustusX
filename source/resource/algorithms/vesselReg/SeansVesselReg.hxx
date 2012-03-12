402862b4c9161fccd6c524b53cbbf2ad0a42a926
Christian <christian.askeland@sintef.no>
2012-03-12 15:37:59 +0100

    added debugging interface to v2v registration
    Former-commit-id: b76879a5da0116eed5bc47ea751773ea78731308

--- source/resource/algorithms/vesselReg/SeansVesselReg.hxx
+++ source/resource/algorithms/vesselReg/SeansVesselReg.hxx
@@ -21,6 +21,25 @@ namespace cx
 class SeansVesselReg
 {
 public:
+	/**Helper for storing all running data
+	 * related to the v2v algorithm in one place.
+	 */
+	struct Context
+	{
+		vtkCellLocatorPtr mTargetPointLocator; ///< input: target data wrapped in a locator
+		vtkPolyDataPtr mTargetPoints; ///< input: target data
+		vtkPointsPtr mSourcePoints; ///< input: current source data, modified according to last iteration
+
+		vtkPointsPtr mSortedSourcePoints; ///< source points sorted according to distance to target, #mSortedSourcePoints==#mSortedTargetPoints
+		vtkPointsPtr mSortedTargetPoints; ///< source points projected onto the target points (closest points) #mSortedSourcePoints==#mSortedTargetPoints
+
+		vtkAbstractTransformPtr mTransform; ///< output: transform from last iteration
+		double mMetric; ///< output: mean least squares from BEFORE last iteration.
+
+		double mLtsRatio; ///< local copy of the lts ratio, can be changed for current iteration.
+	};
+	typedef boost::shared_ptr<Context> ContextPtr;
+
   SeansVesselReg(int lts_ratio, double stop_delta, double lambda, double sigma, bool lin_flag, int sample, int single_point_thre, bool verbose);
   ~SeansVesselReg();
 
@@ -29,6 +48,12 @@ public:
 //  ssc::Transform3D getNonLinearTransform();
   ssc::ImagePtr loadMinc(char* source_file);
   void setDebugOutput(bool on) { mt_verbose = on; }
+
+  // debug interface:
+  ContextPtr createContext(ssc::DataPtr source, ssc::DataPtr target);
+  void performOneRegistration(ContextPtr context, bool linear);
+  void computeDistances(ContextPtr context);
+  vtkPolyDataPtr convertToPolyData(vtkPointsPtr input);
 
   /**
    * Extract polydata from a image.
@@ -42,13 +67,12 @@ private:
   ssc::Transform3D getLinearTransform(vtkGeneralTransformPtr myConcatenation);
 
 protected:
-  bool runAlgorithm(vtkPolyDataPtr currentSourcePolyData, vtkPolyDataPtr targetPolyData, vtkGeneralTransformPtr myConcatenation);
+  bool runAlgorithm(ContextPtr context, vtkGeneralTransformPtr myConcatenation, int largeSteps, double fraction);
   void printOutResults(QString fileNamePrefix, vtkGeneralTransformPtr myConcatenation);
-  vtkAbstractTransformPtr linearRegistration(vtkPointsPtr sortedSourcePoints, vtkPointsPtr sortedTargetPoints, int numPoints);
-  vtkAbstractTransformPtr nonLinearRegistration(vtkPointsPtr sortedSourcePoints, vtkPointsPtr sortedTargetPoints, int numPoints);
+  vtkAbstractTransformPtr linearRegistration(vtkPointsPtr sortedSourcePoints, vtkPointsPtr sortedTargetPoints);
+  vtkAbstractTransformPtr nonLinearRegistration(vtkPointsPtr sortedSourcePoints, vtkPointsPtr sortedTargetPoints);
   vtkPolyDataPtr convertToPolyData(ssc::DataPtr data);
   vtkPointsPtr transformPoints(vtkPointsPtr input, vtkAbstractTransformPtr transform);
-  vtkPolyDataPtr convertToPolyData(vtkPointsPtr input);
   vtkPointsPtr createSortedPoints(vtkIdListPtr sortedIDList, vtkPointsPtr unsortedPoints, int numPoints);
   vtkPolyDataPtr crop(vtkPolyDataPtr input, vtkPolyDataPtr fixed, double margin);
 
