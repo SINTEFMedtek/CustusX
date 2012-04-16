14dc0025b45d41e0f8d701378a5b12006aa028c8
Christian <christian.askeland@sintef.no>
2012-04-16 10:27:28 +0200

    refactored tool getType system
    Former-commit-id: d0ec5882a8a65e3d12225e772e1523a65d675862

--- source/gui/dataAdapters/cxDoubleDataAdapterTemporalCalibration.cpp
+++ source/gui/dataAdapters/cxDoubleDataAdapterTemporalCalibration.cpp
@@ -24,7 +24,7 @@ void DoubleDataAdapterTimeCalibration::dominantToolChanged()
   // ignore tool changes to something non-probeish.
   // This gives the user a chance to use the widget without having to show the probe.
   ssc::ToolPtr newTool = ssc::toolManager()->getDominantTool();
-  if (!newTool || !newTool->isProbe())
+  if (!newTool || !newTool->hasType(ssc::Tool::TOOL_US_PROBE))
     return;
 
   if (mTool)
