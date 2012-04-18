ca748dfac51a4bfede8c04b2eaa4c01245d16e54
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2012-04-18 12:07:50 +0200

    Updated sonix to the new image sender system
    Former-commit-id: b1a9d8e40ab8bfcdb51b62984f09e0f7e1f06263

--- modules/OpenIGTLinkServer/main.cpp
+++ modules/OpenIGTLinkServer/main.cpp
@@ -10,9 +10,48 @@
 //
 #include "cxImageSenderOpenCV.h"
 
+#ifdef WIN32
+#include <windows.h>
+//#include <tchar.h>
+#endif
+
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#ifdef WIN32
+// Catch ctrl + c on windows: http://www.cplusplus.com/forum/beginner/1501/
+BOOL WINAPI ConsoleHandler(
+	DWORD dwCtrlType   //  control signal type
+);
+
+BOOL WINAPI ConsoleHandler(DWORD CEvent)
+{
+    char mesg[128];
+
+    switch(CEvent)
+    {
+    case CTRL_C_EVENT:
+        //MessageBox(NULL,_T("CTRL+C received!"),_T("CEvent"),MB_OK);
+        break;
+    case CTRL_BREAK_EVENT:
+        //MessageBox(NULL, _T("CTRL+BREAK received!"),_T("CEvent"),MB_OK);
+        break;
+    case CTRL_CLOSE_EVENT:
+        //MessageBox(NULL,_T("Program being closed!"),_T("CEvent"),MB_OK);
+        break;
+    case CTRL_LOGOFF_EVENT:
+        //MessageBox(NULL,_T("User is logging off!"),_T("CEvent"),MB_OK);
+        break;
+    case CTRL_SHUTDOWN_EVENT:
+        //MessageBox(NULL,_T("User is logging off!"),_T("CEvent"),MB_OK);
+        break;
+
+    }
+	qApp->quit();
+    return TRUE;
+}
+#endif
 
 #ifndef WIN32
 void my_handler(int s)
@@ -24,6 +63,16 @@ void my_handler(int s)
 
 int main(int argc, char* argv[])
 {
+#ifdef WIN32
+	if (SetConsoleCtrlHandler( (PHANDLER_ROUTINE)ConsoleHandler,TRUE)==FALSE)
+	{
+		// unable to install handler... 
+		// display message to the user
+		printf("Unable to install handler!\n");
+	//return -1;
+	}
+
+#endif
 #ifndef WIN32
 	// nice shutdown of app
 	// http://stackoverflow.com/questions/1641182/how-can-i-catch-a-ctrl-c-event-c
