05858c48cd47dfd0438ece6ff93993ec930f24ab
Christian <christian.askeland@sintef.no>
2012-04-18 08:38:07 +0200

    forgot to add image sender file
    Former-commit-id: cf3268171c54e3c879ecc7fdb0d8bbf11ffea8f9

--- modules/OpenIGTLinkServer/cxImageSession.cpp
+++ modules/OpenIGTLinkServer/cxImageSession.cpp
@@ -30,6 +30,122 @@ typedef vtkSmartPointer<vtkLookupTable> vtkLookupTablePtr;
 namespace cx
 {
 
+/** streambuf subclass: used to override either cout or cerr and
+ * reroute text to both messagemanager and original stream.
+ *
+ */
+class MyStreamBuf: public std::basic_streambuf<char, std::char_traits<char> >
+{
+public:
+  MyStreamBuf() :
+    mEnabledRedirect(true), mOrig(NULL)
+  {
+  }
+  void setOriginal(std::streambuf* orig)
+  {
+    mOrig = orig;
+  }
+  virtual int_type overflow(int_type meta = traits_type::eof())
+  {
+    char single = traits_type::to_char_type(meta);
+//    if (mOrig) // send to original stream as well
+//      mOrig->sputc(single);
+
+    if (mEnabledRedirect)
+    {
+    	std::cout << "got " << single << std::endl;
+//      if (single == '\n')
+//      {
+//        QMutexLocker sentry(&mMutex);
+//        QString buffer = qstring_cast(mBuffer);
+//        mBuffer.clear();
+//        sentry.unlock();
+//
+//        messageManager()->sendMessage(buffer, mMessageLevel, 0);
+//      }
+//      else
+//      {
+//        QMutexLocker sentry(&mMutex);
+//        mBuffer += single;
+//      }
+    }
+    return traits_type::not_eof(meta);
+  }
+  void setEnableRedirect(bool on)
+  {
+    mEnabledRedirect = on;
+  }
+
+  //    virtual int_type overflow(int_type meta=traits_type::eof());
+private:
+  bool mEnabledRedirect;
+  QString mBuffer;
+  std::streambuf* mOrig;
+//  QMutex mMutex;
+};
+
+class SingleStreamerImpl
+{
+private:
+  boost::shared_ptr<class MyStreamBuf> StreamBuf;
+  std::streambuf *OrigBuf;
+  std::istream& mStream;
+public:
+  SingleStreamerImpl(std::istream& str) :
+    mStream(str)
+  {
+	  std::cout << "SingleStreamerImpl starting ...." << std::endl;
+    StreamBuf.reset(new MyStreamBuf());
+    OrigBuf = mStream.rdbuf(StreamBuf.get());
+    StreamBuf->setOriginal(OrigBuf);
+  }
+  ~SingleStreamerImpl()
+  {
+    mStream.rdbuf(OrigBuf);
+  }
+  void setEnableRedirect(bool on)
+  {
+    StreamBuf->setEnableRedirect(on);
+  }
+
+};
+
+QuitCheck::QuitCheck(QObject* parent) : QThread(parent)
+{
+	  new SingleStreamerImpl(std::cin);
+}
+
+void QuitCheck::run()
+{
+	std::cout << "tick" << std::endl;
+
+	while(true)
+	{
+		std::string val;
+		std::cin >> val;
+		if (!val.empty())
+		{
+			std::cout << "Close server..." << std::endl;
+			qApp->quit();
+			return;
+		}
+	}
+	//	return;
+	//	char s;
+	//	std::cin.read(&s, 1);
+		// if any input detected: quit application
+//		std::string val;
+////		std::cout << "dddd" << std::endl;
+//		std::cin >> val;
+//	//	if (s=='q')
+//		if (!val.empty())
+//		{
+//			std::cout << "Close server..." << std::endl;
+//			this->socketDisconnectedSlot();
+//			qApp->quit();
+//		}
+}
+
 //------------------------------------------------------------
 //------------------------------------------------------------
 //------------------------------------------------------------
