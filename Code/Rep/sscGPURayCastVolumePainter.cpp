1c165ce7458d0b23ac75bd6412f6224cb29d6d43
Sigmund Augdal <sigmund.augdal@sonowand.com>
2012-06-13 10:50:24 +0200

    Move GL_TRACE macro to sscGLHelpers.h
    Former-commit-id: 8c303bc26e5221823965de570f3b19f6e16b03f0

--- Code/Rep/sscGPURayCastVolumePainter.cpp
+++ Code/Rep/sscGPURayCastVolumePainter.cpp
@@ -54,8 +54,6 @@ namespace ssc
 vtkStandardNewMacro(GPURayCastVolumePainter);
 vtkCxxRevisionMacro(GPURayCastVolumePainter, "$Revision: 647 $");
 
-#define GL_TRACE(string) if (vtkgl::StringMarkerGREMEDY) {vtkgl::StringMarkerGREMEDY(0, QString("%1:%2 - %3").arg(__func__).arg(__LINE__).arg(string).toUtf8().constData());}
-	
 class GPURayCastSingleVolumePainterHelper
 {
 	ssc::GPUImageDataBufferPtr mVolumeBuffer;
