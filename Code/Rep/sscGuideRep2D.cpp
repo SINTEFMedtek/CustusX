52e1e300a6fec008117a73139a5351f3cd9dda59
Sigmund Augdal <sigmund.augdal@sonowand.com>
2012-06-19 14:54:12 +0200

    Handle properly when tooltip offset position is exactly in the target point plane
    Former-commit-id: 5e2b6ccc23c1f0441e2ea6681c487f0125b7d372

--- Code/Rep/sscGuideRep2D.cpp
+++ Code/Rep/sscGuideRep2D.cpp
@@ -121,11 +121,11 @@ void GuideRep2D::changedSlot()
 	{
 		mCircleActor->GetProperty()->SetColor(0, 1, 1);
 	}
-	else if (offsetDistance < mRequestedAccuracy && position[2] > 0 && distance < 4*mRequestedAccuracy)
+	else if (offsetDistance < mRequestedAccuracy && position[2] >= 0 && distance < 4*mRequestedAccuracy)
 	{
 		mCircleActor->GetProperty()->SetColor(0, 1, 1.0 - (distance-mRequestedAccuracy)/(3*mRequestedAccuracy));
 	}
-	else if (offsetDistance < mRequestedAccuracy && position[2] > 0)
+	else if (offsetDistance < mRequestedAccuracy && position[2] >= 0)
 	{
 		mCircleActor->GetProperty()->SetColor(0, 1, 0);
 	}
