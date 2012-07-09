499bb70ee9309efd5b16fea741d114261c3f92b7
Per Inge Mathisen <per.mathisen@gmail.com>
2012-07-09 16:00:48 +0200

    misc cleanup - no actual changes in behaviour should be here
    Former-commit-id: 254052e29fbd7f794d50c2827e114e4c98f6f926

--- Code/Utilities/sscGLHelpers.h
+++ Code/Utilities/sscGLHelpers.h
@@ -1,16 +1,17 @@
 #ifndef SSCGLHELPERS_H_
 #define SSCGLHELPERS_H_
 
+#include <vtkgl.h>
+
 #define GL_TRACE(string) if (vtkgl::StringMarkerGREMEDY) {vtkgl::StringMarkerGREMEDY(0, QString("%1:%2 - %3").arg(__func__).arg(__LINE__).arg(string).toUtf8().constData());}
 	
-#define report_gl_error() fgl_really_report_gl_errors(__FILE__, __LINE__, 0)
-#define report_gl_error_text(text) fgl_really_report_gl_errors(__FILE__, __LINE__, text)
-
+#define report_gl_error() really_report_gl_errors(__FILE__, __LINE__, 0)
+#define report_gl_error_text(text) really_report_gl_errors(__FILE__, __LINE__, text)
 
 #ifdef WIN32
-	static void fgl_really_report_gl_errors( const char *file, int line, const char* text ) {}
+	static void really_report_gl_errors( const char *file, int line, const char* text ) {}
 #else
-	void fgl_really_report_gl_errors( const char *file, int line, const char* text );
+	void really_report_gl_errors( const char *file, int line, const char* text );
 #endif //WIN32
 
 
