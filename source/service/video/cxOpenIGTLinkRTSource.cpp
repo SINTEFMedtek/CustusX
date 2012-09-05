4d6ce0c9d905d55edf8ea0711b9022b1dad87b41
SINTEF <christian.askeland@sintef.no>
2012-09-05 08:39:40 +0200

    updated temporal calibration - more doc and stability
    Former-commit-id: 371d00f4ad4931b29911c7521e3440db8d1c97bc

--- source/service/video/cxOpenIGTLinkRTSource.cpp
+++ source/service/video/cxOpenIGTLinkRTSource.cpp
@@ -440,6 +440,7 @@ void OpenIGTLinkRTSource::updateImageImportFromIGTMessage(igtl::ImageMessage::Po
  */
 void OpenIGTLinkRTSource::updateSonixStatus(IGTLinkUSStatusMessage::Pointer message)
 {
+	std::cout << "OpenIGTLinkRTSource::updateSonixStatus" << std::endl;
 	// start with getting a valid data object from the probe, in order to keep
 	// existing values (such as temporal calibration).
 	ProbePtr probe = this->getValidProbe();
@@ -456,7 +457,7 @@ void OpenIGTLinkRTSource::updateSonixStatus(IGTLinkUSStatusMessage::Pointer mess
 	imageData.mOrigin_p = ssc::Vector3D(message->GetOrigin());
 	mSonixProbeData.setImage(imageData);
 
-	std::cout << "Received Sonix message:\n" << streamXml2String(mSonixProbeData) << std::cout;
+	//std::cout << "Received Sonix message:\n" << streamXml2String(mSonixProbeData) << std::cout;
 
 	sonixVideo = true; // Temporary hack to turn off ARGB_RGBA for sonix
 
