54960201384e2294ab5153bd41caabdb9b76513c
Christian <christian.askeland@gmail.com>
2012-11-15 18:26:39 +0100

    added smarter update handling in selected widgets (landmark registration and metric)
    Former-commit-id: a985cd98a113b4de104be12c79268ef29f090d17

---
+++ source/gui/utilities/cxBaseWidget.cpp
@@ -0,0 +1,73 @@
+#include "cxBaseWidget.h"
+
+
+namespace cx
+{
+
+BaseWidget::BaseWidget(QWidget* parent, QString objectName, QString windowTitle) :
+    OptimizedUpdateWidget(parent), mObjectName(objectName), mWindowTitle(windowTitle)
+{
+    if (mObjectName=="US Reconstruction")
+        std::cout << ":: [" <<  this->objectName().toAscii().constData() << "]" << std::endl;
+    this->setObjectName(mObjectName);
+    this->setWindowTitle(mWindowTitle);
+}
+
+QWidget* BaseWidget::createMethodWidget(QWidget* inputWidget, QWidget* outputWidget, QString methodname, bool inputChecked, bool outputVisible)
+{
+    QWidget* retval = new QWidget(this);
+    QVBoxLayout* toplayout = new QVBoxLayout(retval);
+    QGridLayout* layout = new QGridLayout();
+    toplayout->addLayout(layout);
+    toplayout->addStretch();
+
+    QLabel* methodLabel = new QLabel("<b>" + methodname + "</b>");
+    QCheckBox* checkBox = new QCheckBox("generate");
+
+    inputWidget->setVisible(inputChecked);
+    outputWidget->setVisible(outputVisible);
+    connect(checkBox, SIGNAL(clicked(bool)), inputWidget, SLOT(setVisible(bool)));
+
+    layout->addWidget(methodLabel, 0, 0);
+    layout->addWidget(checkBox, 0, 1);
+    layout->addWidget(inputWidget, 1, 0, 1, 2);
+    layout->addWidget(outputWidget, 2, 0, 1, 2);
+
+    return retval;
+}
+
+QGroupBox* BaseWidget::createGroupbox(QWidget* widget, QString boxname)
+{
+    QGroupBox* retval = new QGroupBox(this);
+    QVBoxLayout* toplayout = new QVBoxLayout(retval);
+
+    QLabel* nameLabel = new QLabel(boxname);
+    toplayout->addWidget(nameLabel);
+    toplayout->addWidget(widget);
+
+    return retval;
+}
+
+QFrame* BaseWidget::createHorizontalLine() ///< creates a horizontal line witch can be inserted into widgets
+{
+    QFrame* retval = new QFrame();
+    retval->setFrameStyle(QFrame::Sunken + QFrame::HLine);
+    retval->setFixedHeight(12);
+    return retval;
+}
+
+void BaseWidget::adjustSizeSlot()
+{
+    this->parentWidget()->adjustSize();
+    this->adjustSize();
+}
+
+void BaseWidget::showEvent(QShowEvent* event)
+{
+    this->setWhatsThis(this->defaultWhatsThis());
+    QWidget::showEvent(event);
+}
+
+
+}
+
