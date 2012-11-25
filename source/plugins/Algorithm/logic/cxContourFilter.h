963a5eb5309eb4fd41c2a08929ddd202e64cebd9
Christian <christian.askeland@gmail.com>
2012-11-25 13:53:42 +0100

    added Filter descendant ContourFilter
    Former-commit-id: 34abddff72b423042f37951b74b0124c860d0d82

--- source/plugins/Algorithm/logic/cxContourFilter.h
+++ source/plugins/Algorithm/logic/cxContourFilter.h
@@ -16,22 +16,67 @@
 #define CXCONTOURFILTER_H
 
 #include "cxFilterImpl.h"
+#include "sscBoolDataAdapterXml.h"
+#include "sscColorDataAdapterXml.h"
+#include "sscDoubleDataAdapterXml.h"
 
 namespace cx
 {
 
-/** 
+/** Marching cubes surface generation.
  *
  *
  * \ingroup cx
- * \date 11 22, 2012
+ * \date Nov 25, 2012
  * \author christiana
  */
 class ContourFilter : public FilterImpl
 {
+    Q_OBJECT
+
 public:
-    ContourFilter();
+    virtual ~ContourFilter() {}
+
+    virtual QString getType() const;
+    virtual QString getName() const;
+    virtual QString getHelp() const;
+    virtual void setActive(bool on);
+
+    bool preProcess();
+    virtual bool execute();
+    virtual void postProcess();
+
+    // extensions:
+    ssc::BoolDataAdapterXmlPtr getReduceResolutionOption(QDomElement root);
+    ssc::BoolDataAdapterXmlPtr getSmoothingOption(QDomElement root);
+    ssc::BoolDataAdapterXmlPtr getPreserveTopologyOption(QDomElement root);
+    ssc::DoubleDataAdapterXmlPtr getSurfaceThresholdOption(QDomElement root);
+    ssc::DoubleDataAdapterXmlPtr getDecimationOption(QDomElement root);
+    ssc::ColorDataAdapterXmlPtr getColorOption(QDomElement root);
+
+protected:
+    virtual void createOptions(QDomElement root);
+    virtual void createInputTypes();
+    virtual void createOutputTypes();
+
+private slots:
+    /** Set new value+range of the threshold option.
+      */
+    void imageChangedSlot(QString uid);
+    void thresholdSlot();
+
+private:
+    ssc::BoolDataAdapterXmlPtr mReduceResolutionOption;
+//    ssc::BoolDataAdapterXmlPtr mSmoothingOption;
+//    ssc::BoolDataAdapterXmlPtr mPreserveTopologyOption;
+    ssc::DoubleDataAdapterXmlPtr mSurfaceThresholdOption;
+//    ssc::DoubleDataAdapterXmlPtr mDecimationOption;
+//    ssc::ColorDataAdapterXmlPtr mColorOption;
+//    ssc::DoubleDataAdapterXmlPtr mLowerThresholdOption;
+    vtkPolyDataPtr mRawResult;
 };
+typedef boost::shared_ptr<class ContourFilter> ContourFilterPtr;
+
 
 } // namespace cx
 
