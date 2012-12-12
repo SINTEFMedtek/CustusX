d3d221769f85b803112a9f7f30843068e55485b4
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2012-12-12 09:50:33 +0100

    Fixed #653: Sonix interface timestamps were not synched. Fixed by running IGTLinkClientBase::calibrateTimeStamp() for all video streams
    Former-commit-id: 5b77269548dff8cabac8f80a4e66343e45da98ae

--- source/service/video/cxIGTLinkClientBase.cpp
+++ source/service/video/cxIGTLinkClientBase.cpp
@@ -14,9 +14,20 @@
 
 #include "cxIGTLinkClientBase.h"
 #include "sscMessageManager.h"
+#include "sscVector3D.h"
 
 namespace cx
 {
+
+
+IGTLinkClientBase::IGTLinkClientBase(QObject* parent) :
+		QThread(parent)
+{
+	calibrateMsgTimeStamp = true;
+	mGeneratingTimeCalibration = false;
+	mLastReferenceTimestampDiff = 0.0;
+	mLastTimeStamps.reserve(20);
+}
 
 // This was a problem encountered on MacOSX+OpenCV - this does not work well anyway - disable.
 //
@@ -48,6 +59,10 @@ void IGTLinkClientBase::addImageToQueue(IGTLinkImageMessage::Pointer imgMsg)
 		emit fps(mFPSTimer.getFPS());
 		mFPSTimer.reset(2000);
 	}
+
+	//Needed time stamp fix?
+	if (calibrateMsgTimeStamp)
+		calibrateTimeStamp(imgMsg);
 
 	// testcode?
 	//Get modified timestamp
@@ -112,6 +127,75 @@ IGTLinkUSStatusMessage::Pointer IGTLinkClientBase::getLastSonixStatusMessage()
 	IGTLinkUSStatusMessage::Pointer retval = mMutexedSonixStatusMessageQueue.front();
 	mMutexedSonixStatusMessageQueue.pop_front();
 	return retval;
+}
+
+/**
+ * Calibrate the time stamps of the incoming message based on the computer clock.
+ * Calibration is based on an average of several of the last messages.
+ * The calibration is updated every 20-30 sec.
+ */
+void IGTLinkClientBase::calibrateTimeStamp(IGTLinkImageMessage::Pointer imgMsg)
+{
+	igtl::TimeStamp::Pointer timestamp = igtl::TimeStamp::New();
+	imgMsg->GetTimeStamp(timestamp);
+	double timestamp_ms = timestamp->GetTimeStamp() * 1000;
+	QDateTime timestamp_dt = QDateTime::fromMSecsSinceEpoch(timestamp_ms);
+
+	if (ssc::similar(mLastReferenceTimestampDiff, 0.0, 0.000001))
+	{
+		mLastReferenceTimestampDiff = timestamp_dt.msecsTo(QDateTime::currentDateTime());
+//		std::cout << "First timestamp calib: " << mLastReferenceTimestampDiff << " ms" << std::endl;
+	}
+
+	// Start collecting time stamps if 20 sec since last calibration time
+	if(mLastSyncTime.isNull() || ( mLastSyncTime.msecsTo(QDateTime::currentDateTime()) > 2000) )
+		mGeneratingTimeCalibration = true;
+
+	if(mGeneratingTimeCalibration)
+		mLastTimeStamps.push_back(timestamp_dt.msecsTo(QDateTime::currentDateTime()));
+
+	// Perform time calibration if enough time stamps have been collected
+	if(mLastTimeStamps.size() >= 20)
+	{
+		std::sort(mLastTimeStamps.begin(), mLastTimeStamps.end(), AbsDoubleLess(mLastReferenceTimestampDiff));
+
+		//debug print
+	  /*for (std::vector<double>::const_iterator citer = mLastTimeStamps.begin();
+	      citer != mLastTimeStamps.end(); ++citer)
+	  {
+			std::cout << *citer - mLastReferenceTimestampDiff << " ";
+	  }
+	  std::cout << endl;*/
+
+		mLastTimeStamps.resize(15);
+
+		//debug print
+	  /*for (std::vector<double>::const_iterator citer = mLastTimeStamps.begin();
+	      citer != mLastTimeStamps.end(); ++citer)
+	  {
+			std::cout << *citer - mLastReferenceTimestampDiff << " ";
+	  }
+	  std::cout << endl;*/
+
+		double sumTimes = 0;
+	  for (std::vector<double>::const_iterator citer = mLastTimeStamps.begin();
+	      citer != mLastTimeStamps.end(); ++citer)
+	  {
+	  	sumTimes += *citer;
+	  }
+	  mLastReferenceTimestampDiff = sumTimes / 15.0;
+
+//		std::cout << "Timestamp calib: " << mLastReferenceTimestampDiff << " ms" << std::endl;
+
+		//Reset
+		mLastTimeStamps.clear();
+		mLastSyncTime = QDateTime::currentDateTime();
+		mGeneratingTimeCalibration = false;
+	}
+
+	// Update imgMsg timestamp
+	timestamp->SetTime((timestamp_ms + mLastReferenceTimestampDiff) / 1000.0); // in sec
+	imgMsg->SetTimeStamp(timestamp);
 }
 
 } /* namespace cx */
