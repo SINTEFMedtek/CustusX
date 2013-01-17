62b880a9bf88373a80b7b778f94765e12b552d11
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-01-17 13:16:45 +0100

    Added sscDataAdapter.cpp
    Former-commit-id: 935125a5a02c2f5b08d5f7a9c7ac4adbd3438b45

---
+++ Code/Widgets/sscDataAdapter.cpp
@@ -0,0 +1,50 @@
+// This file is part of SSC,
+// a C++ Library supporting Image Guided Therapy Applications.
+//
+// Copyright (C) 2008- SINTEF Medical Technology
+// Copyright (C) 2008- Sonowand AS
+//
+// SSC is owned by SINTEF Medical Technology and Sonowand AS,
+// hereafter named the owners. Each particular piece of code
+// is owned by the part that added it to the library.
+// SSC source code and binaries can only be used by the owners
+// and those with explicit permission from the owners.
+// SSC shall not be distributed to anyone else.
+//
+// SSC is distributed WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// See sscLicense.txt for more information.
+
+#include "sscDataAdapter.h"
+
+/*
+ * cxDataAdapter.cpp
+ *
+ * \date Dec 21, 2012
+ * \author Janne Beate Bakeng, SINTEF
+ */
+
+DataAdapter::DataAdapter() :
+		QObject(), mEnabled(true)
+{}
+
+bool DataAdapter::getEnabled() const
+{
+	return mEnabled;
+}
+
+bool DataAdapter::setEnabled(bool enabling)
+{
+
+	if(mEnabled == enabling)
+		return false;
+
+	mEnabled = enabling;
+	emit changed();
+
+	return true;
+}
+
+
