01c57706059ce397eb94f5cfb91d681f97661a85
Christian <christian.askeland@gmail.com>
2013-01-24 14:25:06 +0100

    some test framework stuff, fixed angio bug (was not fixed in previous commit as stated there)
    Former-commit-id: 2d4691eccc0366939a7be18ff9a11058911f6a53

---
+++ source/plugins/Acquisition/testing/cxCppUnitMain.cpp
@@ -0,0 +1,46 @@
+#include <QApplication>
+//#include <boost/scoped_ptr.hpp>
+
+#include <cppunit/extensions/TestFactoryRegistry.h> 
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h> 
+#include <cppunit/BriefTestProgressListener.h> 
+
+//#define RUN_ALL_TESTS
+
+#ifndef RUN_ALL_TESTS
+#include "cxTestAcquisition.h"
+#endif
+
+/**main function for CX unit tests.
+ * Perform system-wide init/deinit,
+ * run all registered tests.
+ */
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	CppUnit::TextUi::TestRunner runner;
+
+	CppUnit::BriefTestProgressListener listener; 
+	runner.eventManager().addListener( &listener ); 
+		
+#ifdef RUN_ALL_TESTS
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest( registry.makeTest() );
+#else
+	runner.addTest( new CppUnit::TestCaller<TestAcquisition>(
+									   "TestAcquisition::testDualAngio",
+										&TestAcquisition::testConstructor ) );
+//	runner.addTest( new CppUnit::TestCaller<TestAcquisition>(
+//									   "TestAcquisition::testConstructor",
+//										&TestAcquisition::testConstructor ) );
+//	runner.addTest(cxTestExamples::suite());
+#endif
+	
+	
+	bool failed = runner.run();
+	return !failed;
+} 
+
+
