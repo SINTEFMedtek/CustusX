4f9f318874aec2d5f5992957632529e6a5ff283d
Christian <christian.askeland@gmail.com>
2013-01-24 10:15:11 +0100

    refactored us rec threads into separate file, also some cleanup in same code.
    Former-commit-id: df18eebfb528c678b3bbd5b49d6f3f6e732c8a5f

---
+++ source/plugins/UsReconstruction/logic/sscReconstructThreads.h
@@ -0,0 +1,125 @@
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
+#ifndef SSCRECONSTRUCTTHREADS_H
+#define SSCRECONSTRUCTTHREADS_H
+
+#include <QObject>
+#include <QThread>
+#include <math.h>
+#include "sscReconstructAlgorithm.h"
+#include "sscBoundingBox3D.h"
+#include "sscReconstructedOutputVolumeParams.h"
+#include "sscStringDataAdapterXml.h"
+#include "sscDoubleDataAdapterXml.h"
+#include "sscBoolDataAdapterXml.h"
+#include "sscXmlOptionItem.h"
+#include "sscProbeSector.h"
+#include "cxUsReconstructionFileReader.h"
+#include "cxThreadedTimedAlgorithm.h"
+#include "sscReconstructPreprocessor.h"
+
+namespace ssc
+{
+
+//typedef boost::shared_ptr<class ReconstructManager> ReconstructManagerPtr;
+//typedef boost::shared_ptr<class Reconstructer> ReconstructerPtr;
+//typedef boost::shared_ptr<class ReconstructCore> ReconstructCorePtr;
+//typedef boost::shared_ptr<class ReconstructParams> ReconstructParamsPtr;
+
+/**
+ * \file
+ * \addtogroup cxPluginUsReconstruction
+ * @{
+ */
+
+
+typedef boost::shared_ptr<class ThreadedTimedReconstructer> ThreadedTimedReconstructerPtr;
+typedef boost::shared_ptr<class ThreadedTimedReconstructPreprocessor> ThreadedTimedReconstructPreprocessorPtr;
+typedef boost::shared_ptr<class ThreadedTimedReconstructCore> ThreadedTimedReconstructCorePtr;
+
+/**
+ * \brief Threading adapter for the reconstruction algorithm.
+ *
+ * Must be run before ThreadedTimedReconstructCore.
+ *
+ * Executes ReconstructCore functions:
+ *  - threadedPreReconstruct() [main thread]
+ *  - threadablePreReconstruct() [work thread]
+ *
+ * \date Jan 27, 2012
+ * \author Christian Askeland, SINTEF
+ */
+class ThreadedTimedReconstructPreprocessor: public cx::ThreadedTimedAlgorithm<void>
+{
+Q_OBJECT
+public:
+	static ThreadedTimedReconstructPreprocessorPtr create(ReconstructPreprocessorPtr input, std::vector<ReconstructCorePtr> cores)
+	{
+		return ThreadedTimedReconstructPreprocessorPtr(new ThreadedTimedReconstructPreprocessor(input, cores));
+	}
+
+	ThreadedTimedReconstructPreprocessor(ReconstructPreprocessorPtr input, std::vector<ReconstructCorePtr> cores);
+	virtual ~ThreadedTimedReconstructPreprocessor();
+
+private slots:
+	virtual void preProcessingSlot();
+	virtual void postProcessingSlot();
+
+private:
+	virtual void calculate();
+	ReconstructPreprocessorPtr mInput;
+	std::vector<ReconstructCorePtr> mCores;
+};
+
+/**
+ * \brief Threading adapter for the reconstruction algorithm.
+ *
+ * Must be run after ThreadedTimedReconstructerStep2.
+ *
+ * Executes ReconstructCore functions:
+ *  - threadedReconstruct() [work thread]
+ *  - threadedPostReconstruct() [main thread]
+ *
+ * \date Jan 27, 2012
+ * \author Christian Askeland, SINTEF
+ */
+class ThreadedTimedReconstructCore: public cx::ThreadedTimedAlgorithm<void>
+{
+Q_OBJECT
+public:
+	static ThreadedTimedReconstructCorePtr create(ReconstructCorePtr reconstructer)
+	{
+		return ThreadedTimedReconstructCorePtr(new ThreadedTimedReconstructCore(reconstructer));
+	}
+	ThreadedTimedReconstructCore(ReconstructCorePtr reconstructer);
+	virtual ~ThreadedTimedReconstructCore();
+
+//	void start();
+
+private slots:
+	virtual void preProcessingSlot();
+	virtual void postProcessingSlot();
+
+private:
+	virtual void calculate();
+	ReconstructCorePtr mReconstructer;
+};
+
+
+/**
+ * @}
+ */
+}
+
+#endif // SSCRECONSTRUCTTHREADS_H
