d8f1642a3196c8aa8591c1e21807feb72ba89375
Christian <christian.askeland@gmail.com>
2013-01-29 23:44:54 +0100

    added zeroing to lcov
    Former-commit-id: 3a021274cf25b418975c95f97b40eecab99f64b6

--- testing/coverage/cx_run_lcov.sh
+++ testing/coverage/cx_run_lcov.sh
@@ -15,26 +15,33 @@
 
 #USER=`whoami`
 
-echo "Generating coverage from cwd..."
+if [[ "$1" == "" ]] ; then
+    echo "Usage: cx_run_lcov <path_to_build_root>"
+    exit
+fi
+
+echo "Generating coverage from $1"
 
 echo ""
 set -x
 
 # remove previous coverage counts - not working
+lcov -capture -initial --directory $1 --output-file cx_coverage_base.gcov
 #lcov --zerocounters -directory .
 
 #kjør alle tester
 #ctest
 
 # convert coverage data from app runs
-lcov --capture --directory . --output-file cx_coverage.gcov
+lcov --capture --directory $1 --output-file cx_coverage_test.gcov
+lcov -add-tracefile cx_coverage_base.gcov -a cx_coverage_test.gcov -o cx_coverage_total.gcov
 # remove system and external libraries
 lcov --remove cx_coverage.gcov 'eigen3/Eigen/*' '/opt/*' 'external_code/*' '/Library/*' '/usr/*' '/moc*.cxx' --output-file cx_coverage.gcov
 # generate html
-genhtml cx_coverage.gcov -output-directory coverage_info
+genhtml cx_coverage.gcov -output-directory $1/coverage_info
 # open in web browser
 #open coverage_info/index.html # mac
-firefox coverage_info/index.html
+firefox $1/coverage_info/index.html
 
 
 
