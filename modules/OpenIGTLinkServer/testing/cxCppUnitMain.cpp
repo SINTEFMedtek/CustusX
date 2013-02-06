6e9a1f940b9995f5f64d925a5b80e8ef01376efb
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-02-06 09:28:18 +0100

    Added two simple tests for GEStreamer
    Former-commit-id: f8f17545515ce6b324e5acc42bbee9ad3866bebc

---
+++ modules/OpenIGTLinkServer/testing/cxCppUnitMain.cpp
@@ -0,0 +1,46 @@
+#include <QApplication>
+//#include <boost/scoped_ptr.hpp>
+
+#include <cppunit/extensions/TestFactoryRegistry.h> 
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h> 
+#include <cppunit/BriefTestProgressListener.h> 
+
+//#define RUN_ALL_TESTS
+
+#ifndef RUN_ALL_TESTS
+#include "cxTestGEInterface.h"
+#endif
+
+/**main function for CX unit tests.
+ * Perform system-wide init/deinit,
+ * run all registered tests.
+ */
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	CppUnit::TextUi::TestRunner runner;
+
+	CppUnit::BriefTestProgressListener listener; 
+	runner.eventManager().addListener( &listener ); 
+		
+#ifdef RUN_ALL_TESTS
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest( registry.makeTest() );
+#else
+//	runner.addTest( new CppUnit::TestCaller<TestGEInterface>(
+//									   "TestGEInterface::testConstructor",
+//										&TestGEInterface::testConstructor ) );
+	runner.addTest( new CppUnit::TestCaller<TestGEInterface>(
+									   "TestGEInterface::testInit",
+										&TestGEInterface::testInit ) );
+//	runner.addTest(cxTestExamples::suite());
+#endif
+	
+	
+	bool failed = runner.run();
+	return !failed;
+} 
+
+
