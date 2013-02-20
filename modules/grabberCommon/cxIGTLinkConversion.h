605eeeabc9624e80615dc698dfdaffa78dc53d05
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-02-20 06:58:10 +0100

    Added initial version of IGTLinkConversion
    Former-commit-id: eae9fb92b848612a670ae43dc4afa9c34466e892

---
+++ modules/grabberCommon/cxIGTLinkConversion.h
@@ -0,0 +1,35 @@
+/*
+ * cxIGTLinkConversion.h
+ *
+ *  Created on: Feb 19, 2013
+ *      Author: olevs
+ */
+
+#ifndef CXIGTLINKCONVERSION_H_
+#define CXIGTLINKCONVERSION_H_
+
+namespace cx
+{
+
+/**
+ * Encode and decode IGTLink image and status messages to and from CustusX classes
+ */
+class IGTLinkConversion
+{
+public:
+	IGTLinkConversion();
+	virtual ~IGTLinkConversion();
+
+	IGTLinkImageMessage::Pointer encode(ssc::ImagePtr image);
+	ssc::ImagePtr decode(IGTLinkImageMessage::Pointer imgMsg);
+
+	IGTLinkUSStatusMessage::Pointer encode(ssc::ProbeData);
+	ssc::ProbeData decode(IGTLinkUSStatusMessage::Pointer msg);
+
+private:
+	ProbePtr getValidProbe();
+};
+
+} //namespace cx
+
+#endif /* CXIGTLINKCONVERSION_H_ */
