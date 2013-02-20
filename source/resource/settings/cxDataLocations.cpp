9cdbef05a027f4b423ed06ec24f10ee66141af45
Christian <christian.askeland@gmail.com>
2013-02-20 23:07:38 +0100

    added license text etc
    Former-commit-id: 36067626ce9590c75e4beec95671471df54eba87

--- source/resource/settings/cxDataLocations.cpp
+++ source/resource/settings/cxDataLocations.cpp
@@ -1,9 +1,16 @@
-/*
- * cxDataLocations.cpp
- *
- *  \date Jun 22, 2010
- *      \author christiana
- */
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
 #include "cxDataLocations.h"
 
 #include <iostream>
