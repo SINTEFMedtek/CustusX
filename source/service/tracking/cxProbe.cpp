2976dabaee2550a79816664d7e5b83e5d0508204
Christian <christian.askeland@sintef.no>
2013-03-06 15:11:01 +0100

    debugged video server for multistream refactoring: opencv now OK
    Former-commit-id: d798694036e6df1158c1e59448a0cdfbf0fbaa13

--- source/service/tracking/cxProbe.cpp
+++ source/service/tracking/cxProbe.cpp
@@ -111,6 +111,10 @@ void Probe::setSoundSpeedCompensationFactor(double factor)
 ssc::ProbeData Probe::getData(QString uid) const
 {
 	ssc::ProbeData retval = this->getProbeData(uid);
+
+//	std::cout << "Probe::getData " << uid << std::endl;
+//	std::cout << "Probe::getData " << uid << "\n" << streamXml2String(retval) << std::endl;
+
 	return retval;
 }
 
@@ -143,7 +147,9 @@ void Probe::setRTSource(ssc::VideoSourcePtr source)
 			return;
 	}
 
-	mSource[source->getUid()].reset(new ssc::ProbeAdapterRTSource(source->getUid() + "_probe", mSelf.lock(), source));
+	// must have same uid as original: the uid identifies the
+	mSource[source->getUid()].reset(new ssc::ProbeAdapterRTSource(source->getUid(), mSelf.lock(), source));
+//	mSource[source->getUid()].reset(new ssc::ProbeAdapterRTSource(source->getUid() + "_probe", mSelf.lock(), source));
 	emit sectorChanged();
 }
 
@@ -162,6 +168,9 @@ void Probe::setData(ssc::ProbeData probeSector, QString configUid)
 {
 	mProbeData[probeSector.getUid()] = probeSector;
 	mConfigurationId = configUid;
+
+//	std::cout << "Probe::setData \n" << streamXml2String(probeSector) << std::endl;
+
 	emit sectorChanged();
 }
 
@@ -222,6 +231,7 @@ void Probe::setConfigId(QString uid)
 //  std::cout << "probeSector.mTemporalCalibration" << probeSector.mTemporalCalibration << std::endl;
 //	mConfigurationId = uid;
 //	mData = probeSector;
+	probeSector.setUid(mActiveUid);
 	this->setData(probeSector, uid);
 	//Update temporal calibration and sound speed compensation
 	if (mOverrideTemporalCalibration)
