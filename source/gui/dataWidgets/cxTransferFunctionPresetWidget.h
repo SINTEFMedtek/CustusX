e24465a73b54cde4e4b61ed710abbb6dd211e0b4
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-03-13 11:25:34 +0100

    Started refactoring out presets.
    Former-commit-id: fd8f3b1597ad1a853b95de43a201d87bef798b0b

--- source/gui/dataWidgets/cxTransferFunctionPresetWidget.h
+++ source/gui/dataWidgets/cxTransferFunctionPresetWidget.h
@@ -3,12 +3,12 @@
 
 #include "cxPresetWidget.h"
 
-#include "sscPresetTransferFunctions3D.h"
+#include "sscTransferFunctions3DPresets.h"
 #include "cxDataInterface.h"
 
 class QComboBox;
 
-namespace cx{
+namespace cx {
 
 /*
  * cxTransferFunctionPresetWidget.h
@@ -16,38 +16,42 @@ namespace cx{
  * \date Mar 8, 2013
  * \author Janne Beate Bakeng, SINTEF
  */
-class TransferFunctionPresetWidget : public PresetWidget
+class TransferFunctionPresetWidget: public PresetWidget
 {
-  Q_OBJECT
+
+	Q_OBJECT
 
 public:
-  TransferFunctionPresetWidget(QWidget* parent, bool is3D);
-  virtual ~TransferFunctionPresetWidget() {}
-  virtual QString defaultWhatsThis() const;
+	TransferFunctionPresetWidget(QWidget* parent, bool is3D);
+	virtual ~TransferFunctionPresetWidget() {
+	}
+	virtual QString defaultWhatsThis() const;
 
 public slots:
-	void generatePresetListSlot(); ///< Fill the preset list with the available presets (matching active images modality)
-  void presetsBoxChangedSlot(const QString& presetName);
-  void resetSlot();
-  void saveSlot();
-  void deleteSlot();
-  void toggleSlot();
+	virtual void resetSlot();
+	virtual void saveSlot();
+	virtual void deleteSlot();
+	virtual void populatePresetListSlot(); ///< Fill the preset list with the available presets (matching active images modality)
 
-protected:
-  bool use2D() const;
-  bool use3D() const;
-  void updateToggles();
+	void presetsBoxChangedSlot(const QString& presetName);
+	void toggleSlot();
 
-  QVBoxLayout* mLayout;
-  QComboBox* mPresetsComboBox;
-  ssc::PresetTransferFunctions3DPtr mPresets;
-  QAction* mToggleAction;
-  bool mIs3D; ///< true if 3D, false if 2D
-  bool mApplyToAll; ///< true if presets are to be applied to both 2D and 3D
+private:
+	bool use2D() const;
+	bool use3D() const;
+	void updateToggles();
 
-  ActiveImageProxyPtr mActiveImageProxy;
+//  QVBoxLayout* mLayout;
+//  QComboBox* mPresetsComboBox;
+//	ssc::TransferFunctions3DPresetPtr mPresets;
+
+	QAction* mToggleAction;
+	bool mIs3D; ///< true if 3D, false if 2D
+	bool mApplyToAll; ///< true if presets are to be applied to both 2D and 3D
+	ActiveImageProxyPtr mActiveImageProxy;
+
 };
 
-}//namespace cx
+} //namespace cx
 
 #endif /* CXTRANSFERFUNCTIONPRESETWIDGET_H_ */
