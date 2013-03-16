aa01c6a133bb09e87b0229e50570a6b6434b76ab
Christian <christian.askeland@gmail.com>
2013-03-17 00:21:32 +0100

    jenkins work
    Former-commit-id: 399534ace915caf1ce45cfadc7daf185923dc530

--- testing/jenkins/build_step.sh
+++ testing/jenkins/build_step.sh
@@ -16,7 +16,8 @@
 set -x
 
 cd $WORKSPACE/working/CustusX3/build_Release
-pwd
+echo "checking pwd"
+echo pwd
 # execute tests with xml output
 ctest -D ExperimentalTest --no-compress-output -R Tool
 # copy xml to fixed position ./CTestResults.xml
