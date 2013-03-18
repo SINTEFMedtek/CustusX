0b38f981b6f5a98abc9598078eb10442aa21003a
Christian <christian.askeland@sintef.no>
2013-03-18 14:17:20 +0100

    jenkins work
    Former-commit-id: a48ef8d8d901368f91af99826d9e156ec6bbd9ed

--- testing/jenkins/build_step.sh
+++ testing/jenkins/build_step.sh
@@ -7,13 +7,12 @@
 #
 # Description:
 #
+#   Continous integration build: run every time the repo changes.
 #   Experimental!
 #   Build part of jenkins CI
 #
 #       Download, build, and test CustusX
 #       Publish unit tests
-#       Publish doxygen doc to medtek.sintef.no
-#
 #
 #
 #####################################################
@@ -25,14 +24,7 @@ CX_RELEASE_DIR=$WORKSPACE/working/CustusX3/build_Release
 # ==========================================================
 # Download, configure and build the project group.
 # Python script will return success even if some parts failed.
-./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full --all -t Release -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=ON"
-# use during development: update only the CustusX part
-#./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full -t Release -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF" CustusX3
-
-#make -j4
-#ctest -R Thunder -V
-#ctest -D ExperimentalTest --no-compress-output -R Tool
-#$WORKSPACE/working/CustusX3/CustusX3/testing/jenkins/build_step.sh
+./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full --all -t Release -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF"
 
 # ==========================================================
 # make the CustusX project in order to provoke a build failure.
@@ -47,18 +39,12 @@ else
     exit 1
 fi
 
+# break the build:
+exit 1
+
 # ==========================================================
 # Run all tests and write them in xml format to ./CTestResults.xml
 cd $CX_RELEASE_DIR
-#echo "checking pwd"
-#echo `pwd`
-# execute tests with xml output
+rm -rf Testing/201*
 ctest -D ExperimentalTest --no-compress-output
-#ctest -D ExperimentalTest --no-compress-output -R Tool
-# copy xml to fixed position ./CTestResults.xml
 cp Testing/`head -n 1 Testing/TAG`/Test.xml ./CTestResults.xml
-
-# ==========================================================
-# copy/publish doxygen to medtek server (link from wiki):
-REMOTE_DOXY_PATH="/Volumes/medtek_HD/Library/Server/Web/Data/Sites/Default/custusx_doxygen"
-scp -r $CX_RELEASE_DIR/doc/doxygen/html/* medtek.sintef.no:$REMOTE_DOXY_PATH
