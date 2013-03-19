13a2d3b23303ce6cfe8c050408aba855fd7084a3
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-03-19 11:22:58 +0100

    GEStreamer now creates volumes with non-cubic voxels to same time/memory. Also auto volume size is implemented
    Former-commit-id: 2e0f25e05762cfb58cf4fe31e9e6d1dfd6173ce1

--- install/Shared/script/cxInstaller.py
+++ install/Shared/script/cxInstaller.py
@@ -608,7 +608,7 @@ cmake \
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "330"
+        self.mCurrentRevision = "339"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
