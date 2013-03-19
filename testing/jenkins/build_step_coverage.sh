b3ed6f7d0ae4ae03ff0aed57d869fef10b6d482e
Christian <christian.askeland@sintef.no>
2013-03-19 23:31:03 +0100

    jenkins work
    Former-commit-id: ec2c4452a5541e30536f9f1639e6589aaa64392d

--- testing/jenkins/build_step_coverage.sh
+++ testing/jenkins/build_step_coverage.sh
@@ -19,8 +19,8 @@ CX_DEBUG_DIR=$WORKSPACE/working/CustusX3/build_Debug
 
 # ==========================================================
 # configure and build all libs with debug info
-#./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full --all -t Debug -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF" --coverage
-./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full -t Debug -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF" --coverage CustusX3
+./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full --all -t Debug -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF" --coverage
+#./working/CustusX3/CustusX3/install/Shared/script/cxInstaller.py --full -t Debug -j4 -s --isb_password=sintefsvn -u custusx --external_dir=$WORKSPACE/external --working_dir=$WORKSPACE/working --cmake_args="-DBUILD_DOCUMENTATION:BOOL=OFF" --coverage CustusX3
 
 # ==========================================================
 # make the CustusX project in order to provoke a build failure.
