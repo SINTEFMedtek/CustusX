34269026d697776daa3bdad790393652370f2812
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-03-20 09:50:26 +0100

    Use imageSize = -1 instead of 1 for setting auto image size in GESreamer::InitializeClientData()
    Former-commit-id: 50a075a133303c298b967ec2758d89ea927578fc

--- install/Shared/script/cxInstaller.py
+++ install/Shared/script/cxInstaller.py
@@ -608,7 +608,7 @@ cmake \
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "340"
+        self.mCurrentRevision = "343"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
