941041b1855368eb0378f352ed581e32bfadf334
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-03-20 14:01:17 +0100

    Increased GE interface speed by moving transfer function generation out of sscImage constructor
    Former-commit-id: ee29b224f3b3d5cae420bf842ad3372ff4a0c11a

--- install/Shared/script/cxInstaller.py
+++ install/Shared/script/cxInstaller.py
@@ -608,7 +608,7 @@ cmake \
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "343"
+        self.mCurrentRevision = "345"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
