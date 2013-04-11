ad8a3dd5619f18f378f11601f7307dd2bce3ec11
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-04-11 21:52:51 +0200

    GEStreamer update
    Former-commit-id: 2ce1dfa935032942f8b8630520bd5ed61bcbc0b1

--- install/Shared/script/cxInstaller.py
+++ install/Shared/script/cxInstaller.py
@@ -608,7 +608,7 @@ cmake \
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "384"
+        self.mCurrentRevision = "388"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
