5933848ff594ffd0567f4bed07227681c96bf031
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-04-16 12:56:01 +0200

    Split one GE test into 6 separate tests
    Former-commit-id: ec52b5105b59009128bb16b7134a0e363dec8fe9

--- source/resource/videoServer/testing/cxTestGEInterface.h
+++ source/resource/videoServer/testing/cxTestGEInterface.h
@@ -19,7 +19,12 @@ public:
 
 	void testConstructor();///< Test constructor
 	void testStreams();///< Test init of cxImageSenderGE, and Test GE all streams streams. CPU scanconversion
-	void testSingleStreams();///< Test GE single streams. GPU scanconversion if possible
+	void testAllStreamsGPU();///< Test all GE streams. GPU scanconversion if possible
+	void testScanConvertedStreamGPU();///< Test GE scanconverted stream. GPU scanconversion if possible
+	void testTissueStreamGPU();///< Test GE tissue stream. GPU scanconversion if possible
+	void testFrequencyStreamGPU();///< Test GE frequency stream. GPU scanconversion if possible
+	void testBandwidthStreamGPU();///< Test GE bandwidth stream. GPU scanconversion if possible
+	void testVelocityStreamGPU();///< Test GE velocity stream. GPU scanconversion if possible
 	void testGEStreamer();///< Test GEStreamer directly. Don't use cxImageSenderGE
 
 	int getValue(vtkSmartPointer<vtkImageData> img, int x, int y, int z);
@@ -31,7 +36,12 @@ public:
 	CPPUNIT_TEST_SUITE( TestGEInterface );
 //		CPPUNIT_TEST( testConstructor );
 		CPPUNIT_TEST( testStreams );
-		CPPUNIT_TEST( testSingleStreams );
+		CPPUNIT_TEST( testAllStreamsGPU );
+		CPPUNIT_TEST( testScanConvertedStreamGPU );
+		CPPUNIT_TEST( testTissueStreamGPU );
+		CPPUNIT_TEST( testFrequencyStreamGPU );
+		CPPUNIT_TEST( testBandwidthStreamGPU );
+		CPPUNIT_TEST( testVelocityStreamGPU );
 		CPPUNIT_TEST( testGEStreamer );
 	CPPUNIT_TEST_SUITE_END();
 private:
