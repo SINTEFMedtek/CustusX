7d9cc90086f7b5cb4f9a2f3469a05a297546aadd
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-04-30 14:26:58 +0200

    Moved CompactFilterSetupWidget to own files.
    Former-commit-id: 928bc931cdec557f81b3b5510bde4f59d10f0c5f

--- source/plugins/Algorithm/gui/cxFilterWidget.h
+++ source/plugins/Algorithm/gui/cxFilterWidget.h
@@ -101,32 +101,6 @@ private:
 	boost::shared_ptr<WidgetObscuredListener> mObscuredListener;
 };
 
-/** Helper widget for displaying the input/output/options part of a Filter.
- * Intended to be included in other Filter widgets.
- *
- * \ingroup cxPluginAlgorithm
- * \date Dec 13, 2012
- * \author christiana
- */
-class CompactFilterSetupWidget : public BaseWidget
-{
-	Q_OBJECT
-public:
-	CompactFilterSetupWidget(QWidget* parent, ssc::XmlOptionFile options, bool addFrame);
-	void setFilter(FilterPtr filter);
-	QString defaultWhatsThis() const;
-
-private slots:
-	void obscuredSlot(bool obscured);
-private:
-
-	ssc::XmlOptionFile mOptions;
-	FilterPtr mCurrentFilter;
-	OptionsWidget* mOptionsWidget;
-	QGroupBox* mFrame;
-	boost::shared_ptr<WidgetObscuredListener> mObscuredListener;
-};
-
 }
 
 #endif // CXFILTERWIDGET_H
