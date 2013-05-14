007a550faa0c49751eaaad99122f6d601b26f040
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-05-14 16:08:09 +0200

    Working on getting MHDImageStreamer under testing.
    Former-commit-id: 0c17f54ab1527a6b2e81afd75321ee4164cc50d9

--- source/service/video/testing/cxCatchVideo.cpp
+++ source/service/video/testing/cxCatchVideo.cpp
@@ -1,5 +1,7 @@
 #include "catch.hpp"
 
+#include <QEventLoop>
+
 #include <QFile>
 #include <QString>
 #include "sscImage.h"
@@ -8,34 +10,100 @@
 #include "cxMHDImageStreamer.h"
 #include "cxTestSender.h"
 
-namespace cx
+namespace cxtest
 {
-TEST_CASE("Create image sender", "[video][unit]"){
+
+///< Usage:
+///< QObject* object = ...;
+///< waitForSignal(object, SIGNAL(newPackage()));
+void waitForSignal(QObject* object, const char* signal, int maxWaitMilliSeconds = 5000)
+{
+	QTimer* timer = new QTimer;
+	timer->setInterval(maxWaitMilliSeconds);
+	timer->start();
+
+	QEventLoop loop;
+	QObject::connect(timer, SIGNAL(timeout()), &loop, SLOT(quit()));
+	QObject::connect(object, signal, &loop, SLOT(quit()));
+
+	// Execute the event loop here, now we will wait here until the given signal is emitted
+	// which in turn will trigger event loop quit.
+	loop.exec();
+}
+
+
+TEST_CASE("File should be read and sent once", "[video][unit]")
+{
 
 	//import volume
 	QString filename = cx::DataLocations::getTestDataPath()+ "/testing/TubeSegmentationFramework/Default.mhd";
 	REQUIRE(QFile::exists(filename));
 
 	//make imagestreamer
-	StringMap args;
+	cx::StringMap args;
 	args["filename"] = filename;
 	args["type"] = "MHDFile";
 	args["singleshot"] = "true";
 
-	ImageStreamerPtr imagestreamer(new MHDImageStreamer());
+	cx::ImageStreamerPtr imagestreamer(new cx::MHDImageStreamer());
 	imagestreamer->initialize(args);
 	REQUIRE(imagestreamer);
 
 	TestSenderPtr sender(new TestSender());
 
 	CHECK(imagestreamer->startStreaming(sender));
-	imagestreamer->stopStreaming();
 
-	//check that volume is sent from imagestreamer
-	PackagePtr package = sender->getPackage();
+	QObject* object = sender.get();
+	waitForSignal(object, SIGNAL(newPackage()));
+
+	//check that volume is sent from imagestreamer to the sender
+	cx::PackagePtr package = sender->getPackage();
 	REQUIRE(package);
 	ssc::ImagePtr image = package->mImage;
 	REQUIRE(image);
+
+	waitForSignal(object, SIGNAL(newPackage()));
+
+	imagestreamer->stopStreaming();
+}
+
+TEST_CASE("File should be read and slices with a given interval", "[video][unit]")
+{
+	//import volume
+	QString filename = cx::DataLocations::getTestDataPath()+ "/testing/TubeSegmentationFramework/Default.mhd";
+	REQUIRE(QFile::exists(filename));
+
+	//make imagestreamer
+	cx::StringMap args;
+	args["filename"] = filename;
+	args["type"] = "MHDFile";
+
+	cx::ImageStreamerPtr imagestreamer(new cx::MHDImageStreamer());
+	imagestreamer->initialize(args);
+	REQUIRE(imagestreamer);
+
+	TestSenderPtr sender(new TestSender());
+
+	CHECK(imagestreamer->startStreaming(sender));
+
+	QObject* object = sender.get();
+	waitForSignal(object, SIGNAL(newPackage()));
+
+	//check that volume is sent from imagestreamer to the sender
+	cx::PackagePtr package = sender->getPackage();
+	REQUIRE(package);
+	ssc::ImagePtr image = package->mImage;
+	REQUIRE(image);
+
+	waitForSignal(object, SIGNAL(newPackage()));
+
+	//check that volume is sent from imagestreamer to the sender
+	cx::PackagePtr packageA = sender->getPackage();
+	REQUIRE(packageA);
+	ssc::ImagePtr imageA = package->mImage;
+	REQUIRE(imageA);
+
+	imagestreamer->stopStreaming();
 }
 
 
