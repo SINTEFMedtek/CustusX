197f9c4aebd2f9c02749b4babeadc5e4df0815a5
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-05-28 13:11:35 +0200

    Refactored a test.
    Former-commit-id: 00c818f87627ae2b7aedabc1fe3f167175ef7c82

--- source/gui/testing/cxtestTestVideoConnectionWidget.cpp
+++ source/gui/testing/cxtestTestVideoConnectionWidget.cpp
@@ -8,41 +8,44 @@
 #include "cxVideoService.h"
 #include "cxVideoConnectionManager.h"
 #include "cxtestSignalListener.h"
-#include "cxDataLocations.h"
 
 namespace cxtest
 {
 
 TestVideoConnectionWidget::TestVideoConnectionWidget() :
 		VideoConnectionWidget(NULL)
-{}
+{
+}
 
-bool TestVideoConnectionWidget::startStopServer()
+bool TestVideoConnectionWidget::canStream(QString filename)
 {
 	this->show();
 	QTest::qWaitForWindowShown(this);
 
-	QString filename = cx::DataLocations::getTestDataPath()+"/testing/TubeSegmentationFramework/Default.mhd";
+	this->setupWidgetToRunDummyMhdStreamer(filename);
 
-	QString connectionMethod("Direct Link");
-	mConnectionSelector->setValue(connectionMethod);
-	QString connectionArguments("--type MHDFile --filename "+filename);
-	mDirectLinkArguments->addItem(connectionArguments);
-	mDirectLinkArguments->setCurrentIndex(mDirectLinkArguments->findText(connectionArguments));
 	QTest::mouseClick(mConnectButton, Qt::LeftButton); //connect
 
-	bool signalArrived = waitForSignal(this->getConnection().get(), SIGNAL(connected(bool)));
-	bool connected = this->getConnection()->isConnected();
-
+	bool videoConnectionConnected = waitForSignal(this->getConnection().get(), SIGNAL(connected(bool)));
 	bool activeVideoSourceChanged = waitForSignal(cx::videoService(), SIGNAL(activeVideoSourceChanged()));
-
 	ssc::VideoSourcePtr stream = cx::videoService()->getActiveVideoSource();
-	bool newFrame = waitForSignal(stream.get(), SIGNAL(newFrame()));
-	bool isStreaming = stream->isStreaming();
+	bool videoSourceReceivedNewFrame = waitForSignal(stream.get(), SIGNAL(newFrame()));
+	bool canStream = stream->isStreaming();
 
 	QTest::mouseClick(mConnectButton, Qt::LeftButton); //disconnect
+
 	this->close();
-	return isStreaming;
+
+	return canStream;
+}
+
+void TestVideoConnectionWidget::setupWidgetToRunDummyMhdStreamer(QString filename)
+{
+	QString connectionMethod("Direct Link");
+	mConnectionSelector->setValue(connectionMethod);
+	QString connectionArguments("--type MHDFile --filename " + filename);
+	mDirectLinkArguments->addItem(connectionArguments);
+	mDirectLinkArguments->setCurrentIndex(mDirectLinkArguments->findText(connectionArguments));
 }
 
 } /* namespace cxtest */
