be8faaf20b0de9b36ca8e1752cccfdb1bc3721f0
Christian <christian.askeland@sintef.no>
2013-05-31 11:03:45 +0200

    added timing to jenkins scripts
    Former-commit-id: 7eb0b65a7f7530ee95eca56247673bc39b6ddb24

--- install/Shared/script/cx/cxComponentAssembly.py
+++ install/Shared/script/cx/cxComponentAssembly.py
@@ -18,6 +18,7 @@ import os.path
 import urllib
 import getpass
 import platform
+import time
 
 from cxShell import *
 import cxInstallData
@@ -44,6 +45,8 @@ class LibraryAssembly:
                      cxComponents.CustusX3(),
                      cxComponents.CustusX3Data()
                      ]
+        self._startTime = time.time()
+
     def getLibnames(self):
         return [lib.name() for lib in self.libraries]
     
@@ -96,11 +99,19 @@ class LibraryAssembly:
             method()
             
     def printHeader(self, caption, level=3):
+        elapsed = "%.1fs" % (time.time() -self._startTime)
         if level < 3:
             self.printLine()
         else:
             print ''
-        print '%s%s' % ('='*20, (" %s "%caption).ljust(60, '='))
+
+        # create a line, 80 wide, containing caption and elapsed        
+        part1 ='='*20
+        part2 = (" %s "%caption).ljust(50, '=')
+        part3 = (" %s "%elapsed).ljust(10, '=')
+        print '%s%s%s' % (part1, part2, part3)
+
+        #print '%s%s%s' % ('='*20, (" %s "%caption).ljust(50, '='), (" %s "%elapsed).ljust(10, '='))
         for i in range(3-level):
             self.printLine()
             
