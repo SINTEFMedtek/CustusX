bc1812d0da914b94ef1ee702134adcf3e6f04136
Christian <christian.askeland@sintef.no>
2013-06-02 20:48:49 +0200

    work on install test
    Former-commit-id: 47478daad8cfa4565ddd40b26949bdf89a4b5acf

---
+++ install/Shared/script/cxExperimentalTesting.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+#####################################################
+# Unix jenkins script
+# Author: Christian Askeland, SINTEF Medical Technology
+# Date:   2013.05.16
+#
+# Description:
+#
+#   Continous integration build: run every time the repo changes.
+#   Experimental!
+#   Build part of jenkins CI
+#
+#       Download, build, and test CustusX
+#       Publish unit tests
+#
+#
+#####################################################
+
+import logging
+import time    
+import subprocess
+import sys
+import argparse        
+
+from cx.cxShell import *
+import cx.cxInstallData
+import cx.cxComponents
+import cx.cxComponentAssembly
+import cx.cxCustusXBuilder
+import cx.cxJenkinsBuildScriptBase
+
+class Controller(cx.cxJenkinsBuildScriptBase.JenkinsBuildScriptBase):
+    '''
+    '''
+    def __init__(self):
+        ''
+        super(Controller, self).__init__()
+
+        data = self.cxBuilder.assembly.controlData        
+        data.setBuildType("Release")
+
+    def getDescription(self):                  
+        return 'Jenkins script for build, test and deployment of CustusX and dependents.'
+       
+    def run(self):
+        #self.cxBuilder.buildAllComponents()
+        #self.cxBuilder.clearTestData()
+        #self.cxBuilder.runAllTests()
+        
+        #self.cxBuilder.createInstallerPackage()  
+        self.cxBuilder.installPackage()
+        self.cxBuilder.testInstallation()
+        self.cxBuilder.runIntegrationTests()
+        self.cxBuilder.finish()
+                     
+
+if __name__ == '__main__':
+    controller = Controller()
+    controller.run()
