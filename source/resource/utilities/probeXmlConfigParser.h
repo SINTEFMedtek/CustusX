5f8c7b16ac14f5ab88e22c06b943fe3707df1470
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-06-04 15:18:51 +0200

    Added test for cxProbe, including ProbeXmlConfigParserMoc
    Former-commit-id: b8710bd1b9ca29c7c253b9a03551aabe29a1a17e

--- source/resource/utilities/probeXmlConfigParser.h
+++ source/resource/utilities/probeXmlConfigParser.h
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <vector>
+#include <boost/shared_ptr.hpp>
 #include <QDomDocument>
 #include "sscXmlOptionItem.h"
 
@@ -17,7 +18,8 @@
  * \brief Class that works as a interface to a ProbeCalibConfigs.xml
  *
  * \date 11. mai 2010
- * \\author jbake
+ * \author jbake
+ * \author Ole Vegard Solberg, SINTEF
  */
 class ProbeXmlConfigParser
 {
@@ -75,79 +77,18 @@ public:
     bool isEmpty(){return mEmpty;};
   };
 
-  ProbeXmlConfigParser(QString& pathToXml); ///< opens the file and reads it onto the QDomDocument
-  ~ProbeXmlConfigParser();
+  //ProbeXmlConfigParser(QString& pathToXml) {}; ///< opens the file and reads it onto the QDomDocument
+  //~ProbeXmlConfigParser() {};
+  virtual QString getFileName() = 0;
+  virtual void removeConfig(QString scanner, QString probe, QString rtsource, QString configId) = 0;
+  virtual void saveCurrentConfig(Configuration config) = 0;
+  virtual QStringList getScannerList() = 0; ///< get a list of all scanner in the xml
+  virtual QStringList getProbeList(QString scanner) = 0; ///< get a list of all probes for that scanner
+  virtual QStringList getRtSourceList(QString scanner, QString probe) = 0; ///< get a list of rt sources for that scanner/probe combo
+  virtual QStringList getConfigIdList(QString scanner, QString probe, QString rtSource) = 0; ///< get a list of config ids for that scanner/probe/rsource combo
+  virtual Configuration getConfiguration(QString scanner, QString probe, QString rtsource, QString configId) = 0; ///< get a easy-to-work-with struct of a specific config
 
-	void removeConfig(QString scanner, QString probe, QString rtsource, QString configId);
-	void saveCurrentConfig(Configuration config);
-
-  QStringList getScannerList(); ///< get a list of all scanner in the xml
-  QStringList getProbeList(QString scanner); ///< get a list of all probes for that scanner
-  QStringList getRtSourceList(QString scanner, QString probe); ///< get a list of rt sources for that scanner/probe combo
-  QStringList getConfigIdList(QString scanner, QString probe, QString rtSource); ///< get a list of config ids for that scanner/probe/rsource combo
-  
-  Configuration getConfiguration(QString scanner, QString probe, QString rtsource, QString configId); ///< get a easy-to-work-with struct of a specific config
-
-protected:
-//  template<class TYPE> bool readTextNode(TYPE* retval, QDomNode parent, QString name);
-//  template<> bool readTextNode<QString>(QString* retval, QDomNode parent, QString name);
-
-
-  QList<QDomNode> getScannerNodes(QString scanner = "ALL"); ///< get a list of ALL scanner nodes or just the one you are looking for
-  QList<QDomNode> getProbeNodes(QString scanner, QString probe = "ALL"); ///< get a list of ALL probenodes for that scanner, or just the one you are looking for
-  QList<QDomNode> getRTSourceNodes(QString scanner, QString probe, QString rtSource="ALL"); ///< get a list of ALL rtsourcenodes for that scanner/probe combo, or just the one you are looking for
-  QList<QDomNode> getConfigNodes(QString scanner, QString probe, QString rtsource, QString config="ALL"); ///< get a list of ALL confignodes for that scanner/probe/rtsource combo, or just the one you are looking for
-  
-  QList<QDomNode> nodeListToListOfNodes(QDomNodeList list); ///< converts a QDomNodeList to a QList<QDomNode> 
-  void addTextElement(QDomElement parent, QString element, QString text);
-
-//  QDomDocument mDomDoc; ///< the dom document to parse
-  ssc::XmlOptionFile mFile;
 };
-
-
-//------------------------------------------------------------------------------
-/*
-http://stackoverflow.com/questions/1709283/how-can-i-sort-a-coordinate-list-for-a-rectangle-counterclockwise
-
-I need to sort a coordinate list for a rectangle counterclockwise, and make the north-east corner the first coordinate. 
-
-solution seems pretty straightforward:
-
->>> import math
->>> mlat = sum(x['lat'] for x in l) / len(l)
->>> mlng = sum(x['lng'] for x in l) / len(l)
->>> def algo(x):
-    return (math.atan2(x['lat'] - mlat, x['lng'] - mlng) + 2 * math.pi) % 2*math.pi
-
->>> l.sort(key=algo)
-basically, algo normalises the input into the [0, 2pi] space and it would be naturally sorted "counter-clockwise".
-*/
-
-//------------------------------------------------------------------------------
-class Angular_less
-{
-public:
-  Angular_less(ProbeXmlConfigParser::ColRowPair center) : mCenter(center)
-  {
-  }
-  
-  bool operator()(const ProbeXmlConfigParser::ColRowPair& rhs, const ProbeXmlConfigParser::ColRowPair& lhs)
-  {
-    return angle(rhs) < angle(lhs);
-  }
-  
-  int angle(ProbeXmlConfigParser::ColRowPair pair1)
-  {
-	double pi = atan(1.0)*4;
-    return fmod(  ( ( atan2((double)(pair1.first - mCenter.first), (double)(pair1.second - mCenter.second)) ) + 3*pi/4) , 2*pi);
-  }
-  ProbeXmlConfigParser::ColRowPair mCenter;
-};
-
-/**
-* @}
-*/
-
+typedef boost::shared_ptr<ProbeXmlConfigParser> ProbeXmlConfigParserPtr;
 
 #endif /* PROBEXMLCONFIGPARSER_H_ */
