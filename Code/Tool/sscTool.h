e05f485305436e17778049402377d36aa56fce0e
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-06-05 13:15:11 +0200

    Split sscProbe.h out from sscTool.h
    Former-commit-id: 8a74828dd4fe602e4d29f79f0e3c43137949ccae

--- Code/Tool/sscTool.h
+++ Code/Tool/sscTool.h
@@ -24,80 +24,17 @@
 #include <map>
 #include <set>
 #include <boost/shared_ptr.hpp>
-#include <boost/weak_ptr.hpp>
 #include <QObject>
 #include "vtkForwardDeclarations.h"
 #include "sscTransform3D.h"
-#include "sscProbeData.h"
 #include "sscIndent.h"
 #include "sscCoordinateSystemHelpers.h"
+#include "sscProbe.h"
 
 namespace ssc
 {
 typedef std::map<double, Transform3D> TimedTransformMap;
 typedef boost::shared_ptr<TimedTransformMap> TimedTransformMapPtr;
-typedef boost::shared_ptr<class Probe> ProbePtr;
-typedef boost::shared_ptr<class VideoSource> VideoSourcePtr;
-typedef boost::shared_ptr<class ProbeSector> ProbeSectorPtr;
-typedef boost::weak_ptr<class Probe> ProbeWeakPtr;
-
-/**\brief US Probe interface.
- *
- * Available from Tool when Tool is a Probe.
- *
- * \ingroup sscTool
- */
-class Probe: public QObject
-{
-	Q_OBJECT
-public:
-	virtual ~Probe()
-	{
-	}
-	virtual bool isValid() const = 0;
-//	virtual ProbeData getData() const = 0;
-//	virtual ProbeSectorPtr getSector() = 0;
-//	virtual VideoSourcePtr getRTSource() const = 0;
-
-	virtual void addXml(QDomNode& dataNode) = 0;
-	virtual void parseXml(QDomNode& dataNode) = 0;
-
-	virtual QStringList getConfigIdList() const = 0;
-	virtual QString getConfigName(QString uid) = 0;
-	virtual QString getConfigId() const = 0;
-	virtual QString getConfigurationPath() const = 0;
-
-	virtual void setConfigId(QString uid) = 0;
-	virtual void setTemporalCalibration(double val) = 0;
-	virtual void setSoundSpeedCompensationFactor(double val) = 0;
-	virtual void setData(ssc::ProbeData probeSector, QString configUid="") = 0;
-	virtual void setRTSource(ssc::VideoSourcePtr source) = 0;
-	virtual void removeRTSource(ssc::VideoSourcePtr source) = 0;
-
-	virtual void setActiveStream(QString uid) = 0;
-	virtual QString getActiveStream() const = 0;
-
-	/**
-	  * Return a list of all available video source.
-	  * The default is one with uid=='active'.
-	  */
-	virtual QStringList getAvailableVideoSources() = 0;
-	/**
-	  * Return a VideoSource for the given uid. Use 'active' to get the default stream.
-	  */
-	virtual VideoSourcePtr getRTSource(QString uid = "active") const = 0;
-	/**
-	  * Return a ProbeData for the given uid. Use 'active' to get the default.
-	  */
-	virtual ProbeData getData(QString uid = "active") const = 0;
-	/**
-	  * Return a ProbeSectorPtr for the given uid. Use 'active' to get the default.
-	  */
-	virtual ProbeSectorPtr getSector(QString uid = "active") = 0;
-
-signals:
-	void sectorChanged();
-};
 
 /**\brief Interface to a tool,
  * i.e. a pointer, US probe or similar.
