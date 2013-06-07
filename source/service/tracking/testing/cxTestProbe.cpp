7796077553d16c173df1119cb24ef2e462ff101c
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-06-07 12:29:12 +0200

    Made cxTestProbe use more private parameters
    Former-commit-id: beae88c8524192d697b8975ede77fc4488287cb9

--- source/service/tracking/testing/cxTestProbe.cpp
+++ source/service/tracking/testing/cxTestProbe.cpp
@@ -29,7 +29,7 @@ void TestProbe::tearDown()
 //Disabled for now. Test will output a warning. Use the test below with the Mock XmlParser instead
 void TestProbe::testConstructorWithDefaultXmlParser()
 {
-	mProbe = cx::Probe::New("TestProbe", "TestScanner");
+	mProbe = cx::Probe::New(mProbeName, mScannerName);
 
 	CPPUNIT_ASSERT(mProbe);
 	CPPUNIT_ASSERT_MESSAGE("Probe's config id is not empty. It should be since the test probe setup is not present in the config id list",
@@ -42,14 +42,14 @@ void TestProbe::testConstructorWithMockXmlParser()
 {
 	CPPUNIT_ASSERT(mProbe);
 	CPPUNIT_ASSERT(!mProbe->getConfigId().isEmpty());
-	CPPUNIT_ASSERT(mProbe->getConfigId().compare("TestScanner TestProbe DummySource") == 0);
+	CPPUNIT_ASSERT(mProbe->getConfigId().compare(mScannerName + " " + mProbeName + " " + mDefaultRtSourceName) == 0);
 	CPPUNIT_ASSERT(mProbe->isValid());
 }
 
 void TestProbe::testDigitalVideoSetting()
 {
 	CPPUNIT_ASSERT(!mProbe->isUsingDigitalVideo());
-	CPPUNIT_ASSERT(mProbe->getRtSourceName().compare("DummySource") == 0);
+	CPPUNIT_ASSERT(mProbe->getRtSourceName().compare(mDefaultRtSourceName) == 0);
 	mProbe->useDigitalVideo(true);
 	CPPUNIT_ASSERT(mProbe->isUsingDigitalVideo());
 	CPPUNIT_ASSERT(mProbe->getRtSourceName().compare("Digital") == 0);
@@ -69,38 +69,39 @@ void TestProbe::testRTSource()
 void TestProbe::testDefaultProbeSector()
 {
 	CPPUNIT_ASSERT(mProbe->getSector());
-	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare("default") == 0);
-	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData().getTemporalCalibration(), 0));
+	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare(mDefaultProbeDataUid) == 0);
+	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData().getTemporalCalibration(), mDefaultTemporalCalibration));
 }
 
 void TestProbe::testCustomProbeSector()
 {
-	mProbe->setProbeSector(this->createProbeData("TestProbeData"));
-	CPPUNIT_ASSERT(mProbe->getSector("TestProbeData"));
-	CPPUNIT_ASSERT(mProbe->getProbeData("TestProbeData").getUid().compare("TestProbeData") == 0);
-	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData("TestProbeData").getTemporalCalibration(), 1000.5));
+	mProbe->setProbeSector(this->createProbeData());
+	CPPUNIT_ASSERT(mProbe->getSector(mProbeDataUid));
+	CPPUNIT_ASSERT(mProbe->getProbeData(mProbeDataUid).getUid().compare(mProbeDataUid) == 0);
+	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData(mProbeDataUid).getTemporalCalibration(), mTemporalCalibration));
 
 }
 
 void TestProbe::testSetActiveStreamToCustomProbeSector()
 {
-	mProbe->setProbeSector(this->createProbeData("TestProbeData"));
-	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare("default") == 0);
+	mProbe->setProbeSector(this->createProbeData());
+	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare(mDefaultProbeDataUid) == 0);
 
-	mProbe->setActiveStream("TestProbeData");
-	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare("TestProbeData") == 0);
-	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData().getTemporalCalibration(), 1000.5));
+	mProbe->setActiveStream(mProbeDataUid);
+	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare(mProbeDataUid) == 0);
+	CPPUNIT_ASSERT(ssc::similar(mProbe->getProbeData().getTemporalCalibration(), mTemporalCalibration));
 
-	mProbe->setActiveStream("default");
+	mProbe->setActiveStream(mDefaultProbeDataUid);
 	this->testDefaultProbeSector();
 }
 
 void TestProbe::testSetActiveStream()
 {
-	CPPUNIT_ASSERT(mProbe->getActiveStream().compare("default") == 0); //Name set to "default" in ssc::ProbeData()
-	mProbe->setActiveStream("TestStream");
-	CPPUNIT_ASSERT(mProbe->getActiveStream().compare("TestStream") == 0);
-	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare("TestStream") == 0);
+	CPPUNIT_ASSERT(mProbe->getActiveStream().compare(mDefaultProbeDataUid) == 0);
+	QString streamName = "TestStream";
+	mProbe->setActiveStream(streamName);
+	CPPUNIT_ASSERT(mProbe->getActiveStream().compare(streamName) == 0);
+	CPPUNIT_ASSERT(mProbe->getProbeData().getUid().compare(streamName) == 0);
 }
 
 //The setup time of each test is 1.35 sec, so merging 7 tests into 1 saves 8 sec
@@ -118,17 +119,30 @@ void TestProbe::runAllTests()
 
 void TestProbe::createTestProbe()
 {
+	this->createParameters();
 	QString xmlFileName = "testXmlFileName";
 	ProbeXmlConfigParserPtr mXml;
 	mXml.reset(new ProbeXmlConfigParserMock(xmlFileName));
-	mProbe = cx::Probe::New("TestProbe", "TestScanner", mXml);
+	mDefaultRtSourceName = mXml->getRtSourceList(mProbeName, mScannerName)[0];
+	mProbe = cx::Probe::New(mProbeName, mScannerName, mXml);
 }
 
-ssc::ProbeData TestProbe::createProbeData(QString uid)
+void TestProbe::createParameters()
+{
+	mProbeName = "TestProbe";
+	mScannerName = "TestScanner";
+	ssc::ProbeData probeData;
+	mDefaultProbeDataUid = probeData.getUid(); //Uid set to "default" in ssc::ProbeData()
+	mDefaultTemporalCalibration = probeData.getTemporalCalibration();
+	mProbeDataUid = "TestProbeData";
+}
+
+ssc::ProbeData TestProbe::createProbeData()
 {
 	ssc::ProbeData probeData = mProbe->getProbeData();
-	probeData.setUid(uid);
-	probeData.setTemporalCalibration(1000.5);
+	probeData.setUid(mProbeDataUid);
+	mTemporalCalibration = 1000.5;
+	probeData.setTemporalCalibration(mTemporalCalibration);
 	return probeData;
 }
 
