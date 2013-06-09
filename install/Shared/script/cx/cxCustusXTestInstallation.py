1d95b717fb920cb90339a79f676a213fa39fe582
Christian <christian.askeland@sintef.no>
2013-06-09 19:42:50 +0200

    bug in test script
    Former-commit-id: fc6a97c536a53323a79ed6f4e6c6d7ee56e3886e

--- install/Shared/script/cx/cxCustusXTestInstallation.py
+++ install/Shared/script/cx/cxCustusXTestInstallation.py
@@ -138,14 +138,15 @@ class CustusXTestInstallation:
 
     def _runApplicationForDuration(self, application, timeout):
         ''
-        p = subprocess.Popen(application, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=os.path.dirname(application)) 
+        p = subprocess.Popen(application, shell=True, cwd=os.path.dirname(application)) 
+        #p = subprocess.Popen(application, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=os.path.dirname(application)) 
         time.sleep(timeout)
         retcode = p.poll()
         self.assertTrue(retcode==None or retcode==0, 'Process %s has been running successfully for %is' % (application, timeout))
         if retcode==None:
             p.kill()
         p.wait()
-        print p.stdout.read() # drop this: causes app to hang
+        #print p.stdout.read() # drop this: causes app to hang
         print 'Successfully ran %s for %is' % (application, timeout)
         # also consider otool -L
         return
