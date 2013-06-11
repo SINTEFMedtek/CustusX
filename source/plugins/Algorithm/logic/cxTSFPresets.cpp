51ffe73193a0c1167d1e29951d84ab1830e18891
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-06-11 10:24:31 +0200

    Repaired TSFPreset test.
    Former-commit-id: 80ef8d56f83906fb905c4377326706cb62242b5b

--- source/plugins/Algorithm/logic/cxTSFPresets.cpp
+++ source/plugins/Algorithm/logic/cxTSFPresets.cpp
@@ -7,14 +7,18 @@
 #include "sscMessageManager.h"
 #include "cxDataLocations.h"
 
-namespace cx {
+namespace cx
+{
 
 TSFPresets::TSFPresets() :
-	Presets(ssc::XmlOptionFile("Preset"), ssc::XmlOptionFile("Custom"))
+		Presets(ssc::XmlOptionFile("Preset"), ssc::XmlOptionFile("Custom"))
 {
-	mPresetPath = cx::DataLocations::getTSFPath()+"/parameters";
+	mPresetPath = cx::DataLocations::getTSFPath() + "/parameters";
 	this->loadPresetsFromFiles();
 }
+
+TSFPresets::~TSFPresets()
+{}
 
 QDomElement TSFPresets::createPresetElement(QString name, std::map<QString, QString>& parameters)
 {
@@ -26,7 +30,8 @@ QDomElement TSFPresets::createPresetElement(QString name, std::map<QString, QStr
 	QDomElement retval = doc.createElement("Preset");
 	retval.setAttribute("name", name);
 	std::map<QString, QString>::iterator it;
-	for(it = parameters.begin(); it != parameters.end(); ++it){
+	for (it = parameters.begin(); it != parameters.end(); ++it)
+	{
 		if (!ignoreParametersWithName.contains(it->first))
 			retval.setAttribute(it->first, it->second);
 	}
@@ -40,63 +45,29 @@ void TSFPresets::save()
 	for (int i = 0; i < presetNodes.count(); ++i)
 	{
 		QDomNode node = presetNodes.at(i);
-		if(!node.isElement())
+		if (!node.isElement())
 			break;
 		QDomElement element = node.toElement();
 
 		QString folderPath;
 		std::map<QString, QString> parameters;
 		QDomNamedNodeMap attributes = element.attributes();
-        for (int i = 0; i < attributes.count(); ++i) {
-            QDomNode attribute = attributes.item(i);
-            if(attribute.nodeName() != "name")
-            	parameters[attribute.nodeName()] = attribute.nodeValue();
-            if(attribute.nodeName() == "centerline-method")
-            	folderPath = mPresetPath + "/centerline-gpu/";
-            	//folderPath = mPresetPath + "/centerline-"+attribute.nodeValue()+"/";
-        }
+		for (int i = 0; i < attributes.count(); ++i)
+		{
+			QDomNode attribute = attributes.item(i);
+			if (attribute.nodeName() != "name")
+				parameters[attribute.nodeName()] = attribute.nodeValue();
+			if (attribute.nodeName() == "centerline-method")
+				folderPath = mPresetPath + "/centerline-gpu/";
+		}
 		this->saveFile(folderPath, parameters);
 	}
 }
 
 void TSFPresets::remove()
 {
-//	QStringList keep;
-//	QDomDocument doc = this->getCustomFile().getDocument();
-//	QDomNodeList presetNodes = doc.elementsByTagName("Preset");
-//	for (int i = 0; i < presetNodes.count(); ++i)
-//	{
-//		QDomNode node = presetNodes.at(i);
-//		if(!node.isElement())
-//			break;
-//		QDomElement element = node.toElement();
-//
-//		QString folderPath;
-//		QString fileName;
-////		std::map<QString, QString> parameters;
-//		QDomNamedNodeMap attributes = element.attributes();
-//        for (int i = 0; i < attributes.count(); ++i) {
-//            QDomNode attribute = attributes.item(i);
-//            if(attribute.nodeName() != "name")
-//            	fileName = attribute.nodeValue();
-//            if(attribute.nodeName() == "centerline-method")
-//            	folderPath = mPresetPath + "/centerline-"+attribute.nodeValue()+"/";
-//        }
-//        keep << folderPath+fileName;
-//	}
-
-	//TODO
-//	QStringList split = mLastCustomPresetRemoved.split(": ");
-	QString folderPath = mPresetPath+"/centerline-gpu/"+mLastCustomPresetRemoved;
-//	foreach(QString string, split)
-//	{
-//		std::cout << string.toStdString() << std::endl;
-//		if(string.contains("centerline-"))
-//			folderPath+=string;
-//	}
-
+	QString folderPath = mPresetPath + "/centerline-gpu/" + mLastCustomPresetRemoved;
 	this->deleteFile(folderPath);
-
 }
 
 QStringList TSFPresets::generatePresetList(QString tag)
@@ -104,7 +75,7 @@ QStringList TSFPresets::generatePresetList(QString tag)
 	this->loadPresetsFromFiles();
 	QStringList retval;
 	std::map<QString, QString>::iterator it;
-	for(it = mPresetsMap.begin(); it != mPresetsMap.end(); ++it)
+	for (it = mPresetsMap.begin(); it != mPresetsMap.end(); ++it)
 	{
 		retval << it->first;
 	}
@@ -113,180 +84,135 @@ QStringList TSFPresets::generatePresetList(QString tag)
 
 void TSFPresets::loadPresetsFromFiles()
 {
-	mPresetsMap.clear();
-
-	QDir parametersDir(mPresetPath+"/centerline-gpu");
-	if(!parametersDir.exists())
-		return;
-
-	QFileInfoList fileInfoList = parametersDir.entryInfoList(QDir::Files);
-
-	foreach(QFileInfo info, fileInfoList)
-	{
-		QString name = info.baseName();
-		mPresetsMap[name] = info.absoluteFilePath();
-	}
+	this->getPresetsNameAndPath();
 	this->convertToInternalFormat(mPresetsMap);
 }
 
-void TSFPresets::convertToInternalFormat(std::map<QString, QString>& presets)
+void TSFPresets::convertToInternalFormat(std::map<QString,QString>& presets)
 {
-	std::map<QString, QString>::iterator it;
-	for(it = presets.begin(); it != presets.end(); ++it)
-	{
-		std::map<QString, QString> params = this->readFile(it->second);
-		QDomElement preset = TSFPresets::createPresetElement(it->first, params);
-		Presets::addCustomPreset(preset);
-	}
+	std::map<QString,QString>::iterator it;
+	for (it = presets.begin();it != presets.end();++it)
+		this->addAsCustomPreset(it);
+
 }
 
 std::map<QString, QString> TSFPresets::readFile(QString& filePath)
 {
 	std::map<QString, QString> retval;
-
-	if(!QFile::exists(filePath))
+	if (!QFile::exists(filePath))
 	{
 		ssc::messageManager()->sendError("File does not exists: " + filePath);
 		return retval;
 	}
-
 	QFile file(filePath);
 	if (file.open(QFile::ReadOnly))
 	{
-	    QTextStream in(&file);
-	    while (!in.atEnd())
-	    {
-	        QString line = in.readLine();
-	        QStringList lineItems = line.split(" ");
-	        if(lineItems.count() == 2)
+		QTextStream in(&file);
+		while (!in.atEnd())
+		{
+			QString line = in.readLine();
+			QStringList lineItems = line.split(" ");
+			if (lineItems.count() == 2)
 				retval[lineItems.at(0)] = lineItems.at(1);
-	    }
+		}
 	}
+
 	return retval;
 }
 
 void TSFPresets::saveFile(QString folderpath, std::map<QString, QString> parameters)
 {
-
-	QFile file(folderpath+mLastCustomPresetAdded);
+	QFile file(folderpath + mLastCustomPresetAdded);
 	QString customPresetName = QFileInfo(file).fileName();
-
-	//-------------------------------------------------------------
-	//Need to add the name of the new file into the parameters file
-	// ..../parameters/parameters
-	//-------------------------------------------------------------
-	this->editParameterFile(customPresetName, true);
-
-	//-------------------------------------------------------------
-	//Save the new parameters file
-	// ..../parameters/<centerline-method>/<preset-name>
-	//-------------------------------------------------------------
+//	this->editParameterFile(customPresetName, true);
 	QTextStream outPresetFile;
-	if (!file.open(QFile::WriteOnly)) //TODO truncate???
+	if (!file.open(QFile::WriteOnly))
 	{
 		ssc::messageManager()->sendError("Could not open the file " + file.fileName() + " for writing.");
 		return;
 	}
-
 	outPresetFile.setDevice(&file);
 	std::map<QString, QString>::iterator it;
-	for(it=parameters.begin(); it != parameters.end(); ++it){
+	for (it = parameters.begin(); it != parameters.end(); ++it)
+	{
 		QString line = it->first + " " + it->second;
 		outPresetFile << line << "\n";
 	}
 	outPresetFile << flush;
-
 	file.close();
-
 }
 
-void TSFPresets::editParameterFile(QString name, bool addNotRemove)
-{
-	QString parametersFile = mPresetPath+"/parameters";
-	QFile paramFile(parametersFile);
-	if(!paramFile.exists())
-	{
-		ssc::messageManager()->sendError("The file " + parametersFile + " does not exist.");
-		return;
-	}
-
-	//Read the content
-	if (!paramFile.open(QFile::ReadOnly))
-	{
-		ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for reading.");
-		return;
-	}
-	QTextStream inText;
-	inText.setDevice(&paramFile);
-	QString allText = inText.readAll();
-	paramFile.close();
-
-	if(addNotRemove)
-	{
-		if(!allText.contains(name, Qt::CaseInsensitive))
-		{
-			QString searchString = "parameters str none";
-			int index = allText.indexOf(searchString);
-			index += searchString.size();
-			allText.insert(index, " "+name);
-//
-//			//Write the new content
-//			if (!paramFile.open(QFile::WriteOnly | QFile::Truncate))
-//			{
-//				ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for writing.");
-//				return;
-//			}
-//			QTextStream outParametersFile(&paramFile);
-//			outParametersFile << allText;
-//			outParametersFile << flush;
-//			paramFile.close();
-		}
-	}else
-	{
-		allText.replace(" "+name, "");
-
-//		//Write the new content
-//		if (!paramFile.open(QFile::WriteOnly | QFile::Truncate))
+//void TSFPresets::editParameterFile(QString name, bool addNotRemove)
+//{
+//	QString parametersFile = mPresetPath + "/parameters";
+//	QFile paramFile(parametersFile);
+//	if (!paramFile.exists())
+//	{
+//		ssc::messageManager()->sendError("The file " + parametersFile + " does not exist.");
+//		return;
+//	}
+//	if (!paramFile.open(QFile::ReadOnly))
+//	{
+//		ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for reading.");
+//		return;
+//	}
+//	QTextStream inText;
+//	inText.setDevice(&paramFile);
+//	QString allText = inText.readAll();
+//	paramFile.close();
+//	if (addNotRemove)
+//	{
+//		if (!allText.contains(name, Qt::CaseInsensitive))
 //		{
-//			ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for writing.");
-//			return;
+//			QString searchString = "parameters str none";
+//			int index = allText.indexOf(searchString);
+//			index += searchString.size();
+//			allText.insert(index, " " + name);
 //		}
-//		QTextStream outParametersFile(&paramFile);
-//		outParametersFile << allText;
-//		outParametersFile << flush;
-//		paramFile.close();
-	}
-	//Write the new content
-	if (!paramFile.open(QFile::WriteOnly | QFile::Truncate))
-	{
-		ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for writing.");
-		return;
-	}
-	QTextStream outParametersFile(&paramFile);
-	outParametersFile << allText;
-	outParametersFile << flush;
-	paramFile.close();
-}
+//	}
+//	else
+//	{
+//		allText.replace(" " + name, "");
+//	}
+//	if (!paramFile.open(QFile::WriteOnly | QFile::Truncate))
+//	{
+//		ssc::messageManager()->sendError("Could not open the file " + parametersFile + " for writing.");
+//		return;
+//	}
+//	QTextStream outParametersFile(&paramFile);
+//	outParametersFile << allText;
+//	outParametersFile << flush;
+//	paramFile.close();
+//}
 
 void TSFPresets::deleteFile(QString filePath)
 {
-	std::cout << filePath.toStdString() << std::endl;
-
 	QFile file(filePath);
 	QString customPresetName = QFileInfo(file).fileName();
+//	this->editParameterFile(customPresetName, false);
+	if (!file.remove())
+		ssc::messageManager()->sendError("File: " + filePath + " not removed...");
+}
 
-	//-------------------------------------------------------------
-	//Need to remove the name of the new file from the parameters file
-	// ..../parameters/parameters
-	//-------------------------------------------------------------
-	this->editParameterFile(customPresetName, false);
+void TSFPresets::getPresetsNameAndPath()
+{
+	mPresetsMap.clear();
+	QDir parametersDir(mPresetPath + "/centerline-gpu");
+	if (!parametersDir.exists())
+		ssc::messageManager()->sendError("Preset directory "+parametersDir.path()+" not found.");
 
-	//-------------------------------------------------------------
-	//Delete the preset file
-	// ..../parameters/<centerline-method>/<preset-name>
-	//-------------------------------------------------------------
-	if(!file.remove())
-		std::cout << "file: " << filePath.toStdString() << " not removed..." << std::endl;
+	QFileInfoList fileInfoList = parametersDir.entryInfoList(QDir::Files);
+	foreach(QFileInfo info, fileInfoList){
+		QString name = info.baseName();
+		mPresetsMap[name] = info.absoluteFilePath();
+	}
+}
+
+void TSFPresets::addAsCustomPreset(std::map<QString,QString>::iterator it)
+{
+	std::map<QString,QString> params = this->readFile(it->second);
+	QDomElement preset = TSFPresets::createPresetElement(it->first, params);
+	Presets::addCustomPreset(preset);
 }
 
 } /* namespace cx */
--- source/plugins/Algorithm/testing/cxTestTSFPresets.cpp
+++ source/plugins/Algorithm/testing/cxTestTSFPresets.cpp
@@ -15,16 +15,10 @@ void TestTSFPresets::setUp()
 	mFolderPath = cx::DataLocations::getTSFPath()+"/parameters/";
 	mPresetFileName = "CPPUNIT_TEST";
 	mPresetFilePath = mFolderPath+"centerline-gpu/"+mPresetFileName;
-	mParameterFilePath = cx::DataLocations::getTSFPath()+"/parameters/parameters";
-	mBackUpPostFix = "_BackUp";
-
-	mDirty = false;
-	this->backup();
 }
 
 void TestTSFPresets::tearDown()
 {
-	this->restore();
 	cx::LogicManager::shutdown();
 }
 
@@ -39,9 +33,7 @@ void TestTSFPresets::testLoadPresets()
 	CPPUNIT_ASSERT_MESSAGE("List of presets is shorter than expected.", presetList.size() >= 0);
 
 	foreach(QString preset, presetList)
-	{
 		CPPUNIT_ASSERT_MESSAGE("Default preset "+preset.toStdString()+" is defined as default.", !presets->isDefaultPreset(preset));
-	}
 }
 
 void TestTSFPresets::testSaveNewPresets()
@@ -52,15 +44,6 @@ void TestTSFPresets::testSaveNewPresets()
 	CPPUNIT_ASSERT_MESSAGE("Could not locate the newly created preset file: "+mPresetFilePath.toStdString(), QFile::exists(mPresetFilePath));
 
 	CPPUNIT_ASSERT_MESSAGE("New parameter file is empty.", QFile(mPresetFilePath).size() != 0);
-
-	QFile parameters(mParameterFilePath);
-	CPPUNIT_ASSERT_MESSAGE("Could not open parameter file for reading.", parameters.open(QFile::ReadOnly));
-
-	QTextStream in;
-	in.setDevice(&parameters);
-	QString text = in.readAll();
-	parameters.close();
-	CPPUNIT_ASSERT_MESSAGE("New preset name not added to parameter file.", text.contains(mPresetFileName, Qt::CaseInsensitive));
 }
 
 void TestTSFPresets::testDeletePresets()
@@ -69,55 +52,17 @@ void TestTSFPresets::testDeletePresets()
 	this->deletePreset(preset);
 
 	CPPUNIT_ASSERT_MESSAGE("Preset file was not removed when deleted: "+mPresetFilePath.toStdString(), !QFile::exists(mPresetFilePath));
-
-	QFile parameters(mParameterFilePath);
-	CPPUNIT_ASSERT_MESSAGE("Could not open parameter file for reading.", parameters.open(QFile::ReadOnly));
-
-	QTextStream in;
-	in.setDevice(&parameters);
-	QString text = in.readAll();
-	parameters.close();
-	CPPUNIT_ASSERT_MESSAGE("New preset name not removed from parameter file.", !text.contains(mPresetFileName, Qt::CaseInsensitive));
 }
 
 QDomElement TestTSFPresets::createPresetElement(TSFPresetsPtr presets)
 {
-
 	std::map<QString, QString> newPresetMap;
 	newPresetMap["centerline-method"] = "gpu"; //str
 	newPresetMap["centerpoints-only"] = "true"; //bool
 	newPresetMap["radius-min"] = "50.0"; //float/double
 
-	//create xml
 	QDomElement retval = TSFPresets::createPresetElement(mPresetFileName, newPresetMap);
 	return retval;
-}
-
-void TestTSFPresets::backup()
-{
-	if(mDirty)
-		return;
-
-	QFile parameters(mParameterFilePath);
-	parameters.copy(mParameterFilePath+mBackUpPostFix);
-}
-
-void TestTSFPresets::restore()
-{
-	if(!mDirty)
-		return;
-
-	QFile oldParameters(mParameterFilePath);
-	oldParameters.remove();
-
-	QFile parameters(mParameterFilePath+mBackUpPostFix);
-	parameters.rename(mParameterFilePath);
-
-	if(QFile::exists(mPresetFilePath))
-	{
-		QFile newPreset(mPresetFilePath);
-		newPreset.remove();
-	}
 }
 
 TSFPresetsPtr TestTSFPresets::newPreset()
@@ -127,8 +72,6 @@ TSFPresetsPtr TestTSFPresets::newPreset()
 	presets->addCustomPreset(element);
 	presets->save();
 
-	mDirty = true;
-
 	return presets;
 }
 
@@ -136,7 +79,6 @@ void TestTSFPresets::deletePreset(TSFPresetsPtr preset)
 {
 	preset->deleteCustomPreset(mPresetFileName);
 	preset->remove();
-	mDirty = false;
 }
 
 } /* namespace cx */
