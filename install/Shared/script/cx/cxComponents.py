ce3539f989f4fc25670650b78286943ce2bf52ff
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-06-21 10:49:59 +0200

    Updated GEStreamer tests
    Former-commit-id: 09874d61db6e929aab48c041ba7746d666ebf0e5

--- install/Shared/script/cx/cxComponents.py
+++ install/Shared/script/cx/cxComponents.py
@@ -315,7 +315,7 @@ class IGSTK(CppComponent):
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "446"
+        self.mCurrentRevision = "447"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
