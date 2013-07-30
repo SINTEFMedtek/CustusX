761c93edd1b1d9d02c68a45a7609760f5d623680
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-07-30 14:05:06 +0200

    Merged to existing createDummyImageData() functions and moved this into sscImage
    Former-commit-id: ba04a4c9e1787f1eb44da02ebe3837ee5c31a234

--- source/gui/testing/cxtestTestTransferFunctionColorWidget.cpp
+++ source/gui/testing/cxtestTestTransferFunctionColorWidget.cpp
@@ -6,36 +6,6 @@
 
 namespace cxtest {
 
-// Similar to vtkImageDataPtr ApplyLUTToImage2DProxy::createDummyImageData()
-// in file sscSlicedImageProxy.h  <== Reuse code
-vtkImageDataPtr createDummyImageData(int axisSize, int maxVoxelValue)
-{
-	int size = axisSize - 1;//Modify axis size as extent starts with 0, not 1
-	vtkImageDataPtr dummyImageData = vtkImageDataPtr::New();
-	dummyImageData->SetExtent(0, size, 0, size, 0, size);
-	dummyImageData->SetSpacing(1, 1, 1);
-	//dummyImageData->SetScalarTypeToUnsignedShort();
-	dummyImageData->SetScalarTypeToUnsignedChar();
-	dummyImageData->SetNumberOfScalarComponents(1);
-	dummyImageData->AllocateScalars();
-	unsigned char* dataPtr = static_cast<unsigned char*> (dummyImageData->GetScalarPointer());
-
-	//Init voxel colors
-	int minVoxelValue = 0;
-	int numVoxels = size*size*size;
-	for (int i = 0; i < numVoxels; ++i)
-	{
-		int voxelValue = minVoxelValue + i;
-		if (i == numVoxels)
-			dataPtr[i] = maxVoxelValue;
-		else if (voxelValue < maxVoxelValue)
-			dataPtr[i] = voxelValue;
-		else
-			dataPtr[i] = maxVoxelValue;
-	}
-	return dummyImageData;
-}
-
 TestTransferFunctionColorWidget::TestTransferFunctionColorWidget() :
 		TransferFunctionColorWidget(NULL)
 {}
@@ -43,7 +13,7 @@ TestTransferFunctionColorWidget::TestTransferFunctionColorWidget() :
 
 void TestTransferFunctionColorWidget::initWithTestData()
 {
-	vtkImageDataPtr dummyImageData = createDummyImageData(2, 1);
+	vtkImageDataPtr dummyImageData = ssc::Image::createDummyImageData(2, 1);
 	ssc::ImagePtr dummyImage(new ssc::Image("DummyImage", dummyImageData));
 	ssc::ImageTFDataPtr transferFunction;
 	transferFunction = dummyImage->getTransferFunctions3D();
