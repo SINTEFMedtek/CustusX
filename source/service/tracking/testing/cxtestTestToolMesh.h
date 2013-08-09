f5fb07b118c0680254542ca61e956a0059124c79
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-08-09 08:41:32 +0200

    Fixed #826: SW tools STL files do not work with VTK 5.10.1
    Former-commit-id: 4bd4bfa4a90ae66f99b9aa01aeba90edc14ccb42

---
+++ source/service/tracking/testing/cxtestTestToolMesh.h
@@ -0,0 +1,28 @@
+#ifndef CXTESTTESTTOOLMESH_H_
+#define CXTESTTESTTOOLMESH_H_
+
+#include "sscMesh.h"
+#include <QString.h>
+
+namespace cxtest {
+
+/*
+ * TestToolMesh
+ * \brief Helper class for testing tool mesh file
+ * \date Aug 8, 2013
+ * \author Ole Vegard Solberg, SINTEF
+ */
+class TestToolMesh {
+public:
+	TestToolMesh();
+	void setToolPath(QString path);
+	bool canLoadMesh(QString filename);
+private:
+	QString mToolToolPath;
+	QString mCurrentToolPath;
+	QString mMeshFileName;
+	ssc::DataPtr mMesh;
+};
+
+} /* namespace cxtest */
+#endif /* CXTESTTESTTOOLMESH_H_ */
