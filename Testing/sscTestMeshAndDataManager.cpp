b8f0b87f7305131f1f9d17b1ed424414c4738df0
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-08-13 09:59:02 +0200

    Updated failing test
    Former-commit-id: 284593927c4ed95bc1b65da9d57769626796a7d3

--- Testing/sscTestMeshAndDataManager.cpp
+++ Testing/sscTestMeshAndDataManager.cpp
@@ -20,20 +20,14 @@ int main(int argc, char **argv)
 
 	std::cout << "DataFolder: " << ssc::TestUtilities::GetDataRoot() << std::endl;
 
-	QString vtkFileName1 = ssc::TestUtilities::ExpandDataFileName("MetaImage/20070309T105136_MRT1.vtk");
-	QString vtkFileName2 = ssc::TestUtilities::ExpandDataFileName("MetaImage/20070309T102309_MRA.vtk");
+	QString stlFileName1 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/01-117-0329_Planning-Navigator.stl");
 
-	QString stlFileName1 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/5S_joinedpart.STL");
-	QString stlFileName2 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/5S_probeMSector.STL");
-	QString stlFileName3 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/8L_12L Probe.STL");
-	QString stlFileName4 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/Intra_operativ_navigator.STL");
-	QString stlFileName5 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/Planning_navigator.STL");
-	QString stlFileName6 = ssc::TestUtilities::ExpandDataFileName("../Sandbox/Models/Vermon Phase Probe.STL");
-
-	// .vtk
-	//ssc::MeshPtr mesh1 = ssc::DataManager::instance()->loadMesh(vtkFileName1, ssc::mrtPOLYDATA);
-	// .stl
 	ssc::MeshPtr mesh1 = ssc::DataManager::getInstance()->loadMesh(stlFileName1, stlFileName1, ssc::rtSTL);
+	if(!mesh1)
+	{
+		std::cout << "Cannot read mesh" << std::endl;
+		return 1;
+	}
 
 	std::cout << "UID     : " << mesh1->getUid() << std::endl;
 	std::cout << "Name    : " << mesh1->getName() << std::endl;
@@ -50,6 +44,34 @@ int main(int argc, char **argv)
 	std::cout << "numLines  : " << numLines << std::endl;
 	std::cout << "numPolys  : " << numPolys << std::endl;
 	std::cout << "numStrips : " << numStrips << std::endl;
+
+	if(numPoints <= 0)
+	{
+		std::cout << "No points in test mesh" << std::endl;
+		return 1;
+	}
+	if(numPolys <= 0)
+	{
+		std::cout << "No polys in test mesh" << std::endl;
+		return 1;
+	}
+	if(numVerts != 0)
+	{
+		std::cout << "Not expecting verts in test mesh" << std::endl;
+		return 1;
+	}
+	if(numLines != 0)
+	{
+		std::cout << "Not expecting lines in test mesh" << std::endl;
+		return 1;
+	}
+	if(numStrips != 0)
+	{
+		std::cout << "Not expecting strips in test mesh" << std::endl;
+		return 1;
+	}
+
+
 
 
 	//std::vector<QString> names = ssc::DataManager::instance()->getImageNames();
