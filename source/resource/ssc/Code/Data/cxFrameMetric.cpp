aa31b1b66ca11b90322731707f0a4a356155c65d
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-08-26 09:21:00 +0200

    fixed git bug where ssc/../Data were gitignored
    Former-commit-id: b39474d4464247426eecd9834f44439fc9c6eadd

---
+++ source/resource/ssc/Code/Data/cxFrameMetric.cpp
@@ -0,0 +1,123 @@
+#include "cxFrameMetric.h"
+#include "sscBoundingBox3D.h"
+#include "sscTool.h"
+#include "sscToolManager.h"
+#include "sscTypeConversions.h"
+
+namespace cx
+{
+
+ssc::DataPtr FrameMetricReader::load(const QString& uid, const QString& filename)
+{
+	return ssc::DataPtr(new FrameMetric(uid, filename));
+}
+
+FrameMetric::FrameMetric(const QString& uid, const QString& name) :
+		ssc::DataMetric(uid, name),
+		mSpace(ssc::SpaceHelpers::getR()),
+		mFrame(ssc::Transform3D::Identity())
+{
+	mSpaceListener.reset(new ssc::CoordinateSystemListener(mSpace));
+	connect(mSpaceListener.get(), SIGNAL(changed()), this, SIGNAL(transformChanged()));
+}
+
+FrameMetric::~FrameMetric()
+{
+}
+
+void FrameMetric::setFrame(const ssc::Transform3D& rMt)
+{
+	mFrame = rMt;
+}
+
+ssc::Transform3D FrameMetric::getFrame()
+{
+	return mFrame;
+}
+
+ssc::Vector3D FrameMetric::getCoordinate() const
+{
+	ssc::Vector3D point_t = ssc::Vector3D(0,0,0);
+	return mFrame.coord(point_t);
+}
+
+void FrameMetric::setSpace(ssc::CoordinateSystem space)
+{
+	if (space == mSpace)
+		return;
+
+	// keep the absolute position (in ref) constant when changing space.
+	ssc::Transform3D new_M_old = ssc::SpaceHelpers::get_toMfrom(this->getSpace(), space);
+	mFrame = new_M_old*mFrame;
+
+	mSpace = space;
+	mSpaceListener->setSpace(space);
+}
+
+ssc::CoordinateSystem FrameMetric::getSpace() const
+{
+	return mSpace;
+}
+
+void FrameMetric::addXml(QDomNode& dataNode)
+{
+	Data::addXml(dataNode);
+
+	dataNode.toElement().setAttribute("space", mSpace.toString());
+	dataNode.toElement().setAttribute("frame", qstring_cast(mFrame));
+}
+
+void FrameMetric::parseXml(QDomNode& dataNode)
+{
+	Data::parseXml(dataNode);
+
+	this->setSpace(ssc::CoordinateSystem::fromString(dataNode.toElement().attribute("space", mSpace.toString())));
+	this->setFrame(ssc::Transform3D::fromString(dataNode.toElement().attribute("frame", qstring_cast(mFrame))));
+}
+
+ssc::DoubleBoundingBox3D FrameMetric::boundingBox() const
+{
+	// convert both inputs to r space
+	ssc::Transform3D rM0 = ssc::SpaceHelpers::get_toMfrom(this->getSpace(), ssc::CoordinateSystem(ssc::csREF));
+	ssc::Vector3D p0_r = rM0.coord(this->getCoordinate());
+
+	return ssc::DoubleBoundingBox3D(p0_r, p0_r);
+}
+
+QString FrameMetric::convertToSingleLineString()
+{
+//	QString retval = qstring_cast(mFrame);
+	QString retval;
+	retval += mName + " ";
+	retval += mSpace.toString();
+	retval += this->matrixAsSingleLineString();
+	return retval;
+}
+
+QString FrameMetric::matrixAsSingleLineString()
+{
+	QString retval;
+	for(int i = 0; i < 16; ++i)
+	{
+		QString elem;
+		elem.setNum(mFrame.flatten()[i]);
+		retval += " ";
+		retval += elem;
+	}
+	return retval;
+}
+
+QString FrameMetric::pointAsSingleLineString()
+{
+	QString retval;
+	QString elem;
+	elem.setNum(mFrame.flatten()[3]);
+	retval += elem + " ";
+	elem.setNum(mFrame.flatten()[7]);
+	retval += elem + " ";
+	elem.setNum(mFrame.flatten()[11]);
+	retval += elem;
+	return retval;
+}
+
+} //namespace cx
---
+++ source/resource/ssc/Code/Data/cxFrameMetric.h
@@ -0,0 +1,71 @@
+
+#ifndef CXTFRAMEMETRIC_H_
+#define CXTFRAMEMETRIC_H_
+
+#include "sscPointMetric.h"
+#include "sscCoordinateSystemHelpers.h"
+#include "sscDataReaderWriter.h"
+
+namespace cx
+{
+
+typedef boost::shared_ptr<class FrameMetric> FrameMetricPtr;
+
+/** \brief ssc::DataReader implementation for FrameMetric
+ *
+ * \date Aug 16, 2011
+ * \author Ole Vegard Solberg, SINTEF
+ */
+class FrameMetricReader: public ssc::DataReader
+{
+public:
+	virtual ~FrameMetricReader()
+	{
+	}
+	virtual bool canLoad(const QString& type, const QString& filename)
+	{
+		return type == "frameMetric";
+	}
+	virtual ssc::DataPtr load(const QString& uid, const QString& filename);
+};
+
+/**\brief Data class that represents a single transform.
+ *
+ * The transform is attached to a specific coordinate system / frame.
+ *
+ * \date Aug 16, 2011
+ * \author Ole Vegard Solberg, SINTEF
+ */
+class FrameMetric: public ssc::DataMetric
+{
+Q_OBJECT
+public:
+	FrameMetric(const QString& uid, const QString& name = "");
+	virtual ~FrameMetric();
+
+	void setFrame(const ssc::Transform3D& rMt);
+	ssc::Transform3D getFrame();
+	ssc::Vector3D getCoordinate() const;
+	void setSpace(ssc::CoordinateSystem space); // use parentframe from ssc::Data
+	ssc::CoordinateSystem getSpace() const; // use parentframe from ssc::Data
+	virtual QString getType() const
+	{
+		return "frameMetric";
+	}
+
+	virtual void addXml(QDomNode& dataNode); ///< adds xml information about the data and its variabels
+	virtual void parseXml(QDomNode& dataNode); ///< Use a XML node to load data. \param dataNode A XML data representation of this object.
+	virtual ssc::DoubleBoundingBox3D boundingBox() const;
+	QString convertToSingleLineString();
+	QString matrixAsSingleLineString();
+	QString pointAsSingleLineString();
+
+private:
+	ssc::CoordinateSystem mSpace;
+	ssc::CoordinateSystemListenerPtr mSpaceListener;
+	ssc::Transform3D mFrame;
+};
+
+} //namespace cx
+
+#endif /* CXTFRAMEMETRIC_H_ */
