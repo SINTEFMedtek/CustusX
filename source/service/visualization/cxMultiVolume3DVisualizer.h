9c25f4278f3b5d2884dfcc3e71090b41724f5c45
Christian <christian.askeland@sintef.no>
2013-09-04 10:52:48 +0200

    refactored viewmanager, added basis for more refactor
    Former-commit-id: c375ea75112b8519ba50b85ca8ab97b1982d9cf7

---
+++ source/service/visualization/cxMultiVolume3DVisualizer.h
@@ -0,0 +1,38 @@
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
+#ifndef CXMULTIVOLUME3DVISUALIZER_H
+#define CXMULTIVOLUME3DVISUALIZER_H
+
+namespace cx
+{
+
+/** 
+ *
+ *
+ * \ingroup cxServiceVisualization
+ * \date 4 Sep 2013
+ * \author Christian Askeland, SINTEF
+ * \author Ole Vegard Solberg, SINTEF
+ */
+class MultiVolume3DVisualizer
+{
+public:
+	MultiVolume3DVisualizer();
+};
+
+} // namespace cx
+
+
+
+#endif // CXMULTIVOLUME3DVISUALIZER_H
