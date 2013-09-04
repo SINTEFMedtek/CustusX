dcf0c7ddeaee03f8f9202193c32336132b2b8fc9
Christian <christian.askeland@sintef.no>
2013-09-04 11:44:52 +0200

    refactored
    Former-commit-id: 0e04af3bbd3a10115e6c76f1d96d0b4c37dc891c

--- source/service/visualization/cxViewWrapper3D.h
+++ source/service/visualization/cxViewWrapper3D.h
@@ -45,6 +45,7 @@ namespace cx
 {
 typedef boost::shared_ptr<class ImageLandmarkRep> ImageLandmarkRepPtr;
 typedef boost::shared_ptr<class PatientLandmarkRep> PatientLandmarkRepPtr;
+typedef boost::shared_ptr<class MultiVolume3DVisualizer> MultiVolume3DVisualizerPtr;
 
 /**
  * \file
@@ -137,6 +138,7 @@ private:
 
 	void setTranslucentRenderingToDepthPeeling(bool setDepthPeeling);
 
+	MultiVolume3DVisualizerPtr mMultivolumeVisualizer;
 	typedef std::map<QString, ssc::RepPtr> RepMap;
 	RepMap mDataReps;
 	LandmarkRepPtr mLandmarkRep;
