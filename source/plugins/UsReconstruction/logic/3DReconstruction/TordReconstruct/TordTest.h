696e3eed5404b517b896310e096eb76bd5ce2f61
Tord Øygard <tord@oygard.org>
2013-09-12 14:16:55 +0200

    Started writing test code Signed-off-by: Tord Øygard <tord@oygard.org>
    Former-commit-id: 55603e9653e8203f02c55ed651c07135b19cd9c9

---
+++ source/plugins/UsReconstruction/logic/3DReconstruction/TordReconstruct/TordTest.h
@@ -0,0 +1,39 @@
+// This is a preliminary test written by Tord Øygard in order to familiarize himself with the CustusX codebase.
+#ifndef TORD_TEST_H_
+#define TORD_TEST_H_
+
+#include "sscReconstructAlgorithm.h"
+
+
+namespace ssc
+{
+/**
+ * \addtogroup sscUSReconstruction
+ * \{
+ */
+
+
+/**
+ * \brief Tords US test
+ */
+
+class TordTest: public ReconstructAlgorithm
+{
+public:
+	TordTest();
+	virtual ~TordTest()
+	{
+	}
+	virtual QString getName() const
+	{
+		return "TordTest";
+	}
+		
+	virtual std::vector<DataAdapterPtr> getSettings(QDomElement root);
+	virtual bool reconstruct(ProcessedUsInputDataPtr input,
+	                         vtkImageDataPtr outputData,
+	                         QDomElement settings);
+};
+
+}
+		
