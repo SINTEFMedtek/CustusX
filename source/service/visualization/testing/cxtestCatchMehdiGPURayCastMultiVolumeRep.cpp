24bf2a7f1d6fcb8af87a80cc74b3e21139c0958c
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-09-17 12:50:43 +0200

    Added missing file
    Former-commit-id: 7f59b00133132275e30dc461a654b5b8527a41b6

---
+++ source/service/visualization/testing/cxtestCatchMehdiGPURayCastMultiVolumeRep.cpp
@@ -0,0 +1,44 @@
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
+
+#include "catch.hpp"
+#include "cxMehdiGPURayCastMultiVolumeRep.h"
+
+#include "cxtestMultiVolume3DRepProducerFixture.h"
+//#include "cxTestCustusXController.h"
+
+namespace cxtest
+{
+
+TEST_CASE("MehdiGPURayCastMultiVolumeRep can render 3 small volumes.", "[integration][gui]")
+{
+	MultiVolume3DRepProducerFixture fixture;
+
+	unsigned imageCount = 3;
+	fixture.initializeVisualizerAndImages("vtkOpenGLGPUMultiVolumeRayCastMapper", imageCount);
+
+	cx::MehdiGPURayCastMultiVolumeRepPtr rep = fixture.downcastRep<cx::MehdiGPURayCastMultiVolumeRep>(0);
+	REQUIRE(rep);
+
+/*
+	cx::DataLocations::setTestMode();
+	CustusXController custusX(NULL);
+	custusX.start();
+	qApp->exec();
+	custusX.stop();*/
+
+}
+
+
+} // namespace cxtest
