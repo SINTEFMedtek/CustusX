7ea49ee8282e6b5db1e854a75ed3cfd146cda9d6
Christian Askeland <christian.askeland@sintef.no>
2013-09-21 00:03:05 +0200

    removed ssc, all primary code contents moved to resource/core lib. Tests, examples, docs still missing.
    Former-commit-id: a52d749b9078f71eb47665c7abbb18c628176836

---
+++ source/resource/core/utilities/sscConfig.h
@@ -0,0 +1,12 @@
+// position of test data
+#define SSC_DATA_ROOT "/Users/christiana/dev/working/CustusX3/CustusX3/data/"
+
+// default timeout for automated tests (in the Testing folder)
+#define SSC_DEFAULT_TEST_TIMEOUT_SECS 1
+
+// auto-accept tests that require user acceptance
+#define SSC_AUTOMATIC_TEST_ACCEPT
+
+/* #undef USE_GLX_SHARED_CONTEXT */
+
+#define SSC_THUNDER_KERNEL_PATH "/"
