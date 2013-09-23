1afb1dbb5d4bcc6723df91ef8ec815fe7b9d11f9
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-09-23 11:18:29 +0200

    Added a simple test for vtkOpenGLGPUMultiVolumeRayCastMapper
    Former-commit-id: 5d6058f82dc0003bc4d234e772b9a2e302ee8189

--- source/service/visualization/testing/cxtestCatchVolumeReps.cpp
+++ source/service/visualization/testing/cxtestCatchVolumeReps.cpp
@@ -24,6 +24,14 @@
 #include "cxtestRenderTester.h"
 #include "cxtestUtilities.h"
 
+//Needed for vtkOpenGLGPUMultiVolumeRayCastMapper test
+#include <vtkTransform.h>
+#include <vtkVolume.h>
+#include <vtkVolumeProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRendererCollection.h>
+#include "vtkOpenGLGPUMultiVolumeRayCastMapper.h"
+#include "cxVolumeProperty.h"
 
 typedef vtkSmartPointer<class vtkWindowToImageFilter> vtkWindowToImageFilterPtr;
 
@@ -43,6 +51,65 @@ void simpleVolumeRepTest(cx::RepPtr rep, unsigned int viewAxisSize = 30)
 	REQUIRE(numNonZeroPixels > 0);
 	REQUIRE(numNonZeroPixels < viewAxisSize*viewAxisSize);
 }
+
+// Test that tests vtkOpenGLGPUMultiVolumeRayCastMapper directly, without using MehdiGPURayCastMultiVolumeRep
+TEST_CASE("vtkOpenGLGPUMultiVolumeRayCastMapper can render 3 small volumes", "[integration][gui][notmac]")
+{
+	unsigned int imageCount = 3;
+	std::vector<cx::ImagePtr> images = cxtest::Utilities::create3DImages(imageCount, Eigen::Array3i(3,3,3), 200);
+
+
+	vtkOpenGLGPUMultiVolumeRayCastMapperPtr mapper;
+	mapper = vtkOpenGLGPUMultiVolumeRayCastMapperPtr::New();
+	mapper->setNumberOfAdditionalVolumes(images.size()-1);
+
+	vtkVolumePtr volume;
+	volume = vtkVolumePtr::New();
+	volume->SetMapper(mapper);
+
+	//Set transforms
+	cx::Transform3D rMd0 = images[0]->get_rMd(); // use on first volume
+	for (unsigned i=0; i<images.size(); ++i)
+	{
+		cx::Transform3D rMdi = images[i]->get_rMd();
+		cx::Transform3D d0Mdi = rMd0.inv() * rMdi; // use on additional volumescd
+		if (i==0)
+		{
+			volume->SetUserMatrix(rMd0.getVtkMatrix());
+		}
+		else if (i>0)
+		{
+			vtkTransformPtr transform = vtkTransform::New();
+			transform->SetMatrix(d0Mdi.getVtkMatrix());
+			transform->Update();
+			mapper->SetAdditionalInputUserTransform(i-1,transform);
+		}
+	}
+
+	//Set volume properties
+	std::vector<cx::VolumePropertyPtr> volumeProperties;
+	for (unsigned i=0; i<images.size(); ++i)
+	{
+		cx::VolumePropertyPtr property = cx::VolumeProperty::create();
+		property->setImage(images[i]);
+		volumeProperties.push_back(property);
+		mapper->SetInput(i, images[i]->getBaseVtkImageData());
+
+		if (i==0)
+			volume->SetProperty( property->getVolumeProperty() );
+		else if (i>0)
+			mapper->SetAdditionalProperty(i-1, property->getVolumeProperty() );
+	}
+
+
+	vtkRenderWindowPtr renderWindow = vtkRenderWindowPtr::New();
+	vtkRendererPtr renderer = vtkRendererPtr::New();
+	renderer->AddVolume(volume);
+	renderWindow->AddRenderer(renderer);
+	renderWindow->Render();
+	REQUIRE(true);
+}
+
 
 TEST_CASE("MehdiGPURayCastMultiVolumeRep can render 3 small volumes.", "[rep][integration][gui][notmac]")
 {
