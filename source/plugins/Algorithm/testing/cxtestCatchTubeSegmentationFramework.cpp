70eb2ff93062d6d140e5a7bcb01c46d9955ab61a
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-09-23 14:38:52 +0200

    Moved TSF tests to catch
    Former-commit-id: 64696718bf905fda64a7547f8fa02a465551bd0d

---
+++ source/plugins/Algorithm/testing/cxtestCatchTubeSegmentationFramework.cpp
@@ -0,0 +1,77 @@
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
+
+#include "catch.hpp"
+
+#include "cxTestTubeSegmentationFramework.h"
+
+namespace cxtest
+{
+
+TEST_CASE("TubeSegmentationFramework testConstructor works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testConstructor();
+	REQUIRE(true);
+}
+
+TEST_CASE("TubeSegmentationFramework can set parameters", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testParameters();
+}
+
+TEST_CASE("TubeSegmentationFramework can load parameter file", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testLoadParameterFile();
+}
+
+TEST_CASE("TubeSegmentationFramework default preset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testDefaultPreset();
+}
+
+TEST_CASE("TubeSegmentationFramework SyntheticVascusynthPreset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testSyntheticVascusynthPreset();
+}
+
+TEST_CASE("TubeSegmentationFramework PhantomAccUSPreset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testPhantomAccUSPreset();
+}
+
+TEST_CASE("TubeSegmentationFramework NeuroVesselsMRAPreset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testNeuroVesselsMRAPreset();
+}
+
+TEST_CASE("TubeSegmentationFramework testNeuroVesselsUSAPreset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testNeuroVesselsUSAPreset();
+}
+
+TEST_CASE("TubeSegmentationFramework testLungAirwaysCTPreset works", "[integration]")
+{
+	TestTubeSegmentationFramework helper;
+	helper.testLungAirwaysCTPreset();
+}
+
+} // namespace cxtest
