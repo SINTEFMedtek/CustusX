5b093cb53fe1f3d166feea369cfc51f88e90e30e
Christian Askeland <christian.askeland@sintef.no>
2013-09-26 08:57:11 +0200

    converted more tests to catch
    Former-commit-id: a8150fc16006e90e9e2cb49ee2ea56a3d42c7edb

--- source/plugins/Calibration/testing/cxtestTemporalCalibration.cpp
+++ source/plugins/Calibration/testing/cxtestTemporalCalibration.cpp
@@ -1,11 +1,23 @@
-#include "cxTestAlgorithms.h"
-#include <iostream>
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
 
-//#include "cxConfig.h"
+#include "catch.hpp"
+
 #include "cxDataLocations.h"
 #include "cxTemporalCalibration.h"
 
-void TestAlgorithms::testTemporalCalibration()
+TEST_CASE("TemporalCalibration reproduces old results on a test data set", "[unit][plugins][calibration]")
 {
   cx::TemporalCalibration calibrator;
 //  QString filename = "/home/christiana/Patients/20110511T092103_temporal_calib_mac.cx3/US_Acq/US-Acq_01_20110511T092317/US-Acq_01_20110511T092317.mhd";
@@ -19,8 +31,8 @@ void TestAlgorithms::testTemporalCalibration()
 
   double testValue = 115; // shift found on data set during first tests.
 
-  CPPUNIT_ASSERT( success );
-  CPPUNIT_ASSERT( cx::similar(shift, testValue, 1));
+  CHECK( success );
+  CHECK( cx::similar(shift, testValue, 1));
 }
 
 
