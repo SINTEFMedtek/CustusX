7bc325ea3c02a3f2b015d6b323362a2308ea0ab8
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-10-18 11:05:49 +0200

    Remaned class PreferencesTab to PreferenceTab, to match file name
    Former-commit-id: 9e35c8b8a11ca0ea7665c455a4c682cbba1340e9

--- source/gui/preferencesWidgets/cxPreferenceTab.cpp
+++ source/gui/preferencesWidgets/cxPreferenceTab.cpp
@@ -17,7 +17,7 @@ namespace cx
 // PreferencesTab
 //------------------------------------------------------------------------------
 
-PreferencesTab::PreferencesTab(QWidget *parent) :
+PreferenceTab::PreferenceTab(QWidget *parent) :
     QWidget(parent)
     //settings()(settings())
 {
@@ -34,7 +34,7 @@ PreferencesTab::PreferencesTab(QWidget *parent) :
 // PerformanceTab
 //------------------------------------------------------------------------------
 PerformanceTab::PerformanceTab(QWidget *parent) :
-    PreferencesTab(parent)
+		PreferenceTab(parent)
 {
 	mRenderingIntervalSpinBox = NULL;
 	mRenderingRateLabel = NULL;
