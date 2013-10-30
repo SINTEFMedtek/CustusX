e25967ade1a2d0aad4dc84ca486d6aaf39952ebf
Erik Smistad <smistad@idi.ntnu.no>
2013-10-30 16:20:29 +0100

    added an integration test for execute of the level set filter
    Former-commit-id: c57242981b06fd9900e3212d48126bda543d735c

--- source/plugins/Algorithm/testing/cxtestLevelSetFilter.cpp
+++ source/plugins/Algorithm/testing/cxtestLevelSetFilter.cpp
@@ -13,20 +13,27 @@
 #include "cxDataLocations.h"
 #include "cxSelectDataStringDataAdapter.h"
 #include "cxLogicManager.h"
+#include "sscDataAdapter.h"
+#include "sscDoubleDataAdapter.h"
+#include "sscRegistrationTransform.h"
 
 namespace cxtest {
 
-TEST_CASE("LevelSetFilter: getSeedPointFromTool", "[unit][plugins][Algorithm][LevelSetFilter]") 
-{
+void setSeedPoint(cx::Vector3D point) {
     cx::cxToolManager::initializeObject();
     cx::cxToolManager * toolmanager = cx::cxToolManager::getInstance();
     cx::ManualToolPtr tool = toolmanager->getManualTool();
+    tool->set_prMt(cx::createTransformTranslate(point));
+
+}
+
+TEST_CASE("LevelSetFilter: getSeedPointFromTool", "[unit][plugins][Algorithm][LevelSetFilter]")
+{
     cx::Vector3D toolTipPoint;
     toolTipPoint.setRandom();
-    tool->set_prMt(cx::createTransformTranslate(toolTipPoint));
-
-    cx::DataPtr data;
-    cx::Vector3D point = cx::LevelSetFilter::getSeedPointFromTool(data);
+    setSeedPoint(toolTipPoint);
+    cx::ImagePtr image = cxtest::Utilities::create3DImage();
+    cx::Vector3D point = cx::LevelSetFilter::getSeedPointFromTool(image);
     REQUIRE(toolTipPoint(0) == point(0));
     REQUIRE(toolTipPoint(1) == point(1));
     REQUIRE(toolTipPoint(2) == point(2));
@@ -53,13 +60,14 @@ TEST_CASE("LevelSetFilter: execute", "[integration][plugins][Algorithm][LevelSet
 	REQUIRE(lsf);
 	lsf->getInputTypes();
 	lsf->getOutputTypes();
-	lsf->getOptions();
+	std::vector<DataAdapterPtr> options = lsf->getOptions();
 
     //create a new patient
-	QString filename = cx::DataLocations::getTestDataPath()+ "/testing/TubeSegmentationFramework/Lung-Airways-CT.mhd";
+	QString filename = cx::DataLocations::getTestDataPath()+ "/testing/TubeSegmentationFramework/Default.mhd";
 	cx::patientService()->getPatientData()->newPatient(cx::DataLocations::getTestDataPath()+ "/temp/LevelSetFilter/");
 	QString info;
 	cx::DataPtr data = cx::patientService()->getPatientData()->importData(filename, info);
+	data->get_rMd_History()->setRegistration(cx::Transform3D::Identity());
 	REQUIRE(data);
 
 	//set input
@@ -74,12 +82,52 @@ TEST_CASE("LevelSetFilter: execute", "[integration][plugins][Algorithm][LevelSet
 	}
 	{
 		INFO("The name of the input data is not as we requested.");
-		REQUIRE(input[0]->getData()->getName() == "Lung-Airways-CT");
+		REQUIRE(input[0]->getData()->getName() == "Default");
 	}
 
-	// TODO: set seedpoint, threshold, epsilon and alpha
-	// TODO: execute
-	// TODO: check that a volume and contour is generated
+	// set seedpoint, threshold, epsilon and alpha
+	/*
+	cx::cxToolManager::initializeObject();
+    cx::cxToolManager * toolmanager = cx::cxToolManager::getInstance();
+    toolmanager->setDominantTool(data->getUid());
+    */
+	cx::Vector3D seedPoint;
+	seedPoint(0) = 34;
+	seedPoint(1) = 29;
+	seedPoint(2) = 50;
+	setSeedPoint(seedPoint);
+	cx::Vector3D point = cx::LevelSetFilter::getSeedPointFromTool(data);
+	boost::dynamic_pointer_cast<cx::DoubleDataAdapter>(options[0])->setValue(60);
+	boost::dynamic_pointer_cast<cx::DoubleDataAdapter>(options[1])->setValue(100);
+	boost::dynamic_pointer_cast<cx::DoubleDataAdapter>(options[2])->setValue(0.1);
+	// Execute
+	{
+		INFO("Preprocessing LevelSetFilter failed.");
+		REQUIRE(lsf->preProcess());
+	}
+
+	{
+		REQUIRE(lsf->execute());
+	}
+	{
+		INFO("Post processing data from Level Set Filter failed.");
+		REQUIRE(lsf->postProcess());
+	}
+
+	// Check output
+	std::vector < cx::SelectDataStringDataAdapterBasePtr > output = lsf->getOutputTypes();
+	{
+		INFO("Number of outputs has changed.");
+		REQUIRE(output.size() == 2);
+	}
+	{
+		INFO("Segmentation volume not generated.");
+		REQUIRE(output[0]->getData());
+	}
+	{
+		INFO("Surface/contour not generated.");
+		REQUIRE(output[1]->getData());
+	}
     cx::LogicManager::shutdown();
 }
 
