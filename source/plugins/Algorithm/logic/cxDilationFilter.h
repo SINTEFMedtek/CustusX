385e0bf49974279a783ede9e7757538f7818ca62
Erik Smistad <smistad@idi.ntnu.no>
2013-11-06 13:57:18 +0100

    added DilationFilter class
    Former-commit-id: a39613a4ac4430f4463fe1052c26f1b9c78b8c6e

---
+++ source/plugins/Algorithm/logic/cxDilationFilter.h
@@ -0,0 +1,40 @@
+#ifndef CX_DILATION_FILTER_H
+#define CX_DILATION_FILTER_H
+
+#include "cxFilterImpl.h"
+
+namespace cx {
+class DilationFilter : public FilterImpl
+{
+	Q_OBJECT
+
+public:
+	virtual ~DilationFilter() {}
+
+	virtual QString getType() const;
+	virtual QString getName() const;
+	virtual QString getHelp() const;
+	virtual void setActive(bool on);
+
+	bool preProcess();
+	virtual bool execute();
+	virtual bool postProcess();
+
+	// extensions:
+	DoubleDataAdapterXmlPtr getDilationAmountOption(QDomElement root);
+
+protected:
+	virtual void createOptions();
+	virtual void createInputTypes();
+	virtual void createOutputTypes();
+
+private:
+	DoubleDataAdapterXmlPtr mDilationAmountOption;
+	vtkImageDataPtr mRawResult;
+	vtkPolyDataPtr mRawContour;
+};
+typedef boost::shared_ptr<class DilationFilter> DilationFilterPtr;
+
+} // namespace cx
+
+#endif
