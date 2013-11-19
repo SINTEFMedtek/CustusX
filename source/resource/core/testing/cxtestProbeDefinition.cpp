03a3559164a5329137b5e3a947b562dc29fb6d60
Janne Beate Lervik Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2013-11-19 13:07:45 +0100

    Fixed crashing test.
    Former-commit-id: 4266f6bb955317c291758a9b76984534a8f86304

--- source/resource/core/testing/cxtestProbeDefinition.cpp
+++ source/resource/core/testing/cxtestProbeDefinition.cpp
@@ -8,16 +8,17 @@
 namespace cxtest
 {
 
-TEST_CASE("ProbeDefinition can be constructed", "[unit][resource][core][ProbeDefinition][hide]")
+TEST_CASE("ProbeDefinition can be constructed", "[unit][resource][core][ProbeDefinition][unstable]")
 {
 	cx::ProbeDataPtr probeDefinition = cx::ProbeDataPtr(new cx::ProbeData());
 	CHECK(probeDefinition.get());
 	CHECK(probeDefinition->getType() == cx::ProbeData::tNONE);
 }
 
-TEST_CASE("ProbeDefinition: Validating set/get", "[unit][resource][core][ProbeDefinition][hide]")
+TEST_CASE("ProbeDefinition: Validating set/get", "[unit][resource][core][ProbeDefinition][unstable]")
 {
-	cx::ProbeDataPtr probeDefinition;
+	cx::ProbeDataPtr probeDefinition(new cx::ProbeData());
+	CHECK(probeDefinition.get());
 
 	cx::Vector3D origin_p = probeDefinition->getOrigin_p();
 	probeDefinition->setOrigin_p(origin_p);
@@ -27,11 +28,9 @@ TEST_CASE("ProbeDefinition: Validating set/get", "[unit][resource][core][ProbeDe
 	probeDefinition->setSpacing(spacing);
 	CHECK(spacing ==  probeDefinition->getSpacing());
 
-
 	cx::DoubleBoundingBox3D clipRect_p = probeDefinition->getClipRect_p();
 	probeDefinition->setClipRect_p(clipRect_p);
 	CHECK(clipRect_p ==  probeDefinition->getClipRect_p());
-
 
 	QSize size = probeDefinition->getSize();
 	probeDefinition->setSize(size);
