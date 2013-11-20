d8328a12dbf427923e627f1d12eae1b258aef077
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2013-11-20 08:56:32 +0100

    Added another simple set/get test
    Former-commit-id: 3c14dfe605b318ab1d6dc159953eb75d13d58f93

--- source/resource/core/testing/cxtestProbeDefinition.cpp
+++ source/resource/core/testing/cxtestProbeDefinition.cpp
@@ -15,7 +15,7 @@ TEST_CASE("ProbeDefinition can be constructed", "[unit][resource][core][ProbeDef
 	CHECK(probeDefinition->getType() == cx::ProbeData::tNONE);
 }
 
-TEST_CASE("ProbeDefinition: Validating set/get", "[unit][resource][core][ProbeDefinition][unstable]")
+TEST_CASE("ProbeDefinition: Validating set/get with default values", "[unit][resource][core][ProbeDefinition][unstable]")
 {
 	cx::ProbeDataPtr probeDefinition(new cx::ProbeData());
 	CHECK(probeDefinition.get());
@@ -33,6 +33,28 @@ TEST_CASE("ProbeDefinition: Validating set/get", "[unit][resource][core][ProbeDe
 	CHECK(clipRect_p ==  probeDefinition->getClipRect_p());
 
 	QSize size = probeDefinition->getSize();
+	probeDefinition->setSize(size);
+	CHECK(size ==  probeDefinition->getSize());
+}
+
+TEST_CASE("ProbeDefinition: Validating set/get with dummy values", "[unit][resource][core][ProbeDefinition][unstable]")
+{
+	cx::ProbeDataPtr probeDefinition(new cx::ProbeData());
+	CHECK(probeDefinition.get());
+
+	cx::Vector3D origin_p(10, 20, 30);
+	probeDefinition->setOrigin_p(origin_p);
+	CHECK(origin_p == probeDefinition->getOrigin_p());
+
+	cx::Vector3D spacing(1, 2, 3);
+	probeDefinition->setSpacing(spacing);
+	CHECK(spacing ==  probeDefinition->getSpacing());
+
+	cx::DoubleBoundingBox3D clipRect_p(10, 20, 5, 50);
+	probeDefinition->setClipRect_p(clipRect_p);
+	CHECK(clipRect_p ==  probeDefinition->getClipRect_p());
+
+	QSize size(100, 200);
 	probeDefinition->setSize(size);
 	CHECK(size ==  probeDefinition->getSize());
 }
