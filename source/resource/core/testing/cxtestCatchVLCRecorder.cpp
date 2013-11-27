2e1086329f000657b4ed28a64b3f65e08e6414af
Janne Beate Lervik Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2013-11-27 19:35:23 +0100

    Added a proof of concept test for QProcess and VLC.
    Former-commit-id: fce7cdda2c37ee1b6048bdfcf1c9c845a34503f4

--- source/resource/core/testing/cxtestCatchVLCRecorder.cpp
+++ source/resource/core/testing/cxtestCatchVLCRecorder.cpp
@@ -21,6 +21,10 @@
 #include <windows.h>
 #endif
 
+#include <QProcess>
+#include "cxtestUtilities.h"
+#include <QDebug>
+
 namespace cxtest
 {
 
@@ -40,9 +44,34 @@ TEST_CASE("VLCRecorder can record for 7 seconds", "[integration][resource][core]
 {
 	VLCRecorderFixture vlc;
 
-	vlc.checkThatVLCCanRecordTheScreen(10);
+	vlc.checkThatVLCCanRecordTheScreen(7);
 
 	vlc.checkIsMovieFilePlayable();
 }
+
+
+TEST_CASE("QProcess and VLC can use cli for starting and stopping screen capture.", "[VLC][proof_of_concept][hide][not_linux][not_win32][not_win64][not_apple]")
+{
+	QProcess* p = new QProcess();
+	p->setProcessChannelMode(QProcess::MergedChannels);
+	p->setReadChannel(QProcess::StandardOutput);
+
+	QString pathToVLC = "/Applications/VLC.app/Contents/MacOS/VLC";
+	QString saveFile = "/Users/jbake/Desktop/video/file.mp4";
+
+	p->start(pathToVLC+" -I cli screen:// \":sout=#transcode{vcodec=h264,vb=800,fps=10,scale=1,acodec=none}:duplicate{dst=standard{access=file,mux=mp4,dst="+saveFile+"}}\"");
+	REQUIRE(p->waitForStarted());
+
+	//How long the thread needs too sleep (startup time) depends on several things, cpu load is one of them...
+	Utilities::sleep_sec(9);
+
+	std::cout << p->write("quit\n") << std::endl;
+
+	REQUIRE(p->waitForFinished());
+
+	qDebug() << p->readAllStandardOutput();
+}
+
+
 
 } //namespace cxtest
