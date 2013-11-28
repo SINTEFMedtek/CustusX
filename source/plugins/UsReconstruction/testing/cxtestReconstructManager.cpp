8bf04a0b7219c102c23b30ca540137e81bf0848f
Christian Askeland <christian.askeland@sintef.no>
2013-11-28 15:03:45 +0100

    moved slerp interpolation test
    Former-commit-id: 1d500dfc94b77c301bd104bae362d64430873550

--- source/plugins/UsReconstruction/testing/cxtestReconstructManager.cpp
+++ source/plugins/UsReconstruction/testing/cxtestReconstructManager.cpp
@@ -65,7 +65,6 @@ public:
 	void setUp();
 	void tearDown();
 
-	void testSlerpInterpolation();///< Test position matrix slerp interpolation
 	void testConstructor();///< Test reconstructer constructor
 	void testAngioReconstruction();///< Test reconstruction of US angio data (#318)
 	void testThunderGPUReconstruction();///< Test Thunder GPU reconstruction
@@ -140,65 +139,6 @@ void ReconstructManagerTestFixture::tearDown()
 	// this stuff will be performed just after all tests in this class
 }
 
-void ReconstructManagerTestFixture::testSlerpInterpolation()
-{
-	//  ReconstructManagerPtr reconstructer(new ReconstructManager(XmlOptionFile(),""));
-
-	//  ReconstructerPtr reconstructer(new Reconstructer(XmlOptionFile(),""));
-	cx::ReconstructCorePtr reconstructer(new cx::ReconstructCore());
-
-	cx::Transform3D a;
-	cx::Transform3D b;
-
-	Eigen::Matrix3d am;
-	am =
-			Eigen::AngleAxisd(0/*M_PI / 3.0*/, Eigen::Vector3d::UnitX()) * //60 deg
-			Eigen::AngleAxisd(M_PI / 180.0*2.0, Eigen::Vector3d::UnitY()) * // 2 deg
-			Eigen::AngleAxisd(0/*M_PI / 180.0*10.0*/, Eigen::Vector3d::UnitZ()); // 10 deg
-
-	a.matrix().block<3, 3>(0, 0) = am;
-	a.matrix().block<4, 1>(0, 3) = Eigen::Vector4d(0.0, 0.0, 0.0, 1.0);
-
-	Eigen::Matrix3d bm;
-	bm =
-			Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()) * //0 deg
-			Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY()) *
-			Eigen::AngleAxisd(0, Eigen::Vector3d::UnitZ());
-
-	b.matrix().block<3, 3>(0, 0) = bm;
-	b.matrix().block<4, 1>(0, 3) = Eigen::Vector4d(10.0, 10.0, 10.0, 1.0);
-
-	double t = 0.5;
-
-	cx::Transform3D c = cx::USReconstructInputDataAlgorithm::slerpInterpolate(a, b, t);
-	//Transform3D c = reconstructer->interpolate(a, b, t);
-
-	Eigen::Matrix3d goalm;
-	goalm =
-			Eigen::AngleAxisd(0/*M_PI / 6.0*/, Eigen::Vector3d::UnitX()) * //30 deg
-			Eigen::AngleAxisd(M_PI / 180.0, Eigen::Vector3d::UnitY()) * // 1 deg
-			Eigen::AngleAxisd(0/*M_PI / 180.0*5.0*/, Eigen::Vector3d::UnitZ()); // 5 deg
-
-	cx::Transform3D goal;
-	goal.matrix().block<3, 3>(0, 0) = goalm;
-	goal.matrix().block<4, 1>(0, 3) = Eigen::Vector4d(5.0, 5.0, 5.0, 1.0);
-
-	if (!cx::similar(c, goal))
-	{
-		std::cout << "result: "<< std::endl << c << std::endl;
-		std::cout << "goal: "<< std::endl << goal << std::endl;
-	}
-	REQUIRE(cx::similar(c, goal));
-
-	// Test if normalized = the column lengths are 1
-	double norm = goal.matrix().block<3, 1>(0, 0).norm();
-	//	std::cout << "norm: " << norm << std::endl;
-	REQUIRE(cx::similar(norm, 1.0));
-	norm = goal.matrix().block<3, 1>(0, 1).norm();
-	REQUIRE(cx::similar(norm, 1.0));
-	norm = goal.matrix().block<3, 1>(0, 2).norm();
-	REQUIRE(cx::similar(norm, 1.0));
-}
 
 TEST_CASE("ReconstructManager: PNN on sphere","[unit][usreconstruction][synthetic][ca_rec6][ca_rec]")
 {
@@ -514,11 +454,6 @@ void ReconstructManagerTestFixture::testTordTest()
 #endif // SSC_USE_OpenCL
 
 
-TEST_CASE("ReconstructManager: Slerp Interpolation", "[usreconstruction][unit]")
-{
-	ReconstructManagerTestFixture fixture;
-	fixture.testSlerpInterpolation();
-}
 TEST_CASE("ReconstructManager: Angio Reconstruction", "[usreconstruction][integration]")
 {
 	ReconstructManagerTestFixture fixture;
