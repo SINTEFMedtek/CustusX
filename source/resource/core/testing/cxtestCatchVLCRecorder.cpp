c580ec8a640a1517be14f3ecb942c2933973f8fa
Janne Beate Bakeng <janne.beate.lervik.bakeng@sintef.no>
2013-11-29 15:00:21 +0100

    Added some testing stuff for windows vlc. not working...
    Former-commit-id: 3eccbfccb1639260e7d0c2a718290f66f71af4af

--- source/resource/core/testing/cxtestCatchVLCRecorder.cpp
+++ source/resource/core/testing/cxtestCatchVLCRecorder.cpp
@@ -50,26 +50,57 @@ TEST_CASE("VLCRecorder can record for 15 seconds", "[integration][resource][core
 }
 
 
-TEST_CASE("QProcess and VLC can use cli for starting and stopping screen capture.", "[VLC][proof_of_concept][hide][not_linux][not_win32][not_win64][not_apple]")
+TEST_CASE("QProcess and VLC can use cli for starting and stopping screen capture.", "[jb][VLC][proof_of_concept][hide][not_linux][not_win32][not_win64][not_apple]")
 {
 	QProcess* p = new QProcess();
 	p->setProcessChannelMode(QProcess::MergedChannels);
 	p->setReadChannel(QProcess::StandardOutput);
 
+#ifdef CX_APPLE
 	QString pathToVLC = "/Applications/VLC.app/Contents/MacOS/VLC";
 	QString saveFile = "/Users/jbake/Desktop/video/file.mp4";
 
 	p->start(pathToVLC+" -I cli screen:// \":sout=#transcode{vcodec=h264,vb=800,fps=10,scale=1,acodec=none}:duplicate{dst=standard{access=file,mux=mp4,dst="+saveFile+"}}\"");
+#endif
+#ifdef CX_WINDOWS
+	QString pathToVLC = "\"C:/Program Files (x86)/VideoLAN/VLC/vlc.exe\"";
+	QString saveFile = "\"C:/Users/Dev/Desktop/test.mp4\"";
+
+	//p->start(pathToVLC+" -I luacli screen:// :screen-fps=10.000000 :live-caching=300 :sout=#transcode{vcodec=h264,acodec=none}:file{dst="+saveFile+"} :sout-keep");
+	p->start(pathToVLC+" -I luacli");
+#endif
+
 	REQUIRE(p->waitForStarted());
 
 	//How long the thread needs too sleep (startup time) depends on several things, cpu load is one of them...
-	Utilities::sleep_sec(9);
+	Utilities::sleep_sec(3);
 
+	qDebug() << "PID: " << QString::number((qint64)p->pid());
+	//qDebug() << "Program: " << p->program();
+	qDebug() << "ErrorString: " << p->errorString();
+	qDebug() << "isOpen: " << p->isOpen();
+	qDebug() << "isReadable: " << p->isReadable();
+	qDebug() << "isWritable: " << p->isWritable();
+	qDebug() << "workingDirectory: " << p->workingDirectory();
+	//qDebug() << "" << p->
+	//qDebug() << "" << p->
+	//qDebug() << "" << p->
+	qDebug() << "State: " << p->state();
+
+#ifdef CX_APPLE
 	std::cout << p->write("quit\n") << std::endl;
+#endif
+#ifdef CX_WINDOWS
+	qDebug() << p->write("quit\r\n");
+	REQUIRE(p->waitForBytesWritten(-1));
+	p->closeWriteChannel();
+#endif
 
 	REQUIRE(p->waitForFinished());
 
 	qDebug() << p->readAllStandardOutput();
+
+	delete p;
 }
 
 
