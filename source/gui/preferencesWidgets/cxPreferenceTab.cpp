dfaaed10b85af308ed39aa9af4021722ccbf4e33
Christian Askeland <christian.askeland@sintef.no>
2013-12-02 11:24:19 +0100

    changed texts and defaults related to rendering
    Former-commit-id: a389caa3037c6ed748a89701aaec425cf36170f3

--- source/gui/preferencesWidgets/cxPreferenceTab.cpp
+++ source/gui/preferencesWidgets/cxPreferenceTab.cpp
@@ -84,9 +84,11 @@ void PerformanceTab::init()
   m3DVisualizer->setDisplayNames(MultiVolume3DRepProducer::getAvailableVisualizerDisplayNames());
 
   bool useGPU2DRender = settings()->value("useGPU2DRendering").toBool();
-	mGPU2DRenderCheckBox = new QCheckBox("Use GPU 2D Renderer");
+	mGPU2DRenderCheckBox = new QCheckBox("2D Overlay");
 	mGPU2DRenderCheckBox->setChecked(useGPU2DRender);
-	mGPU2DRenderCheckBox->setToolTip("Use a GPU-based 2D renderer instead of the software-based one, if available.");
+	mGPU2DRenderCheckBox->setToolTip("<p>Use a GPU-based 2D renderer instead of "
+									 "the software-based one, if available.</p>"
+									 "<p>This enables multiple volume rendering in 2D.<p>");
 
 //#ifndef USE_GLX_SHARED_CONTEXT
 //	mGPU2DRenderCheckBox->setChecked(false);
