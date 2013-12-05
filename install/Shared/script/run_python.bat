2087e6a2d9f8dbc8b75bd21cee421aea087ae330
Christian <christian.askeland@sintef.no>
2013-12-05 16:02:24 +0100

    added jenkins scripting test code
    Former-commit-id: 15fb36fb37fd27d238805f4c312b3f05a1e403db

--- install/Shared/script/run_python.bat
+++ install/Shared/script/run_python.bat
@@ -1,33 +1,28 @@
-REM Experimental
-REM http://stackoverflow.com/questions/357315/get-list-of-passed-arguments-in-windows-batch-script-bat
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Jenkins control script
+::   Author: Christian Askeland, SINTEF Medical Technology
+::   Date:   2013.12.05
+:: Run this script as a bridge form Jenkins XShell plugin to python calls in the Windows case.
+:: Usage: path/to/run_python <target_os> <cx_envscripts_path> "<python_script> <arguments>"
+::   Note the hyphens around the third argument containing script name + args.
+:: 
+:: http://stackoverflow.com/questions/357315/get-list-of-passed-arguments-in-windows-batch-script-bat
+
+echo "Batch arguments: " %*
 
 set TARGET_PLATFORM=%1
 set CX_ENVSCRIPTS=%2
 set COMMAND=%~3
-
 set SCRIPT_PATH=%~dp0
-echo "hit the win batch script!"
-
-echo "args:" %*
 
 if "%TARGET_PLATFORM%" == "win64" (
   set INIT_VS=%CX_ENVSCRIPTS%\cxVars_x64.bat
-  set ARCH_FLAG="--b32"
+  set ARCH_FLAG=--b32
 )
-
 if "%TARGET_PLATFORM%" == "win32" (
   set INIT_VS=%CX_ENVSCRIPTS%\cxVars_x86.bat
 )
 
-REM %INIT_VS% && python -u %SCRIPT_PATH%\cxJenkinsBuildStep.py
-REM echo "NO1 win batch script complete!"
-
-REM %INIT_VS% && python -u %SCRIPT_PATH%\cxJenkinsBuildStep.py --root_dir D:\j\ws\unit\TARGET\win64
-REM echo "NO1 win batch script complete!"
-
-
 %INIT_VS% && python -u %SCRIPT_PATH%\%COMMAND% %ARCH_FLAG%
-echo "NO2 win batch script complete!"
 
-echo "win batch script complete!"
 
