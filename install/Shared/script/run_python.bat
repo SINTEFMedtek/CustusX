c396db2be2ffe109e74dcc2389d5b7f15d7ef1c1
Christian <christian.askeland@sintef.no>
2013-12-05 12:16:52 +0100

    added jenkins scripting test code
    Former-commit-id: 1de8f743de76c946f619ccb26005b36f723eb53c

--- install/Shared/script/run_python.bat
+++ install/Shared/script/run_python.bat
@@ -1,8 +1,10 @@
 REM Experimental
+REM http://stackoverflow.com/questions/357315/get-list-of-passed-arguments-in-windows-batch-script-bat
 
 set TARGET_PLATFORM=%1
 set CX_ENVSCRIPTS=%2
 set COMMAND=%3
+set SCRIPT_PATH=%~dp0
 echo "hit the win batch script!"
 
 echo "args:" %*
@@ -16,13 +18,13 @@ if "%TARGET_PLATFORM%" == "win32" (
   set INIT_VS=%CX_ENVSCRIPTS%\cxVars_x86.bat
 )
 
-%INIT_VS% && python -u cxJenkinsBuildStep.py
+%INIT_VS% && python -u %SCRIPT_PATH%\cxJenkinsBuildStep.py
 echo "NO1 win batch script complete!"
 
-%INIT_VS% && python -u cxJenkinsBuildStep.py --root_dir D:\j\ws\unit\TARGET\win64
-echo "NO1 win batch script complete!"
+REM %INIT_VS% && python -u cxJenkinsBuildStep.py --root_dir D:\j\ws\unit\TARGET\win64
+REM echo "NO1 win batch script complete!"
 
-%INIT_VS% && python -u %COMMAND% %ARCH_FLAG%
+%INIT_VS% && python -u %SCRIPT_PATH%\%COMMAND% %ARCH_FLAG%
 echo "NO2 win batch script complete!"
 
 echo "win batch script complete!"
