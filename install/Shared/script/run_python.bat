4adce31c49c38b49a75407337704cab1c193ab34
Christian <christian.askeland@sintef.no>
2013-12-05 11:51:45 +0100

    added jenkins scripting test code
    Former-commit-id: 1bfad7f70d55251828814820be8de8ba7681cff8

--- install/Shared/script/run_python.bat
+++ install/Shared/script/run_python.bat
@@ -1,7 +1,22 @@
-
 REM Experimental
 
-
+TARGET_PLATFORM=%1
+CX_ENVSCRIPTS=%2
+COMMAND=%3
 echo "hit the win batch script!"
 
 echo "args:" %*
+
+if "%TARGET_PLATFORM%" == "win64" 
+(
+  INIT_VS=%CX_ENVSCRIPTS%\cxVars_x64.bat
+  ARCH_FLAG="--b32"
+)
+
+if "%TARGET_PLATFORM%" == "win32" 
+(
+  INIT_VS=%CX_ENVSCRIPTS%\cxVars_x86.bat
+)
+
+%INIT_VS% && python -u %COMMAND% %ARCH_FLAG%
+
