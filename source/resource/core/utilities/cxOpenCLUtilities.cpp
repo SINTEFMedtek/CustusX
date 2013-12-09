25cd954a7c640117b4c8c5c4d85c95a0eede9cd6
Janne Beate Lervik Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2013-12-09 14:48:42 +0100

    Added more OpenCL print info.
    Former-commit-id: ae6dcefbf9265a3ce8babb560eb18e2f2a758607

--- source/resource/core/utilities/cxOpenCLUtilities.cpp
+++ source/resource/core/utilities/cxOpenCLUtilities.cpp
@@ -37,7 +37,19 @@ void OpenCLUtilities::printPlatformAndDeviceInfo()
 	cl_ulong maxAlocatableMem;			//this variable holds the maximum allocatable memory
 	cl_ulong localMem;					//this variable holds local memory for a device
 	cl_bool	available;					//this variable holds if the device is available
-
+	size_t maxWorkGroupSize;			//Maximum number of work-items in a work-group executing a kernel using the data parallel execution model.
+	cl_uint maxWorkItemDimensions;		//Maximum dimensions that specify the global and local work-item IDs used by the data parallel execution model. (Refer to clEnqueueNDRangeKernel). The minimum value is 3.
+	size_t maxWorkItemSize[1024];		//Maximum number of work-items that can be specified in each dimension of the work-group to clEnqueueNDRangeKernel
+	cl_bool imageSupport;				//Is CL_TRUE if images are supported by the OpenCL device and CL_FALSE otherwise.
+	size_t image3DMaxHeight;			//Max height of 3D image in pixels.
+	size_t image3DMaxWidth;				//Max width of 3D image in pixels.
+	size_t image3DMaxDepth;				//Max depth of 3D image in pixels.
+	size_t image2DMaxHeight;			//Max height of 2D image in pixels.
+	size_t image2DMaxWidth;				//Max width of 2D image in pixels.
+	cl_bool errorCorrectionSupport;		//Is CL_TRUE if the device implements error correction for the memories, caches, registers etc. in the device. Is CL_FALSE if the device does not implement error correction. This can be a requirement for certain clients of OpenCL.
+	size_t profilingTimerResolution;	//Describes the resolution of device timer. This is measured in nanoseconds.
+	cl_bool endianLittle;				//Is CL_TRUE if the OpenCL device is a little endian device and CL_FALSE otherwise.
+	char deviceProfile[1024];			//OpenCL profile string. Returns the profile name supported by the device.
 
 	//get the number of platforms
 	clGetPlatformIDs (32, platforms, &num_platforms);
@@ -68,7 +80,19 @@ void OpenCLUtilities::printPlatformAndDeviceInfo()
 			clGetDeviceInfo(devices[j], CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(maxAlocatableMem), &maxAlocatableMem, NULL);
 			clGetDeviceInfo(devices[j], CL_DEVICE_LOCAL_MEM_SIZE, sizeof(localMem), &localMem, NULL);
 			clGetDeviceInfo(devices[j], CL_DEVICE_AVAILABLE, sizeof(available), &available, NULL);
-
+			clGetDeviceInfo(devices[j], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(maxWorkGroupSize), &maxWorkGroupSize, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(maxWorkItemDimensions), &maxWorkItemDimensions, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(maxWorkItemSize), &maxWorkItemSize, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE_SUPPORT, sizeof(imageSupport), &imageSupport, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE2D_MAX_WIDTH, sizeof(image2DMaxWidth), &image2DMaxWidth, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE2D_MAX_HEIGHT, sizeof(image2DMaxHeight), &image2DMaxHeight, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE3D_MAX_WIDTH, sizeof(image3DMaxWidth), &image3DMaxWidth, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE3D_MAX_HEIGHT, sizeof(image3DMaxHeight), &image3DMaxHeight, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_IMAGE3D_MAX_DEPTH, sizeof(image3DMaxDepth), &image3DMaxDepth, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_ERROR_CORRECTION_SUPPORT, sizeof(errorCorrectionSupport), &errorCorrectionSupport, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_PROFILING_TIMER_RESOLUTION, sizeof(profilingTimerResolution), &profilingTimerResolution, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_ENDIAN_LITTLE, sizeof(endianLittle), &endianLittle, NULL);
+			clGetDeviceInfo(devices[j], CL_DEVICE_PROFILE, sizeof(deviceProfile), &deviceProfile, NULL);
 
 
 			//print out device information
@@ -77,12 +101,38 @@ void OpenCLUtilities::printPlatformAndDeviceInfo()
 			printf("\t\tVendor:\t\t\t\t%s\n", vendor);
 			printf("\t\tDevice supports:\t\t%s \n", deviceVersion);
 			printf("\t\tOpenCL driver version:\t\t%s \n", driverVersion);
+			printf("\t\tDevice Profile:\t\t\t%s \n", deviceProfile);
 			printf("\t\tAvailable:\t\t\t%s\n", available ? "Yes" : "No");
+			printf("\n");
+
 			printf("\t\tCompute Units:\t\t\t%u\n", numberOfCores);
 			printf("\t\tClock Frequency:\t\t%u mHz\n", clockFreq);
 			printf("\t\tGlobal Memory:\t\t\t%0.00f mb\n", (double)amountOfMemory/1048576);
 			printf("\t\tMax Allocateable Memory:\t%0.00f mb\n", (double)maxAlocatableMem/1048576);
-			printf("\t\tLocal Memory:\t\t\t%u kb\n\n", (unsigned int)localMem);
+			printf("\t\tLocal Memory:\t\t\t%u kb\n", (unsigned int)localMem);
+			printf("\n");
+
+			printf("\t\tError correction support:\t%s\n", errorCorrectionSupport ? "Yes" : "No");
+			printf("\t\tProfiling Timer Resolution:\t%u ns\n", profilingTimerResolution);
+			printf("\t\tEndian Little:\t\t\t%s\n", endianLittle ? "Yes" : "No");
+			printf("\n");
+
+			printf("\t\tMax work group size:\t\t%u\n", (unsigned int)maxWorkGroupSize);
+			printf("\t\tMax work item dimensions:\t%u\n", maxWorkItemDimensions);
+			for(int k = 0; k < maxWorkItemDimensions; k++)
+			{
+				printf("\t\t\tDimension %u supports maximum %u work items\n", k, (unsigned int)maxWorkItemSize[k]);
+			}
+			printf("\n");
+
+			printf("\t\tImage support:\t\t\t%s\n", imageSupport ? "Yes" : "No");
+			printf("\t\tImage2D max width:\t\t%u\n", (unsigned int)image2DMaxWidth);
+			printf("\t\tImage2D max height:\t\t%u\n", (unsigned int)image2DMaxHeight);
+			printf("\t\tImage3D max width:\t\t%u\n", (unsigned int)image3DMaxWidth);
+			printf("\t\tImage3D max height:\t\t%u\n", (unsigned int)image3DMaxHeight);
+			printf("\t\tImage3D max depth:\t\t%u\n", (unsigned int)image3DMaxWidth);
+
+			printf("\n");
 		}
 
 	}
