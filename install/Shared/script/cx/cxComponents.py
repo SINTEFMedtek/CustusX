087afb90057b6a082588e3296250ce5190c1c659
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-01-20 09:55:47 +0100

    Another try to set SHA to a functional TSF
    Former-commit-id: fd1086b41cded57facbd81d913a14b15c810cbf8

--- install/Shared/script/cx/cxComponents.py
+++ install/Shared/script/cx/cxComponents.py
@@ -439,7 +439,7 @@ class TubeSegmentationFramework(CppComponent):
     def _rawCheckout(self):
         self._getBuilder().gitClone('git@github.com:SINTEFMedisinskTeknologi/Tube-Segmentation-Framework.git')
     def update(self):
-        self._getBuilder().gitCheckout('c0c9594eac009b9ac026e8522dc01c0b8f89b378', submodules=True)
+        self._getBuilder().gitCheckout('80416ea1fd0245208b42c1d67f76ca177507a61a', submodules=True)
     def configure(self):
         builder = self._getBuilder()
         add = builder.addCMakeOption
