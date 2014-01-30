dab327c77885ed822744f751e331e8610e2702e3
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-01-30 13:41:51 +0100

    Created a script with aliases for simpler testing
    Former-commit-id: a7addffbb455c52070946945e24842d34a079431

---
+++ install/Shared/script/testing_aliases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#  linux_aliases.sh
+#
+#  Useful aliases for running tests
+#  To apply aliases to current shell. Run script this way: . ./linux_aliases.sh
+#
+#  Created by Ole Vegard Solberg on 1/30/14.
+#
+
+alias appleunitw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [unit]~[unstable]~[hide]~[not_apple]"
+alias appleunit="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [unit]~[unstable]~[hide]~[not_apple]"
+alias appleint="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [integration]~[unstable]~[hide]~[not_apple]"
+
+
+alias unitw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [unit]~[unstable]~[hide]~[not_linux]"
+alias unit="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [unit]~[unstable]~[hide]~[not_linux]"
+alias int="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [integration]~[unstable]~[hide]~[not_linux]"
