e3b2264cb54774eeac9e8229419a91b2893db4cd
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-02-05 09:07:07 +0100

    Made another test alias
    Former-commit-id: af5beb2ee1d5a4afa9ae497ff3bd0af0a08d2486

--- install/Shared/script/testing_aliases.sh
+++ install/Shared/script/testing_aliases.sh
@@ -11,8 +11,10 @@
 alias appleunitw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [unit]~[unstable]~[hide]~[not_apple]"
 alias appleunit="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [unit]~[unstable]~[hide]~[not_apple]"
 alias appleint="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [integration]~[unstable]~[hide]~[not_apple]"
+alias appletestw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest ~[unstable]~[hide]~[not_apple]"
 
 
 alias unitw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [unit]~[unstable]~[hide]~[not_linux]"
 alias unit="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch [unit]~[unstable]~[hide]~[not_linux]"
 alias int="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest [integration]~[unstable]~[hide]~[not_linux]"
+alias testw="~/dev/working/CustusX3/CustusX3/install/Shared/script/cxRunTests.py --run_catch --wrap_in_ctest ~[unstable]~[hide]~[not_linux]"
