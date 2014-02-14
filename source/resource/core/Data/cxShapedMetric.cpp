2638344c8b01ce8565cccace75f88c5c51c0a755
Christian Askeland <christian.askeland@sintef.no>
2014-02-14 10:49:57 +0100

    fixed zero-length direction in donutmetric
    Former-commit-id: 9209569d258d0e2e6d035bafafc730186f5d1d9a

--- source/resource/core/Data/cxShapedMetric.cpp
+++ source/resource/core/Data/cxShapedMetric.cpp
@@ -101,7 +101,10 @@ Vector3D DonutMetric::getDirection()
 	std::vector<Vector3D> coords = mArguments->getRefCoords();
 	if (coords.size()<2)
 		return Vector3D::UnitZ();
-	return (coords[1]-coords[0]).normal();
+	Vector3D diff = (coords[1]-coords[0]);
+	if (similar(diff.length(), 0.0))
+		return Vector3D(0,1,0);
+	return diff.normal();
 }
 
 
