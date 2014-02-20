122fa8cf6d197017cc50b9be285458d9ef41efc3
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-02-20 14:37:10 +0100

    #865: First version of multi volume picker, with tests that succeeds
    Former-commit-id: 10fb8ff051b4f1790a24c65932ee98d2d2c60643

--- source/ThirdParty/VTKMultiVolume/vtkMultiVolumePicker.cxx
+++ source/ThirdParty/VTKMultiVolume/vtkMultiVolumePicker.cxx
@@ -20,7 +20,16 @@
 #include "vtkImageData.h"
 #include "vtkVolume.h"
 #include "vtkVolumeMapper.h"
-#
+#include "vtkOpenGLGPUMultiVolumeRayCastMapper.h"
+#include "vtkGPUVolumeRayCastMapper.h"
+#include "vtkTransform.h"
+#include "vtkImageChangeInformation.h"
+
+typedef vtkSmartPointer<class vtkGPUVolumeRayCastMapper> vtkGPUVolumeRayCastMapperPtr;
+typedef vtkSmartPointer<class vtkVolume> vtkVolumePtr;
+typedef vtkSmartPointer<class vtkTransform> vtkTransformPtr;
+typedef vtkSmartPointer<class vtkMatrix4x4> vtkMatrix4x4Ptr;
+typedef vtkSmartPointer<vtkImageChangeInformation> vtkImageChangeInformationPtr;
 
 vtkStandardNewMacro(vtkMultiVolumePicker);
 
@@ -63,7 +72,57 @@ double vtkMultiVolumePicker::IntersectVolumeWithLine(const double p1[3],
                                                 vtkProp3D *prop, 
                                                 vtkAbstractVolumeMapper *mapper)
 {
-	return this->Superclass::IntersectVolumeWithLine(p1, p2, t1, t2, prop, mapper);
+	vtkOpenGLGPUMultiVolumeRayCastMapper* multivolumeMapper = dynamic_cast<vtkOpenGLGPUMultiVolumeRayCastMapper*>(mapper);
+	if(multivolumeMapper)
+	{
+//		std::cout << "multivolumeMapper NUMBER_OF_ADDITIONAL_VOLUMES: " << multivolumeMapper->NUMBER_OF_ADDITIONAL_VOLUMES << std::endl;
+		double retval = VTK_DOUBLE_MAX;
+
+		for(int i = 0; i < multivolumeMapper->NUMBER_OF_ADDITIONAL_VOLUMES; ++i)
+		{
+			double tempRetval;
+
+			vtkVolumePtr volume = vtkVolumePtr::New();
+			vtkImageData* image = multivolumeMapper->GetInput(i+1);
+			vtkVolumeProperty* property = multivolumeMapper->GetAdditionalProperty(i);
+
+			vtkMatrix4x4Ptr rMd0 = prop->GetUserMatrix();
+			if (!rMd0)
+			{
+//				std::cout << "No user matrix in prop" << std::endl;
+				rMd0 = vtkMatrix4x4Ptr::New();
+				rMd0->Identity();
+			}
+			vtkTransform* transform = multivolumeMapper->GetAdditionalInputUserTransform(i); //transform is d0Mdi
+			transform->PostMultiply();
+			transform->Concatenate(rMd0);//rMd0 * d0Mdi -> transform is now rMdi
+
+			double trans_pos[3];
+			transform->GetPosition(trans_pos);
+
+			vtkImageChangeInformationPtr info = vtkImageChangeInformationPtr::New();
+			info->SetInput(image);
+			info->SetOutputOrigin(trans_pos);
+			vtkImageData* tempImage = info->GetOutput();
+
+			vtkGPUVolumeRayCastMapperPtr singleVolumeMapper = vtkGPUVolumeRayCastMapperPtr::New();
+			singleVolumeMapper->SetBlendModeToComposite();
+			singleVolumeMapper->SetInput(tempImage);
+			singleVolumeMapper->Update();
+
+			volume->SetMapper(singleVolumeMapper);
+			volume->SetProperty(property);
+//			volume->SetUserTransform(transform); //Transform is not used by IntersectVolumeWithLine(), only vtkImageData::Origin()
+//			volume->Update();
+
+			tempRetval = this->Superclass::IntersectVolumeWithLine(p1, p2, t1, t2, volume, singleVolumeMapper);
+//			std::cout << "temp retval: " << tempRetval << std::endl;
+			retval = fmin(retval, tempRetval);
+		}
+
+		return retval;
+	} else
+		return this->Superclass::IntersectVolumeWithLine(p1, p2, t1, t2, prop, mapper);
 /*
 	double tMin = VTK_DOUBLE_MAX;
 
