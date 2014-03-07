be536233926135fc29b4024a698519e8970f1667
Christian Askeland <christian.askeland@sintef.no>
2014-03-07 10:31:16 +0100

    Refactor in progress: Removed toolManager() - compiles without tests, run fails.
    Former-commit-id: c27ceeb67bca6d911429d5cbd6abaa95e4c86244

---
+++ source/logic/cxLegacySingletons.h
@@ -0,0 +1,45 @@
+// This file is part of CustusX, an Image Guided Therapy Application.
+//
+// Copyright (C) 2008- SINTEF Technology & Society, Medical Technology
+//
+// CustusX is fully owned by SINTEF Medical Technology (SMT). CustusX source
+// code and binaries can only be used by SMT and those with explicit permission
+// from SMT. CustusX shall not be distributed to anyone else.
+//
+// CustusX is a research tool. It is NOT intended for use or certified for use
+// in a normal clinical setting. SMT does not take responsibility for its use
+// in any way.
+//
+// See CustusX_License.txt for more information.
+#ifndef CXLEGACYSINGLETONS_H
+#define CXLEGACYSINGLETONS_H
+
+#include "boost/shared_ptr.hpp"
+
+namespace cx
+{
+class DataManager;
+class ToolManager;
+typedef boost::shared_ptr<class SpaceProvider> SpaceProviderPtr;
+typedef boost::shared_ptr<class ToolManager> TrackingServicePtr;
+
+/** 
+ *
+ *
+ * \ingroup cx
+ * \date 23.02.2014, 2014
+ * \author christiana
+ */
+
+// old style singleton accessors:
+DataManager* dataManager();
+ToolManager* toolManager();
+SpaceProviderPtr spaceProvider();
+
+// access to new shared ptrs:
+TrackingServicePtr trackingService();
+
+} // namespace cx
+
+
+#endif // CXLEGACYSINGLETONS_H
