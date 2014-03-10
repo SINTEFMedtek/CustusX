f661b3a7753041e220779106c63a9b4a17476643
Christian <christian.askeland@sintef.no>
2014-03-10 13:11:53 +0100

    fixed compile error in tsf (due to recent refactor of services)
    Former-commit-id: 40209348f5a67dbe2ee580b401157e6adc2551ef

--- source/plugins/Algorithm/testing/cxTestTubeSegmentationFramework.h
+++ source/plugins/Algorithm/testing/cxTestTubeSegmentationFramework.h
@@ -24,8 +24,6 @@ class TestTubeSegmentationFramework
 public:
 	TestTubeSegmentationFramework();
 	~TestTubeSegmentationFramework();
-	void setUp();
-	void tearDown();
 
 	void testConstructor();///< Test constructor
 
