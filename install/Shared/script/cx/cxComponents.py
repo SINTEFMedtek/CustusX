adc0a4548acca481ab15f8152e1fdcf91e8ea048
Christian Askeland <christian.askeland@sintef.no>
2014-03-30 00:12:40 +0100

    new ge_streamer version, with bugfix for vtk6
    Former-commit-id: bacc3710f1af3f155b968ed586751c499a3612c4

--- install/Shared/script/cx/cxComponents.py
+++ install/Shared/script/cx/cxComponents.py
@@ -276,7 +276,7 @@ class IGSTK(CppComponent):
 
 class ISB_DataStreaming(CppComponent):
     def name(self):
-        self.mCurrentRevision = "536"
+        self.mCurrentRevision = "538"
         return "ISB_DataStreaming"
     def help(self):
         return 'ISB GE Digital Interface stuff'
