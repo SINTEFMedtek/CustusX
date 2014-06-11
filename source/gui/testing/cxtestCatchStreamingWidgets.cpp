822be5a5d7268a3467bb8f173bd8b12c43e9054d
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-06-11 09:08:05 +0200

    Enabled test: VideoConnectionWidget can stream. Reverted it to only test MHD file streaming
    Former-commit-id: 8154837f6d300d310206869aef366dd132efaa72

--- source/gui/testing/cxtestCatchStreamingWidgets.cpp
+++ source/gui/testing/cxtestCatchStreamingWidgets.cpp
@@ -6,33 +6,17 @@
 #include "cxtestQueuedSignalListener.h"
 #include "cxDataLocations.h"
 
-#include "cxReporter.h"
 #include "cxDummyToolManager.h"
-#include "cxDataManager.h"
-#include "cxToolManager.h"
-#include "cxVideoService.h"
-#include "cxVideoServiceBackend.h"
-#include "cxtestDummyDataManager.h"
 #include "cxLogicManager.h"
 
 namespace cxtest
 {
 
-//Test disabled. Create new tests for the pugin
-TEST_CASE("VideoConnectionWidget can stream", "[unit][gui][not_win32][widget][streaming][hide]")
+//Disabled test of SimulatedImageStreamer
+TEST_CASE("VideoConnectionWidget can stream", "[unit][gui][not_win32][widget][streaming]")
 {
 	cx::LogicManager::initialize();
-//	cxtest::TestServicesPtr services = cxtest::TestServices::create();
-//	cx::Reporter()->initialize();
-//	cx::cxDataManager::getInstance()->initialize();
 
-//	cx::cxToolManager::initializeObject();
-//	cx::cxToolManager* tm = cx::cxToolManager::getInstance();
-//	cx::TrackingServicePtr ts = services->trackingService();
-
-//	cx::VideoService::initialize(cx::VideoServiceBackend::create(services->dataService(),
-//																 services->trackingService(),
-//																 services->spaceProvider()));
 	cx::TrackingServicePtr ts = cx::logicManager()->getTrackingService();
 
 	cx::DummyToolPtr tool = cx::DummyToolTestUtilities::createDummyTool(cx::DummyToolTestUtilities::createProbeDataLinear(), ts);
@@ -43,14 +27,11 @@ TEST_CASE("VideoConnectionWidget can stream", "[unit][gui][not_win32][widget][st
 	QString filename = cx::DataLocations::getTestDataPath() + "/testing/TubeSegmentationFramework/Default.mhd";
 	REQUIRE(QFile::exists(filename));
 
-//	TestVideoConnectionWidget* widget = new TestVideoConnectionWidget();
-//	REQUIRE(widget->canStream(filename, "MHDFile"));
+	TestVideoConnectionWidget* widget = new TestVideoConnectionWidget();
+	REQUIRE(widget->canStream(filename, "MHDFile"));
 //	REQUIRE(widget->canStream(filename, "SimulatedImageStreamer"));
-//	delete widget;
+	delete widget;
 
-//	DummyToolManager::getInstance()->shutdown();
-//	cx::cxDataManager::getInstance()->shutdown();
-//	cx::Reporter()->shutdown();
 	ts.reset();
 	cx::LogicManager::shutdown();
 }
