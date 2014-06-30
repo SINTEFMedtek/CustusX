56bbbbf7ad65942ee341c68c3a27b210e6fc4d0a
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-06-30 13:11:49 +0200

    Reverted code from #967 to temporarily fix #972
    Former-commit-id: b1fd04ea5a95d166b32d335556d9adf016428cdb

--- source/modules/UsReconstruction/logic/cxReconstructPreprocessor.cpp
+++ source/modules/UsReconstruction/logic/cxReconstructPreprocessor.cpp
@@ -393,7 +393,8 @@ void ReconstructPreprocessor::findExtentAndOutputTransform()
 
 	// Calculate optimal output image spacing and dimensions based on US frame spacing
 	double inputSpacing = std::min(mFileData.mUsRaw->getSpacing()[0], mFileData.mUsRaw->getSpacing()[1]);
-	mOutputVolumeParams = OutputVolumeParams(extent, inputSpacing, mInput.mMaxOutputVolumeSize);
+	mOutputVolumeParams = OutputVolumeParams(extent, inputSpacing);
+	mOutputVolumeParams.setMaxVolumeSize(mInput.mMaxOutputVolumeSize);
 
 	if (dataManager())
 		mOutputVolumeParams.set_rMd((dataManager()->get_rMpr()) * prMd);
