2b20a878d0efc320fae0b08f944d6103d86e18ec
Christian Askeland <christian.askeland@sintef.no>
2014-08-28 13:07:41 +0200

    removed unused public method

--- source/modules/Registration/logic/cxRegistrationManager.cpp
+++ source/modules/Registration/logic/cxRegistrationManager.cpp
@@ -155,66 +155,6 @@ std::vector<QString> RegistrationManager::getUsableLandmarks(const LandmarkMap& 
   return retval;
 }
 
-/** Update the registration for data and all data connected to its space.
- *
- * Registration is done relative to masterFrame, i.e. data is moved relative to the masterFrame.
- *
- */
-void RegistrationManager::updateRegistration(QDateTime oldTime, RegistrationTransform deltaTransform, DataPtr data, QString masterFrameUid)
-{
-	mRegistrationService->updateRegistration(oldTime, deltaTransform, data, masterFrameUid);
-////	std::cout << "==== RegistrationManager::updateRegistration" << std::endl;
-//	FrameForest forest(dataService());
-//  QDomNode target = forest.getNode(qstring_cast(data->getUid()));
-//  QDomNode masterFrame = target;
-//  QDomNode targetBase = target;
-//  if (masterFrameUid!="")
-//  {
-//	  masterFrame = forest.getNode(masterFrameUid);
-//	  targetBase = forest.getOldestAncestorNotCommonToRef(target, masterFrame);
-//  }
-//  std::vector<DataPtr> targetData = forest.getDataFromDescendantsAndSelf(targetBase);
-
-//  std::stringstream ss;
-//  ss << "Update Registration using " << std::endl;
-//  ss << "\tFixed:\t" << masterFrameUid << std::endl;
-//  ss << "\tMoving:\t" << data->getUid() << std::endl;
-//  ss << "\tDelta matrix (rMd'=Delta*rMd)\n"+qstring_cast(deltaTransform.mValue) << std::endl;
-//  report(qstring_cast(ss.str()));
-
-//  // update the transform on all target data:
-//  for (unsigned i=0; i<targetData.size(); ++i)
-//  {
-//    RegistrationTransform newTransform = deltaTransform;
-//    newTransform.mValue = deltaTransform.mValue * targetData[i]->get_rMd();
-//    targetData[i]->get_rMd_History()->updateRegistration(oldTime, newTransform);
-
-//    report("Updated registration of data " + targetData[i]->getName());
-//    //std::cout << "rMd_new\n" << newTransform.mValue << std::endl; // too much noise for large patients
-//  }
-
-//  // reconnect only if master and target are unconnected, i.e. share a common ancestor.
-//  // If we are registrating inside an already connected tree we only want to change transforms,
-//  // not change the topology of the tree.
-//  if (forest.getOldestAncestor(target) != forest.getOldestAncestor(masterFrame))
-//  {
-//    // connect the target to the master's ancestor, i.e. replace targetBase with masterAncestor:
-//    QDomNode masterAncestor = forest.getOldestAncestor(masterFrame);
-//    // iterate over all target data,
-//    for (unsigned i=0; i<targetData.size(); ++i)
-//    {
-//      QString masterAncestorUid = masterAncestor.toElement().tagName();
-//      QString targetBaseUid = targetBase.toElement().tagName();
-
-//      if (targetData[i]->getParentSpace() == targetBaseUid)
-//      {
-//        report("Reset parent frame of " + targetData[i]->getName() + " to " + masterAncestorUid + ". targetbase=" + targetBaseUid);
-//        targetData[i]->get_rMd_History()->updateParentSpace(oldTime, ParentSpace(masterAncestorUid, deltaTransform.mTimestamp, deltaTransform.mType));
-//      }
-//    }
-//  }
-}
-
 /**Convert the landmarks given by uids to vtk points.
  * The coordinates are given by the input data,
  * and should be transformed by M.
@@ -547,15 +487,6 @@ void RegistrationManager::applyPatientOrientation(const Transform3D& tMtm)
 void RegistrationManager::applyImage2ImageRegistration(Transform3D delta_pre_rMd, QString description)
 {
 	mRegistrationService->applyImage2ImageRegistration(delta_pre_rMd, description);
-//	RegistrationTransform regTrans(delta_pre_rMd, QDateTime::currentDateTime(), description);
-//	DataPtr fixedData = this->getFixedData();
-//	regTrans.mFixed = fixedData ? fixedData->getUid() : "";
-//	DataPtr movingData = this->getMovingData();
-//	regTrans.mMoving = movingData ? movingData->getUid() : "";
-//	this->updateRegistration(mLastRegistrationTime, regTrans, movingData, regTrans.mFixed);
-//	mLastRegistrationTime = regTrans.mTimestamp;
-//	reportSuccess(QString("Image registration [%1] has been performed on %2").arg(description).arg(regTrans.mMoving) );
-//	patientService()->getPatientData()->autoSave();
 }
 
 /**\brief apply a new patient registration
@@ -567,13 +498,6 @@ void RegistrationManager::applyImage2ImageRegistration(Transform3D delta_pre_rMd
 void RegistrationManager::applyPatientRegistration(Transform3D rMpr_new, QString description)
 {
 	mRegistrationService->applyPatientRegistration(rMpr_new, description);
-//	RegistrationTransform regTrans(rMpr_new, QDateTime::currentDateTime(), description);
-//	DataPtr fixedData = this->getFixedData();
-//	regTrans.mFixed = fixedData ? fixedData->getUid() : "";
-//	dataManager()->get_rMpr_History()->updateRegistration(mLastRegistrationTime, regTrans);
-//	mLastRegistrationTime = regTrans.mTimestamp;
-//	reportSuccess(QString("Patient registration [%1] has been performed.").arg(description));
-//	patientService()->getPatientData()->autoSave();
 }
 
 void RegistrationManager::addXml(QDomNode& parentNode)
