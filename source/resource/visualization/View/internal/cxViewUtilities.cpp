af576eb5aeb158f0b1593cd025e7f008809849b4
Christian Askeland <christian.askeland@sintef.no>
2014-09-26 13:56:11 +0200

    refactored view system - renamed classes - forgot one file from last commit

---
+++ source/resource/visualization/View/internal/cxViewUtilities.cpp
@@ -0,0 +1,56 @@
+/*=========================================================================
+This file is part of CustusX, an Image Guided Therapy Application.
+
+Copyright (c) 2008-2014, SINTEF Department of Medical Technology
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=========================================================================*/
+
+#include "cxViewUtilities.h"
+#include <QGridLayout>
+
+namespace cx
+{
+namespace view_utils
+{
+
+void setStretchFactors(QGridLayout* layout, LayoutRegion region, int stretchFactor)
+{
+	// set stretch factors for the affected cols to 1 in order to get even distribution
+	for (int i = region.pos.col; i < region.pos.col + region.span.col; ++i)
+	{
+		layout->setColumnStretch(i, stretchFactor);
+	}
+	// set stretch factors for the affected rows to 1 in order to get even distribution
+	for (int i = region.pos.row; i < region.pos.row + region.span.row; ++i)
+	{
+		layout->setRowStretch(i, stretchFactor);
+	}
+}
+
+} // namespace view_utils
+} /* namespace cx */
