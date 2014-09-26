10b129abb47d3a985d15c3d85e07d7065412bb05
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-09-26 08:24:55 +0200

    Moved Pipeline to resource/filter

--- source/gui/dataAdapters/cxDataAdapterHelper.cpp
+++ source/gui/dataAdapters/cxDataAdapterHelper.cpp
@@ -33,7 +33,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "cxHelperWidgets.h"
 #include "cxLabeledLineEditWidget.h"
-#include "cxDataInterface.h"
 #include "cxDataSelectWidget.h"
 #include "cxSelectDataStringDataAdapter.h"
 
