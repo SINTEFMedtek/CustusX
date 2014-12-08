0a9313ba424727ae2bb1bf49690daff138e57cba
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2014-12-08 13:19:38 +0100

    Converted tests in logic to the new catch system. 98 test cases registered. Also added two files that should have been in the previous commit.

---
+++ source/resource/videoServer/testing/cxtestGrabberDummy.cpp
@@ -0,0 +1,12 @@
+/*
+ * cxtestGrabberDummy.cpp
+ *
+ *  Created on: 8. des. 2014
+ *      Author: jbake
+ */
+
+#include <testing/cxtestGrabberDummy.h>
+
+namespace cxtest {
+
+} /* namespace cxtest */
