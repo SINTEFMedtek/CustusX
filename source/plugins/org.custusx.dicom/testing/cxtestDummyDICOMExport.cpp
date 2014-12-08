6f99ef7316dd1630e9028b940f2b57fe9176baa4
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2014-12-08 15:51:06 +0100

    Converted test in dicom plugin to the new catch system. 166 test cases registered.

---
+++ source/plugins/org.custusx.dicom/testing/cxtestDummyDICOMExport.cpp
@@ -0,0 +1,12 @@
+/*
+ * cxtestDummyDICOMExport.cpp
+ *
+ *  Created on: 8. des. 2014
+ *      Author: jbake
+ */
+
+#include <testing/cxtestDummyDICOMExport.h>
+
+namespace cxtest {
+
+} /* namespace cxtest */
