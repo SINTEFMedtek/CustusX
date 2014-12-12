adebb21eae76a3427a09407f22136d7625084e9b
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2014-12-12 08:17:35 +0100

    Try to fix Linux build

--- source/resource/core/utilities/cxXmlOptionItem.cpp
+++ source/resource/core/utilities/cxXmlOptionItem.cpp
@@ -47,6 +47,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QTextStream>
 #include <QDomElement>
 #include <QStringList>
+#include <QMutex>
 #include "cxLogger.h"
 #include "cxTypeConversions.h"
 
