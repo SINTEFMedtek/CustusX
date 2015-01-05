f543f1fb8db821932fc52c476191862d539df0ee
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2015-01-05 14:44:09 +0100

    Fixed a typo.

--- source/plugins/org.custusx.registration.method.manual/testing/cxtestDummyRegistrationManual.h
+++ source/plugins/org.custusx.registration.method.manual/testing/cxtestDummyRegistrationManual.h
@@ -2,12 +2,12 @@
 #ifndef CXTESTDUMMYREGISTRATIONMANUAL_H_
 #define CXTESTDUMMYREGISTRATIONMANUAL_H_
 
-#include "cxtets_org_custusx_registration_method_manual_export.h"
+#include "cxtest_org_custusx_registration_method_manual_export.h"
 
 namespace cxtest
 {
 
-class CXTETS_ORG_CUSTUSX_REGISTRATION_METHOD_MANUAL_EXPORT DummyRegistrationManual
+class CXTEST_ORG_CUSTUSX_REGISTRATION_METHOD_MANUAL_EXPORT DummyRegistrationManual
 {
 };
 
