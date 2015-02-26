ee03679b214b14ea9274ebca49dfeff522761b17
Christian Askeland <christian.askeland@sintef.no>
2015-02-26 20:28:28 +0100

    debugged cx_get_git_is_on_tag() - did not return correct value

--- CMake/cxGetVersion.cmake
+++ CMake/cxGetVersion.cmake
@@ -40,14 +40,13 @@
 function(cx_get_git_is_on_tag _varResult)
 	find_package(Git REQUIRED)
 	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --dirty=IS_DIRTY
+		COMMAND ${GIT_EXECUTABLE} describe --exact-match
 		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		OUTPUT_VARIABLE GIT_OUTPUT
+		RESULT_VARIABLE GIT_RESULT
 		)
 
-	string(REGEX MATCH IS_DIRTY MATCHING_STRING ${GIT_OUTPUT})
-
-	if("${MATCHING_STRING}" STREQUAL "")
+	if(${GIT_RESULT} EQUAL 0)
 		set(RESULT ON)
 	else()
 		set(RESULT OFF)
