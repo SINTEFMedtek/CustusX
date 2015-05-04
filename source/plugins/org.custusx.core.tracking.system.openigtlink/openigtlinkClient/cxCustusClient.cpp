e72f7407b2bb6171082c5b0eeb213bea19c0ab4e
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2015-05-04 13:39:09 +0200

    Added class for handling old Custus/Ultrasonix igtlink server.

---
+++ source/plugins/org.custusx.core.tracking.system.openigtlink/openigtlinkClient/cxCustusClient.cpp
@@ -0,0 +1,20 @@
+#include "cxCustusClient.h"
+
+namespace cx
+{
+void CustusClient::process(const igtl::TransformMessage::Pointer body)
+{
+}
+
+void CustusClient::process(const igtl::ImageMessage::Pointer body)
+{
+}
+
+void CustusClient::process(const igtl::StatusMessage::Pointer body)
+{
+}
+
+void CustusClient::process(const igtl::StringMessage::Pointer body)
+{
+}
+}
