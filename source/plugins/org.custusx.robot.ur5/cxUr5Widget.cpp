ce400c1445d7192e74160aa4fb1822082b7923a3
Ole Vegard Solberg <ole.v.solberg@sintef.no>
2015-06-23 09:56:36 +0200

    Made inital (empty) version of ur5 plugin

---
+++ source/plugins/org.custusx.robot.ur5/cxUr5Widget.cpp
@@ -0,0 +1,65 @@
+/*=========================================================================
+This file is part of CustusX, an Image Guided Therapy Application.
+
+Copyright (c) 2008-2014, SINTEF Department of Medical Technology
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+   this list of conditions and the following disclaimer in the documentation 
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+   may be used to endorse or promote products derived from this software 
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=========================================================================*/
+
+#include "cxUr5Widget.h"
+#include <QLabel>
+#include <QVBoxLayout>
+
+namespace cx
+{
+
+Ur5Widget::Ur5Widget(QWidget* parent) :
+    QWidget(parent),
+    mVerticalLayout(new QVBoxLayout(this))
+{
+    this->setObjectName("Ur5Widget");
+    this->setWindowTitle("UR5 Robot");
+    this->setWhatsThis(this->defaultWhatsThis());
+
+    mVerticalLayout->addWidget(new QLabel("Hello Plugin!"));
+}
+
+Ur5Widget::~Ur5Widget()
+{
+}
+
+QString Ur5Widget::defaultWhatsThis() const
+{
+  return "<html>"
+      "<h3>Example plugin.</h3>"
+      "<p>Used for developers as a starting points for developing a new plugin</p>"
+      "</html>";
+}
+
+
+
+} /* namespace cx */
