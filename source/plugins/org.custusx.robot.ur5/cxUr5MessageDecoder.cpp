8aceb6d95232f1ef145ca7be97ba5ab577957c41
androst <a.ostvik@gmail.com>
2015-09-09 11:23:15 +0200

    Changed from sscanf_s to pickDouble usage

--- source/plugins/org.custusx.robot.ur5/cxUr5MessageDecoder.cpp
+++ source/plugins/org.custusx.robot.ur5/cxUr5MessageDecoder.cpp
@@ -42,35 +42,35 @@ Ur5State Ur5MessageDecoder::setRTState(QByteArray data)
 {
     Ur5State state;
 
-    sscanf_s(data.mid(0*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.timeSinceStart);
+    state.timeSinceStart = pickDouble(data,0);
 
-    sscanf_s(data.mid(31*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxis(0));
-    sscanf_s(data.mid(32*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxis(1));
-    sscanf_s(data.mid(33*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxis(2));
-    sscanf_s(data.mid(34*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngles(0));
-    sscanf_s(data.mid(35*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngles(1));
-    sscanf_s(data.mid(36*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngles(2));
+    state.jointPosition(0) = pickDouble(data,31*sizeof(double));
+    state.jointPosition(1) = pickDouble(data,32*sizeof(double));
+    state.jointPosition(2) = pickDouble(data,33*sizeof(double));
+    state.jointPosition(3) = pickDouble(data,34*sizeof(double));
+    state.jointPosition(4) = pickDouble(data,35*sizeof(double));
+    state.jointPosition(5) = pickDouble(data,36*sizeof(double));
 
-    sscanf_s(data.mid(37*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxisVelocity(0));
-    sscanf_s(data.mid(38*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxisVelocity(1));
-    sscanf_s(data.mid(39*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAxisVelocity(2));
-    sscanf_s(data.mid(40*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngleVelocity(0));
-    sscanf_s(data.mid(41*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngleVelocity(1));
-    sscanf_s(data.mid(42*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.jointAngleVelocity(2));
+    state.jointVelocity(0) = pickDouble(data,37*sizeof(double));
+    state.jointVelocity(1) = pickDouble(data,38*sizeof(double));
+    state.jointVelocity(2) = pickDouble(data,39*sizeof(double));
+    state.jointVelocity(3) = pickDouble(data,40*sizeof(double));
+    state.jointVelocity(4) = pickDouble(data,41*sizeof(double));
+    state.jointVelocity(5) = pickDouble(data,42*sizeof(double));
 
-    sscanf_s(data.mid(55*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAxis(0));
-    sscanf_s(data.mid(56*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAxis(1));
-    sscanf_s(data.mid(57*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAxis(2));
-    sscanf_s(data.mid(58*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAngles(0));
-    sscanf_s(data.mid(59*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAngles(1));
-    sscanf_s(data.mid(60*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.tcpAngles(2));
+    state.tcpAxis(0) = pickDouble(data,55*sizeof(double));
+    state.tcpAxis(1) = pickDouble(data,56*sizeof(double));
+    state.tcpAxis(2) = pickDouble(data,57*sizeof(double));
+    state.tcpAngles(0) = pickDouble(data,58*sizeof(double));
+    state.tcpAngles(1) = pickDouble(data,59*sizeof(double));
+    state.tcpAngles(2) = pickDouble(data,60*sizeof(double));
 
-    sscanf_s(data.mid(67*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.force(0));
-    sscanf_s(data.mid(68*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.force(1));
-    sscanf_s(data.mid(69*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.force(2));
-    sscanf_s(data.mid(70*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.torque(0));
-    sscanf_s(data.mid(71*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.torque(1));
-    sscanf_s(data.mid(72*sizeof(double),sizeof(double)).toHex().data(), "%llx",(unsigned long long *)&state.torque(2));
+    state.force(0) = pickDouble(data,67*sizeof(double));
+    state.force(1) = pickDouble(data,68*sizeof(double));
+    state.force(2) = pickDouble(data,69*sizeof(double));
+    state.torque(0) = pickDouble(data,70*sizeof(double));
+    state.torque(1) = pickDouble(data,71*sizeof(double));
+    state.torque(2) = pickDouble(data,72*sizeof(double));
 
     state.updated = true;
     return state;
@@ -124,7 +124,7 @@ bool Ur5MessageDecoder::isValidHeader(QByteArray data)
 //    return (typeLength.find(headerLength(data)) != typeLength.end()
 //            && types.find(headerID(data)) !=types.end());
 
-    return ((headerLength(data)==53 && headerID(data)==4) || (headerLength(data)==251 && headerID(data)==1));
+    return (headerLength(data)==53 && headerID(data)==4);
 }
 
 void Ur5MessageDecoder::pushState(QByteArray data,Ur5State &state)
@@ -163,16 +163,8 @@ void Ur5MessageDecoder::setJointData(QByteArray jointData, Ur5State &state)
 {
     for(int i=0;i<6;i++)
     {
-        if(i<3)
-        {
-            state.jointAxis(i) = pickDouble(jointData,i*41);
-            state.jointAxisVelocity(i) = pickDouble(jointData, i*41+2*sizeof(double));
-        }
-        else
-        {
-            state.jointAngles(i-3) = pickDouble(jointData,i*41);
-            state.jointAngleVelocity(i-3) = pickDouble(jointData,i*41+2*sizeof(double));
-        }
+        state.jointPosition(i) = pickDouble(jointData,i*41);
+        state.jointVelocity(i) = pickDouble(jointData,i*41+sizeof(double));
     }
 }
 
