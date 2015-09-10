1af6cdcfabda2fff82fc44ddf00d11a04d9d1a4b
androst <a.ostvik@gmail.com>
2015-09-10 12:15:29 +0200

    Removed unnecessary code from Misc information (advanced info) tab.

--- source/plugins/org.custusx.robot.ur5/widgets/cxUr5MiscInformation.cpp
+++ source/plugins/org.custusx.robot.ur5/widgets/cxUr5MiscInformation.cpp
@@ -12,7 +12,6 @@
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QScrollBar>
 #include <QtWidgets/QSpacerItem>
@@ -30,7 +29,6 @@ Ur5MiscInformationTab::Ur5MiscInformationTab(Ur5RobotPtr Ur5Robot, QWidget *pare
     setupUi(this);
 
     connect(mUr5Robot.get(),SIGNAL(stateUpdated()),this,SLOT(updateForceSlot()));
-    connect(mUr5Robot.get(),SIGNAL(stateUpdated()),this,SLOT(updatePositionSlot()));
 }
 
 Ur5MiscInformationTab::~Ur5MiscInformationTab()
@@ -84,158 +82,9 @@ void Ur5MiscInformationTab::setupUi(QWidget *parent)
     FLineEdit = new QLineEdit();
     coordInfoLayout->addWidget(FLineEdit,1,5,1,1);
 
-    QFrame *line = new QFrame();
-    line->setFrameShape(QFrame::HLine);
-    line->setFrameShadow(QFrame::Sunken);
-    coordInfoLayout->addWidget(line,8,0,1,10);
-
-    // Position label
-    coordInfoLayout->addWidget(new QLabel("X"), 9, 0, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Y"), 10, 0, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Z"), 11, 0, 1, 1, Qt::AlignHCenter);
-
-    // mm label
-    coordInfoLayout->addWidget(new QLabel("mm"), 9, 3, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("mm"), 10, 3, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("mm"), 11, 3, 1, 1);
-
-    // Ri orientation label
-    coordInfoLayout->addWidget(new QLabel("RX"), 12, 0, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("RZ"), 13, 0, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("RY"), 14, 0, 1, 1);
-
-    // Rad label
-    coordInfoLayout->addWidget(new QLabel("Rad"), 12, 3, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 13, 3, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 14, 3, 1, 1);
-
-    // X coordinate line edit
-    xPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(xPosLineEdit, 9, 2, 1, 1);
-
-    // Y coordinate line edit
-    yPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(yPosLineEdit, 10, 2, 1, 1);
-
-    // Line edit for Z position
-    zPosLineEdit = new QLineEdit();
-    zPosLineEdit->setObjectName(QStringLiteral("zPosLineEdit"));
-    coordInfoLayout->addWidget(zPosLineEdit, 11, 2, 1, 1);
-
-    // Line edit for RX orientation
-    rxLineEdit = new QLineEdit();
-    rxLineEdit->setObjectName(QStringLiteral("rxLineEdit"));
-    coordInfoLayout->addWidget(rxLineEdit, 12, 2, 1, 1);
-
-    // Line edit for RY orientation
-    ryLineEdit = new QLineEdit();
-    ryLineEdit->setObjectName(QStringLiteral("ryLineEdit"));
-    coordInfoLayout->addWidget(ryLineEdit, 13, 2, 1, 1);
-
-    // Line edit for RZ orientation
-    rzLineEdit = new QLineEdit();
-    rzLineEdit->setObjectName(QStringLiteral("rzLineEdit"));
-    coordInfoLayout->addWidget(rzLineEdit, 14, 2, 1, 1);
-
-
-    // JOINT
-
-    // Position label
-    coordInfoLayout->addWidget(new QLabel("Base"), 9, 4, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Shoulder"), 10, 4, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Elbow"), 11, 4, 1, 1, Qt::AlignHCenter);
-
-    // mm label
-    coordInfoLayout->addWidget(new QLabel("Rad"), 9, 6, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 10, 6, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 11, 6, 1, 1);
-
-    // Ri orientation label
-    coordInfoLayout->addWidget(new QLabel("Wrist 1"), 12, 4, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Wrist 2"), 13, 4, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Wrist 3"), 14, 4, 1, 1);
-
-    // Rad label
-    coordInfoLayout->addWidget(new QLabel("Rad"), 12, 6, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 13, 6, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 14, 6, 1, 1);
-
-    // X coordinate line edit
-    jxPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(jxPosLineEdit, 9, 5, 1, 1);
-
-    // Y coordinate line edit
-    jyPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(jyPosLineEdit, 10, 5, 1, 1);
-
-    // Line edit for Z position
-    jzPosLineEdit = new QLineEdit();
-    jzPosLineEdit->setObjectName(QStringLiteral("zPosLineEdit"));
-    coordInfoLayout->addWidget(jzPosLineEdit, 11, 5, 1, 1);
-
-    // Line edit for RX orientation
-    jrxLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(jrxLineEdit, 12, 5, 1, 1);
-
-    // Line edit for RY orientation
-    jryLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(jryLineEdit, 13, 5, 1, 1);
-
-    // Line edit for RZ orientation
-    jrzLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(jrzLineEdit, 14, 5, 1, 1);
-
-
-    // TCP
-
-    // Position label
-    coordInfoLayout->addWidget(new QLabel("X"), 9, 7, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Y"), 10, 7, 1, 1, Qt::AlignHCenter);
-    coordInfoLayout->addWidget(new QLabel("Z"), 11, 7, 1, 1, Qt::AlignHCenter);
-
-    // mm label
-    coordInfoLayout->addWidget(new QLabel("mm"), 9, 9, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("mm"), 10, 9, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("mm"), 11, 9, 1, 1);
-
-    // Ri orientation label
-    coordInfoLayout->addWidget(new QLabel("RX"), 12, 7, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("RZ"), 13, 7, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("RY"), 14, 7, 1, 1);
-
-    // Rad label
-    coordInfoLayout->addWidget(new QLabel("Rad"), 12, 9, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 13, 9, 1, 1);
-    coordInfoLayout->addWidget(new QLabel("Rad"), 14, 9, 1, 1);
-
-    // X coordinate line edit
-    txPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(txPosLineEdit, 9, 8, 1, 1);
-
-    // Y coordinate line edit
-    tyPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(tyPosLineEdit, 10, 8, 1, 1);
-
-    // Line edit for Z position
-    tzPosLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(tzPosLineEdit, 11, 8, 1, 1);
-
-    // Line edit for RX orientation
-    trxLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(trxLineEdit, 12, 8, 1, 1);
-
-    // Line edit for RY orientation
-    tryLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(tryLineEdit, 13, 8, 1, 1);
-
-    // Line edit for RZ orientation
-    trzLineEdit = new QLineEdit();
-    coordInfoLayout->addWidget(trzLineEdit, 14, 8, 1, 1);
-
     hLayout1->addLayout(coordInfoLayout);
     mainLayout->addLayout(hLayout1);
 }
-
 
 void Ur5MiscInformationTab::updateForceSlot()
 {
