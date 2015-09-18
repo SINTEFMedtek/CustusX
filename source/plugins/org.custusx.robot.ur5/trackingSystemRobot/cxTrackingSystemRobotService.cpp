3a5d343476ab7b0ede2f29f2630a14caf945577c
androst <a.ostvik@gmail.com>
2015-09-18 10:07:59 +0200

    Ported TrackingSystem functions from TrackingSystemBronchoscopyService

--- source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxTrackingSystemRobotService.cpp
+++ source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxTrackingSystemRobotService.cpp
@@ -1,15 +1,75 @@
+
 #include "cxTrackingSystemRobotService.h"
+
+#include "cxRobotTool.h"
 
 namespace cx
 {
 
-TrackingSystemRobotService::TrackingSystemRobotService()
+TrackingSystemRobotService::TrackingSystemRobotService(TrackingServicePtr trackingService, Ur5RobotPtr robot):
+    mBase(trackingService->getTrackingSystems().front()),
+    mTrackingService(trackingService),
+    mUr5Robot(robot)
 {
-
+    connect(mBase.get(),&TrackingSystemService::stateChanged, this, &TrackingSystemRobotService::onStateChanged);
+    this->onStateChanged();
 }
 
 TrackingSystemRobotService::~TrackingSystemRobotService()
 {
 }
+
+void TrackingSystemRobotService::onStateChanged()
+{
+    ToolPtr activeTool = mTrackingService->getActiveTool();
+    std::vector<ToolPtr> tools = mBase->getTools();
+    mTools.clear();
+
+    for(unsigned i=0; i<tools.size(); ++i)
+    {
+        if(tools[i] == activeTool)
+        {
+            RobotToolPtr current(new RobotTool(tools[i],mUr5Robot));
+            mTools.push_back(current);
+        }
+        else
+        {
+            mTools.push_back(tools[i]);
+        }
+    }
+    emit stateChanged();
+}
+
+ToolPtr TrackingSystemRobotService::getReference()
+{
+    return mBase->getReference();
+}
+
+
+std::vector<ToolPtr> TrackingSystemRobotService::getTools()
+{
+    return mTools;
+}
+
+Tool::State TrackingSystemRobotService::getState() const
+{
+    return mBase->getState();
+}
+
+void TrackingSystemRobotService::setState(const Tool::State val)
+{
+    mBase->setState(val);
+}
+
+void TrackingSystemRobotService::setLoggingFolder(QString loggingFolder)
+{
+    mBase->setLoggingFolder(loggingFolder);
+}
+
+TrackerConfigurationPtr TrackingSystemRobotService::getConfiguration()
+{
+    return mBase->getConfiguration();
+}
+
 
 } // namespace cx
