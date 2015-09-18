871d1de3118e09a738d1706d58d238e332c62b17
androst <a.ostvik@gmail.com>
2015-09-18 10:07:08 +0200

    Ported tool functions from BronchoscopyTool

--- source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxRobotTool.cpp
+++ source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxRobotTool.cpp
@@ -1,16 +1,112 @@
 #include "cxRobotTool.h"
+#include "cxTypeConversions.h"
+#include "cxDoubleProperty.h"
 
 namespace cx
 {
 
-RobotTool::RobotTool()
+RobotTool::RobotTool(ToolPtr base, Ur5RobotPtr robot):
+    ToolImpl(base->getUid(), "Robot tracker" +base->getName()),
+    mBase(base),
+    mUr5Robot(robot)
 {
-
+    connect(mBase.get(), &Tool::toolProbeSector, this, &Tool::toolProbeSector);
+    connect(mBase.get(), &Tool::tooltipOffset, this, &Tool::tooltipOffset);
+    connect(mBase.get(), &Tool::toolTransformAndTimestamp, this, &RobotTool::onToolTransformAndTimestamp);
+    connect(mBase.get(), &Tool::toolVisible, this, &Tool::toolVisible);
+    connect(mBase.get(), &Tool::tooltipOffset, this, &Tool::tooltipOffset);
+    connect(mBase.get(), &Tool::tps, this, &Tool::tps);
 }
 
 RobotTool::~RobotTool()
 {
 
 }
+
+void RobotTool::onToolTransformAndTimestamp(Transform3D prMt, double timestamp)
+{
+    m_prMt = mUr5Robot->getCurrentState().baseMee;
+    emit toolTransformAndTimestamp(m_prMt, timestamp);
+}
+
+std::set<Tool::Type> RobotTool::getTypes() const
+{
+    return mBase->getTypes();
+}
+
+vtkPolyDataPtr RobotTool::getGraphicsPolyData() const
+{
+    return mBase->getGraphicsPolyData();
+}
+
+Transform3D RobotTool::get_prMt() const
+{
+    return m_prMt;
+}
+
+bool RobotTool::getVisible() const
+{
+    return mBase->getVisible();
+}
+
+QString RobotTool::getUid() const
+{
+    return mUid;
+}
+
+QString RobotTool::getName() const
+{
+    return mName;
+}
+
+bool RobotTool::isCalibrated() const
+{
+    return mBase->isCalibrated();
+}
+
+double RobotTool::getTimestamp() const
+{
+    return mBase->getTimestamp();
+}
+
+// Just use the tool tip offset from the tool manager
+double RobotTool::getTooltipOffset() const
+{
+    return mBase->getTooltipOffset();
+}
+
+// Just use the tool tip offset from the tool manager
+void RobotTool::setTooltipOffset(double val)
+{
+    mBase->setTooltipOffset(val);
+}
+
+Transform3D RobotTool::getCalibration_sMt() const
+{
+    return mBase->getCalibration_sMt();
+}
+
+std::map<int, Vector3D> RobotTool::getReferencePoints() const
+{
+    return mBase->getReferencePoints();
+}
+
+bool RobotTool::isInitialized() const
+{
+    return mBase->isInitialized();
+}
+
+void RobotTool::set_prMt(const Transform3D& prMt, double timestamp)
+{
+    mBase->set_prMt(prMt, timestamp);
+
+}
+
+void RobotTool::setVisible(bool vis)
+{
+    mBase->setVisible(vis);
+}
+
+
 
 } // namespace cx
--- source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxRobotTool.h
+++ source/plugins/org.custusx.robot.ur5/trackingSystemRobot/cxRobotTool.h
@@ -3,15 +3,55 @@
 
 #include "org_custusx_robot_ur5_Export.h"
 #include "cxToolImpl.h"
+#include "cxUr5Robot.h"
 
 namespace cx
 {
+typedef boost::shared_ptr<class RobotTool> RobotToolPtr;
 
 class org_custusx_robot_ur5_EXPORT RobotTool: public ToolImpl
 {
+Q_OBJECT
+
 public:
-    RobotTool();
+    explicit RobotTool(ToolPtr base, Ur5RobotPtr robot);
     virtual ~RobotTool();
+
+    virtual std::set<Type> getTypes() const;
+    virtual vtkPolyDataPtr getGraphicsPolyData() const;
+    virtual Transform3D get_prMt() const;
+    virtual bool getVisible() const;
+    virtual QString getUid() const;
+    virtual QString getName() const;
+    virtual bool isCalibrated() const;
+    virtual double getTimestamp() const;
+
+    virtual double getTooltipOffset() const;
+    virtual void setTooltipOffset(double val);
+
+    virtual Transform3D getCalibration_sMt() const;
+    virtual std::map<int, Vector3D> getReferencePoints() const;
+
+    virtual TimedTransformMapPtr getPositionHistory() { return mBase->getPositionHistory(); }
+    virtual bool isInitialized() const;
+    virtual ProbePtr getProbe() const { return mBase->getProbe(); }
+    virtual bool hasReferencePointWithId(int id) { return mBase->hasReferencePointWithId(id); }
+    virtual TimedTransformMap getSessionHistory(double startTime, double stopTime) { return mBase->getSessionHistory(startTime, stopTime); }
+
+    virtual void set_prMt(const Transform3D& prMt, double timestamp);
+    virtual void setVisible(bool vis);
+
+    // extensions
+    ToolPtr getBase() { return mBase; }
+
+private slots:
+    void onToolTransformAndTimestamp(Transform3D matrix, double timestamp);
+
+private:
+    ToolPtr mBase;
+    Transform3D m_prMt;
+    Ur5RobotPtr mUr5Robot;
+
 };
 
 } /* namespace cx */
