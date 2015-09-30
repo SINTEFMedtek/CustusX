8ed883acbf93f9a2c36732d579e035ab8ffcc7de
androst <a.ostvik@gmail.com>
2015-09-30 16:58:31 +0200

    Setup LungSimulation class header for CustusX environment

--- source/plugins/org.custusx.robot.ur5/widgets/cxUr5LungSimulation.h
+++ source/plugins/org.custusx.robot.ur5/widgets/cxUr5LungSimulation.h
@@ -2,11 +2,29 @@
 #define UR5LUNGSIMULATION_H
 
 
-class Ur5LungSimulation
+#include "org_custusx_robot_ur5_Export.h"
+
+#include <QWidget>
+
+#include "cxUr5Robot.h"
+
+namespace cx
 {
+
+class org_custusx_robot_ur5_EXPORT Ur5LungSimulationTab: public QWidget
+{
+    Q_OBJECT
 public:
-    Ur5LungSimulation();
-    ~Ur5LungSimulation();
+    Ur5LungSimulationTab(Ur5RobotPtr Ur5Robot,QWidget *parent = 0);
+    ~Ur5LungSimulationTab();
+
+private:
+    Ur5RobotPtr mUr5Robot;
 };
+
+
+
+} // cx
+
 
 #endif // UR5LUNGSIMULATION_H
