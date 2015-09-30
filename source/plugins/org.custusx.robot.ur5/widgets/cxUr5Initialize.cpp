7ecb1d16a60a72b5af4358191ca88174a2aa26ce
androst <a.ostvik@gmail.com>
2015-09-30 17:10:28 +0200

    Changed initializetab to include start/stop tracking buttons. Made setLayout functions for better readability.

--- source/plugins/org.custusx.robot.ur5/widgets/cxUr5Initialize.cpp
+++ source/plugins/org.custusx.robot.ur5/widgets/cxUr5Initialize.cpp
@@ -4,9 +4,11 @@
 
 #include <QGridLayout>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QComboBox>
 #include <QLabel>
 #include <QApplication>
+#include <QGroupBox>
 
 namespace cx
 {
@@ -18,10 +20,11 @@ Ur5InitializeTab::Ur5InitializeTab(Ur5RobotPtr Ur5Robot,QWidget *parent) :
 {
     setupUi(this);
 
-    connect(connectButton,SIGNAL(clicked()),this,SLOT(connectButtonSlot()));
-    connect(initializeButton,SIGNAL(clicked()),this,SLOT(initializeButtonSlot()));
-    connect(disconnectButton,SIGNAL(clicked()),this,SLOT(disconnectButtonSlot()));   
-    connect(shutdownButton,SIGNAL(clicked()),this,SLOT(shutdownButtonSlot()));
+    connect(connectButton,&QPushButton::clicked,this,&Ur5InitializeTab::connectButtonSlot);
+    connect(disconnectButton,&QPushButton::clicked,this,&Ur5InitializeTab::disconnectButtonSlot);
+    connect(shutdownButton,&QPushButton::clicked,this,&Ur5InitializeTab::shutdownButtonSlot);
+    connect(startTrackingButton,&QPushButton::clicked,this,&Ur5InitializeTab::startTrackingSlot);
+    connect(stopTrackingButton,&QPushButton::clicked,this,&Ur5InitializeTab::stopTrackingSlot);
 }
 
 Ur5InitializeTab::~Ur5InitializeTab()
@@ -30,8 +33,66 @@ Ur5InitializeTab::~Ur5InitializeTab()
 
 void Ur5InitializeTab::setupUi(QWidget *parent)
 {   
-    QGridLayout *mainLayout = new QGridLayout(this);
-    mainLayout->setAlignment(Qt::AlignTop);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+    setRobotConnectionLayout(mainLayout);
+    setRobotTrackingLayout(mainLayout);
+
+    mainLayout->setSpacing(5);
+    mainLayout->setMargin(5);
+
+//    row ++;
+//    mainLayout->addWidget(new QLabel("Set origo for robot workspace"),row,0,1,2);
+
+//    row++;
+//    QTabWidget *setCoordinatesTab = new QTabWidget();
+//    QWidget *presetCoordinatesTab = new QWidget();
+//    QWidget *manualCoordinatesTab = new QWidget();
+//    setCoordinatesTab->addTab(presetCoordinatesTab,tr("Preset coordinates"));
+//    setCoordinatesTab->addTab(manualCoordinatesTab,tr("Manual coordinates"));
+//    mainLayout->addWidget(setCoordinatesTab,row,0,1,4);
+
+//    QHBoxLayout *presetCoordLayout = new QHBoxLayout(presetCoordinatesTab);
+//    presetCoordLayout->setAlignment(Qt::AlignTop);
+
+//    presetCoordLayout->addWidget(new QLabel("Choose origo: "));
+//    presetOrigoComboBox = new QComboBox();
+//    initializeButton=new QPushButton();
+//    presetCoordLayout->addWidget(presetOrigoComboBox);
+//    presetCoordLayout->addWidget(initializeButton);
+
+//    presetOrigoComboBox->clear();
+//    presetOrigoComboBox->insertItems(0, QStringList()
+//                                     << QApplication::translate("Ur5Widget", "Buttom right corner", 0)
+//                                     << QApplication::translate("Ur5Widget", "Current position", 0)
+//                                     );
+
+//    initializeButton->setToolTip(QApplication::translate("Ur5Widget", "Initialize workspace", 0));
+//    initializeButton->setText(QApplication::translate("Ur5Widget", "Initialize", 0));
+
+//    QGridLayout *manualCoordLayout = new QGridLayout(manualCoordinatesTab);
+//    manualCoordinatesLineEdit=new QLineEdit();
+//    initializeButton_2=new QPushButton();
+//    manualCoordLayout->setAlignment(Qt::AlignTop);
+//    manualCoordLayout->addWidget(new QLabel("Set coordinates: "), 0, 0, 1, 1);
+//    manualCoordLayout->addWidget(manualCoordinatesLineEdit,0,1,1,1);
+//    manualCoordLayout->addWidget(initializeButton_2,0,2,1,1);
+
+//    manualCoordinatesLineEdit->setText(QApplication::translate("Ur5Widget", "(x,y,z,rx,ry,rz)", 0));
+//    initializeButton_2->setToolTip(QApplication::translate("Ur5Widget", "Initialize workspace", 0));
+//    initializeButton_2->setText(QApplication::translate("Ur5Widget", "Initialize", 0));
+
+
+}
+
+void Ur5InitializeTab::setRobotConnectionLayout(QVBoxLayout *parent)
+{
+    QGroupBox* group = new QGroupBox("Robot connection");
+    group->setFlat(true);
+    parent->addWidget(group);
+
+    QGridLayout *mainLayout = new QGridLayout();
+    group->setLayout(mainLayout);
 
     int row = 0;
     ipLineEdit = new QLineEdit();
@@ -47,56 +108,9 @@ void Ur5InitializeTab::setupUi(QWidget *parent)
     icon.addFile("/icons/network-transmit-receive.ico", QSize(), QIcon::Normal, QIcon::On);
     connectButton->setIcon(icon);
     connectButton->setToolTip("Connect to robot");
-    connectButton->setText("Connect");   
+    connectButton->setText("Connect");
     connectButton->setCheckable(true);
     connectButton->setStyleSheet("QPushButton:checked { background-color: none; }");
-
-    row ++;
-    QFrame *line = new QFrame();
-    line->setFrameShape(QFrame::HLine);
-    line->setFrameShadow(QFrame::Sunken);
-    mainLayout -> addWidget(line,row,0,1,4);
-
-    row ++;
-    mainLayout->addWidget(new QLabel("Set origo for robot workspace"),row,0,1,2);
-
-    row++;
-    QTabWidget *setCoordinatesTab = new QTabWidget();
-    QWidget *presetCoordinatesTab = new QWidget();
-    QWidget *manualCoordinatesTab = new QWidget();
-    setCoordinatesTab->addTab(presetCoordinatesTab,tr("Preset coordinates"));
-    setCoordinatesTab->addTab(manualCoordinatesTab,tr("Manual coordinates"));
-    mainLayout->addWidget(setCoordinatesTab,row,0,1,4);
-
-    QHBoxLayout *presetCoordLayout = new QHBoxLayout(presetCoordinatesTab);
-    presetCoordLayout->setAlignment(Qt::AlignTop);
-
-    presetCoordLayout->addWidget(new QLabel("Choose origo: "));
-    presetOrigoComboBox = new QComboBox();
-    initializeButton=new QPushButton();
-    presetCoordLayout->addWidget(presetOrigoComboBox);
-    presetCoordLayout->addWidget(initializeButton);
-
-    presetOrigoComboBox->clear();
-    presetOrigoComboBox->insertItems(0, QStringList()
-                                     << QApplication::translate("Ur5Widget", "Buttom right corner", 0)
-                                     << QApplication::translate("Ur5Widget", "Current position", 0)
-                                     );
-
-    initializeButton->setToolTip(QApplication::translate("Ur5Widget", "Initialize workspace", 0));
-    initializeButton->setText(QApplication::translate("Ur5Widget", "Initialize", 0));
-
-    QGridLayout *manualCoordLayout = new QGridLayout(manualCoordinatesTab);
-    manualCoordinatesLineEdit=new QLineEdit();
-    initializeButton_2=new QPushButton();
-    manualCoordLayout->setAlignment(Qt::AlignTop);
-    manualCoordLayout->addWidget(new QLabel("Set coordinates: "), 0, 0, 1, 1);
-    manualCoordLayout->addWidget(manualCoordinatesLineEdit,0,1,1,1);
-    manualCoordLayout->addWidget(initializeButton_2,0,2,1,1);
-
-    manualCoordinatesLineEdit->setText(QApplication::translate("Ur5Widget", "(x,y,z,rx,ry,rz)", 0));
-    initializeButton_2->setToolTip(QApplication::translate("Ur5Widget", "Initialize workspace", 0));
-    initializeButton_2->setText(QApplication::translate("Ur5Widget", "Initialize", 0));
 
     row++;
     shutdownButton = new QPushButton(QIcon("/icons/application-exit-4.png"),"Shutdown");
@@ -104,6 +118,42 @@ void Ur5InitializeTab::setupUi(QWidget *parent)
     mainLayout->addWidget(shutdownButton,row,0,1,1);
     mainLayout->addWidget(disconnectButton,row,3,1,1);
 }
+
+void Ur5InitializeTab::setRobotTrackingLayout(QVBoxLayout *parent)
+{
+    QGroupBox* group = new QGroupBox("Robot Tracking");
+    group->setFlat(true);
+    parent->addWidget(group);
+
+    QGridLayout *mainLayout = new QGridLayout();
+    group->setLayout(mainLayout);
+
+    startTrackingButton = new QPushButton(tr("Start tracking"));
+    stopTrackingButton = new QPushButton(tr("Stop tracking"));
+
+    int row = 0;
+//    mainLayout->addWidget(configureTrackingButton, row, 0,1,1);
+//    mainLayout->addWidget(deconfigureTrackingButton,row,3,1,1);
+
+//    row++;
+//    mainLayout->addWidget(initializeTrackingButton,row,0,1,1);
+//    mainLayout->addWidget(deinitializeTrackingButton,row,3,1,1);
+
+//    row++;
+    mainLayout->addWidget(startTrackingButton,row,0,1,1);
+    mainLayout->addWidget(stopTrackingButton,row,3,1,1);
+}
+
+void Ur5InitializeTab::startTrackingSlot()
+{
+    mUr5Robot->startTracking();
+}
+
+void Ur5InitializeTab::stopTrackingSlot()
+{
+    mUr5Robot->stopTracking();
+}
+
 
 void Ur5InitializeTab::connectButtonSlot()
 {
