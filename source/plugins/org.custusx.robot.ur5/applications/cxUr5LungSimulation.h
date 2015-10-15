1a11237d5031461ab653a7e4b24ae38c32597522
androst <a.ostvik@gmail.com>
2015-10-15 15:16:06 +0200

    Added new lungsimulation class for handling application

--- source/plugins/org.custusx.robot.ur5/applications/cxUr5LungSimulation.h
+++ source/plugins/org.custusx.robot.ur5/applications/cxUr5LungSimulation.h
@@ -2,11 +2,42 @@
 #define UR5LUNGSIMULATION_H
 
 
-class Ur5LungSimulation
+#include "org_custusx_robot_ur5_Export.h"
+#include "cxUr5Robot.h"
+
+#include <QObject>
+
+namespace cx
 {
+
+class org_custusx_robot_ur5_EXPORT Ur5LungSimulation : public QObject
+{
+    Q_OBJECT
+
 public:
+    Ur5LungSimulation(Ur5RobotPtr Ur5Robot);
     Ur5LungSimulation();
     ~Ur5LungSimulation();
+
+signals:
+    void lungMovement(double t1, double t2, double t3, double t4);
+    void stopLungMovement();
+
+private slots:
+    void lungMovementSlot(double t1, double t2, double t3, double t4);
+    void continueLungMove();
+    void stopLungMove();
+    void startInspirationSequence();
+    void startExpirationSequence();
+
+private:
+    Ur5RobotPtr mUr5Robot;
+    QTimer *inspiration, *expiration;
+    Eigen::RowVectorXd lungMovementTiming;
+
+    std::vector<QString> mProgramQueue;
 };
+
+} // cx
 
 #endif // UR5LUNGSIMULATION_H
