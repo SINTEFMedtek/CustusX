896f56ea2e014b003f6f0fcdaf22fa3eda1080e8
androst <a.ostvik@gmail.com>
2015-10-23 10:03:47 +0200

    Changed access modifiers for several objects

--- source/plugins/org.custusx.robot.ur5/widgets/cxUr5PlannedMove.h
+++ source/plugins/org.custusx.robot.ur5/widgets/cxUr5PlannedMove.h
@@ -7,6 +7,7 @@
 #include <QPushButton>
 #include <QLineEdit>
 #include <QVBoxLayout>
+#include <QTextEdit>
 
 
 namespace cx
@@ -19,23 +20,31 @@ public:
     Ur5PlannedMoveTab(Ur5RobotPtr Ur5Robot, QWidget *parent = 0);
     virtual ~Ur5PlannedMoveTab();
 
-    QPushButton *runVTKButton, *openVTKButton;
-    QLineEdit *vtkLineEdit;
 
-    QLineEdit *accelerationLineEdit, *velocityLineEdit, *timeLineEdit, *blendRadiusLineEdit;
-
-protected slots:
+private slots:
     void runVTKfileSlot();
     void openVTKfileSlot();
     void goToOrigoButtonSlot();
     void blendRadiusChangedSlot();
+    void sendMessageSlot();
 
 private:
+    QPushButton *runVTKButton, *openVTKButton;
+    QPushButton *sendMessageButton;
+
+    QLineEdit *vtkLineEdit;
+
+    QLineEdit *accelerationLineEdit, *velocityLineEdit, *timeLineEdit, *blendRadiusLineEdit;
+
+
     void setupUi(QWidget *parent);
     Ur5RobotPtr mUr5Robot;
 
     void setMoveVTKWidget(QVBoxLayout *parent);
     void setMoveSettingsWidget(QVBoxLayout *parent);
+    void setTextEditorWidget(QVBoxLayout *parent);
+
+    QTextEdit *textEditor;
 };
 
 } // cx
