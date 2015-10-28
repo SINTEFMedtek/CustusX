736ecf506acda8afcafaf0b2ffa9a7275f9954bb
Christian <christian.askeland@sintef.no>
2015-10-28 15:05:14 +0100

    accept plugins with names containing -, this is primarily for external plugins with a path in their full name

--- CMake/cxUtilities.cmake
+++ CMake/cxUtilities.cmake
@@ -268,7 +268,7 @@ endmacro(cx_opengl_version)
 ###############################################################################
 function(getListOfVarsStartingWith _prefix _varResult)
     get_cmake_property(_vars VARIABLES)
-    string (REGEX MATCHALL "(^|;)${_prefix}[A-Za-z0-9/_\\.:]*" _matchedVars "${_vars}")
+    string (REGEX MATCHALL "(^|;)${_prefix}[A-Za-z0-9/_\\.:-]*" _matchedVars "${_vars}")
     set (${_varResult} ${_matchedVars} PARENT_SCOPE)
 endfunction()
 
