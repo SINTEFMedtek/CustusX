713bd3f6268a796366a67838f45cf96cbbccfe6b
Ole Vegard Solberg <Ole.V.Solberg@sintef.no>
2015-12-10 15:11:18 +0100

    Updated FAST library

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -517,7 +517,7 @@ class FAST(CppComponent):
         self._getBuilder().gitClone('git@github.com:smistad/FAST')
     def update(self):
         #self._getBuilder().gitCheckoutBranch('development', submodules=True)
-        self._getBuilder().gitCheckout('c288f30dd5b2e684355dc02d4078b0c642cbd8c4')
+        self._getBuilder().gitCheckout('92f3ebbc7e98330cf569909c5287eb091347a0a9')
 #        self._getBuilder()._gitSubmoduleUpdate()
     def configure(self):
         builder = self._getBuilder()
