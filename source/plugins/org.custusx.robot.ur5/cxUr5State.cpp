cc88c8d1c172cfeb8c922de6bcf9e9479a92b48b
androst <a.ostvik@gmail.com>
2016-01-22 12:22:43 +0100

    Removed unused and combined functionality into one initialisation

--- source/plugins/org.custusx.robot.ur5/cxUr5State.cpp
+++ source/plugins/org.custusx.robot.ur5/cxUr5State.cpp
@@ -4,36 +4,27 @@
 namespace cx
 {
 
-Ur5State::Ur5State()
-{
-    jointPosition = Eigen::RowVectorXd(6);
-    jointVelocity = Eigen::RowVectorXd(6);
-    jointVelocity << 0,0,0,0,0,0;
-    baseMee = Transform3D::Identity();
-    jacobian = Eigen::MatrixXd(6,6);
-}
-
-Ur5State::Ur5State(double x,double y,double z,double rx,double ry,double rz)
-{
-    cartAxis << x, y, z;
-    cartAngles << rx,ry,rz;
-
-    jointPosition = Eigen::RowVectorXd(6);
-    jointVelocity = Eigen::RowVectorXd(6);
-}
+//Ur5State::Ur5State()
+//{
+//    jointConfiguration = Eigen::RowVectorXd(6);
+//    jointVelocity = Eigen::RowVectorXd(6);
+//    jointVelocity << 0,0,0,0,0,0;
+//    jacobian = Eigen::MatrixXd(6,6);
+//}
 
 Ur5State::Ur5State(bool updt)
 {
     updated = updt;
 
-    jointPosition = Eigen::RowVectorXd(6);
+    jointConfiguration = Eigen::RowVectorXd(6);
     jointVelocity = Eigen::RowVectorXd(6);
-    baseMee = Transform3D::Identity();
+    jacobian = Eigen::MatrixXd(6,6);
 }
 
 Ur5State::~Ur5State()
 {
 }
+
 //===============================================
 
 Ur5MovementInfo::~Ur5MovementInfo()
