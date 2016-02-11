fd5df61cf19ed5a12a8592d9fc35adac5efaaa2e
androst <a.ostvik@gmail.com>
2016-02-11 11:33:19 +0100

    Added additional initialisations to state object

--- source/plugins/org.custusx.robot.ur5/cxUr5State.cpp
+++ source/plugins/org.custusx.robot.ur5/cxUr5State.cpp
@@ -11,10 +11,11 @@ Ur5State::Ur5State(bool updt)
     jointConfiguration = Eigen::RowVectorXd(6);
     jointVelocity = Eigen::RowVectorXd(6);
     jacobian = Eigen::MatrixXd(6,6);
-
     operationalVelocity = Eigen::RowVectorXd(6);
 
     operationalVelocity << 0,0,0,0,0,0;
+    jointVelocity << 0,0,0,0,0,0;
+    timeSinceStart = 0;
 }
 
 Ur5State::~Ur5State()
