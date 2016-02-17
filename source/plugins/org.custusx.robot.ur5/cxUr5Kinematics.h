f016da32ae3fe5604b44627709884d6f66824387
Developer <a.ostvik@gmail.com>
2016-02-17 16:10:50 +0100

    Bug fix in laboratory. Conflict with eigen constants

--- source/plugins/org.custusx.robot.ur5/cxUr5Kinematics.h
+++ source/plugins/org.custusx.robot.ur5/cxUr5Kinematics.h
@@ -17,12 +17,12 @@ namespace cx
  * \author Andreas Østvik
  */
 
-#define d1 0.089159
-#define a2 -0.42500
-#define a3 -0.39225
-#define d4 0.10915
-#define d5 0.09465
-#define d6 0.0823
+//#define d1 0.089159
+//#define a2 -0.42500
+//#define a3 -0.39225
+//#define d4 0.10915
+//#define d5 0.09465
+//#define d6 0.0823
 
 class org_custusx_robot_ur5_EXPORT Ur5Kinematics
 {
@@ -53,6 +53,13 @@ private:
     Eigen::MatrixXd getRotation(Transform3D pose) const;
     Eigen::RowVectorXd errorVector(Transform3D desiredPose, Transform3D currentPose) const;
     Eigen::MatrixXd pseudoInverse(Eigen::MatrixXd matrix) const;
+
+    static const double d1 = 0.089159;
+    static const double a2 = -0.42500;
+    static const double a3 = -0.39225;
+    static const double d4 = 0.10915;
+    static const double d5 = 0.09465;
+    static const double d6 = 0.0823;
 };
 
 } // cx
