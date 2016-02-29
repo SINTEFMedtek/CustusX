9882e3d2c954a358d5524bf94aee8dc0f8e09c10
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2016-02-29 13:37:09 +0100

    Added script for commiting to git using a specific name and email address.

---
+++ install/cxGitCommit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+#####################################################
+# Script for commiting to git with specified name, email and commit message
+# Author: Janne Beate Bakeng, SINTEF Medical Technology
+# Date:   2016.02.29
+#
+#################################################
+
+import argparse
+from cx.utils.cxShell import *
+
+#################################################
+#
+# This idiom means the below code only runs when executed from command line
+#
+#################################################
+def commit(name, email, message, directory):
+    shell.changeDir(directory)
+    shell.run('git -c user.name="'+name+'" -c user.email="'+email+'" commit -m"'+message+'"', keep_output=True, silent=False)
+
+
+#################################################
+#
+# This idiom means the below code only runs when executed from command line
+#
+#################################################
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Commit using supplied name, email and message.')
+    parser.add_argument('-n', '--name', required=True, help='specify your name')
+    parser.add_argument('-e', '--email',required=True, help='specify your email')
+    parser.add_argument('-m', '--message',required=True, help='specify your commit  message')
+    parser.add_argument('-d', '--directory', required=False, help='specify directory to run the commit command in', default=os.getcwd())
+    args = parser.parse_args()
+
+    commit(args.name, args.email, args.message, args.directory)
+    
