2a8fcc7b1aabe85d46a369efbdcf453228984702
Christian Askeland <christian.askeland@sintef.no>
2016-03-15 11:02:30 +0100

    CUSTUSX-32 training widget, created plugin and widget shell

---
+++ source/plugins/org.custusx.training/widgets/cxTrainingWidget.cpp
@@ -0,0 +1,71 @@
+/*=========================================================================
+This file is part of CustusX, an Image Guided Therapy Application.
+
+Copyright (c) 2008-2014, SINTEF Department of Medical Technology
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=========================================================================*/
+
+#include "cxTrainingWidget.h"
+#include <QtWidgets>
+#include <QPushButton>
+#include "cxHelpEngine.h"
+#include "cxHelpBrowser.h"
+
+namespace cx {
+
+TrainingWidget::TrainingWidget(ctkPluginContext* context, QWidget* parent) :
+		BaseWidget(parent, "TrainingWidget", "Training")
+{
+	mEngine.reset(new HelpEngine);
+
+	HelpBrowser *browser = new HelpBrowser(this, mEngine);
+//	connect(this, &HelpWidget::requestShowLink,
+//			browser, &HelpBrowser::setSource);
+//	mBrowser = browser;
+
+	QVBoxLayout* topLayout = new QVBoxLayout(this);
+
+	topLayout->addWidget(browser);
+
+	QHBoxLayout* buttonLayout = new QHBoxLayout;
+	topLayout->addLayout(buttonLayout);
+
+	mPreviousStepButton = new QPushButton("Previous");
+//	mPreviousStepButton->add
+	mNextStepButton = new QPushButton("Next");
+
+	buttonLayout->addWidget(mPreviousStepButton);
+	buttonLayout->addStretch(1);
+	buttonLayout->addWidget(mNextStepButton);
+}
+
+TrainingWidget::~TrainingWidget()
+{
+}
+
+} /* namespace cx */
