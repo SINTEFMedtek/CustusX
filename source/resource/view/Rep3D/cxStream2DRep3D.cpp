2ab4c5f8e41dd6d57c6af1a2509c260e12b7ba2d
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-03-30 14:24:12 +0200

    Fixed missing video in playback by updating PatientModelImplService::mProbeTools when TrackingService::stateChanged. Also removed now unused signal: TrackingService::newProbe

--- source/resource/view/Rep3D/cxStream2DRep3D.cpp
+++ source/resource/view/Rep3D/cxStream2DRep3D.cpp
@@ -38,6 +38,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "cxTool.h"
 #include "cxVideoSourceGraphics.h"
 #include "cxView.h"
+#include "cxLogger.h"
 
 namespace cx
 {
