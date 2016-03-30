be957c8ed18dc819f352d3a44d1358848968f4f5
Christian Askeland <christian.askeland@sintef.no>
2016-03-30 09:18:33 +0200

    added debug msg in build scripts

--- install/cx/utils/cxSSH.py
+++ install/cx/utils/cxSSH.py
@@ -41,6 +41,7 @@ class RemoteFileTransfer:
     def connect(self, remoteServer, user=None):
         self.host_name = remoteServer
 
+        print "connecting to %s, user=%s" % (remoteServer, user)
         self.client = self.paramiko.client.SSHClient()
         self.client.set_missing_host_key_policy(self.paramiko.client.AutoAddPolicy)
         self.client.load_system_host_keys()
