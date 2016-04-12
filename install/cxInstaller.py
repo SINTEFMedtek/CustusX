e2f6041ca2be746d767586d5eb1767786a8e09a5
Christian Askeland <christian.askeland@sintef.no>
2016-04-12 09:52:03 +0200

    removed dead code

--- install/cxInstaller.py
+++ install/cxInstaller.py
@@ -4,38 +4,6 @@
 # Unix setup script
 # Author: Christian Askeland, SINTEF Medical Technology
 # Date:   2012.01.19
-#
-# Description:
-#
-# Script for installation of new workstation for CustusX
-# based on description from:
-# http://cxserver.sintef.no:16080/wiki/index.php/Installeringsveiviser
-#
-# Run cxInstaller --help to get usage info.
-#
-# prerequisites:
-# boost, cmake, cppunit, eclipse
-# Need a github key installed.
-#
-# Default values:
-# - change the class Common
-#
-# default folder setup (feel free to modify)
-#
-# --root---external---ITK---ITK
-#    |         |       |----build_Debug  
-#    |         |       |----build_Release
-#    |         |       |----build32_Debug
-#    |         |
-#    |         |------VTK---VTK
-#    |                 |----build_Debug  
-#    |                 |----build_Release
-#    |                 |----build32_Debug
-#    |-----workspace
-#              |------CustusX---CustusX
-#                      |--------build_Debug  
-#                      |--------build_Release
-#                      |--------build32_Debug
 #             
 #################################################             
 
