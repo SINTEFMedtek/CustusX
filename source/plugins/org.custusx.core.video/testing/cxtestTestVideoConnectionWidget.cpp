2d58735b4127890b55fc2ee6d0d27ce28875268f
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-05-20 09:56:13 +0200

    Added more checks in failing test

--- source/plugins/org.custusx.core.video/testing/cxtestTestVideoConnectionWidget.cpp
+++ source/plugins/org.custusx.core.video/testing/cxtestTestVideoConnectionWidget.cpp
@@ -64,10 +64,10 @@ bool TestVideoConnectionWidget::canStream(QString filename)
 
 	QTest::mouseClick(mConnectButton, Qt::LeftButton); //connect
 
-	waitForQueuedSignal(mServices->video().get(), SIGNAL(connected(bool)), 1000);
-	waitForQueuedSignal(mServices->video().get(), SIGNAL(activeVideoSourceChanged()), 2000);
+	REQUIRE(waitForQueuedSignal(mServices->video().get(), SIGNAL(connected(bool)), 1000));
+	REQUIRE(waitForQueuedSignal(mServices->video().get(), SIGNAL(activeVideoSourceChanged()), 2000));
 	cx::VideoSourcePtr stream = mServices->video()->getActiveVideoSource();
-	waitForQueuedSignal(stream.get(), SIGNAL(newFrame()), 1000);
+	REQUIRE(waitForQueuedSignal(stream.get(), SIGNAL(newFrame()), 1000));
 	bool canStream = stream->isStreaming();
 
 	QTest::mouseClick(mConnectButton, Qt::LeftButton); //disconnect
