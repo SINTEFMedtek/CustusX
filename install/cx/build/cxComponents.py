80409e874eb5d58468bb631ca1edc0f3d47777f0
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-06-09 11:15:03 +0200

    CS-13: Use sha for CustusXData

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -569,7 +569,7 @@ class CustusXData(CppComponent):
         return '%s/CustusXData.git' % self.controlData.gitrepo_main_site_base
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckoutDefaultBranch()    
+        self._getBuilder().gitCheckout('1726a9b5c0c489ee75498c5c86c5666dedc19f17')
     def configure(self):
         pass
     def build(self):
