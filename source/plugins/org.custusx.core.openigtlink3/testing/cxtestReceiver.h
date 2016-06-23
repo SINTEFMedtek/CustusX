f7bf960322b3d1f8b21ecaa7d35a00cf8384d42f
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-06-23 18:12:34 +0200

    CX-114: Try to fix Window declspec error. Was using the wrong export define (and export define header file).

--- source/plugins/org.custusx.core.openigtlink3/testing/cxtestReceiver.h
+++ source/plugins/org.custusx.core.openigtlink3/testing/cxtestReceiver.h
@@ -1,7 +1,7 @@
 #ifndef CXTEST_RECEIVER_H
 #define CXTEST_RECEIVER_H
 
-#include "org_custusx_core_openigtlink3_Export.h"
+#include "cxtest_org_custusx_core_openigtlink3_export.h"
 
 #include "catch.hpp"
 
@@ -12,7 +12,7 @@
 namespace cxtest
 {
 
-class org_custusx_core_openigtlink3_EXPORT Receiver : public QObject
+class cxtest_org_custusx_core_openigtlink3_EXPORTS Receiver : public QObject
 {
 	Q_OBJECT
 	QVTK_OBJECT
