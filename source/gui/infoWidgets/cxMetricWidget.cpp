6525798c681f87df83e6cf669d9a93aaf51b6214
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-08-05 09:38:27 +0200

    CX-123: One line of text for each metric

--- source/gui/infoWidgets/cxMetricWidget.cpp
+++ source/gui/infoWidgets/cxMetricWidget.cpp
@@ -150,7 +150,7 @@ void MetricWidget::createActions(QActionGroup* group)
 	this->createAction(group, ":/icons/metric_distance.png", "Dist", "Create a new Distance Metric", SLOT(addDistanceButtonClickedSlot()));
 	this->createAction(group, ":/icons/metric_angle.png", "Angle", "Create a new Angle Metric",   SLOT(addAngleButtonClickedSlot()));
 	this->createAction(group, ":/icons/metric_plane.png", "Plane", "Create a new Plane Metric",   SLOT(addPlaneButtonClickedSlot()));
-	this->createAction(group, ":/icons/metric_sphere.png", "Sphere", "Create a new SphereMetric",   SLOT(addSphereButtonClickedSlot()));
+	this->createAction(group, ":/icons/metric_sphere.png", "Sphere", "Create a new Sphere Metric",   SLOT(addSphereButtonClickedSlot()));
 	this->createAction(group, ":/icons/metric_torus.png", "Torus", "Create a new Torus Metric",   SLOT(addDonutButtonClickedSlot()));
     this->createAction(group, ":/icons/metric_custom.png", "Custom", "Create a new Custom Metric",   SLOT(addCustomButtonClickedSlot()));
 	this->createAction(group, ":/icons/metric.png", "ROI", "Create a new Region of Interest Metric",   SLOT(addROIButtonClickedSlot()));
