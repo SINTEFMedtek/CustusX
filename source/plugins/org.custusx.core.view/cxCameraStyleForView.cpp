3407293eef0b386b7bfc281035fb026e073f82e4
Christian Askeland <christian.askeland@sintef.no>
2016-09-16 14:58:38 +0200

    MG-36 work in progress on creating adaptive camera on tool

--- source/plugins/org.custusx.core.view/cxCameraStyleForView.cpp
+++ source/plugins/org.custusx.core.view/cxCameraStyleForView.cpp
@@ -133,25 +133,19 @@ void CameraStyleForView::setModified()
 }
 
 void CameraStyleForView::applyCameraStyle()
-{	        
+{
 	vtkCameraPtr camera = this->getCamera();
 	if (!camera)
 		return;
 
-	double cameraOffset = camera->GetDistance();
-	Vector3D pos_old(camera->GetPosition());
-	Vector3D focus_old(camera->GetFocalPoint());
-	Vector3D vup_old(camera->GetViewUp());
-
-	Vector3D camera_r = pos_old;
-	Vector3D focus_r = focus_old;
-	Vector3D vup_r = vup_old;
+	CameraInfo cam_old(camera);
+	CameraInfo cam_new = cam_old;
 
 	if (!mStyle.mFocusROI.isEmpty())
 	{
 		DoubleBoundingBox3D roi_r = this->getROI(mStyle.mFocusROI).getBox();
 		if (roi_r != DoubleBoundingBox3D::zero())
-			focus_r = roi_r.center();
+			cam_new.focus = roi_r.center();
 	}
 
 	if (mFollowingTool)
@@ -162,113 +156,172 @@ void CameraStyleForView::applyCameraStyle()
 		double offset = mFollowingTool->getTooltipOffset();
 
 		// view up is relative to tool
-		vup_r = rMt.vector(Vector3D(-1, 0, 0));
+		cam_new.vup = rMt.vector(Vector3D(-1, 0, 0));
 
 		if (mStyle.mFocusFollowTool)
 		{
 			// set focus to tool offset point
-			focus_r = rMt.coord(Vector3D(0, 0, offset));
+			cam_new.focus = rMt.coord(Vector3D(0, 0, offset));
 		}
 
 		if (mStyle.mCameraFollowTool)
 		{
-			// set camera on the tool line, at a distance 'cameraOffset' from the focus.
+			// set camera on the tool line, keeping the previous distance from the focus.
 			Vector3D tooloffset = rMt.coord(Vector3D(0, 0, offset));
 			Vector3D e_tool = rMt.vector(Vector3D(0, 0, 1));
-			camera_r = NavigationAlgorithms::findCameraPosOnLineFixedDistanceFromFocus(tooloffset,
+			cam_new.pos = NavigationAlgorithms::findCameraPosOnLineFixedDistanceFromFocus(tooloffset,
 																					   e_tool,
-																					   cameraOffset,
-																					   focus_r);
+																					   cam_old.distance(),
+																					   cam_new.focus);
 		}
 	}
 
 	if (mStyle.mTableLock)
 	{
 		Vector3D table_up = mBackend->patient()->getOperatingTable().getVectorUp();
-		vup_r = table_up;
+		cam_new.vup = table_up;
 	}
 
 	if (mStyle.mCameraFollowTool)
-		camera_r = NavigationAlgorithms::elevateCamera(mStyle.mElevation, camera_r, focus_r, vup_r);
+	{
+		cam_new.pos = NavigationAlgorithms::elevateCamera(mStyle.mElevation, cam_new.pos, cam_new.focus, cam_new.vup);
+	}
 
 	// reset vup based on vpn (do not change vpn after this point)
-	if (!similar(vup_r, vup_old))
+	if (!similar(cam_new.vup, cam_old.vup))
 	{
-		Vector3D vpn_r = (camera_r-focus_r).normal();
-		vup_r = NavigationAlgorithms::orthogonalize_vup(vup_r, vpn_r, vup_old);
+		cam_new.vup = NavigationAlgorithms::orthogonalize_vup(cam_new.vup, cam_new.vpn(), cam_old.vup);
 	}
 
 	if (!mStyle.mAutoZoomROI.isEmpty())
 	{
-		RegionOfInterest roi_r = this->getROI(mStyle.mAutoZoomROI);
-		if (roi_r.isValid())
-		{
-			double viewAngle = camera->GetViewAngle()/180.0*M_PI;
-			Vector3D vpn = (camera_r-focus_r).normal();
-
-			this->getRenderer()->ComputeAspect();
-			double aspect[2];
-			this->getRenderer()->GetAspect(aspect);
-
-			double viewAngle_vertical = viewAngle;
-			double viewAngle_horizontal = viewAngle*aspect[0];
-
-			// move all calculations into a space (x,y,c)=(left,vup,focus)
-			// the space is oriented towards the camera,
-			// and can be used to define a ROI bounding box aligned to the viewport.
-			Vector3D left = cross(vup_r,vpn);
-			Transform3D M_proj = createTransformIJC(left, vup_r, focus_r).inv();
-			Vector3D proj_focus(0,0,0);
-			Vector3D proj_vup(0,1,0);
-			Vector3D proj_vpn(0,0,1);
-			DoubleBoundingBox3D proj_bb = roi_r.getBox(M_proj);
-			Vector3D camera_r_t = NavigationAlgorithms::findCameraPosByZoomingToROI(viewAngle_vertical,
-																					viewAngle_horizontal,
-																					proj_focus,
-																					proj_vup,
-																					proj_vpn,
-																					proj_bb);
-			camera_r_t = this->smoothZoomedCameraPosition(camera_r_t);
-			camera_r_t = M_proj.inv().coord(camera_r_t);
-			camera_r = camera_r_t;
-		}
+		cam_new = this->viewEntireAutoZoomROI(cam_new);
 	}
 
+//	if (mStyle.mCameraOnTooltip && mFollowingTool)
+//	{
+//		// Move the camera onto the tool tip, keeping the distance vector constant.
+//		// This gives the effect of _sitting on the tool tip_ while moving.
+//		// Alternative: Dont change focal point, change view angle instead.
+//		Vector3D delta = this->getToolTip_r() - cam_new.pos;
+//		cam_new.pos += delta;
+//		cam_new.focus += delta;
+//	}
+
+	this->updateCamera(cam_new);
+}
+
+Vector3D CameraStyleForView::getToolTip_r()
+{
+	Transform3D rMpr = mBackend->patient()->get_rMpr();
+	Transform3D prMt = mFollowingTool->get_prMt();
+	Transform3D rMt = rMpr * prMt;
+	double offset = mFollowingTool->getTooltipOffset();
+	Vector3D tool_r = rMt.coord(Vector3D(0, 0, offset));
+	return tool_r;
+}
+
+void debugPrint(CameraInfo info)
+{
+	CX_LOG_CHANNEL_DEBUG("CA") << "  info.focus="<<info.focus;
+	CX_LOG_CHANNEL_DEBUG("CA") << "  info.pos="<<info.pos;
+	CX_LOG_CHANNEL_DEBUG("CA") << "  info.vup="<<info.vup;
+	CX_LOG_CHANNEL_DEBUG("CA") << "  info.vpn() ="<<info.vpn();
+	CX_LOG_CHANNEL_DEBUG("CA") << "  info.distance() ="<<info.distance();
+}
+
+
+
+CameraInfo CameraStyleForView::viewEntireAutoZoomROI(CameraInfo info)
+{
+	CameraInfo retval = info;
+	if (mStyle.mAutoZoomROI.isEmpty())
+		return retval;
+
+	RegionOfInterest roi_r = this->getROI(mStyle.mAutoZoomROI);
+	if (!roi_r.isValid())
+		return retval;
+
+	double viewAngle = info.viewAngle/180.0*M_PI;
+
+	this->getRenderer()->ComputeAspect();
+	double aspect[2];
+	this->getRenderer()->GetAspect(aspect);
+
+	double viewAngle_vertical = viewAngle;
+	double viewAngle_horizontal = viewAngle*aspect[0];
+
+	// move all calculations into a space p: (x,y,c)=(left,vup,focus)
+	// used to define a ROI bounding box aligned to the viewport.
+	Vector3D left = cross(info.vup, info.vpn());
+	Transform3D pMr = createTransformIJC(left, info.vup, info.focus).inv();
+	CameraInfo cam_proj;
+	cam_proj.focus = pMr.coord(info.focus);
+	cam_proj.vup = pMr.vector(info.vup);
+	cam_proj.pos = pMr.coord(info.pos);
+
+	DoubleBoundingBox3D proj_bb = roi_r.getBox(pMr);
+
+	Vector3D dist = cam_proj.focus - cam_proj.pos;
+	cam_proj.pos = NavigationAlgorithms::findCameraPosByZoomingToROI(viewAngle_vertical,
+																	 viewAngle_horizontal,
+																	 cam_proj.focus,
+																	 cam_proj.vup,
+																	 cam_proj.vpn(),
+																	 proj_bb);
+	// keep focus at a const distance in front of the camera (if we dont do this, vpn might swap)
+	cam_proj.focus = (cam_proj.pos + dist).normal() * 100;
+
+	cam_proj.pos = this->smoothZoomedCameraPosition(cam_proj.pos);
+
+	retval.pos = pMr.inv().coord(cam_proj.pos);
+	retval.focus = pMr.inv().coord(cam_proj.focus);
+
+	// experimental:
+	// IF inside bb: move pos to tool tip
+	// IF in front of bb: do nothing.
+	// IF in bbx2: interpolate between the two
+	// IF behind bb: keep pos inside bb
 	if (mStyle.mCameraOnTooltip && mFollowingTool)
 	{
+		CX_LOG_CHANNEL_DEBUG("CA") << "";
+		Vector3D proj_tool = pMr.coord(this->getToolTip_r());
+		Vector3D e_z(0,0,1);
+		double tool_z = dot(proj_tool, e_z);
+		double bb_min_z = proj_bb[4];
+		double bb_max_z = proj_bb[5];
+		double bb_ext_z = proj_bb[5] + 50;
+		double bb_extension = 50; // distance from bb where we want to interpolate between on-tool and off-tool
+		CX_LOG_CHANNEL_DEBUG("CA") << "  tool_z="<<tool_z
+								   <<", bb_min_z="<<bb_min_z
+								   <<", bb_max_z="<<bb_max_z
+								   <<", bb_ext_z="<<bb_ext_z;
+
+		Vector3D new_pos;
+		if (tool_z < bb_min_z)
+		{
+			// behind roi: lock camera pos to closest pos inside bb
+			new_pos = proj_tool + (bb_min_z-tool_z)*e_z;
+			CX_LOG_CHANNEL_DEBUG("CA") << "  **  behind roi";
+		}
+		else
+		{
+			double s = (tool_z-bb_max_z)/bb_extension;
+			s = std::min(1.0, s);
+			s = std::max(0.0, s);
+			new_pos = (1.0-s)*proj_tool + (s)*cam_proj.pos;
+			CX_LOG_CHANNEL_DEBUG("CA") << "  **  roi pos= s=" << s;
+		}
+
 		// Move the camera onto the tool tip, keeping the distance vector constant.
 		// This gives the effect of _sitting on the tool tip_ while moving.
 		// Alternative: Dont change focal point, change view angle instead.
-
-		Transform3D rMpr = mBackend->patient()->get_rMpr();
-		Transform3D prMt = mFollowingTool->get_prMt();
-		Transform3D rMt = rMpr * prMt;
-		double offset = mFollowingTool->getTooltipOffset();
-		Vector3D tool_r = rMt.coord(Vector3D(0, 0, offset));
-
-		Vector3D delta = tool_r - camera_r;
-
-		camera_r += delta;
-		focus_r += delta;
+		Vector3D delta = pMr.inv().coord(new_pos) - retval.pos;
+		retval.pos += delta;
+		retval.focus += delta;
 	}
 
-	// When viewing a scene of 10mm size, errors of 0.01mm are noticeable.
-	// Keep tolerance well below this level:
-	double tol = 0.001;
-	if (similar(pos_old, camera_r, tol) && similar(focus_old, focus_r, tol) && similar(vup_old, vup_r,tol ))
-		return; // break update loop: this event is triggered by camera change.
-
-//	this->handleLights();
-
-	mBlockCameraUpdate = true;
-	camera->SetPosition(camera_r.begin());
-	camera->SetFocalPoint(focus_r.begin());
-	camera->SetViewUp(vup_r.begin());
-	camera->SetClippingRange(1, std::max<double>(1000, cameraOffset * 10));
-	if (mStyle.mCameraFollowTool && mFollowingTool)
-		camera->SetClippingRange(1, std::max<double>(1000, cameraOffset * 1.5));
-//	CX_LOG_CHANNEL_DEBUG("CA") << (camera_r-focus_r).length() <<  " |||  camera_r " << camera_r << ", focus_r " << focus_r;
-	mBlockCameraUpdate = false;
+	return retval;
 }
 
 void CameraStyleForView::handleLights()
@@ -294,18 +347,10 @@ Vector3D CameraStyleForView::smoothZoomedCameraPosition(Vector3D pos)
 {
 	Vector3D filteredPos = pos;
 	filteredPos[2] = mZoomJitterFilter.newValue(pos[2]);
-//	if (similar(pos[2], filteredPos[2]))
-//	{
-//		CX_LOG_CHANNEL_DEBUG("CA") << "---CHANGED " << filteredPos[2];
-//	}
-//	else
-//	{
-//		CX_LOG_CHANNEL_DEBUG("CA") << "---filtered " << pos[2] << ", --> " << filteredPos[2];
-//	}
 	return filteredPos;
 }
 
-RegionOfInterest CameraStyleForView::getROI(QString uid)
+RegionOfInterest CameraStyleForView::getROI(QString uid) const
 {
 	DataPtr data = mBackend->patient()->getData(uid);
 	RegionOfInterestMetricPtr roi = boost::dynamic_pointer_cast<RegionOfInterestMetric>(data);
@@ -407,6 +452,35 @@ CameraStyleData CameraStyleForView::getCameraStyle()
 	return mStyle;
 }
 
+void CameraStyleForView::updateCamera(CameraInfo info)
+{
+	vtkCameraPtr camera = this->getCamera();
+	if (!camera)
+		return;
+	CameraInfo cam_old(camera);
+
+	// When viewing a scene of 10mm size, errors of 0.01mm are noticeable.
+	// Keep tolerance well below this level:
+	double tol = 0.001;
+	if (similar(cam_old, info, tol))
+		return; // break update loop: this event is triggered by camera change.
+
+//	this->handleLights();
+
+	mBlockCameraUpdate = true;
+	camera->SetPosition(info.pos.begin());
+	camera->SetFocalPoint(info.focus.begin());
+	camera->SetViewUp(info.vup.begin());
+	camera->SetClippingRange(1, std::max<double>(1000, info.distance() * 10));
+	if (mStyle.mCameraFollowTool && mFollowingTool)
+		camera->SetClippingRange(1, std::max<double>(1000, info.distance() * 1.5));
+	mBlockCameraUpdate = false;
+}
+
+///--------------------------------------------------------
+///--------------------------------------------------------
+///--------------------------------------------------------
+
 JitterFilter::JitterFilter()
 {
 	currentValue = 0;
@@ -433,6 +507,27 @@ double JitterFilter::newValue(double value)
 		currentValue = value;
 	}
 	return currentValue;
+}
+
+///--------------------------------------------------------
+///--------------------------------------------------------
+///--------------------------------------------------------
+
+CameraInfo::CameraInfo(vtkCameraPtr camera)
+{
+	pos = Vector3D(camera->GetPosition());
+	focus = Vector3D(camera->GetFocalPoint());
+	vup = Vector3D(camera->GetViewUp());
+	viewAngle = camera->GetViewAngle();
+}
+
+bool similar(const CameraInfo &lhs, const CameraInfo &rhs, double tol)
+{
+	return (similar(lhs.pos, rhs.pos, tol) &&
+			similar(lhs.focus, rhs.focus, tol) &&
+			similar(lhs.vup, rhs.vup, tol) &&
+			similar(lhs.viewAngle, rhs.viewAngle, tol)
+			);
 }
 
 }//namespace cx
