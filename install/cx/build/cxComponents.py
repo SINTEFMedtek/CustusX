b9e3f58b03389b6955475359d877402ae1964391
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2016-10-05 11:11:18 +0200

    MG-57: Fixed VTK PNG reader: Reader now use spacing from png file.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -197,7 +197,7 @@ class VTK(CppComponent):
         return '%s/VTK' % self.controlData.gitrepo_open_site_base
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        tag = 'VTK-6-3-0.cx_patch_4'
+        tag = 'VTK-6-3-0.cx_patch_5'
         self._getBuilder().gitCheckout(tag)
     def configure(self):
         builder = self._getBuilder()
