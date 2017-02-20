734948fc23a40c6608d1987beef1392445bb2e94
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2017-02-20 10:53:45 +0100

    CX-117: Removed include

--- source/gui/infoWidgets/cxFrameMetricWrapper.cpp
+++ source/gui/infoWidgets/cxFrameMetricWrapper.cpp
@@ -35,7 +35,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QHBoxLayout>
 #include "cxLabeledComboBoxWidget.h"
 #include "cxSpaceProvider.h"
-#include "cxPatientModelService.h"
 #include "cxSpaceEditWidget.h"
 #include "cxVisServices.h"
 
