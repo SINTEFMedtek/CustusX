84b48f47c94cbe96666d0a46d8503125540096c7
Janne Beate Bakeng <janne.beate.bakeng@sintef.no>
2017-05-03 13:00:33 +0200

    Changed SHA for CX data repo.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -575,7 +575,7 @@ class CustusXData(CppComponent):
         return '%s/CustusXData.git' % self.controlData.gitrepo_main_site_base
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckout('cd17744e2657c7ca15859ef2cef7560b4057ebfa')
+        self._getBuilder().gitCheckout('a9b32e4019a38fbbe66d499208cb61889f0042f1')
     def configure(self):
         pass
     def build(self):
