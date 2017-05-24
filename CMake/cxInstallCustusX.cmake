bfb4300103b892378a34bf7d3772adee5e729ee1
Jon Eiesland <jon.eiesland@sintef.no>
2017-05-24 19:03:26 +0200

    FX-19: Need Cuda 8.0 for the latest FAST. Works manually and hopefully this script sets the build environment on the build machine as well. FAST is also now built dynamically on windows. So we need the cmake to write the bin folder to the set_run_environment.bat.

--- CMake/cxInstallCustusX.cmake
+++ CMake/cxInstallCustusX.cmake
@@ -69,6 +69,7 @@ cx_install_add_library_dirs(
     ${Level-Set-Segmentation_LIBRARY_DIRS}
     ${Tube-Segmentation-Framework_LIBRARY_DIRS}
     ${FAST_LIBRARY_DIRS}
+    ${FAST_DIR}/bin
     ${OpenCLUtilityLibrary_LIBRARY_DIRS}
     ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
     ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/plugins
