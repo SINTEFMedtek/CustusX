9a673e09f7d7268574d28d6bc7e53098821ad8f7
Erik Smistad <ersmistad@gmail.com>
2017-05-26 16:52:39 +0200

    Updated FAST SHA

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -531,7 +531,7 @@ class FAST(CppComponent):
 		return 'git@github.com:smistad/FAST'
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckout('afcbf93d71d4b05eb5fb81b67af65f22a8f85d53')
+        self._getBuilder().gitCheckout('5e3ccf7f45d23c7aef2515a43e48323395a37f68')
 #        branch = 'set_kernel_root_dir'
 #        self._getBuilder()._changeDirToSource()
 #        runShell('git checkout %s' % branch, ignoreFailure=False)
