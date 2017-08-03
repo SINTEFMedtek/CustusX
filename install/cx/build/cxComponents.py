bc35be991c3ad48eb78b3420cf6c25e7dbbfce23
Jon Eiesland <jon.eiesland@sintef.no>
2017-08-03 12:11:16 +0200

    FX-30: Updated the sha to the data repo.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -513,7 +513,7 @@ class CustusXData(CppComponent):
         return '%s/CustusXData.git' % self.controlData.gitrepo_main_site_base
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckout('2826cc1d7e96188d4857f515e7022b1fca6bded1')
+        self._getBuilder().gitCheckout('56c5d483d615e7c37ec48f0865ff043383ba8308')
     def configure(self):
         pass
     def build(self):
