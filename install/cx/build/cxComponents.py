68977b6643b4e61761f7d799ef03a97af755639b
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2017-08-09 09:50:11 +0200

    CX-267: Updated CTK to get bugfix for Sierra build

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -237,7 +237,7 @@ class CTK(CppComponent):
         base = self.controlData.gitrepo_open_site_base
         return '%s/CTK.git' % base
     def update(self):
-        self._getBuilder().gitCheckout('9440d3c9d4cc49de79470fdab2f27218d549c224')
+        self._getBuilder().gitCheckout('36458b3b373fe19d30bb1759abc8d25e8bf2df33')
         self._getBuilder().gitSetRemoteURL(self.repository())
     def configure(self):
         builder = self._getBuilder()
