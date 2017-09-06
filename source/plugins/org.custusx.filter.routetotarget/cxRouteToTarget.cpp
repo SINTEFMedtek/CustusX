6b723860a54567438c986515377277d095e6aa66
Erlend Fagertun Hofstad <Erlend.Hofstad@sintef.no>
2017-09-06 12:19:21 +0200

    CX-268: Added comments and help text according to code review recommendations.

--- source/plugins/org.custusx.filter.routetotarget/cxRouteToTarget.cpp
+++ source/plugins/org.custusx.filter.routetotarget/cxRouteToTarget.cpp
@@ -98,6 +98,12 @@ void RouteToTarget::findRoutePositions()
 	searchBranchUp(mProjectedBranchPtr, mProjectedIndex);
 }
 
+/*
+    RouteToTarget::searchBranchUp is finding all positions from a given index on a branch and up
+    the airway tree to the top of trachea. All positions are added to mRoutePositions, which stores
+    all positions along the route-to-target.
+    Before the positions are added they are smoothed by RouteToTarget::smoothBranch.
+*/
 void RouteToTarget::searchBranchUp(BranchPtr searchBranchPtr, int startIndex)
 {
     std::vector< Eigen::Vector3d > positions = smoothBranch(searchBranchPtr, startIndex, searchBranchPtr->getPositions().col(startIndex));
@@ -165,6 +171,16 @@ vtkPolyDataPtr RouteToTarget::addVTKPoints(std::vector<Eigen::Vector3d> position
 	return retval;
 }
 
+/*
+    RouteToTarget::smoothBranch is smoothing the positions of a centerline branch by using vtkCardinalSpline.
+    The degree of smoothing is dependent on the branch radius and the shape of the branch.
+    First, the method tests if a straight line from start to end of the branch is sufficient by the condition of
+    all positions along the line being within the lumen of the airway (max distance from original centerline
+    is set to branch radius).
+    If this fails, one more control point is added to the spline at the time, until the condition is fulfilled.
+    The control point added for each iteration is the position with the larges deviation from the original/unfiltered
+    centerline.
+*/
 std::vector< Eigen::Vector3d > RouteToTarget::smoothBranch(BranchPtr branchPtr, int startIndex, Eigen::MatrixXd startPosition)
 {
     vtkCardinalSplinePtr splineX = vtkSmartPointer<vtkCardinalSpline>::New();
@@ -182,7 +198,9 @@ std::vector< Eigen::Vector3d > RouteToTarget::smoothBranch(BranchPtr branchPtr, 
     splineZ->AddPoint(0,startPosition(2));
 
 
-    //add last position if no parent branch, else add parents first position
+    // Add last position if no parent branch, else add parents position closest to current branch.
+    // Branch positions are stored in order from head to feet (e.g. first position is top of trachea),
+    // while route-to-target is generated from target to top of trachea.
     if(!branchPtr->getParentBranch())
     {
         splineX->AddPoint(startIndex,positions(0,0));
@@ -198,7 +216,8 @@ std::vector< Eigen::Vector3d > RouteToTarget::smoothBranch(BranchPtr branchPtr, 
 
     }
 
-    //add points untill all filtered postions are within branch radius distance of original position
+    //Add points until all filtered/smoothed postions are within branch radius distance of the unfiltered branch centerline posiition.
+    //This is to make sure the smoothed centerline is within the lumen of the airways.
     double maxDistanceToOriginalPosition = branchRadius;
     int maxDistanceIndex = -1;
     std::vector< Eigen::Vector3d > smoothingResult;
