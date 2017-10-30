088879daf175f10ab9f08a1a9084e0f004b2d3af
Ole Veagrd Solberg <olevegard.solberg@sintef.no>
2017-10-30 11:44:32 +0100

    CX-89: Don't traverse subdirectories, use GLOB instead of GLOB_RECURSE

--- CMake/cxInstallUtilities.cmake
+++ CMake/cxInstallUtilities.cmake
@@ -501,7 +501,7 @@ function(cx_fixup_and_add_qtplugins_to_bundle APPS_LOCAL INSTALL_BINARY_DIR DIRS
     #	PATTERN "libpthread.so" EXCLUDE
     #	PATTERN "qtcreator/*" EXCLUDE)
 
-    file(GLOB_RECURSE INSTALL_FILE_LIST
+    file(GLOB INSTALL_FILE_LIST
       LIST_DIRECTORIES false
       "${QT_LIBS_DIR}/*xcb*"
       "${QT_LIBS_DIR}/libfontconfig.*"
