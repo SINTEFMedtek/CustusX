9898d15187b7fc3f1d2015130ac66ba92c6afa82
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2018-08-09 15:00:02 +0200

    CX-373: Only test OpenIGTLinkServer app with -h argument

--- install/cx/build/cxCustusXTestInstallation.py
+++ install/cx/build/cxCustusXTestInstallation.py
@@ -56,7 +56,7 @@ class CustusXTestInstallation:
         appPath = self._getInstalledBinaryPath()
         self._testExecutable(appPath, 'Catch', '-h')
         self._testExecutable(appPath, self.system_base_name) # defaults to CustusX
-        self._testExecutable(appPath, 'OpenIGTLinkServer')
+        self._testExecutable(appPath, 'OpenIGTLinkServer', '-h')
         self._testExecutable(appPath, 'LogConsole')
                      
     def runUnstableTests(self):
