fe9b73a91be38ec53463c84482a2174e47f2c204
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2018-10-17 15:55:23 +0200

    CX-256: Added new copyright notice to new files.

--- source/plugins/org.custusx.core.filemanager/filereaderwriters/cxPolyDataMeshReader.cpp
+++ source/plugins/org.custusx.core.filemanager/filereaderwriters/cxPolyDataMeshReader.cpp
@@ -1,3 +1,14 @@
+/*=========================================================================
+This file is part of CustusX, an Image Guided Therapy Application.
+
+Copyright (c) SINTEF Department of Medical Technology.
+All rights reserved.
+
+CustusX is released under a BSD 3-Clause license.
+
+See Lisence.txt (https://github.com/SINTEFMedtek/CustusX/blob/master/License.txt) for details.
+=========================================================================*/
+
 #include "cxPolyDataMeshReader.h"
 
 #include <QFileInfo>
