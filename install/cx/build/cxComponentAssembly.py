942fb76dc235e10d4a30147376d744d78695a9de
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2018-11-02 11:37:36 +0100

    FX-60: Add component for org.custusx.bronchoscopynavigation plugin to build script.

--- install/cx/build/cxComponentAssembly.py
+++ install/cx/build/cxComponentAssembly.py
@@ -51,6 +51,7 @@ class LibraryAssembly(object):
         self.addComponent(cxComponents.FAST())
         self.addComponent(cxComponents.org_custusx_angleCorrection())
         self.addComponent(cxComponents.org_custusx_mariana())
+        self.addComponent(cxComponents.org_custusx_bronchoscopynavigation())
         self.addComponent(self.custusx)
         self.addComponent(cxComponents.CustusXData())
         
