6cef7d4a89f5d0f10c03649b437ac9a9e22a114a
Torgrim Lie <torgrim.lie@sintef.no>
2019-04-24 09:34:53 +0200

    CX-361: Fixed whitespace

--- source/plugins/org.custusx.dicom/core/cxDicomConverter.cpp
+++ source/plugins/org.custusx.dicom/core/cxDicomConverter.cpp
@@ -56,15 +56,15 @@ QString DicomConverter::generateUid(DicomImageReaderPtr reader)
 	// name: find something from series
 	QString currentTimestamp = QDateTime::currentDateTime().toString(timestampSecondsFormat());
 	QString uid = QString("%1_%2_%3").arg(seriesDescription).arg(seriesNumber).arg(currentTimestamp);
-    uid = this->convertToValidName(uid);
+	uid = this->convertToValidName(uid);
 	return uid;
 }
 
 QString DicomConverter::convertToValidName(QString text) const
 {
 	QStringList illegal;
-    illegal << "\\s" << "\\." << ":" << ";" << "\\<" << "\\>" << "\\*"
-            << "\\^" << "," << "\\%";
+	illegal << "\\s" << "\\." << ":" << ";" << "\\<" << "\\>" << "\\*"
+			<< "\\^" << "," << "\\%";
 	QRegExp regexp(QString("(%1)").arg(illegal.join("|")));
 	text = text.replace(regexp, "_");
 	return text	;
@@ -74,8 +74,8 @@ QString DicomConverter::convertToValidName(QString text) const
 QString DicomConverter::generateName(DicomImageReaderPtr reader)
 {
 	QString seriesDescription = reader->item()->GetElementAsString(DCM_SeriesDescription);
-    QString name = convertToValidName(seriesDescription);
-    return name;
+	QString name = convertToValidName(seriesDescription);
+	return name;
 }
 
 ImagePtr DicomConverter::createCxImageFromDicomFile(QString filename, bool ignoreLocalizerImages)
@@ -202,16 +202,16 @@ double DicomConverter::getMeanSliceDistance(std::map<double, ImagePtr> sorted) c
 
 	// check for multislice image
 	vtkImageDataPtr first = sorted.begin()->second->getBaseVtkImageData();
-    if (first->GetDimensions()[2]>1)
-        return first->GetSpacing()[2];
+	if (first->GetDimensions()[2]>1)
+		return first->GetSpacing()[2];
 
 	if (sorted.size()<2)
 		return 0;
 
-    // use average of all slices
+	// use average of all slices
 	double zValueLastImage = sorted.rbegin()->first;
-    double zValueFirstImage = sorted.begin()->first;
-    unsigned long numHolesBetweenImages = sorted.size() - 1;
+	double zValueFirstImage = sorted.begin()->first;
+	unsigned long numHolesBetweenImages = sorted.size() - 1;
 	return (zValueLastImage-zValueFirstImage)/numHolesBetweenImages;
 }
 
@@ -244,8 +244,8 @@ ImagePtr DicomConverter::mergeSlices(std::map<double, ImagePtr> sorted) const
 	appender->Update();
 
 	vtkImageDataPtr wholeImage = appender->GetOutput();
-    Eigen::Array3d spacing(wholeImage->GetSpacing());
-    spacing[2] = this->getMeanSliceDistance(sorted);
+	Eigen::Array3d spacing(wholeImage->GetSpacing());
+	spacing[2] = this->getMeanSliceDistance(sorted);
 	wholeImage->SetSpacing(spacing.data());
 
 	retval->setVtkImageData(wholeImage);
