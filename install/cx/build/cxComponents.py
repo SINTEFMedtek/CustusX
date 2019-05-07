8b2d270ee92fbda5eed74d359df0eb82f9d4feb3
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2019-05-07 14:51:45 +0200

    FX-64: Mariana: Stream positions. Now with correct sha to Mariana plugin.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -624,7 +624,7 @@ class org_custusx_mariana(CppComponent):
       self._getBuilder().gitSetRemoteURL(self.repository())
 #      self._getBuilder().gitCheckout('')
 #      self._getBuilder().gitCheckoutDefaultBranch()#Not using sha here because we always want to use the develop branch in the Mariana repo
-      self._getBuilder().gitCheckout('0538c9f581276dacb06d0bbc7fe08bdfe0853acd')
+      self._getBuilder().gitCheckout('0484ba81c7e9e383d77b972964c93dec1c663bae')
     def configure(self):
         pass
     def build(self):
