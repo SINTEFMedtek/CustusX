170f6b837b0fc6f4467d15bfc35da5708b277cbf
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2019-05-22 09:47:04 +0200

    FX-64: Updated Mariana plugin repository

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -624,7 +624,7 @@ class org_custusx_mariana(CppComponent):
       self._getBuilder().gitSetRemoteURL(self.repository())
 #      self._getBuilder().gitCheckout('')
 #      self._getBuilder().gitCheckoutDefaultBranch()#Not using sha here because we always want to use the develop branch in the Mariana repo
-      self._getBuilder().gitCheckout('d3dc6316387eb38156232257293ee6ef7c949a47')
+      self._getBuilder().gitCheckout('b817a3e5d5e02ed1bf1c96a6b40c2fef479a8dae')
     def configure(self):
         pass
     def build(self):
