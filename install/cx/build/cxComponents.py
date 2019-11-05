8a54f17d033bb612629b2d145145262f2d99d362
ehof <erlend.hofstad@sintef.no>
2019-11-05 11:19:27 +0100

    CS-100: Updated SHA for Mariana.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -624,7 +624,8 @@ class org_custusx_mariana(CppComponent):
       self._getBuilder().gitSetRemoteURL(self.repository())
 #      self._getBuilder().gitCheckout('')
 #      self._getBuilder().gitCheckoutDefaultBranch()#Not using sha here because we always want to use the develop branch in the Mariana repo
-      self._getBuilder().gitCheckout('b0983699c2fca41f45633b1e06c1d9cde6259bb0')
+#      self._getBuilder().gitCheckout('b0983699c2fca41f45633b1e06c1d9cde6259bb0')
+       self._getBuilder().gitCheckout('e28fcc14c42fba1bd96ed13e37410663274a4b4e')
     def configure(self):
         pass
     def build(self):
