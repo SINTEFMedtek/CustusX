40905e902f6e878405fe08b34b38bfd21ad46e2d
Erlend Fagertun Hofstad <Erlend.Hofstad@sintef.no>
2020-01-14 15:20:09 +0100

    CX-422: Bugfix.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -629,7 +629,7 @@ class org_custusx_mariana(CppComponent):
       self._getBuilder().gitSetRemoteURL(self.repository())
 #      self._getBuilder().gitCheckout('')
 #      self._getBuilder().gitCheckoutDefaultBranch()#Not using sha here because we always want to use the develop branch in the Mariana repo
-      self._getBuilder().gitCheckout('55d76dddf8750e4b2198265d97d432b0a02dcece')
+      self._getBuilder().gitCheckout('186fdb8b7c9d48b91a14b4b750e0a07b29a5c819')
     def configure(self):
         pass
     def build(self):
