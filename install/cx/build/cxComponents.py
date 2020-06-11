350ef3d0759b9da6c670205e3f6c6db781011b9d
ehof <erlend.hofstad@sintef.no>
2020-06-11 14:07:05 +0200

    FX-71: Updated Mariana sha.

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -638,7 +638,7 @@ class org_custusx_mariana(CppComponent):
 #      self._getBuilder().gitCheckout('')
 #      self._getBuilder().gitCheckoutDefaultBranch()#Not using sha here because we always want to use the develop branch in the Mariana repo
 #      self._getBuilder().gitCheckout('186fdb8b7c9d48b91a14b4b750e0a07b29a5c819')
-      self._getBuilder().gitCheckout('37398c58a045056db30727de6d1dae11edbe263b')
+      self._getBuilder().gitCheckout('fe59da209bf4e5eb8caf23ed142f16c3e9b6d690')
     def configure(self):
         pass
     def build(self):
