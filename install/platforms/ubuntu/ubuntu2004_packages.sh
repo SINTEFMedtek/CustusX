bbdbd5903cafdd9929424e81dd373ddac0eb6409
Ole Vegard Solberg <olevegard.solberg@sintef.no>
2023-01-25 11:19:59 +0100

    CX-509: Fixed ubuntu2004_packages.sh

--- install/platforms/ubuntu/ubuntu2004_packages.sh
+++ install/platforms/ubuntu/ubuntu2004_packages.sh
@@ -22,7 +22,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 # developer stuff
 sudo apt-get install -y build-essential opencl-headers libssl-dev 
-sudo apt-get install -y qt4-dev-tools qt4-designer libboost-all-dev libcppunit-dev libglew-dev
+sudo apt-get install -y libboost-all-dev libcppunit-dev libglew-dev
 sudo apt-get install -y qttools5-dev qttools5-dev-tools qttools5-doc qt5-doc libqt5webkit5-dev libqt5xmlpatterns5-dev libqt5x11extras5-dev qtmultimedia5-dev
 sudo apt-get install -y ninja-build
 # developer IDE's and apps
@@ -103,3 +103,6 @@ sudo apt install -y vlc-plugin-access-extra gstreamer1.0-libav
 
 #VTK 9: GLVND libraries
 sudo apt install -y libglvnd-dev
+
+#For more convenient use of CustusX build scripts
+sudo apt install -y python-is-python3
