c5d003118f03d01625d9a9e3ca86b9486412c9ea
Ole Vegard Solberg <olevegard.solberg@sintef.no>
2023-02-14 14:06:44 +0100

    CX-509: Fixed incorrect Python syntax in previous commit

--- install/cx/build/cxComponents.py
+++ install/cx/build/cxComponents.py
@@ -477,7 +477,7 @@ class CustusX(CppComponent):
         add('SSC_USE_GCOV:BOOL', self.controlData.mCoverage);
         add('CX_SYSTEM_BASE_NAME:STRING', self.controlData.system_base_name)
         add('CX_SYSTEM_DEFAULT_APPLICATION:STRING', self.controlData.system_base_name)
-        if(platform.system() == 'Darwin')
+        if(platform.system() == 'Darwin'):
           add('CMAKE_PREFIX_PATH:PATH', "/opt/local/libexec/qt5-mac")
         # See CX-208 about this Eigen flag and about updating Eigen.
         # The second one should be used when upgrading
