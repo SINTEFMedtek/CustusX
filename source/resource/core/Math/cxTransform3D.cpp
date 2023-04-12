6bc275b80b3724d199845590bb4f966eea34767f
Geir Arne Tangen <Geir.A.Tangen@sintef.no>
2023-04-12 12:06:45 +0200

    CS-107: Fixed build error on macOS. Use of math function in boost.

--- source/resource/core/Math/cxTransform3D.cpp
+++ source/resource/core/Math/cxTransform3D.cpp
@@ -18,6 +18,7 @@ See Lisence.txt (https://github.com/SINTEFMedtek/CustusX/blob/master/License.txt
 #include "cxTypeConversions.h"
 #include "cxBoundingBox3D.h"
 #include "vtkForwardDeclarations.h"
+#include <boost/math/special_functions/fpclassify.hpp> // isnan
 
 // --------------------------------------------------------
 namespace cx_transform3D_internal
