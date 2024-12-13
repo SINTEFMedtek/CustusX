93d44d2b11590a63e1626ae409fb4b90bc44d27a
Erlend Fagertun Hofstad <ehof@sintef.no>
2024-12-13 13:03:35 +0100

    Removed cxTotalSegmentatorFilter before merging into develop. Fraxinus#20

--- source/gui/filterWidgets/cxFiltersWidget.cpp
+++ source/gui/filterWidgets/cxFiltersWidget.cpp
@@ -26,7 +26,6 @@ See Lisence.txt (https://github.com/SINTEFMedtek/CustusX/blob/master/License.txt
 #include "cxResampleImageFilter.h"
 #include "cxColorVariationFilter.h"
 #include "cxIslandsFilter.h"
-#include "cxTotalSegmentatorFilter.h"
 #include "cxFilterPresetWidget.h"
 #include "cxDilationFilter.h"
 #include "cxMeshesFromLabelsFilter.h"
@@ -129,7 +128,6 @@ void FiltersWidget::appendFiltersThatAreNotServices(VisServicesPtr services)
     this->appendFilterIfWanted(FilterPtr(new ContourFilter(services)));
 	this->appendFilterIfWanted(FilterPtr(new IslandsFilter(services)));
     this->appendFilterIfWanted(FilterPtr(new ColorVariationFilter(services)));
-	this->appendFilterIfWanted(FilterPtr(new TotalSegmentatorFilter(services)));
 	this->appendFilterIfWanted(FilterPtr(new SmoothingImageFilter(services)));
     this->appendFilterIfWanted(FilterPtr(new ResampleImageFilter(services)));
 	this->appendFilterIfWanted(FilterPtr(new DilationFilter(services)));
