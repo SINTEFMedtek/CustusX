3c02e9bc99df0c5ee85e4fa2728ad3b48391a15c
Ole Vegard Solberg <olevegard.solberg@sintef.no>
2024-12-20 10:52:21 +0100

    Update cxCreateVenv.sh to install TotalSegmentator. Fraxinus#20

--- config/profiles/Laboratory/filter_scripts/cxCreateVenv.sh
+++ config/profiles/Laboratory/filter_scripts/cxCreateVenv.sh
@@ -22,12 +22,29 @@ if [ -z $reqPath ]; then
 fi
 
 cd "$venvBasePath";
-python3 -m venv venv;
-source venv/bin/activate;
-pip install --upgrade pip;
-if [[ $string == *"."* ]] || [[ $string == *"/"* ]]; then
+if [[ $reqPath == *"."* ]] || [[ $reqPath == *"/"* ]]; then # If using requirements.txt
+  python3 -m venv venv;
+  source venv/bin/activate;
+  pip install --upgrade pip;
   python -m pip install -r "$reqPath/requirements.txt";
-else
+elif [[ $reqPath == "TotalSegmentator" ]]; then
+  ubuntuVersion="$(lsb_release -rs)"
+  if [[ $ubuntuVersion == *"20.04"* ]]; then
+    echo "Ubuntu 20.04 - Installing Python 3.10 packages"
+    sudo apt install software-properties-common -y #Needed for Python 3.10 on Ubuntu20.04
+    sudo add-apt-repository ppa:deadsnakes/ppa -y #Needed for Python 3.10 on Ubuntu20.04
+  fi
+  sudo apt install -y python3.10-venv
+  python3.10 -m venv venv
+  source venv/bin/activate
+  pip install --upgrade pip
+  pip install TotalSegmentator
+  totalseg_download_weights -t total
+  totalseg_download_weights -t lung_vessels
+else #Install other program, not tested
+  python3 -m venv venv;
+  source venv/bin/activate;
+  pip install --upgrade pip;
   python -m pip install $reqPath;
 fi
 
