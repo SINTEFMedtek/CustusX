98ae49d977ea678dcd887e85f6ceb3899b7e9748
Erlend Fagertun Hofstad <ehof@sintef.no>
2026-01-26 14:31:21 +0100

    Reduced batch_size for Raidionics from 4 to 2, as 4 was too heavy for the laptop. 4 might be an option for speed increase on superior hardware. Fraxinus#33

--- source/resource/filter/filters/cxRaidionics.cpp
+++ source/resource/filter/filters/cxRaidionics.cpp
@@ -88,7 +88,7 @@ QString Raidionics::createRaidionicsIniFile()
 	settings.endGroup();
 
 	settings.beginGroup("Runtime");
-	settings.setValue("batch_size", "4");
+	settings.setValue("batch_size", "2");
 	settings.setValue("non_overlapping", "False");
 	settings.setValue("reconstruction_method", "thresholding");
 	settings.setValue("reconstruction_order", "resample_first");
